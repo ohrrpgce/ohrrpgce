cf31a503d4f3908c608269f9fca7f192d32837b1
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2005-02-25 23:05:48 +0000

    added NeoTA's basclean.py utility
    git-svn-id: https://rpg.hamsterrepublic.com/source@11 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ basclean.py
@@ -0,0 +1,77 @@
+""" Autoindent a .BAS file """
+import sys
+import re
+
+def sw(s,l):
+    for j in l:
+        if s.startswith(j):
+            return True
+    return False
+
+tab_kw = [ "FUNCTION", "SUB", "DO", "WHILE", "FOR"]
+untab_kw = [ "END SUB", "END FUNCTION", "LOOP", "WEND", "NEXT"]
+
+# You can swap this for \t when using the simple-indenter
+# beware excessive line length though
+indentstr = "  "
+
+#customize the adaptive indenter using this.
+#make sure the last item uses a threshold which your code will not exceed.
+adaptive_data = (
+    #threshold, indentstring
+    (        0, "\t"),
+    (        2, "    "),
+    ( 99999999, "  "),
+)
+
+def adaptive(n):
+    r = ""
+    for i in adaptive_data:
+	take = min(i[0], n)
+	n -= take
+	r += i[1] * take
+
+    return r
+
+def simple(n):
+    return indentstr * n
+
+indent = simple
+
+#delete the \x0d to use unix endline format rather than dos.
+endline = "\x0d\x0a"
+
+# change in tabulation after the end of this line
+latertabs = 0
+tabs = 0
+
+
+for fname in sys.argv[1:]:
+    f = open(fname)
+    o = open(fname + ".i","w")
+    for i in f.readlines():
+	i = i.strip()
+        
+	if sw(i, untab_kw):
+    	    tabs -= 1
+	if sw(i, tab_kw):
+            latertabs += 1
+	
+ 	# if structures
+	if i.startswith("IF") and i.endswith("THEN"):
+    	    latertabs += 1
+
+        if i.startswith("ELSE"):
+            tabs -=1
+    	    latertabs += 1
+
+        if i.startswith("END IF"):
+            tabs -=1
+        
+	o.write(indent(tabs) )
+	o.write(i)
+	o.write(endline)
+        tabs += latertabs
+        latertabs = 0
+    f.close()
+    o.close()
