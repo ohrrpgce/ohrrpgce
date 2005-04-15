33250bf30839d97ce413cfbea888347dd70891a1
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2005-04-15 06:50:49 +0000

    distrib work
    git-svn-id: https://rpg.hamsterrepublic.com/source@57 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.bat
+++ distrib.bat
@@ -4,23 +4,24 @@ REM Compress
 CALL compress.bat
 
 REM copy distrib files
+copy LICENSE.txt distrib
 copy whatsnew.txt distrib
 copy custom.txt distrib
-copy wh_demo.txt distrib
-copy howto.txt distrib
-copy plotscripttutor.html distrib
-copy plotdictionary.html distrib
-copy hamsterspeak.html distrib
+copy wandering_hamster_readme.txt distrib
+copy wandering_hamster_license.txt distrib
+copy docs\plotscripttutor.html distrib
+copy docs\plotdictionary.html distrib
+copy docs\hamsterspeak.html distrib
 copy sample\*.rpg .
 
 REM Create wh_demo package
 copy game.exe wander.exe
-pkzip distrib\wh_demo.zip wander.exe wander.rpg wh_demo.txt
+pkzip distrib\wh_demo.zip wander.exe wander.rpg wandering_hamster_*.txt
 del wander.exe
 
 REM Create custom package
 support\pkzip distrib\custom.zip game.exe custom.exe hspeak.exe unlump.exe
-support\pkzip distrib\custom.zip ohrrpgce.mas ohrrpgce.fnt ohrrpgce.new custom.txt whatsnew.txt howto.txt
+support\pkzip distrib\custom.zip ohrrpgce.mas ohrrpgce.fnt ohrrpgce.new custom.txt whatsnew.txt LICENSE.txt
 support\pkzip distrib\custom.zip sample.rpg npc_tag.rpg pstutor.rpg
 support\pkzip distrib\custom.zip cleanup.bat plotscr.hsd scancode.hsi game.ico
 support\pkzip distrib\custom.zip hamsterspeak.html plotscripttutor.html plotdictionary.html
