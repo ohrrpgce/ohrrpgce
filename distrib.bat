3f5b29c65047ddd3170e6a4c13d3a2ccebe62e6a
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2005-05-17 18:16:50 +0000

    custom distrib batch file
    git-svn-id: https://rpg.hamsterrepublic.com/source@63 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.bat
+++ distrib.bat
@@ -2,6 +2,8 @@ cd %OHRRPGCE%
 
 del tmp\*.???
 
+ECHO Packaging binary distribution of CUSTOM
+
 copy game.exe tmp
 copy custom.exe tmp
 copy hspeak.exe tmp
@@ -9,14 +11,25 @@ copy unlump.exe tmp
 copy ohrrpgce.mas tmp
 copy ohrrpgce.fnt tmp
 copy ohrrpgce.new tmp
-copy custom.txt tmp
-
-ECHO unfinished
-EXIT
-
+copy README-custom.txt tmp
+copy LICENSE.txt tmp
 copy LICENSE-binary.txt tmp
+copy whatsnew.txt tmp
+copy sample\sample.rpg tmp
+copy sample\npc_tag.rpg tmp
+copy sample\pstutor.rpg tmp
+copy plotscr.hsd tmp
+copy scancode.hsi tmp
+copy game.ico tmp
+copy custom.ico tmp
+copy hamsterspeak.html tmp
+copy plotscripttutor.html tmp
+copy plotdictionary.html tmp
 
-del distrib\pushpush.zip
+del distrib\custom.zip
 
 cd tmp
-..\support\pkzip ..\distrib\pushpush.zip *.*
+..\support\pkzip ..\distrib\custom.zip *.*
+cd ..
+support\pkzip -p -r distrib\custom.zip import
+
