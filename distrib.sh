f3168dcb04e5d345af625706fd0b46a3b9313089
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2005-05-23 20:40:17 +0000

    bash shell script for binary distribution
    git-svn-id: https://rpg.hamsterrepublic.com/source@76 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ distrib.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ ! -f distrib.sh ] ; then
+  echo You should only run this script from the ohrrpgce directory.
+  exit 1
+fi
+
+echo Erasing contents of temporary directory
+rm -Rf tmp/*
+
+echo Packaging binary distribution of CUSTOM
+cp -p game.exe tmp
+cp -p custom.exe tmp
+cp -p hspeak.exe tmp
+cp -p unlump.exe tmp
+cp -p ohrrpgce.mas tmp
+cp -p ohrrpgce.fnt tmp
+cp -p ohrrpgce.new tmp
+cp -p README-custom.txt tmp
+cp -p LICENSE.txt tmp
+cp -p LICENSE-binary.txt tmp
+cp -p whatsnew.txt tmp
+cp -p sample/sample.rpg tmp
+cp -p sample/npc_tag.rpg tmp
+cp -p sample/pstutor.rpg tmp
+cp -p plotscr.hsd tmp
+cp -p scancode.hsi tmp
+cp -p game.ico tmp
+cp -p custom.ico tmp
+
+echo Erasing old distribution zip file
+rm distrib/custom.zip
+
+echo Zipping up custom
+cd tmp
+zip ../distrib/custom.zip ./*
+
+cd ..
+echo Zipping up import and docs
+zip -r distrib/custom.zip import docs -x \*.svn\*
+
+echo Erasing contents of temporary directory
+rm -Rf tmp/*
+
