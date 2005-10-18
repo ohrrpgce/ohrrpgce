d15ca3d1b5ddc6bc2dbdf04c755f9ff1a6cde6e7
simonb <simonb@a1daf2fc-2201-0410-9b0b-a943190fd082>
2005-10-18 22:17:56 +0000

    Fix timer bug.
    git-svn-id: svn://gilgamesh.hamsterrepublic.com/fbohr@14 a1daf2fc-2201-0410-9b0b-a943190fd082

--- makegame.bat
+++ makegame.bat
@@ -1,3 +1,3 @@
 verprint
-fbc -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gicon.rc
+fbc -s gui -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gicon.rc
 
