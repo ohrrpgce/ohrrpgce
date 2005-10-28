72ebb3ebfb177f7964a488f9215ad1d893785ea1
pkmnfrk <pkmnfrk@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2005-10-28 19:05:52 +0000

    Linux versions of the windows compiling tools
    git-svn-id: https://rpg.hamsterrepublic.com/source@205 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ makegame.sh
@@ -0,0 +1,5 @@
+cp --reply=yes fbcompat.bi compat.bi
+cp --reply=yes fbcompat.bas compat.bas
+./verprint
+fbc -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gicon.rc
+
