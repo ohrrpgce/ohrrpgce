16fed395fe23c574d669b2a4b6ad71674a7ae33f
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2005-11-10 07:54:52 +0000

    include both versions of game.exe in distrib
    git-svn-id: https://rpg.hamsterrepublic.com/source@210 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib-game.sh
+++ distrib-game.sh
@@ -10,6 +10,7 @@ rm -Rf tmp/*
 
 echo Packaging binary distribution of GAME
 cp -p game.exe tmp
+cp -p game-qb.exe tmp
 cp -p ohrrpgce.fnt tmp
 cp -p README-game.txt tmp
 cp -p LICENSE-binary.txt tmp
