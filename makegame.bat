80d8e65f9cc8abf03cca54765d02290aff7c8f64
simonb <simonb@a1daf2fc-2201-0410-9b0b-a943190fd082>
2006-02-05 14:58:03 +0000

    Add res-doubled fbgfx module and change bat files accordingly.
    git-svn-id: svn://gilgamesh.hamsterrepublic.com/fbohr@52 a1daf2fc-2201-0410-9b0b-a943190fd082

--- makegame.bat
+++ makegame.bat
@@ -1,3 +1,3 @@
 copy /y codename-sdl.txt codename.txt
 verprint
-fbc -s gui -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_fb.bas music_sdl.bas gicon.rc
+fbc -s gui -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_fb2.bas music_sdl.bas gicon.rc
