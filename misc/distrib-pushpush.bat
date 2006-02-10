97464aba9be4ebf2029d5b79d2aecbbaf09ff7d2
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-02-10 06:56:26 +0000

    move some unimportant files to misc subdirectory
    git-svn-id: https://rpg.hamsterrepublic.com/source@329 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- misc/distrib-pushpush.bat
+++ misc/distrib-pushpush.bat
@@ -4,9 +4,9 @@ del tmp\*.???
 
 copy game.exe tmp\pushpush.exe
 copy pushpush.rpg tmp
-copy pushpush.ico tmp
-copy pushpush.hss tmp
-copy readme-pushpush.txt tmp
+copy misc\pushpush.ico tmp
+copy misc\pushpush.hss tmp
+copy misc\readme-pushpush.txt tmp
 copy LICENSE-binary.txt tmp
 
 del distrib\pushpush.zip
