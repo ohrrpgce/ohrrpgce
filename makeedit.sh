b6e04f316d473aa8d7877eb1d5f4e201104da828
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-02-23 05:43:17 +0000

    Linux: compile scripts now accept gfx and music library names on the command-line
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@450 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makeedit.sh
+++ makeedit.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+GFX=${1}
+MUSIC=${2}
+
+if [ -z "${GFX}" ] ; then
+  GFX=fb2
+fi
+
+if [ -z "${MUSIC}" ] ; then
+  MUSIC=sdl
+fi
+
 cp -f fbcompat.bi compat.bi
 cp -f fbcompat.bas compat.bas
 ./verprint
 fbc -v -m custom -d IS_CUSTOM \
-  custom.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas gfx_fb2.bas music_sdl.bas cicon.rc %1 %2 %3 %4 %5 %6 \
+  custom.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas gfx_"${GFX}".bas music_"${MUSIC}".bas cicon.rc %1 %2 %3 %4 %5 %6 \
 && mv custom ohrrpgce-custom
--- makegame.sh
+++ makegame.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+GFX=${1}
+MUSIC=${2}
+
+if [ -z "${GFX}" ] ; then
+  GFX=fb2
+fi
+
+if [ -z "${MUSIC}" ] ; then
+  MUSIC=sdl
+fi
+
 cp -f fbcompat.bi compat.bi
 cp -f fbcompat.bas compat.bas
 ./verprint
 fbc -v -m game -d IS_GAME \
-  game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_fb2.bas music_sdl.bas gicon.rc \
+  game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_"${GFX}".bas music_"${MUSIC}".bas gicon.rc \
 && mv game ohrrpgce-game
