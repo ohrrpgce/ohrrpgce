c73eab272611595cf5387e17b79fe26eef0a196f
pkmnfrk <pkmnfrk@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-04-11 13:52:35 +0000

    A number of changes:
    1. paths are more easily customizable (if someone else wants to use this file on their wiki, for example)
    
    2. filters based on changed lines only (using diff)
    
    3. the comment describing how to install it is now valid.
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@555 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- mediawiki-spamcallback.php
+++ mediawiki-spamcallback.php
@@ -6,15 +6,18 @@
 //
 // USAGE: In LocalSettings.php:
 //
-//    require_once('wikimedia-spamcallback.php');
+//    require_once('mediawiki-spamcallback.php');
 //    $wgFilterCallback = spamCallBack;
 //
 ///////////////////////////////////////////////////////////////////////
 
-$spamNOTEVIL   = '../not.evil.txt';
-$spamCHONGQED  = '../blacklist.chongqed.org.txt';
-$spamSPAMWORDS = '../blacklist.spamwords.txt';
-$spamLOG       = '/var/www/wiki/ohrrpgce/spammer.log';
+//$basePath = '/var/www/wiki/ohrrpgce/wiki/';
+$basePath = '/home/finalfan/public_html/ohr/wiki/';
+
+$spamNOTEVIL   = $basePath . '../not.evil.txt';
+$spamCHONGQED  = $basePath . '../blacklist.chongqed.org.txt';
+$spamSPAMWORDS = $basePath . '../blacklist.spamwords.txt';
+$spamLOG       = $basePath . 'spammer.log';
 
 ///////////////////////////////////////////////////////////////////////
 function checkBlackList($filename,$name,$body,&$reason){
@@ -78,20 +81,28 @@ function spamCallBack($title, $body, $section){
   }
 
   if($do_filter){
+	
+	//Create a diff, for better filtering  
+	$old_page = new Article($title);
+	$old = $old_page->fetchContent();
+	$diff = getDiff($old,$body);
+	$diff = implode("\n",$diff);
+	$old_page = NULL;
+	  
     // special handling if there are any external links
-    if(preg_match('/https?:\/\//', $body)){
+    if(preg_match('/https?:\/\//', $diff)){
       // check the chonqed blocklist for spammer urls.
       // the blacklist is downloaded to a text file by a nightly cron job
-      $block = checkBlackList($spamCHONGQED,'chonqed',$body,$reason);
+      $block = checkBlackList($spamCHONGQED,'chonqed',$diff,$reason);
     }
     if (!$block){
       // check the spammy keyword list
-      $block = checkBlackList($spamSPAMWORDS,'spammy keyword',$body,$reason);
+      $block = checkBlackList($spamSPAMWORDS,'spammy keyword',$diff,$reason);
     }
     if (!$block){
       // The main page is the most-spammed, and therefore may need extra rules  
       if(!$block and 'Main Page' == $title->mTextform and !$title->mPrefixedText){
-        if(preg_match('/http:\/\//',$body)){
+        if(preg_match('/http:\/\//',$diff)){
           $reason = 'direct links are forbidden on the main page';
           $block = true;
         }
@@ -127,7 +138,7 @@ function spamCallBack($title, $body, $section){
     mail($wgEmergencyContact,
          sprintf('%s %s',$wgSitename,$title->mTextform),
          sprintf("spam attempt blocked from \"%s\"\nReason: %s\n\n%s",
-                 $log_name,$reason,$body),
+                 $log_name,$reason,$diff),
          sprintf('From: %s',$wgEmergencyContact));
 
     // attempt to deceive the spammer into thinking their edit succeeded
@@ -141,4 +152,36 @@ function spamCallBack($title, $body, $section){
   return false;
 }
 
+/**
+* Get a diff, as an array of changed lines.
+* Returns false on error
+*/
+function getDiff($old, $new) {
+  $hash = md5(rand(1,1000000));
+  $o = fopen($oldfile = "/tmp/$hash.wiki.old","w");
+  $n = fopen($newfile = "/tmp/$hash.wiki.new","w");
+  
+  fwrite($o,$old);
+  fwrite($n,$new);
+  
+  fclose($o);
+  fclose($n);
+  
+  $res = shell_exec("diff $oldfile $newfile");
+  
+  unlink($oldfile);
+  unlink($newfile);
+  
+  $line = "";
+  $lines = explode("\n",$res);
+  
+  $diff = array();
+  
+  for($i=0;$i<count($lines);$i++,$line=$lines[$i]) {
+    if($line[0] == ">") {
+        $diff[] = substr($line,2);
+    }
+  }
+  return $diff;
+}
 ?>
