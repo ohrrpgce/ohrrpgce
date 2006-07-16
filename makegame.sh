404b242727a2a19d2dc229ca79ba72cfea66a160
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-07-16 08:51:29 +0000

    added util.bas to .sh scripts, an oversight IM pointed out
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@698 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makegame.sh
+++ makegame.sh
@@ -12,5 +12,5 @@ fi
 
 ./verprint
 fbc -v -m game -d IS_GAME \
-  game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas loading.bas common.bas gfx_"${GFX}".bas music_"${MUSIC}".bas gicon.rc \
+  game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas loading.bas common.bas util.bas gfx_"${GFX}".bas music_"${MUSIC}".bas gicon.rc \
 && mv game ohrrpgce-game
