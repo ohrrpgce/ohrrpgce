bc9d53569594a0fdb5ccfe5d19ec7955d3d733b6
pkmnfrk <pkmnfrk@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-08-08 00:22:24 +0000

    Now the backends are optional when calling "make". Thus, you can specify compiler flags without also having to specify the backends. However, if you specify one backend, you must specify both.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@779 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- make.bat
+++ make.bat
@@ -1,15 +1,24 @@
 @echo off
 if "%1"=="" goto gen
+if Not exist "gfx_%1" goto def
+
 pause
 
-call makeedit-gfx-music.bat %1 %2
-call makegame-gfx-music.bat %1 %2
+call makeedit-gfx-music.bat %1 %2 %3 %4 %5 %6 %7
+call makegame-gfx-music.bat %1 %2 %3 %4 %5 %6 %7
+goto end
+
+:def
+
+call makegame %1 %2 %3 %4 %5
+call makeedit %1 %2 %3 %4 %5
 goto end
 
 :gen
 
 call makegame
 call makeedit
+goto end
 
 :end
 
