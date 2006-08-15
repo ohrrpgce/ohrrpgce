6a98556505fd30bb323a1ea83da3c8a1102ba436
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-08-15 16:33:55 +0000

    Oops! I can't believe I forgot to release the Linux binary of Hasta-la-qb+
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/hasta-la-qb@804 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.sh
+++ distrib.sh
@@ -24,31 +24,18 @@ cp -p plotscr.hsd tmp
 cp -p scancode.hsi tmp
 
 echo Erasing old distribution files
-rm distrib/custom.zip
 rm distrib/ohrrpgce-*.tar.bz2
-
-echo Include Windows Binaries
-cp -p game.exe tmp
-cp -p game-qb.exe tmp
-cp -p custom.exe tmp
-cp -p customqb.exe tmp
-cp -p hspeak.exe tmp
-cp -p unlump.exe tmp
-
-echo Zipping up custom
-cd tmp
-zip ../distrib/custom.zip ./*
-
-cd ..
-echo Zipping up import and docs
-zip -r distrib/custom.zip import docs -x \*.svn\*
-
-echo Removing windows binaries
-rm tmp/*.exe
 
 echo Including Linux binaries
 cp -p ohrrpgce-game tmp
 cp -p ohrrpgce-custom tmp
+cp -p unlump tmp
+cp -p relump tmp
+
+echo including hspeak
+cp -p hspeak.exw tmp
+cp -p hsspiffy.e tmp
+
 echo Including import
 mkdir tmp/import
 mkdir tmp/docs
@@ -59,6 +46,10 @@ echo tarring and bzip2ing distribution
 mv tmp ohrrpgce
 tar -jcf distrib/ohrrpgce-linux-x86.tar.bz2 ./ohrrpgce --exclude .svn
 mv ohrrpgce tmp
+
+TODAY=`date "+%Y-%m-%d"`
+CODE=`cat codename.txt | tr -d "\r"`
+mv distrib/ohrrpgce-linux-x86.tar.bz2 distrib/ohrrpgce-linux-x86-$TODAY-$CODE.tar.bz2
 
 echo Erasing contents of temporary directory
 rm -Rf tmp/*
