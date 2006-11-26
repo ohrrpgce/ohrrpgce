9f632bed53f6c5f23b546e350296e58f8568d016
pkmnfrk <pkmnfrk@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2006-11-26 03:21:04 +0000

    Fixing a stupid crash in the sound code. This one was bugging me for aeons...
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@933 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- audwrap/audwrap.cpp
+++ audwrap/audwrap.cpp
@@ -7,8 +7,8 @@ elegant, so no appologies are necessary.
 
 #include "stdafx.h"
 #include "audwrap.h"
-BOOL APIENTRY DllMain( HANDLE hModule, 
-                       DWORD  ul_reason_for_call, 
+BOOL APIENTRY DllMain( HANDLE hModule,
+                       DWORD  ul_reason_for_call,
                        LPVOID lpReserved
 					 )
 {
@@ -35,6 +35,8 @@ AUDWRAP_API int fnaudwrap(void)
 
 */
 
+#include <assert.h>
+
 #include "audiere.h"
 using namespace audiere;
 
@@ -48,14 +50,19 @@ inline bool isvalid(int s);
 
 AUDWRAP_API int AudInit(void) {
 	device = OpenDevice();
-
+    //assert(false);
 	if (!device) {
 		return(-1);
 	}
 
 	numSounds = 10; //10 sounds by default, but can grow
 	sounds = new OutputStreamPtr[numSounds];
-    if (!sounds) return(-1);
+    if (!sounds) {
+        //device = 0;
+        device->unref();
+        device = 0;
+        return(-1);
+    }
 
 	return(0);
 
@@ -69,45 +76,45 @@ AUDWRAP_API void AudClose(void) {
 
 AUDWRAP_API int AudLoadSound(const char *f, bool st) {
     int s = findFreeSlot();
-    
+
     if(s < 0) return (-1);
 
     sounds[s] = OpenSound(device, f, st);
-    
+
     if(!sounds[s]) return(-1);
-    
+
     sounds[s]->setVolume(1.0f);
-    
+
     return(s);
 }
 
 AUDWRAP_API void AudUnloadSound(int s) {
     if(!isvalid(s)) return;
-    
+
     sounds[s] = 0;
 }
 
 AUDWRAP_API void AudSetVolume(int s, float v) {
     if(!isvalid(s)) return;
-    
+
     sounds[s]->setVolume(v);
 }
 
 AUDWRAP_API float AudGetVolume(int s) {
     if(!isvalid(s)) return(0.0);
-    
+
     return(sounds[s]->getVolume());
 }
 
 AUDWRAP_API void AudSetRepeat(int s, bool r) {
     if(!isvalid(s)) return;
-    
+
     sounds[s]->setRepeat(r);
 }
 
 AUDWRAP_API bool AudGetRepeat(int s) {
     if(!isvalid(s)) return(false);
-    
+
     return(sounds[s]->getRepeat());
 }
 
@@ -117,28 +124,28 @@ AUDWRAP_API bool AudIsValidSound(int s) {
 
 AUDWRAP_API bool AudIsPlaying(int s) {
     if(!isvalid(s)) return(false);
-    
+
     return(sounds[s]->isPlaying() == 1);
 }
 
 AUDWRAP_API void AudPlay(int s) {
     if(!isvalid(s)) return;
-    
+
     if(sounds[s]->isPlaying()) return;
-    
+
     sounds[s]->play();
 }
 
 AUDWRAP_API void AudStop(int s) {
     if(!isvalid(s)) return;
-    
+
     if(sounds[s]->isPlaying()) sounds[s]->stop();
     sounds[s]->reset();
 }
 
 AUDWRAP_API void AudPause(int s) {
     if(!isvalid(s)) return;
-    
+
     if(sounds[s]->isPlaying()) sounds[s]->stop();
 }
 
@@ -149,21 +156,21 @@ int findFreeSlot(void) {
 		    return(i);
 		}
 	}
-	
+
 	int newMax = numSounds * 2;
 	OutputStreamPtr * s = new OutputStreamPtr[newMax];
 	if(s == 0) return(-1);
-	
+
 	for(i=0;i < numSounds; i++) {
 	    s[i] = sounds[i];
 	}
-	
+
 	delete [] sounds;
 	sounds = s;
 	numSounds = newMax;
-	
+
 	return(i);
-	
+
 }
 
 inline bool isvalid(int s) {
