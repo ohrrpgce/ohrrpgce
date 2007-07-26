7fac996ef1530edfce00e1606dd859890a4331a8
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2007-07-26 21:39:00 +0000

    web: scripts to make a nightly archive of the nightlies
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@1240 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ mirror/ohrnightly/mirror.sh
@@ -0,0 +1,48 @@
+#/bin/sh
+
+URL="http://HamsterRepublic.com/ohrrpgce/nightly/"
+WEBDIR="/var/www/nightly-archive"
+NOW=`date "+%Y-%m-%d"`
+
+echo "Mirroring ${URL} on ${NOW}"
+
+if [ -d "${NOW}" ] ; then
+  echo "Oops! we have already mirrored today."
+  exit 1
+fi
+
+cd "${WEBDIR}"
+mkdir "${NOW}"
+cd "${NOW}"
+
+echo "Downloading..."
+httrack \
+  --quiet \
+  --robots=0 \
+  -N "%n.%t" \
+  "${URL}" \
+  -"*" \
+  +"*.zip" \
+  -"*-default.zip" \
+  > /dev/null
+
+rm -R *.gif hts-* *.html
+
+YEST=`date -d "Yesterday" "+%Y-%m-%d"`
+YDIR="../${YEST}"
+
+for i in *.zip ; do
+  OLD="${YDIR}/${i}"
+  if [ ! -f "${OLD}" ] ; then
+    echo "${i} (NEW)"
+    continue
+  fi
+  DIF=`diff "${i}" "${OLD}"`
+  if [ "${DIF}" ] ; then
+    echo "${i} (Updated)"
+  else
+    echo "${i} (Not changed)"
+    rm "${i}"
+    ln -s "${OLD}" "${i}"
+  fi
+done
