c18af5b3108ea08654393fc487a522c8ccd27bfc
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2007-08-31 16:44:55 +0000

    Emacs lisp script which can detect all occurrences of bug422. To use: *load the file (load-library or execute the defines) *open a .bas and run bug422-test, or run bug422-test-all-files and specify a directory *compile the program with FB *investigate any branch crossing warnings you see
    Game and Custom check out clean. Going back before r1248, there were only 3 warnings generated, and James just fixed the last of them.
    
    Also, fixing some forgotten lineending properties.
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@1399 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ misc/gosub.el
@@ -0,0 +1,36 @@
+(defun bug422-test ()
+  "Run on a buffer to insert GOTOs which raise warnings on bug422 symptoms when compiled"
+  (interactive)
+  (widen)
+  (save-excursion
+    (let ((labelstart 1) (labelcnt 1))
+      (goto-char (point-min))
+      (while (< (point) (point-max))
+        ;consider one sub/function or toplevel at a time
+        (narrow-to-region (point) (or (re-search-forward "^end \\(sub\\|function\\)$\\|\\(^sub \\|^function \\)" nil t) (point-max)))
+        (goto-char (point-min))
+        ;place a label and goto for each gosub
+        (while (re-search-forward "^[^'\n]*gosub " nil 0)
+          (end-of-line)
+          (insert (concat "\nlab_" (number-to-string labelcnt) ":"))
+          (setq labelcnt (1+ labelcnt)))
+        (end-of-line 0)
+        (let ((x labelstart)) 
+          (while (< x labelcnt)
+            (insert (concat "\ngoto lab_" (number-to-string x)))
+            (setq x (1+ x))))
+        (setq labelstart labelcnt)
+        ;find beginning of next block
+        (forward-line)
+        (widen)
+        (re-search-forward "^\\(sub\\|function\\) " nil 0)))))
+
+
+(defun bug422-test-all-files (directory)
+  "Runs bug422-test on all files in a directory"
+  (interactive "DDirectory to find .bas files? ")
+  (dolist (file (directory-files directory t "\\.bas$"))
+    (find-file file)
+    (bug422-test)
+    (save-buffer)
+    (kill-buffer nil)))
