34e0c0ecf0634b67bae69782eb533cefefff54c2
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2007-09-10 22:50:45 +0000

    There is no need to build with debugging symbols. Reversing my previous change. Now I understand how to use gdb's symbol-file command to load symbols from a separate copy of the executable.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@1433 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makegame-gfx-music.bat
+++ makegame-gfx-music.bat
@@ -2,5 +2,5 @@
 echo Now compiling GAME with %1 graphics module, and %2 music module
 fbc -lang deprecated verprint.bas
 verprint %1 %2
-fbc -lang deprecated -g -s gui -mt -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_%1.bas music_%2.bas loading.bas common.bas browse.bas util.bas gicon.rc -d IS_GAME  %3 %4 %5 %6 %7
+fbc -lang deprecated -s gui -mt -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_%1.bas music_%2.bas loading.bas common.bas browse.bas util.bas gicon.rc -d IS_GAME  %3 %4 %5 %6 %7
 echo.
