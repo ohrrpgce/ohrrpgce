483b8ac2c43fe0c9cfb8e478e4355cac77ba1804
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2007-09-10 18:53:31 +0000

    Build with debugging symbols on Windows (Linux was already) This is critical for catching heisenbugs like bug 458 (which this change unfortunately conceals) When somebody mails me a testcase for a crasher, I need to be able to use gdb.
    (And no, this does not make -floppy bigger than a floppy)
    
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/ubersetzung@1432 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makeedit-gfx-music.bat
+++ makeedit-gfx-music.bat
@@ -2,5 +2,5 @@
 echo Now compiling CUSTOM with %1 graphics module, and %2 music module
 fbc -lang deprecated verprint.bas
 verprint %1 %2
-fbc -lang deprecated -s gui -m custom custom.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas gfx_%1.bas music_%2.bas loading.bas common.bas browse.bas util.bas cicon.rc -d IS_CUSTOM  %3 %4 %5 %6 %7
+fbc -lang deprecated -g -s gui -m custom custom.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas gfx_%1.bas music_%2.bas loading.bas common.bas browse.bas util.bas cicon.rc -d IS_CUSTOM  %3 %4 %5 %6 %7
 echo.
