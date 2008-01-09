dd84e1757123296250349f72351918f058275aea
pkmnfrk <pkmnfrk@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2008-01-09 02:06:01 +0000

    Added a "call" command to the front of each "fbc" line in the compilation batch files.
    The reason I added it is because on my computer, "fbc" is actually a batch file. I keep several versions around, for compatability testing, in folders like "freebasic.17", "freebasic.18", etc, and the batch file redirects to the correct folder with the "FBVER" environment variable. Works really nice.
    
    I know that I'm the only such person, but the call is benign when used on real executables, so I am just reclaiming the ability to type "svn ci" to commit everything.
    
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@1827 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makegame.bat
+++ makegame.bat
@@ -16,7 +16,7 @@ if Not exist "gfx_%OHRGFX%.bas" set OHRGFX=fb
 if Not exist "music_%OHRMUSIC%.bas" set OHRMUSIC=sdl
 
 echo Now compiling GAME with %OHRGFX% graphics module, and %OHRMUSIC% music module
-fbc -lang deprecated verprint.bas
+call fbc -lang deprecated verprint.bas
 verprint %OHRGFX% %OHRMUSIC%
-fbc -lang deprecated -s gui -mt -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas gicon.rc -d IS_GAME  %1 %2 %3 %4 %5
+call fbc -lang deprecated -s gui -mt -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas gicon.rc -d IS_GAME  %1 %2 %3 %4 %5
 echo.
