4d933bfec7b87de0f27bf4e58c7dc14189ff88e8
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2008-02-21 17:33:52 +0000

    Enabling -exx for windows wip builds
    I have no idea why we didn't do that long ago. Anything that will
    crash with -exx will corrupt memory without it, and memory corruption
    errors are hella harder to deal with (and harder for users to report)
    
    Unfortunately windows builds never display the error message when they
    crash from -exx errors (which I guess was actually the reason we
    didn't do this sooner) but I would much rather deal with a bunch of
    "my game closes suddenly whenever I do X" bug reports rather than a
    bunch of "my game crashes randomly" bug reports.
    
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@1947 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makegame.bat
+++ makegame.bat
@@ -18,5 +18,5 @@ if Not exist "music_%OHRMUSIC%.bas" set OHRMUSIC=sdl
 echo Now compiling GAME with %OHRGFX% graphics module, and %OHRMUSIC% music module
 call fbc -lang deprecated verprint.bas
 verprint %OHRGFX% %OHRMUSIC%
-call fbc -lang deprecated -s gui -mt -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas gicon.rc -d IS_GAME  %1 %2 %3 %4 %5
+call fbc -lang deprecated -g -exx -s gui -mt -m game game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas compat.bas bam2mid.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas gicon.rc -d IS_GAME  %1 %2 %3 %4 %5
 echo.
