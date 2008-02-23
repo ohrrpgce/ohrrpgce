e19ad8205c380af03ce16a4674cfbd5d38d4f02d
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2008-02-23 21:28:56 +0000

    Backing out svn revision 1947. No longer use -exx -g for Windows builds (although I plan on adding a separate nightly wip build with those additional options enabled)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@1955 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makeedit.bat
+++ makeedit.bat
@@ -19,5 +19,5 @@ if Not exist "music_%OHRMUSIC%.bas" set OHRMUSIC=sdl
 echo Now compiling CUSTOM with %OHRGFX% graphics module, and %OHRMUSIC% music module
 call fbc -lang deprecated verprint.bas
 verprint %OHRGFX% %OHRMUSIC%
-call fbc -lang deprecated -s gui -g -exx -m custom custom.bas customsubs.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas cicon.rc -d IS_CUSTOM  %1 %2 %3 %4 %5
+call fbc -lang deprecated -s gui -m custom custom.bas customsubs.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas cicon.rc -d IS_CUSTOM  %1 %2 %3 %4 %5
 echo.
