b224f4cf0f07e6ccf973981a8cfd0a583543d308
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2008-10-02 18:12:35 +0000

    Update linux distrib scripts for wander and vikings
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@2312 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/wander.py
+++ linux/wander.py
@@ -6,7 +6,7 @@ from ohr_debian import *
 
 package_name = "wandering-hamster"
 maintainer = '"James Paige" <ohrrpgce@lists.motherhamster.org>'
-depends = "ohrrpgce (>=2007.08)"
+depends = "ohrrpgce (>=2008.10)"
 
 prefix = "../../games/wander/"
 relump(prefix + "wander.rpgdir", "wander.rpg")
