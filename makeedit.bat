98a66893f7412f9d96f58c6631cbe71ca210879e
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2009-04-06 01:14:36 +0000

    Copy Mike's makegame.bat improvements (r2321) over to makeedit.bat and make.bat. Yikes, my arguments to makeedit were being eaten this whole time.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@2639 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makeedit.bat
+++ makeedit.bat
@@ -1,15 +1,17 @@
 @echo off
-
 if "%1"=="~" goto noparam
 if "%1"=="" goto noparam
+if Not exist "gfx_%1.bas" goto noparam2
 set OHRGFX=%1
 shift
 if "%1"=="" goto noparam
+if Not exist "music_%1.bas" goto noparam2
 set OHRMUSIC=%1
 
 :noparam
-
 shift
+
+:noparam2
 
 IF "%OHRGFX%"=="" set OHRGFX=fb
 IF "%OHRMUSIC%"=="" set OHRMUSIC=sdl
@@ -19,5 +21,5 @@ if Not exist "music_%OHRMUSIC%.bas" set OHRMUSIC=sdl
 echo Now compiling CUSTOM with %OHRGFX% graphics module, and %OHRMUSIC% music module
 call fbc -lang deprecated verprint.bas
 verprint %OHRGFX% %OHRMUSIC%
-call fbc -lang deprecated -s gui -m custom custom.bas customsubs.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas slices.bas sliceedit.bas reload.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas cicon.rc -d IS_CUSTOM  %1 %2 %3 %4 %5
+call fbc -lang deprecated -s gui -m custom custom.bas customsubs.bas drawing.bas subs.bas subs2.bas subs3.bas mapsubs.bas flexmenu.bas menus.bas allmodex.bas compat.bas bam2mid.bas slices.bas sliceedit.bas reload.bas gfx_%OHRGFX%.bas music_%OHRMUSIC%.bas loading.bas common.bas browse.bas util.bas cicon.rc -d IS_CUSTOM  %1 %2 %3 %4 %5 %6 %7 %8 %9
 echo.
