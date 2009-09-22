569b9372bf6b6e204bf352dcb2a5e778dddccd73
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2009-09-22 15:57:04 +0000

    For some reason on my new slackware 13.0 64-bit system, makegame.sh and makeedit.sh did not work, though they've always worked for me in linux previously. FB0.20 doesn't like getting blank arguments, and prints out its usage information. Has noone else seen this?
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@2878 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makegame.sh
+++ makegame.sh
@@ -21,8 +21,8 @@ fi
 fbc -lang deprecated verprint.bas
 ./verprint ${GFX} ${MUSIC}
 fbc -lang deprecated -g -v -exx -m game -d IS_GAME -d DATAFILES='"/usr/share/games/ohrrpgce"' \
-  "${3}" "${4}" "${5}" "${6}" "${7}" "${8}" \
+  ${3} ${4} ${5} ${6} ${7} ${8} \
   game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas \
   compat.bas bam2mid.bas loading.bas common.bas browse.bas util.bas slices.bas gfxsubs.bas \
-  gfx_"${GFX}".bas music_"${MUSIC}".bas ${EXTRA} \
+  gfx_${GFX}.bas music_${MUSIC}.bas ${EXTRA} \
 && mv game ohrrpgce-game
