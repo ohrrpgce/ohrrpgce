ohrrpgce/ohrrpgce

commit 118a39b2109309cfd7d5b2d59936a91e3adfa08b

Added sprite_new_view: returns a new frame which is a 'view' of rectangular subarea of a frame
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@2965 7d344553-34f0-0310-a9b1-970ce8f1c3a2

allmodex.bas
@@ -40,7 +40,6 @@ declare function matchmask(match as string, mask as string) as integer
 declare function calcblock(byval x as integer, byval y as integer, byval l as integer, byval t as integer) as integer
 
 'internal allmodex use only sprite functions
-declare function sprite_describe(byval p as frame ptr) as string
 declare sub sprite_freemem(byval f as frame ptr)
 declare sub Palette16_delete(byval f as Palette16 ptr ptr)
 
@@ -183,7 +182,7 @@ FUNCTION registerpage (byval spr as Frame ptr) as integer
 	for i as integer = 0 to ubound(vpages)
 		if vpages(i) = NULL then
 			vpages(i) = spr
-			spr->refcount += 1
+			if spr->refcount <> NOREFC then	spr->refcount += 1
 			return i
 		end if
 	next
@@ -3935,14 +3934,14 @@ function sprite_new(byval w as integer, byval h as integer, byval frames as inte
 			if i > 0 then .arrayelem = 1
 			.w = w
 			.h = h
-			.pitch = w
+			.pitch = w '+ 10  'test pitch conversion work
 			.mask = NULL
 			if clr then
-				.image = callocate(w * h)
-				IF wantmask THEN .mask = callocate(w * h)
+				.image = callocate(.pitch * h)
+				if wantmask then .mask = callocate(.pitch * h)
 			else
-				.image = allocate(w * h)
-				IF wantmask THEN .mask = allocate(w * h)
+				.image = allocate(.pitch * h)
+				if wantmask then .mask = allocate(.pitch * h)
 			end if
 
 			if .image = 0 or (.mask = 0 and wantmask <> 0) then
@@ -3956,9 +3955,40 @@ function sprite_new(byval w as integer, byval h as integer, byval frames as inte
 	return ret
 end function
 
+'create a frame which is a view onto part of a larger frame
+function sprite_new_view(byval spr as Frame ptr, byval x as integer, byval y as integer, byval w as integer, byval h as integer) as Frame ptr
+	dim ret as frame ptr = callocate(sizeof(Frame))
+
+	if ret = 0 then
+		debug "Could not create sprite view, no memory"
+		return 0
+	end if
+
+	with *ret
+		.w = bound(w, 1, spr->w - x)
+		.h = bound(h, 1, spr->h - y)
+		.pitch = spr->pitch
+		.image = spr->image + .pitch * y + x
+		if spr->mask then
+			.mask = spr->mask + .pitch * y + x
+		end if
+		.refcount = 1
+		.arraylen = 1 'at the moment not actually used anywhere on sprites with isview = 1
+		.isview = 1
+		'we point .base at the 'root' frame which really owns these pixel buffer(s)
+		if spr->isview then
+			.base = spr->base
+		else
+			.base = spr
+		end if
+		if .base->refcount <> NOREFC then .base->refcount += 1
+	end with
+	return ret
+end function
+
 ' unconditionally frees a sprite from memory. 
 ' You should never need to call this: use sprite_unload
-' Should only be called on the head of an array!
+' Should only be called on the head of an array (and not a view, obv)!
 ' Warning: not all code calls sprite_freemem to free sprites! Grrr!
 private sub sprite_freemem(byval f as frame ptr)
 	if f = 0 then exit sub
@@ -4080,16 +4110,20 @@ sub sprite_unload(byval p as frame ptr ptr)
 			.refcount -= 1
 			if .refcount < 0 then debug sprite_describe(*p) & " has refcount " & .refcount
 		end if
-		' the cache counts as a reference!!
-		if (.refcount - .cached) <= 0 and .arrayelem = 0 then
-			'current policy is to unload all sprite as soon as they are unused.
-			if .cached then sprite_remove_cache(*p)
-			for i as integer = 1 to .arraylen - 1
-				if (*p)[i].refcount <> 1 then
-					debug sprite_describe(*p) & " freed with refcount " & .refcount
-				end if
-			next
-			sprite_freemem(*p)
+		if .refcount <= 0 and .arrayelem = 0 then
+			if .isview then
+				sprite_unload @.base
+				deallocate(*p)
+			else
+				'current policy is to unload all sprite as soon as they are unused, but this could be changed easily.
+				if .cached then sprite_remove_cache(*p)
+				for i as integer = 1 to .arraylen - 1
+					if (*p)[i].refcount <> 1 then
+						debug sprite_describe(*p) & " freed with refcount " & .refcount
+					end if
+				next
+				sprite_freemem(*p)
+			end if
 		end if
 	end with
 	*p = 0
@@ -4099,7 +4133,7 @@ function sprite_describe(byval p as frame ptr) as string
 	if p = 0 then return "'(null)'"
 	return "'(0x" & hex(p) & ") " & p->arraylen & "x" & p->w & "x" & p->h & " img=0x" & hex(p->image) _
 	       & " msk=0x" & hex(p->mask) & " pitch=" & p->pitch & " cached=" & p->cached & " aelem=" _
-	       & p->arrayelem & " refc=" & p->refcount & "'"
+	       & p->arrayelem & " view=" & p->isview & " base=0x" & hex(p->base) & " refc=" & p->refcount & "'"
 end function
 
 'this is mostly just a gimmick
@@ -4509,10 +4543,7 @@ private sub flip_image(byval pixels as ubyte ptr, byval d1len as integer, byval 
 end sub
 
 'Public:
-' returns a copy of the sprite flipped horizontally. The new sprite is refc'd of course.
-' direct means modify it in-place, forbidden if the sprite is cached!
-' (Note: might be modified in place if no other references exist, you are
-' encouraged to use the ref counting system)
+' flips a sprite horizontally. In place: you are only allowed to do this on sprites with no other references
 sub sprite_flip_horiz(byval spr as frame ptr)
 	if spr = 0 then exit sub
 	

allmodex.bi
@@ -136,6 +136,7 @@ DECLARE FUNCTION getmusictype (file as string) as integer
 
 'new sprite functions
 declare function sprite_new(byval w as integer, byval h as integer, byval frames as integer = 1, byval clr as integer = NO, byval wantmask as integer = NO) as Frame ptr
+declare function sprite_new_view(byval spr as Frame ptr, byval x as integer, byval y as integer, byval w as integer, byval h as integer) as Frame ptr
 declare function sprite_load overload (byval ptno as integer, byval rec as integer) as frame ptr
 declare function sprite_load(byval as string, byval as integer, byval as integer , byval as integer, byval as integer) as frame ptr
 declare function sprite_reference(byval p as frame ptr) as frame ptr
@@ -149,6 +150,7 @@ declare sub sprite_clear(byval spr as frame ptr)
 declare sub sprite_empty_cache()
 declare function sprite_is_valid(byval p as frame ptr) as integer
 declare sub sprite_debug_cache()
+declare function sprite_describe(byval p as frame ptr) as string
 
 declare function palette16_load overload (byval num as integer, byval autotype as integer = 0, byval spr as integer = 0) as palette16 ptr
 declare function palette16_load(byval fil as string, byval num as integer, byval autotype as integer = 0, byval spr as integer = 0) as palette16 ptr

udts.bi
@@ -32,8 +32,10 @@ type Frame
 	mask as ubyte ptr
 	refcount as integer  'see sprite_unload in particular for documentation
 	arraylen as integer  'how many frames were contiguously allocated in this frame array
-	cached:1 as integer
+	base as Frame ptr    'the Frame which actually owns this memory
+	cached:1 as integer  '(not set for views onto cached sprites)
 	arrayelem:1 as integer  'not the first frame in a frame array
+	isview:1 as integer
 end type
 
 TYPE GraphicPair

commit 33e74d9e025b784ff7c4164928d927646368ae40

Fix some use of the Frame .cached flag that's been causing debug message, including bug introduced in r2965
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@2970 7d344553-34f0-0310-a9b1-970ce8f1c3a2

allmodex.bas
@@ -1562,7 +1562,7 @@ SUB font_create_edged (byval font as Font ptr, byval basefont as Font ptr)
 		.w = size  'garbage
 		.h = 1
 		.pitch = size 'more garbage, not sure whether there's a sensible value
-		.refcount = 1   'NOREFC
+		.refcount = 2  '1  'NOREFC  '?????
 		.arrayelem = 1 ' ??????
 		.mask = null
 		.image = callocate(size)
@@ -3859,8 +3859,7 @@ sub sprite_empty_cache()
 		with sprcache(i)
 			if .p <> 0 then
 				debug "warning: leaked sprite: " & .key & " with " & .p->refcount & " references"
-				sprite_freemem(.p)
-				.p = 0
+				sprite_unload @.p
 			elseif .key <> 0 then
 				debug "warning: phantom cached sprite " & .key
 			end if
@@ -3885,6 +3884,11 @@ end sub
 private sub sprite_add_cache(byval key as integer, byval p as frame ptr)
 	if p = 0 then exit sub
 	dim as integer i, sec = -1
+
+	'the cache counts as a reference, but only to the head element of an array!!
+	p->cached = 1
+	p->refcount += 1
+
 	for i = 0 to ubound(sprcache)
 		with sprcache(i)
 			'we don't check whether the key is equal; we can't reliably
@@ -3901,6 +3905,8 @@ private sub sprite_add_cache(byval key as integer, byval p as frame ptr)
 
 	if sec > -1 then
 		'NOTE: currently, sprites are always freed as soon as all references are unloaded, so this never happens
+		'NOTE: we can not call sprite_unload from in here
+		'NOTE: the following is no longer correct! sprite_unload has lots of extra logic which can't be skipped
 		sprite_freemem(sprcache(sec).p)
 		sprcache(sec).key = key
 		sprcache(sec).p = p
@@ -4019,11 +4025,6 @@ function sprite_load(byval ptno as integer, byval rec as integer) as frame ptr
 	with sprite_sizes(ptno)
 		ret = sprite_load(game + ".pt" & ptno, rec, .frames, .size.x, .size.y)
 		if ret = 0 then return 0
-		for i as integer = 0 to .frames - 1
-			ret[i].cached = 1
-		next
-		'the cache counts as an (additional) reference, but only to head!!
-		ret[0].refcount += 1
 	end with
 
 	sprite_add_cache(key, ret)
@@ -4110,7 +4111,13 @@ sub sprite_unload(byval p as frame ptr ptr)
 			.refcount -= 1
 			if .refcount < 0 then debug sprite_describe(*p) & " has refcount " & .refcount
 		end if
-		if .refcount <= 0 and .arrayelem = 0 then
+		'if cached, can free two references at once
+		if (.refcount - .cached) <= 0 then
+			if .arrayelem then
+				'this should not happen, because each arrayelem gets an extra refcount
+				debug "arrayelem with refcount = " & .refcount
+				exit sub
+			end if
 			if .isview then
 				sprite_unload @.base
 				deallocate(*p)