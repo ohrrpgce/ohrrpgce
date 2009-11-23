d5b989d20154a5cc2a0d4481a85b463d26efd67f
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2009-11-23 18:37:44 +0000

    Add gfx_directx support to makegame/edit.bat, fix makegame.bat mistake, and explicitly pass -mt makegame/edit.bat act slightly differently now: it's no longer possible to specify just the graphics backend: "makegame sdl -g" We really need to switch to a proper makefile.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3109 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makegame.sh
+++ makegame.sh
@@ -20,7 +20,7 @@ fi
 
 fbc -lang deprecated verprint.bas
 ./verprint ${GFX} ${MUSIC}
-fbc -lang deprecated -g -v -exx -m game -d IS_GAME -d DATAFILES='"/usr/share/games/ohrrpgce"' \
+fbc -lang deprecated -g -v -exx -mt -m game -d IS_GAME -d DATAFILES='"/usr/share/games/ohrrpgce"' \
   ${3} ${4} ${5} ${6} ${7} ${8} \
   game.bas bmod.bas bmodsubs.bas allmodex.bas menustuf.bas moresubs.bas yetmore.bas yetmore2.bas \
   compat.bas bam2mid.bas loading.bas common.bas browse.bas util.bas slices.bas gfxsubs.bas \
