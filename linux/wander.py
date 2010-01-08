2349745c8474b01ddca9dcce630254b2f788436c
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2010-01-08 21:47:47 +0000

    Fixes for the debian package scripts for Wander and Viking
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/ypsiliform@3314 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/wander.py
+++ linux/wander.py
@@ -6,9 +6,9 @@ from ohr_debian import *
 
 package_name = "wandering-hamster"
 maintainer = '"James Paige" <ohrrpgce@lists.motherhamster.org>'
-depends = "ohrrpgce (>=2008.10)"
+depends = "ohrrpgce (>=2010.01)"
 
-prefix = "../../games/wander/"
+prefix = "../../../games/wander/"
 relump(prefix + "wander.rpgdir", "wander.rpg")
 files = [
   "wander.rpg",
