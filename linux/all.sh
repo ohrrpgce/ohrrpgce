86bd87472301b65b3f5a0664e238d0b19d3db15f
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2010-06-11 17:36:43 +0000

    Linux nightly build explicity uses python2.6 to run the debian package building scripts because of problems with shutil in python2.5
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3603 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/all.sh
+++ linux/all.sh
@@ -4,14 +4,14 @@ rm *.deb
 if [ -d ohrrpgce ] ; then
   rm -R ohrrpgce
 fi
-./ohrrpgce.py
+python2.6 ./ohrrpgce.py
 
 if [ -d wandering-hamster ] ; then
   rm -R wandering-hamster
 fi
-./wander.py
+python2.6 ./wander.py
 
 if [ -d vikings-of-midgard ] ; then
   rm -R vikings-of-midgard
 fi
-./viking.py
+python2.6 ./viking.py
