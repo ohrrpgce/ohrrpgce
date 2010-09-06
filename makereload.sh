69c84ce05116f382b02c2e6698b578481cc12be0
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2010-09-06 15:37:55 +0000

    Opps, I'd broken makereload.sh
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3711 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makereload.sh
+++ makereload.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 fbc -v -c -g -lang deprecated reload.bas reloadext.bas lumpfile.bas util.bas || exit 1
-gcc -c -g -O3 base64.c || exit 1
-gcc -c -g -O3 utf8toisolat1.o
+gcc -c -g -O3 base64.c --std=c99 || exit 1
 
 fbc -v -g -profile -lang deprecated reloadtest.bas reload.o reloadext.o lumpfile.o util.o base64.o
-fbc -v -g -profile -lang deprecated xml2reload.bas reload.o reloadext.o lumpfile.o util.o base64.o utf8toisolat1.o -p . -l xml2
+fbc -v -g -profile -lang deprecated xml2reload.bas reload.o reloadext.o lumpfile.o util.o base64.o -p . -l xml2
 fbc -v -g -profile -lang deprecated reload2xml.bas reload.o lumpfile.o util.o base64.o
 fbc -v -g -profile -lang deprecated reloadutil.bas reload.o reloadext.o lumpfile.o util.o base64.o
 
-rm reload.o reloadext.o lumpfile.o util.o base64.o utf8toisolat1.o
+rm reload.o reloadext.o lumpfile.o util.o base64.o
