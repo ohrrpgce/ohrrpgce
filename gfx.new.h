c93bb5519dc0590be6cd7334472a3f6a6e844663
jay <jay@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2010-10-07 03:57:46 +0000

    Updated new proposed backend interface. Removed gfx_Acquire* functions. Added gfx_ClipCursor, gfx_ShowCursor, and gfx_HideCursor to replace io_setmousevisibility and io_mouserect. Added gfx_GetJoystickCount. Removed gfx_Get*Movement. Organized functions a little.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3835 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- gfx.new.h
+++ gfx.new.h
@@ -18,9 +18,10 @@ struct GFX_INIT
 	void (__cdecl *PostTerminateSignal)(void);
 	void (__cdecl *OnCriticalError)(const char* szError);
 	void (__cdecl *SendDebugString)(const char* szMessage);
-	int (__cdecl *DefGfxMessageProc)(unsigned int msg, unsigned int dwParam, void* pvParam);
 };
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+//basic backend functions
 int gfx_Initialize(const GFX_INIT* pCreationData); //initializes the backend; if failed, returns 0
 void gfx_Shutdown(); //shuts down the backend--does not post the termination signal
 
@@ -28,8 +29,8 @@ int gfx_SendMessage(unsigned int msg, unsigned int dwParam, void* pvParam); //se
 
 int gfx_GetVersion(); //returns the backend version
 
-void gfx_PumpMessages(); //pumps the backend's message queues and polls input
-
+/////////////////////////////////////////////////////////////////////////////////////////////
+//graphical functions
 //presents a surface from ohr to the backend's backbuffer, converting it with the palette supplied;
 //if pSurface == NULL, a maintained copy of the surface will be used
 //if pPalette == NULL, a maintained copy of the palette will be used
@@ -37,26 +38,27 @@ void gfx_Present(unsigned char *pSurface, int nWidth, int nHeight, unsigned int 
 
 int gfx_ScreenShot(const char* szFileName); //takes a screenshot; if failed, returns 0
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+//messaging functions
+void gfx_PumpMessages(); //pumps the backend's message queues and polls input
+
 void gfx_SetWindowTitle(const char* szTitle); //sets the window title; the backend may add messages to the window title to describe further option
 const char* gfx_GetWindowTitle(); //returns the window title without the backend's possible additions
 
-int gfx_AcquireKeyboard(int bEnable); //alerts backend of the engine's request for keyboard input; if bEnable == 0, the keyboard is freed; returns 0 on failure
-int gfx_AcquireMouse(int bEnable); //alerts backend of the engine's request for mouse input; if bEnable == 0, the mouse is freed; returns 0 on failure
-//alerts backend of the engine's request for an indexed joystick;
-//the backend may allow a user to order the input devices as he/she sees fit;
-//if bEnable == 0, the joystick is freed;
-//returns 0 on failure;
-int gfx_AcquireJoystick(int bEnable, int nDevice);
+void gfx_ShowCursor(); //sets the os cursor to display over the client area
+void gfx_HideCursor(); //sets the os cursor to hide while over the client area
+void gfx_ClipCursor(int left, int top, int right, int bottom); //clips the os cursor to the ohr rectangle, which is scaled to the client area; passing a negative for any value disables the clip
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+//input functions
 int gfx_GetKeyboard(int *pKeyboard); //gets the keyboard state in a format the engine understands; returns 0 on failure
 
-int gfx_GetMouseMovement(int& dx, int& dy, int& dWheel, int& buttons); //gets the mouse movement since the last input poll and the button state; returns 0 on failure
-int gfx_GetMousePosition(int& x, int& y, int& wheel, int& buttons); //gets the mouse position and button state; returns 0 on failure
-int gfx_SetMousePosition(int x, int y); //sets the mouse position; returns 0 on failure
+int gfx_GetMouse(int& x, int& y, int& wheel, int& buttons); //gets the mouse position and button state; returns 0 on failure
+int gfx_SetMouse(int x, int y); //sets the mouse position; returns 0 on failure
 
-int gfx_GetJoystickMovement(int nDevice, int& dx, int& dy, int& buttons); //gets the indexed joystick movement since last input poll and button state; returns 0 on failure
-int gfx_GetJoystickPosition(int nDevice, int& x, int& y, int& buttons); //gets the indexed joystick position and button state; returns 0 on failure
-int gfx_SetJoystickPosition(int nDevice, int x, int y); //sets the indexed joystick position; returns 0 on failure
+int gfx_GetJoystick(int nDevice, int& x, int& y, int& buttons); //gets the indexed joystick position and button state; returns 0 on failure
+int gfx_SetJoystick(int nDevice, int x, int y); //sets the indexed joystick position; returns 0 on failure
+int gfx_GetJoystickCount(); //returns the number of joysticks attached to the system
 
 #ifdef __cplusplus
 } //extern "C"
