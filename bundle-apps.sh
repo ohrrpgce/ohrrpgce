fa967e96273bd95374b1009c7aa4cb6985850485
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2010-10-09 01:02:24 +0000

    Added -e to the sed lines in bundle-apps.sh without which sed gets confused, thinking the filename is part of the pattern
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3849 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- bundle-apps.sh
+++ bundle-apps.sh
@@ -10,14 +10,14 @@ rm -rf OHRRPGCE-Custom.app
 echo Bundling OHRRPGCE-Game.app
 cp -R mac/OHRRPGCE-Game.app.template OHRRPGCE-Game.app
 mkdir -p OHRRPGCE-Game.app/Contents/MacOS
-sed -i "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Game.app/Contents/Info.plist
+sed -i -e "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Game.app/Contents/Info.plist
 cp ohrrpgce-game OHRRPGCE-Game.app/Contents/MacOS/
 tar xf mac/Frameworks.tar.gz -C OHRRPGCE-Game.app/Contents
 
 echo Bundling OHRRPGCE-Custom.app
 cp -R mac/OHRRPGCE-Custom.app.template OHRRPGCE-Custom.app
 mkdir -p OHRRPGCE-Custom.app/Contents/MacOS
-sed -i "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Custom.app/Contents/Info.plist
+sed -i -e "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Custom.app/Contents/Info.plist
 cp ohrrpgce-custom OHRRPGCE-Custom.app/Contents/MacOS/
 tar xf mac/Frameworks.tar.gz -C OHRRPGCE-Custom.app/Contents
 cp -R ohrhelp/*.txt OHRRPGCE-Custom.app/Contents/Resources/
