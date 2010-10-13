508a017c8502f38b5ac7e66618200cd1a6c098f6
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2010-10-13 01:19:43 +0000

    makeutil scripts were broken.
    I really should move get_sys_err_string out of blit.c...
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3875 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makeutil.bat
+++ makeutil.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Now compiling OHRRPGCE utilities
-fbc -lang deprecated unlump.bas util.bas
-fbc -lang deprecated relump.bas util.bas
+fbc -lang deprecated unlump.bas util.bas win32\blit.o
+fbc -lang deprecated relump.bas util.bas win32\blit.o
 echo.
