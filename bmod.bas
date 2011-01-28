ohrrpgce/ohrrpgce

commit 9c725c9930a6158745b158dd8b01c177cf9d1bb2

Add DOUBLE overloads of small, large. Valuable lesson: INT() returns a double, you probably want CINT() instead.
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4004 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bmod.bas
@@ -866,7 +866,7 @@ SUB battle_display (BYREF bat AS BattleState, bslot() AS BattleSprite, menubits(
    'if keyval(scS) > 1 then gen(genMaxInventory) += 3
    'if keyval(scA) > 1 then gen(genMaxInventory) -= 3
    IF bat.menu_mode = batMENUITEM THEN '--draw item menu
-    DIM inv_height AS INTEGER = small(78, 8 + INT((last_inv_slot() + 1) / 3) * 8)
+    DIM inv_height AS INTEGER = small(78, 8 + CINT((last_inv_slot() + 1) / 3) * 8)
     WITH bat.inv_scroll
      .top = INT(bat.item.top / 3)
      .pt = INT(bat.item.pt / 3)

customsubs.bas
@@ -366,7 +366,7 @@ END FUNCTION
 FUNCTION xy_from_int(n AS INTEGER, wide AS INTEGER, high AS INTEGER) AS XYPair
  DIM pair AS XYPair
  pair.x = n MOD wide
- pair.y = small(INT(n / wide), high - 1)
+ pair.y = small(CINT(n / wide), high - 1)
  RETURN pair
 END FUNCTION
 

drawing.bas
@@ -2524,7 +2524,7 @@ IF keyval(scTilde) > 1 THEN ss.hidemouse = ss.hidemouse XOR 1
 IF keyval(scComma) > 1 AND ss.palindex > 0 THEN ss.palindex -= 1
 IF keyval(scPeriod) > 1 AND ss.palindex < 15 THEN ss.palindex += 1
 IF ss.zonenum = 2 THEN
- IF mouse.clicks > 0 THEN ss.palindex = small(INT(ss.zone.x / 4), 15)
+ IF mouse.clicks > 0 THEN ss.palindex = small(CINT(ss.zone.x / 4), 15)
 END IF
 IF keyval(scLeftBrace) > 1 OR (ss.zonenum = 5 AND mouse.clicks > 0) THEN
  changepal poffset(state.pt), -1, workpal(), state.pt - state.top

menustuf.bas
@@ -1752,7 +1752,7 @@ FUNCTION items_menu () as integer
   .x = 8
   .y = 5
   .wide = 304
-  .high = small(180, 12 + (INT((last_inv_slot() + 1) / 3) + 1) * 8)
+  .high = small(180, 12 + (CINT((last_inv_slot() + 1) / 3) + 1) * 8)
  END WITH
  WITH istate.scrollrect
   .x = 20

allmodex.bas
@@ -4017,7 +4017,7 @@ function frame_dissolved(byval spr as frame ptr, byval tlength as integer, byval
 		case 6 'vapourise
 			'vapoury is the bottommost vapourised row
 			dim vapoury as integer = (cpy->h - 1) * (t / tlength)
-			dim vspeed as integer = large(cpy->h / tlength, 1)
+			dim vspeed as integer = large(cint(cpy->h / tlength), 1)
 			for sx = 0 to cpy->w - 1
 				dim chunklength as integer = rnd * (vspeed + 5)
 				for i = -2 to 9999

moresubs.bas
@@ -882,8 +882,8 @@ SUB teleporttooltend (BYREF mini AS Frame Ptr, maptilesX() AS TileMap, tilesets2
  mini = createminimap(maptilesX(), tilesets2(), zoom)
  mapsize.x = maptilesX(0).wide
  mapsize.y = maptilesX(0).high
- offset.x = large(160 - mini->w / 2, 0)
- offset.y = large(100 - mini->h / 2, 0)
+ offset.x = large(cint(160 - mini->w / 2), 0)
+ offset.y = large(cint(100 - mini->h / 2), 0)
  minisize.x = 320 - offset.x * 2
  minisize.y = 200 - offset.y * 2
 END SUB

util.bas
@@ -75,6 +75,10 @@ FUNCTION large (BYVAL n1 as longint, BYVAL n2 as longint) as longint
  IF n2 > n1 THEN large = n2
 END FUNCTION
 
+FUNCTION large (BYVAL n1 as double, BYVAL n2 as double) as double
+ IF n2 > n1 THEN RETURN n2 ELSE RETURN n1
+END FUNCTION
+
 FUNCTION loopvar (BYVAL value as integer, BYVAL min as integer, BYVAL max as integer, BYVAL inc as integer) as integer
  DIM as integer a = value + inc
  IF a > max THEN RETURN a - ((max - min) + 1)
@@ -97,6 +101,10 @@ END FUNCTION
 FUNCTION small (BYVAL n1 as longint, BYVAL n2 as longint) as longint
  small = n1
  IF n2 < n1 THEN small = n2
+END FUNCTION
+
+FUNCTION small (BYVAL n1 as double, BYVAL n2 as double) as double
+ IF n2 < n1 THEN RETURN n2 ELSE RETURN n1
 END FUNCTION
 
 FUNCTION range (number AS INTEGER, percent AS INTEGER) AS INTEGER

util.bi
@@ -280,10 +280,12 @@ declare function bound overload (byval n as double, byval lowest as double, byva
 declare function in_bound (BYVAL n as integer, BYVAL lowest as integer, BYVAL highest as integer) as integer
 declare function large overload (byval n1 as integer, byval n2 as integer) as integer
 declare function large overload (byval n1 as longint, byval n2 as longint) as longint
+declare function large overload (byval n1 as double, byval n2 as double) as double
 declare function loopvar overload (byval var as integer, byval min as integer, byval max as integer, byval inc as integer) as integer
 declare function loopvar overload (byval value as longint, byval min as longint, byval max as longint, byval inc as longint) as longint
 declare function small overload (byval n1 as integer, byval n2 as integer) as integer
 declare function small overload (byval n1 as longint, byval n2 as longint) as longint
+declare function small overload (byval n1 as double, byval n2 as double) as double
 declare function range (number as integer, percent as integer) as integer
 declare function rpad (s as string, pad_char as string, size as integer) as string
 declare function instr_nth overload (byval start as integer, s as string, substring as string, byval nth as integer) as integer

commit 50cceac64a36624186cd560c3d036680146fa4dc

Fix the 256-colour picker, broken in r4004. Also fixed a couple other extremely minor mistakes in r4004, even if they didn't change anything, and also a few related changes (I hate INT).
To correct the r4004 commit message: while you usually want CINT instead of INT, it's NOT a drop in replacement; see Developer Notes page.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4142 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bmod.bas
@@ -868,7 +868,7 @@ SUB battle_display (BYREF bat AS BattleState, bslot() AS BattleSprite, menubits(
    'if keyval(scS) > 1 then gen(genMaxInventory) += 3
    'if keyval(scA) > 1 then gen(genMaxInventory) -= 3
    IF bat.menu_mode = batMENUITEM THEN '--draw item menu
-    DIM inv_height AS INTEGER = small(78, 8 + CINT((last_inv_slot() + 1) / 3) * 8)
+    DIM inv_height AS INTEGER = small(78, 8 + ((last_inv_slot() + 1) \ 3) * 8)
     WITH bat.inv_scroll
      .top = INT(bat.item.top / 3)
      .pt = INT(bat.item.pt / 3)

common.bas
@@ -4273,7 +4273,7 @@ FUNCTION last_inv_slot() AS INTEGER
  IF gen(genMaxInventory) = 0 THEN RETURN inventoryMax
  '--Otherwise round genMaxInventory up to the nearest
  '-- multiple of three (counting the zero-slot) and return it.
- RETURN INT((gen(genMaxInventory) + 3) / 3) * 3 - 1
+ RETURN ((gen(genMaxInventory) + 3) \ 3) * 3 - 1
 END FUNCTION
 
 SUB setup_sprite_sizes ()

customsubs.bas
@@ -873,10 +873,11 @@ FUNCTION color_browser_256(start_color AS INTEGER=0) AS INTEGER
  LOOP
 END FUNCTION
 
+'Supports negative n too
 FUNCTION xy_from_int(n AS INTEGER, wide AS INTEGER, high AS INTEGER) AS XYPair
  DIM pair AS XYPair
  pair.x = n MOD wide
- pair.y = small(CINT(n / wide), high - 1)
+ pair.y = small(CINT(INT(n / wide)), high - 1)
  RETURN pair
 END FUNCTION
 

drawing.bas
@@ -2526,7 +2526,7 @@ IF keyval(scTilde) > 1 THEN ss.hidemouse = ss.hidemouse XOR 1
 IF keyval(scComma) > 1 AND ss.palindex > 0 THEN ss.palindex -= 1
 IF keyval(scPeriod) > 1 AND ss.palindex < 15 THEN ss.palindex += 1
 IF ss.zonenum = 2 THEN
- IF mouse.clicks > 0 THEN ss.palindex = small(CINT(ss.zone.x / 4), 15)
+ IF mouse.clicks > 0 THEN ss.palindex = small(ss.zone.x \ 4, 15)
 END IF
 IF keyval(scLeftBrace) > 1 OR (ss.zonenum = 5 AND mouse.clicks > 0) THEN
  changepal poffset(state.pt), -1, workpal(), state.pt - state.top

menustuf.bas
@@ -1731,7 +1731,7 @@ FUNCTION items_menu () as integer
   .x = 8
   .y = 5
   .wide = 304
-  .high = small(180, 12 + (CINT((last_inv_slot() + 1) / 3) + 1) * 8)
+  .high = small(180, 12 + (((last_inv_slot() + 1) \ 3) + 1) * 8)
  END WITH
  WITH istate.scrollrect
   .x = 20
@@ -1741,7 +1741,7 @@ FUNCTION items_menu () as integer
  END WITH
  WITH istate.scroll
   .first = -1
-  .last = INT(last_inv_slot() / 3)
+  .last = last_inv_slot() \ 3
   .size = 20
  END WITH
 