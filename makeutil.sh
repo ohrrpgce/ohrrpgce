78d3a9307a1381b0562a682005a981b0c28819ff
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2011-02-25 22:27:22 +0000

    Remove pointless hspeak.sh and switch makeutil.sh to use Eu4
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4173 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- makeutil.sh
+++ makeutil.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 fbc -lang deprecated -v unlump.bas util.bas lumpfile.bas os_unix.bas blit.o || exit 1
 fbc -lang deprecated -v relump.bas util.bas lumpfile.bas os_unix.bas blit.o || exit 1
-if [ `which ecu` ] ; then
-  ecu hspeak.exw
-  ./emake
-fi
+euc hspeak.exw || exit 1
