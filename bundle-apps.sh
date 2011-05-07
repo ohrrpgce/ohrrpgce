e2545eb025d7ee67290b79ce13cd62d4a8bc58a6
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2011-05-07 15:50:21 +0000

    Fix Mac implementation of spawn_and_wait. Also, include plotscr.hsd and scancode.hsi in the app bundle next to hspeak; easier than teaching it about app bundles.
    I'm aware that Terminal_wrapper.sh still has a bug: if you switch the active terminal window while it's running something, it will attempt to close the wrong one (Terminal will ask whether it should really do this if you have anything open). I tried everything to get a reference to the actual window which the script creates, but for some reason none of the code I found online would work (OS version difference maybe).
    
    AppleScript IS a horror story afterall!
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4293 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- bundle-apps.sh
+++ bundle-apps.sh
@@ -24,4 +24,5 @@ tar xf mac/Frameworks.tar.gz -C OHRRPGCE-Custom.app/Contents &&
 cp -R ohrhelp/*.txt OHRRPGCE-Custom.app/Contents/Resources/ohrhelp &&
 cp ohrrpgce.new OHRRPGCE-Custom.app/Contents/Resources/ &&
 cp mac/Terminal_wrapper.sh OHRRPGCE-Custom.app/Contents/MacOS/support/ &&
+cp plotscr.hsd scancode.hsi OHRRPGCE-Custom.app/Contents/MacOS/support/ &&
 tar xf mac/utilities.tar.gz -C OHRRPGCE-Custom.app/Contents/MacOS/support/ || exit 1
--- mac/Terminal_wrapper.sh
+++ mac/Terminal_wrapper.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-APP=$0
-shift 1
+#APP=$0
+#shift 1
 osascript <<EOF
 tell application "Terminal"
     activate
-    tell window 1
-        do script "$@"
-        repeat while busy
-            delay 1
-        end repeat
-        close
-    end tell
-end tell
-tell application "OHRRPGCE-Custom"
-    activate
+    set mytab to do script "$@"
+    --set mytab to do script "~/src2/zenzizenzic/hspeak -y ~/src2/zenzizenzic/plotscr.hsd ; exit"
+    delay 0.5
+    repeat while mytab is busy
+        delay 1
+    end repeat
+    --display dialog "fin"
+    close window 1
+    --close (first window whose selected tab is mytab) saving no
 end tell
 EOF
+#tell application "OHRRPGCE-Custom"
+#    activate
+#end tell
