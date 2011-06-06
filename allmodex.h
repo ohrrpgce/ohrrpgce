5af8fe0ede67dc789a04394a71b02941f4b7f85f
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2011-06-06 08:03:25 +0000

    Move struct Frame definition from blit.c to a separate header. It's probably most logical to put it in surface.h. But that doesn't work because surface.h is C++. Switching blit.c to C++ is a trivial change of course.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4405 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ allmodex.h
@@ -0,0 +1,39 @@
+// structs translated from udts.bi, for C interoperability with allmodex.bas
+
+#ifndef ALLMODEX_H
+#define ALLMODEX_H
+
+struct XYPair {
+	int w;
+	int h;
+};
+
+struct Palette16 {
+	unsigned char col[16];  //indicies into the master palette
+	int refcount; //private
+};
+
+struct SpriteCacheEntry;
+struct SpriteSet;
+
+struct Frame {
+	int w;
+	int h;
+	int pitch;     //pixel (x,y) is at .image[.x + .pitch * .y]; mask and image pitch are the same!
+	unsigned char *image;
+	unsigned char *mask;
+	int refcount;  //see sprite_unload in particular for documentation
+	int arraylen;  //how many frames were contiguously allocated in this frame array
+	struct Frame *base;   //the Frame which actually owns this memory
+	struct SpriteCacheEntry *cacheentry;
+	int cached:1;  //(not set for views onto cached sprites)
+	int arrayelem:1;  //not the first frame in a frame array
+	int isview:1;
+
+	//used only by frames in a SpriteSet, for now
+	struct XYPair offset;
+	struct SpriteSet *sprset;  //if not NULL, this Frame array is part of a SpriteSet which
+	                           //will need to be freed at the same time
+};
+
+#endif
