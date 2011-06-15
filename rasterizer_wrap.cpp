32ec10dadf3fd4952a9f1c3f2e40224066e9529c
jay <jay@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2011-06-15 11:57:56 +0000

    Updated the wrapper function to call gfx_renderQuadTexture() from gfx_newRenderPlan.h.
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4455 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- rasterizer_wrap.cpp
+++ rasterizer_wrap.cpp
@@ -1,4 +1,4 @@
-#include "rasterizer.h"
+#include "gfx_newRenderPlan.h"
 #include "matrixMath.h"
 #include "allmodex.h"
 
@@ -7,24 +7,26 @@ extern "C" {
 }
 
 void frame_draw_transformed(Frame *dest, Frame *src, float3 vertices[4]) {
-	QuadRasterizer rasterizer;
-	Quad quad;
+	QuadT quad;
 	quad.pnt[0].tex = TexCoord(0, 0);
 	quad.pnt[1].tex = TexCoord(1, 0);
 	quad.pnt[2].tex = TexCoord(1, 1);
 	quad.pnt[3].tex = TexCoord(0, 1);
 	for (int i = 0; i < 4; i++) {
+		quad.pnt[i].col = 0xffffffff;
 		quad.pnt[i].pos.x = vertices[i].x;
 		quad.pnt[i].pos.y = vertices[i].y;
 	}
 	Surface srcsurf, destsurf;
 	srcsurf.width = src->w;
 	srcsurf.height = src->h;
-	srcsurf.format = SFMT_P8;
+	srcsurf.format = SF_8bit;
+	srcsurf.usage = SU_Source;
 	srcsurf.pPaletteData = src->image;
 	destsurf.width = dest->w;
 	destsurf.height = dest->h;
-	destsurf.format = SFMT_P8;
+	destsurf.format = SF_8bit;
+	destsurf.usage = SF_RenderTarget;
 	destsurf.pPaletteData = dest->image;
-	rasterizer.drawTexture(&destsurf, &quad, &srcsurf);
+	gfx_renderQuadTexture( &quad, &srcsurf, 0, 0xffffffff, 0, &destsurf );
 }
