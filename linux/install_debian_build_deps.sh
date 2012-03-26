530ff34dab4d6849ebbd4756844ff1e99066032c
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2012-03-26 21:42:42 +0000

    g++ is not installed by default on a clean Debian install
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@5113 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/install_debian_build_deps.sh
+++ linux/install_debian_build_deps.sh
@@ -3,5 +3,10 @@
 echo "This script installs the debian packages that are required to"
 echo "compile the OHRRPGCE. You must manually install FreeBasic and"
 echo "Euphoria because they are not currently available as deb packages"
+echo ""
+echo "http://freebasic.net/"
+echo "http://openeuphoria.org/"
+echo "------------------------------------------------------------------"
 
-sudo apt-get install scons libx11-dev libxpm-dev libxrandr-dev libxrender-dev libsdl-mixer1.2-dev timidity
+sudo apt-get install scons g++ libx11-dev libxpm-dev libxrandr-dev libxrender-dev libsdl-mixer1.2-dev timidity gdb
+# gdb isn't strictly required, but is useful anyway
