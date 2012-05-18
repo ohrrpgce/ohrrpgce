5049f9889c38c8dcf8f0489b28793e2eba7326f6
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2012-05-18 19:23:03 +0000

    web: add the script that I run on hamsterrepublic.com to switch the symlinks for a new stable version
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@5178 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ ohrstable.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+WEBROOT=~/HamsterRepublic.com
+ARCHIVE="${WEBROOT}"/ohrrpgce/archive
+DL="${WEBROOT}"/dl
+REL="../ohrrpgce/archive"
+
+STABLE=`
+ls -l "${DL}"/ohrrpgce-win-installer.exe \
+  | sed -e s/".*\/"/""/ \
+        -e s/"ohrrpgce-win-installer-"/""/ \
+        -e s/"\.exe$"/""/ \
+  | cut -d "-" -f 4- \
+  `
+echo "Current stable milestone is: ${STABLE}"
+
+VER="${1}"
+USAGE="true"
+
+if [ -e "$ARCHIVE"/ohrrpgce-win-installer-????-??-??-"${VER}".exe ] ; then
+  unset USAGE
+fi
+
+if [ "${USAGE}" ] ; then
+  if [ -z "${VER}" ] ; then
+    echo "You must specify a milestone on the command-line."
+  else
+    echo "Milestone \"${VER}\" is not valid."
+  fi
+  echo "Available milestones:"
+  ls -1 "${ARCHIVE}"/ohrrpgce-win-installer-*.exe \
+    | sed -e s/"^.*\/ohrrpgce-win-installer-"/""/ \
+          -e s/"\.exe$"/""/ \
+    | sort \
+    | uniq \
+    | cut -d "-" -f "4-"
+  exit
+fi
+
+echo "Updating links to point to ${VER} milestone..."
+cd "${DL}"
+
+function sourcefile () {
+  REL="${1}"
+  PREFIX="${2}"
+  VER="${3}"
+  EXT="${4}"
+  ls -1 "${REL}/${PREFIX}"-????-??-??-"${VER}.${EXT}" \
+         2>&1 \
+         | grep -v ": No such file or directory" \
+         | sed s/".*\/"/""/
+}
+
+function updatelink () {
+  REL="${1}"
+  VER="${2}"
+  PREFIX="${3}"
+  EXT="${4}"
+  OLDPREFIX="${5}"
+  DFILE="${PREFIX}.${EXT}"
+  printf "  ${DFILE}"
+  SFILE=`sourcefile "${REL}" "${PREFIX}" "${VER}" "${EXT}"`
+  if [ ! -f "${REL}/${SFILE}" ] ; then
+    if [ -n "${OLDPREFIX}" ] ; then
+      SFILE=`sourcefile "${REL}" "${OLDPREFIX}" "${VER}" "${EXT}"`
+      if [ ! -f "${REL}/${SFILE}" ] ; then
+        if [ -n "${OLDPREFIX2}" ] ; then
+          SFILE=`sourcefile "${REL}" "${OLDPREFIX2}" "${VER}" "${EXT}"`
+          if [ -n "${OLDPREFIX2}" ] ; then
+            printf " (NONE OF 3 FOUND!)\n"
+            exit
+          fi
+        else
+          printf " (NEITHER FOUND!)\n"
+          exit
+        fi
+      fi
+    else
+      printf " (NOT FOUND!)\n"
+      exit
+    fi
+  fi
+  printf " (${SFILE})"
+  rm "${DFILE}"
+  ln -s "${REL}/${SFILE}" "${DFILE}"
+  printf "\n"
+}
+
+updatelink "${REL}" "${VER}" "ohrrpgce-win-installer" "exe" "" ""
+updatelink "${REL}" "${VER}" "ohrrpgce"               "zip" "custom" ""
+updatelink "${REL}" "${VER}" "ohrrpgce-minimal"        "zip" "ohrrpgce-floppy" "ohrrpgce_play"
+updatelink "${REL}" "${VER}" "ohrrpgce-linux-x86"     "tar.bz2" "" ""
+updatelink "${REL}" "${VER}" "OHRRPGCE"               "dmg" "" ""
