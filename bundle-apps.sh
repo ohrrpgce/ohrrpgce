3021061153958801d9196050ec76679f97e5a3c0
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2012-05-18 22:30:04 +0000

    Remove .svn folders from inside the Mac app bundles
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@5183 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- bundle-apps.sh
+++ bundle-apps.sh
@@ -9,6 +9,9 @@ rm -rf OHRRPGCE-Custom.app
 
 echo Bundling OHRRPGCE-Game.app
 cp -R mac/OHRRPGCE-Game.app.template OHRRPGCE-Game.app &&
+rm -Rf OHRRPGCE-Game.app/.svn &&
+rm -Rf OHRRPGCE-Game.app/Contents/.svn &&
+rm -Rf OHRRPGCE-Game.app/Contents/Resources/.svn &&
 mkdir -p OHRRPGCE-Game.app/Contents/MacOS &&
 sed -i -e "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Game.app/Contents/Info.plist &&
 cp ohrrpgce-game OHRRPGCE-Game.app/Contents/MacOS/ &&
@@ -16,6 +19,9 @@ tar xf mac/Frameworks.tar.gz -C OHRRPGCE-Game.app/Contents || exit 1
 
 echo Bundling OHRRPGCE-Custom.app
 cp -R mac/OHRRPGCE-Custom.app.template OHRRPGCE-Custom.app &&
+rm -Rf OHRRPGCE-Custom.app/.svn &&
+rm -Rf OHRRPGCE-Custom.app/Contents/.svn &&
+rm -Rf OHRRPGCE-Custom.app/Contents/Resources/.svn &&
 mkdir -p OHRRPGCE-Custom.app/Contents/MacOS/support &&
 mkdir -p OHRRPGCE-Custom.app/Contents/Resources/ohrhelp &&
 sed -i -e "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Custom.app/Contents/Info.plist &&
