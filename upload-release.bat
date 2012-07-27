df1cc8ab70e17e7a92b33cb76b34589c2042592e
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2012-07-27 15:52:48 +0000

    The Windows upload-release.bat script should trust putty to know the correct login key and not attempt to hard-code it on the command line
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@5279 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- upload-release.bat
+++ upload-release.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-pscp -i "C:\program files\putty\id_rsa.ppk" distrib/ohrrpgce-* james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
+pscp distrib/ohrrpgce-* james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
