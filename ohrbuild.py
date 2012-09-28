8290437d2d93147e10890ac4fc425078e665bf95
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2012-09-28 16:26:32 +0000

    scons: allow whitespace before #include
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@5391 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- ohrbuild.py
+++ ohrbuild.py
@@ -14,7 +14,7 @@ def get_run_command(cmd):
         raise Exception("subprocess.Popen(%s) returned stderr:\n%s" % (cmd, errtext))
     return proc.stdout.read().strip()
 
-include_re = re.compile(r'^#include\s+"(\S+)"', re.M | re.I)
+include_re = re.compile(r'^\s*#include\s+"(\S+)"', re.M | re.I)
 
 standard_bi = ['crt', 'SDL', 'libxml', 'fbgfx.bi', 
                'file.bi', 'allegro.bi', 'string.bi', 'curses.bi']
