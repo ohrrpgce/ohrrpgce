ohrrpgce/ohrrpgce

commit 1a3910fec5aa902e81412b8fd62f2985bbe8d5fa

findfiles now returns its results in a array instead of a file. Lots of cleanup! ~100 lines of code fewer.
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@3897 7d344553-34f0-0310-a9b1-970ce8f1c3a2

lumpfile.bas
@@ -441,28 +441,20 @@ end function
 
 function indexunlumpeddir (whichdir as string) as LumpIndex ptr
 	dim index as LumpIndex ptr
-	dim fh as integer
-	dim filename as string
 
 	index = callocate(sizeof(LumpIndex))
 	construct_LumpIndex(*index)
 	index->unlumpeddir = whichdir
 
-	'ideally findfiles would have an overload to return files in an array, but that's a nontrivial project
-	findfiles whichdir, ALLFILES, fileTypeFile, NO, tmpdir + "filelist.tmp"
-	fh = freefile
-	open "filelist.tmp" for input as #fh
-	do until eof(fh)
-		line input #fh, filename
-		
+	dim filelist() as string
+	findfiles whichdir, ALLFILES, fileTypeFile, NO, filelist()
+	for i as integer = 0 to ubound(filelist)
 		dim lmp as FileLump ptr
 		lmp = callocate(sizeof(FileLump))
 		lmp->type = LT_FILE
-		lmp->lumpname = filename
+		lmp->lumpname = filelist(i)
 		LumpIndex_addlump(*index, cast(Lump ptr, lmp))
-	loop
-	close fh
-	kill "filelist.tmp"
+	next
 
 	return index
 end function
@@ -825,45 +817,36 @@ function islumpfile (lumpfile as string, fmask as string) as integer
 	close #lf
 end function
 
-sub lumpfiles (listf as string, lumpfile as string, path as string)
-	dim as integer lf, fl, tl	'lumpfile, filelist, tolump
+sub lumpfiles (filelist() as string, lumpfile as string, path as string)
+	dim as integer lf, tl  'lumpfile, tolump
 
 	dim dat as ubyte
 	dim size as integer
-	dim lumpname as string 'name of the file to lump
 	dim lname as string 'name actually written
 	dim bufr as ubyte ptr
 	dim csize as integer
 
-	fl = freefile
-	if open(listf for input as #fl) <> 0 then
-		exit sub
-	end if
-
 	lf = freefile
 	if open(lumpfile for binary access write as #lf) <> 0 then
 		debug "lumpfiles: Could not open file " + lumpfile
-		close #fl
 		exit sub
 	end if
 
 	bufr = callocate(16384)
 
 	'get file to lump
-	do until eof(fl)
-		line input #fl, lumpname
-
-		lname = ucase(lumpname)
+	for i as integer = 0 to ubound(filelist)
+		lname = ucase(filelist(i))
 		if len(lname) > 50 orelse lname <> exclusive(lname, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.") then
 			debug "lumpfiles: bad lump name '" & lname & "'"
-			continue do
+			continue for
 		end if
 
 		tl = freefile
-		open path + lumpname for binary access read as #tl
+		open path + filelist(i) for binary access read as #tl
 		if err <> 0 then
 			debug "failed to open " + path + lname + " for lumping"
-			continue do
+			continue for
 		end if
 
 		'write lump name
@@ -892,10 +875,9 @@ sub lumpfiles (listf as string, lumpfile as string, path as string)
 		wend
 
 		close #tl
-	loop
+	next
 
 	close #lf
-	close #fl
 
 	deallocate bufr
 end sub
@@ -971,55 +953,26 @@ function matchmask(match as string, mask as string) as integer
 	end if
 end function
 
-sub fixlumporder (f as string)
-	DIM AS INTEGER ofh, ifh
-        DIM AS STRING a, b, c
-	filecopy f$, "fixorder.tmp"
-	
-	ofh = FREEFILE
-	OPEN f$ FOR OUTPUT AS #ofh
-	
-	ifh = FREEFILE
-	OPEN "fixorder.tmp" FOR INPUT AS #ifh
-	
-	'--first output the archinym.lmp and browse.txt files
-	WHILE NOT EOF(ifh)
-		LINE INPUT #ifh, a$
-		b$ = LCASE$(a$)
-		IF b$ = "archinym.lmp" OR b$ = "browse.txt" THEN
-			PRINT #ofh, a$
-		END IF
-	WEND
-	
-	'--close and re-open
-	CLOSE #ifh
-	OPEN "fixorder.tmp" FOR INPUT AS #ifh
-	
-	'--output the other files, excluding illegal files
-	WHILE NOT EOF(ifh)
-		LINE INPUT #ifh, a$
-		b$ = LCASE$(a$)
-		SELECT CASE b$
-			CASE "archinym.lmp", "browse.txt"
-				'--do nothing
-			CASE ELSE
-				'--check extenstion
-				c$ = RIGHT$(b$, 4)
-				SELECT CASE c$
-					CASE ".tmp"
-						'--do nothing
-					CASE ELSE
-						'--output all other names
-						PRINT #ofh, a$
-				END SELECT
-		END SELECT
-	WEND
-	CLOSE #ifh
-	
-	CLOSE #ofh
-	
-	safekill "fixorder.tmp"
-END SUB
+'Given a list of lumps reorder them to speed up RPG browsing, and exclude *.tmp files
+sub fixlumporder (filelist() as string)
+	dim as integer temp, readpos, writepos
+
+	'--move archinym.lmp and browse.txt to front
+	temp = str_array_findcasei(filelist(), "browse.txt")
+	if temp > -1 then swap filelist(0), filelist(temp)
+	temp = str_array_findcasei(filelist(), "archinym.lmp")
+	if temp > -1 then swap filelist(1), filelist(temp)
+
+	'--exclude illegal *.tmp files; shuffle them to the end of the array, then trimming them
+	writepos = 0
+	for readpos = 0 to ubound(filelist)
+		if lcase(right(filelist(readpos), 4)) <> ".tmp" then
+			swap readpos, writepos
+			writepos += 1
+		end if
+	next
+	redim preserve filelist(-1 to writepos - 1)
+end sub
 
 '----------------------------------------------------------------------
 '                           BufferedFile

browse.bas
@@ -323,40 +323,36 @@ END SUB
 SUB browse_add_files(wildcard$, BYVAL filetype AS INTEGER, BYREF br AS BrowseMenuState, tree() AS BrowseMenuEntry)
 DIM bmpd AS BitmapInfoHeader
 DIM tempbuf(79)
-DIM f AS STRING
+DIM filename AS STRING
 
-DIM filelist AS STRING
-filelist = br.tmp + "hrbrowse.tmp"
-findfiles br.nowdir, wildcard$, filetype, br.showhidden, filelist
+DIM filelist() AS STRING
+findfiles br.nowdir, wildcard$, filetype, br.showhidden, filelist()
 
-
-DIM fh AS INTEGER = FREEFILE
-OPEN filelist FOR INPUT AS #fh
-DO UNTIL EOF(fh)
+FOR i AS INTEGER = 0 TO UBOUND(filelist)
  br.treesize = br.treesize + 1
  IF br.treesize = UBOUND(tree) THEN REDIM PRESERVE tree(UBOUND(tree) + 256)
  tree(br.treesize).kind = 3
- LINE INPUT #fh, tree(br.treesize).filename
- f = br.nowdir & tree(br.treesize).filename
+ tree(br.treesize).filename = filelist(i)
+ filename = br.nowdir & tree(br.treesize).filename
  '---music files
  IF br.special = 1 OR br.special = 5 THEN
-  IF validmusicfile(f, VALID_MUSIC_FORMAT) = 0 THEN
+  IF validmusicfile(filename, VALID_MUSIC_FORMAT) = 0 THEN
    tree(br.treesize).kind = 6
    tree(br.treesize).about = "Not a valid music file"
   END IF
  END IF
  IF br.special = 6 THEN
-  IF validmusicfile(f, VALID_FX_FORMAT) = 0 THEN
+  IF validmusicfile(filename, VALID_FX_FORMAT) = 0 THEN
    tree(br.treesize).kind = 6
    tree(br.treesize).about = "Not a valid sound effect file"
-  ELSEIF FILELEN(f) > 500 * 1024 AND justextension(tree(br.treesize).filename) <> "wav" THEN
+  ELSEIF FILELEN(filename) > 500 * 1024 AND justextension(filename) <> "wav" THEN
    tree(br.treesize).kind = 6
    tree(br.treesize).about = "File is too large (limit 500kB)"
   END IF
  END IF
  '---4-bit BMP browsing
  IF br.special = 2 THEN
-  IF bmpinfo(f, bmpd) THEN
+  IF bmpinfo(filename, bmpd) THEN
    IF bmpd.biBitCount <> 4 OR bmpd.biWidth > 320 OR bmpd.biHeight > 200 THEN
     tree(br.treesize).kind = 6
    END IF
@@ -366,7 +362,7 @@ DO UNTIL EOF(fh)
  END IF
  '---320x200x24/8bit BMP files
  IF br.special = 3 THEN
-  IF bmpinfo(f, bmpd) THEN
+  IF bmpinfo(filename, bmpd) THEN
    IF (bmpd.biBitCount <> 24 AND bmpd.biBitCount <> 8) OR bmpd.biWidth <> 320 OR bmpd.biHeight <> 200 THEN
     tree(br.treesize).kind = 6
    END IF
@@ -376,17 +372,17 @@ DO UNTIL EOF(fh)
  END IF
  '--master palettes  (why isn't this up there?)
  IF br.special = 4 THEN
-  IF LCASE$(justextension$(tree(br.treesize).filename)) = "mas" THEN
+  IF LCASE$(justextension$(filename)) = "mas" THEN
    DIM masfh AS INTEGER = FREEFILE
-   OPEN f FOR BINARY AS #masfh
+   OPEN filename FOR BINARY AS #masfh
    DIM a AS STRING = "       "
    GET #masfh, 1, a
    CLOSE #masfh
    IF a <> br.mashead AND a <> br.paledithead THEN
     tree(br.treesize).kind = 6
    END IF
-  ELSE
-   IF bmpinfo(f, bmpd) THEN
+  ELSE  'BMP as a master palette
+   IF bmpinfo(filename, bmpd) THEN
     IF (bmpd.biBitCount = 8 OR bmpd.biBitCount = 24 AND (bmpd.biWidth = 16 AND bmpd.biHeight = 16)) = 0 THEN tree(br.treesize).kind = 6
    ELSE
     br.treesize = br.treesize - 1
@@ -395,7 +391,7 @@ DO UNTIL EOF(fh)
  END IF
  '--RPG files
  IF br.special = 7 THEN
-  copylump f, "browse.txt", br.tmp, -1
+  copylump filename, "browse.txt", br.tmp, -1
   IF loadrecord(tempbuf(), br.tmp + "browse.txt", 40, , NO) THEN
    tree(br.treesize).caption = readbinstring(tempbuf(), 0, 38)
    tree(br.treesize).about = readbinstring(tempbuf(), 20, 38)
@@ -408,14 +404,12 @@ DO UNTIL EOF(fh)
  END IF
  '--RELOAD files
  IF br.special = 8 THEN
-  IF browse_sanity_check_reload(f, tree(br.treesize).about) = NO THEN
+  IF browse_sanity_check_reload(filename, tree(br.treesize).about) = NO THEN
    tree(br.treesize).kind = 6 'grey out bad ones
   END IF
  END IF
  draw_browse_meter br
-LOOP
-CLOSE #fh
-safekill filelist
+NEXT
 
 END SUB
 
@@ -606,14 +600,13 @@ SUB build_listing(tree() AS BrowseMenuEntry, BYREF br AS BrowseMenuState)
    END IF
   LOOP
   '---FIND ALL SUB-DIRECTORIES IN THE CURRENT DIRECTORY---
-  findfiles br.nowdir, ALLFILES, fileTypeDirectory, br.showhidden, br.tmp + "hrbrowse.tmp"
-  DIM fh AS INTEGER = FREEFILE
-  OPEN br.tmp + "hrbrowse.tmp" FOR INPUT AS #fh
-  DO UNTIL EOF(fh)
+  DIM filelist() AS STRING
+  findfiles br.nowdir, ALLFILES, fileTypeDirectory, br.showhidden, filelist()
+  FOR i AS INTEGER = 0 TO UBOUND(filelist)
    br.treesize = br.treesize + 1
    IF br.treesize = UBOUND(tree) THEN REDIM PRESERVE tree(UBOUND(tree) + 256)
    tree(br.treesize).kind = 2
-   LINE INPUT #fh, tree(br.treesize).filename
+   tree(br.treesize).filename = filelist(i)
    IF tree(br.treesize).filename = "." OR tree(br.treesize).filename = ".." OR RIGHT$(tree(br.treesize).filename, 4) = ".tmp" THEN br.treesize = br.treesize - 1
    IF br.special = 7 THEN ' Special handling in RPG mode
     IF justextension$(tree(br.treesize).filename) = "rpgdir" THEN br.treesize = br.treesize - 1
@@ -626,8 +619,7 @@ SUB build_listing(tree() AS BrowseMenuEntry, BYREF br AS BrowseMenuState)
    IF justextension$(tree(br.treesize).filename) = "app" THEN br.treesize = br.treesize - 1
 #ENDIF
    draw_browse_meter br
-  LOOP
-  CLOSE #fh
+  NEXT
   safekill br.tmp + "hrbrowse.tmp"
   '---FIND ALL FILES IN FILEMASK---
   DIM filetype AS INTEGER = fileTypeFile

custom.bas
@@ -201,15 +201,11 @@ touchfile workingdir + SLASH + "__danger.tmp"
 
 IF isdir(sourcerpg) THEN
  'work on an unlumped RPG file. Don't take hidden files
- findfiles sourcerpg, ALLFILES, fileTypeFile, NO, "filelist.tmp"
- fh = FREEFILE
- OPEN "filelist.tmp" FOR INPUT AS #fh
- DO UNTIL EOF(fh)
-  LINE INPUT #fh, filename$
-  filecopy sourcerpg + SLASH + filename$, workingdir + SLASH + filename$
- LOOP
- CLOSE #fh
- KILL "filelist.tmp"
+ DIM filelist() AS STRING
+ findfiles sourcerpg, ALLFILES, fileTypeFile, NO, filelist()
+ FOR i = 0 TO UBOUND(filelist)
+  filecopy sourcerpg + SLASH + filelist(i), workingdir + SLASH + filelist(i)
+ NEXT
 ELSE
  unlump sourcerpg, workingdir + SLASH
 END IF
@@ -1044,33 +1040,28 @@ FUNCTION newRPGfile (templatefile$, newrpg$)
 END FUNCTION
 
 SUB dolumpfiles (filetolump$)
-'--build the list of files to lump. We don't need hidden files
-findfiles workingdir, ALLFILES, fileTypeFile, NO, "temp.lst"
-fixlumporder "temp.lst"
-IF isdir(filetolump$) THEN
- '---copy changed files back to source rpgdir---
- IF NOT fileiswriteable(filetolump$ & SLASH & "archinym.lmp") THEN
-  move_unwritable_rpg filetolump$
-  makedir filetolump$
+ '--build the list of files to lump. We don't need hidden files
+ DIM filelist() AS STRING
+ findfiles workingdir, ALLFILES, fileTypeFile, NO, filelist()
+ fixlumporder filelist()
+ IF isdir(filetolump$) THEN
+  '---copy changed files back to source rpgdir---
+  IF NOT fileiswriteable(filetolump$ & SLASH & "archinym.lmp") THEN
+   move_unwritable_rpg filetolump$
+   makedir filetolump$
+  END IF
+  FOR i AS INTEGER = 0 TO UBOUND(filelist)
+   safekill filetolump$ + SLASH + filelist(i)
+   filecopy workingdir + SLASH + filelist(i), filetolump$ + SLASH + filelist(i)
+   'FIXME: move file instead? (warning: can't move from different mounted filesystem)
+  NEXT
+ ELSE
+  '---relump data into lumpfile package---
+  IF NOT fileiswriteable(filetolump$) THEN
+   move_unwritable_rpg filetolump$
+  END IF
+  lumpfiles filelist(), filetolump$, workingdir + SLASH
  END IF
- fh = FREEFILE
- OPEN "temp.lst" FOR INPUT AS #fh
- DO UNTIL EOF(fh)
-  LINE INPUT #fh, filename$
-  safekill filetolump$ + SLASH + filename$
-  filecopy workingdir + SLASH + filename$, filetolump$ + SLASH + filename$
-  'FIXME: move file?
- LOOP
- CLOSE #fh
- safekill "temp.lst"
-ELSE
- '---relump data into lumpfile package---
- IF NOT fileiswriteable(filetolump$) THEN
-  move_unwritable_rpg filetolump$
- END IF
- lumpfiles "temp.lst", filetolump$, workingdir + SLASH
- safekill "temp.lst"
-END IF
 END SUB
 
 FUNCTION readarchinym$ ()

lumpfile.bi
@@ -152,11 +152,11 @@ declare sub storerecord overload (buf() as integer, filename as string, recordsi
 
 declare function indexunlumpeddir (whichdir as string) as LumpIndex ptr
 declare function indexlumpfile (lumpfile as string, byval keepopen as integer = YES) as LumpIndex ptr
-declare sub lumpfiles (listf as string, lump as string, path as string)
+declare sub lumpfiles (filelist() as string, lump as string, path as string)
 declare sub unlump(lump as string, ulpath as string)
 declare sub unlumpfile(lump as string, fmask as string, path as string)
 declare function islumpfile (lump as string, fmask as string) as integer
-declare sub fixlumporder (f as string)
+declare sub fixlumporder (filelist() as string)
 
 
 '----------------------------------------------------------------------

relump.bas
@@ -67,11 +67,11 @@ END IF
 IF isdir(dest$) THEN fatalerror "destination file " + dest$ + " already exists as a folder."
 
 '--build the list of files to lump
-findfiles src$, ALLFILES, fileTypefile, NO, "temp.lst"
-fixlumporder "temp.lst"
+DIM filelist() as string
+findfiles src$, ALLFILES, fileTypefile, NO, filelist()
+fixlumporder filelist()
 '---relump data into lumpfile package---
-lumpfiles "temp.lst", dest$, src$ + SLASH
-KILL "temp.lst"
+lumpfiles filelist(), dest$, src$ + SLASH
 
 SYSTEM
 

util.bas
@@ -431,6 +431,14 @@ SUB str_array_append (array() AS STRING, s AS STRING)
  array(UBOUND(array)) = s
 END SUB
 
+FUNCTION str_array_findcasei (array() AS STRING, value AS STRING) AS INTEGER
+ DIM valuei AS STRING = LCASE(value)
+ FOR i AS INTEGER = LBOUND(array) TO UBOUND(array)
+  IF LCASE(array(i)) = value THEN RETURN i
+ NEXT
+ RETURN -1
+END FUNCTION
+
 SUB int_array_append (array() AS INTEGER, BYVAL k AS INTEGER)
  REDIM PRESERVE array(LBOUND(array) TO UBOUND(array) + 1) AS INTEGER
  array(UBOUND(array)) = k
@@ -769,12 +777,14 @@ SUB touchfile (filename as string)
   CLOSE #fh
 END SUB
 
-'Finds files in a directory, writing them one to a line in outfile
-'By default, find all files in directory, otherwise it is a case-insensitive filename mask
+'Finds files in a directory, writing them into an array without their path
+'filelist() must be resizeable; it'll be resized so that LBOUND = -1, with files, if any, in filelist(0) up
+'By default, find all files in directory, otherwise namemask is a case-insensitive filename mask
 'filetype is one of fileTypeFile, fileTypeDirectory
-SUB findfiles (directory AS STRING, namemask AS STRING = ALLFILES, BYVAL filetype AS INTEGER = fileTypeFile, BYVAL findhidden AS INTEGER = 0, outfile AS STRING)
-  DIM AS STRING fmask = directory
-  IF RIGHT(fmask, 1) <> SLASH THEN fmask += SLASH
+SUB findfiles (directory AS STRING, namemask AS STRING = ALLFILES, BYVAL filetype AS INTEGER = fileTypeFile, BYVAL findhidden AS INTEGER = 0, filelist() AS STRING)
+  DIM AS STRING searchdir = directory
+  IF RIGHT(searchdir, 1) <> SLASH THEN searchdir += SLASH
+  REDIM filelist(-1 TO -1)
 
 #ifdef __UNIX__
   'this is super hacky, but works around the apparent uselessness of DIR
@@ -783,38 +793,30 @@ SUB findfiles (directory AS STRING, namemask AS STRING = ALLFILES, BYVAL filetyp
   grep = "-v '/$'"
   IF filetype = fileTypeDirectory THEN grep = "'/$'"
   realmask = anycase(namemask)
-  fmask = """" + escape_string(fmask, """`\$") + """"
+  searchdir = """" + escape_string(searchdir, """`\$") + """"
   IF findhidden THEN
-    fmask = fmask + realmask + " " + fmask + "." + realmask
+    searchdir = searchdir + realmask + " " + searchdir + "." + realmask
   ELSE
-    fmask = fmask + realmask
+    searchdir = searchdir + realmask
   END IF
 
-  SHELL "ls -d1p " + fmask + " 2>/dev/null |grep "+ grep + ">" + shellout + " 2>&1"
-  DIM AS INTEGER f1, f2
+  SHELL "ls -d1p " + searchdir + " 2>/dev/null |grep "+ grep + ">" + shellout + " 2>&1"
+  DIM AS INTEGER f1
   f1 = FreeFile
   OPEN shellout FOR INPUT AS #f1
-  f2 = FreeFile
-  OPEN outfile FOR OUTPUT AS #f2
-  DIM s AS STRING
+  DIM filename AS STRING
   DO UNTIL EOF(f1)
-    LINE INPUT #f1, s
-    IF RIGHT(s, 3) = "/./" ORELSE RIGHT(s, 4) = "/../" _
-         ORELSE s = "/dev/" ORELSE s = "/proc/" ORELSE s = "/sys/" THEN CONTINUE DO
-    IF RIGHT(s, 1) = "/" THEN s = LEFT(s, LEN(s) - 1)
-    DO WHILE INSTR(s, "/")
-      s = RIGHT(s, LEN(s) - INSTR(s, "/"))
-    LOOP
-    PRINT #f2, s
+    LINE INPUT #f1, filename
+    IF RIGHT(filename, 3) = "/./" ORELSE RIGHT(filename, 4) = "/../" _
+         ORELSE filename = "/dev/" ORELSE filename = "/proc/" ORELSE filename = "/sys/" THEN CONTINUE DO
+    str_array_append filelist(), trimpath(s)
   LOOP
   CLOSE #f1
-  CLOSE #f2
   KILL shellout
 
 #else
   DIM foundfile AS STRING
   DIM attrib AS INTEGER
-  DIM AS INTEGER tempf, realf
   /'---DOS directory attributes
   CONST attribReadOnly = 1
   CONST attribHidden = 2
@@ -830,51 +832,31 @@ SUB findfiles (directory AS STRING, namemask AS STRING = ALLFILES, BYVAL filetyp
     attrib = (253 XOR 16)
   END IF
   IF findhidden THEN attrib += 2
-  tempf = FreeFile
-  foundfile = DIR(fmask + namemask, attrib)
-  if foundfile = "" then
-    'create an empty file
-    OPEN outfile FOR OUTPUT AS #tempf
-    close #tempf
-    exit sub
-  end if
-  OPEN outfile + ".tmp" FOR OUTPUT AS #tempf
+  foundfile = DIR(searchdir + namemask, attrib)
+  IF foundfile = "" THEN EXIT SUB
+  REDIM tempfilelist(-1 TO -1) AS STRING
   DO UNTIL foundfile = ""
-    PRINT #tempf, foundfile
+    str_array_append tempfilelist(), foundfile
     foundfile = DIR '("", attrib)
   LOOP
-  CLOSE #tempf
-  OPEN outfile + ".tmp" FOR INPUT AS #tempf
-  realf = FREEFILE
-  OPEN outfile FOR OUTPUT AS #realf
-  DO UNTIL EOF(tempf)
-    LINE INPUT #tempf, foundfile
+  FOR i AS INTEGER = 0 TO UBOUND(tempfilelist)
+    foundfile = tempfilelist(i)
     IF filetype = fileTypeDirectory THEN
       'alright, we want directories, but DIR is too broken to give them to us
       'files with attribute 0 appear in the list, so single those out
-      IF DIR(fmask + foundfile, 55) <> "" AND DIR(fmask + foundfile, 39) = "" THEN PRINT #realf, foundfile
-    ELSE
-      PRINT #realf, foundfile
+      IF DIR(searchdir + foundfile, 55) = "" OR DIR(searchdir + foundfile, 39) <> "" THEN CONTINUE FOR
     END IF
-  LOOP
-  CLOSE #tempf
-  CLOSE #realf
-  KILL outfile + ".tmp"
+    str_array_append filelist(), foundfile
+  NEXT
 #endif
 END SUB
 
 SUB killdir(directory as string)
-  dim fh as integer
-  dim filename as string
-  findfiles directory, ALLFILES, fileTypeFile, -1, "filelist.tmp"
-  fh = FREEFILE
-  OPEN "filelist.tmp" FOR INPUT AS #fh
-  DO UNTIL EOF(fh)
-    LINE INPUT #fh, filename
-    KILL directory + SLASH + filename
-  LOOP
-  CLOSE #fh
-  KILL "filelist.tmp"
+  DIM filelist() as string
+  findfiles directory, ALLFILES, fileTypeFile, -1, filelist()
+  FOR i as integer = 0 TO UBOUND(filelist)
+    KILL directory + SLASH + filelist(i)
+  NEXT
   IF RMDIR(directory) THEN
     'errno would get overwritten while building the error message
     DIM err_string AS STRING = *get_sys_err_string()
@@ -893,7 +875,10 @@ SUB makedir (directory as string)
   IF MKDIR(directory) THEN
     'errno would get overwritten while building the error message
     DIM err_string AS STRING = *get_sys_err_string()
-    debug "Could not mkdir(" & directory & "): " & err_string
+    'The heck? On Windows at least, MKDIR throws this false error
+    IF err_string <> "File exists" THEN
+     debug "Could not mkdir(" & directory & "): " & err_string
+    END IF
     EXIT SUB
   END IF
 #ifdef __FB_LINUX__

util.bi
@@ -113,6 +113,7 @@ TYPE IntStrPair
 END TYPE
 
 declare sub str_array_append (array() as string, s as string)
+declare function str_array_findcasei (array() as string, value as string) as integer
 declare sub int_array_append (array() as integer, byval k as integer)
 declare sub intstr_array_append (array() as IntStrPair, byval k as integer, s as string)
 declare function int_array_find (array() as integer, byval value as integer) as integer
@@ -256,7 +257,7 @@ declare function is_absolute_path (sDir as string) as integer
 declare function parentdir (pathname as string, BYVAL upamount as integer = 1) as string
 declare function anycase (filename as string) as string
 declare sub touchfile (filename as string)
-declare sub findfiles (directory AS STRING, namemask AS STRING = ALLFILES, BYVAL filetype AS INTEGER = fileTypeFile, BYVAL findhidden AS INTEGER = 0, outfile AS STRING)
+declare sub findfiles (directory AS STRING, namemask AS STRING = ALLFILES, BYVAL filetype AS INTEGER = fileTypeFile, BYVAL findhidden AS INTEGER = 0, filelist() AS STRING)
 declare sub killdir (directory as string)
 declare sub makedir (directory as string)
 declare sub safekill (f as string)

yetmore2.bas
@@ -338,39 +338,29 @@ END IF
 END SUB
 
 SUB cleanuptemp
- findfiles workingdir, ALLFILES, fileTypeFile, NO, tmpdir + "filelist.tmp"
- fh = FREEFILE
-  OPEN tmpdir + "filelist.tmp" FOR INPUT AS #fh
-  DO UNTIL EOF(fh)
-   LINE INPUT #fh, filename$
-   IF usepreunlump = 0 THEN
-    'normally delete everything
-    KILL workingdir + SLASH + filename$
-   ELSE
-    'but for preunlumped games only delete specific files
-    ext$ = justextension$(filename$)
-    IF ext$ = "tmp" OR ext$ = "bmd" THEN
-     KILL workingdir + SLASH + filename$
-    END IF
+ 'Delete contents of/clean up workingdir
+ DIM filelist() as string
+ findfiles workingdir, ALLFILES, fileTypeFile, NO, filelist()
+ FOR i as integer = 0 TO UBOUND(filelist)
+  IF usepreunlump = 0 THEN
+   'normally delete everything
+   KILL workingdir + SLASH + filelist(i)
+  ELSE
+   'but for preunlumped games only delete specific files
+   DIM ext$ = justextension$(filelist(i))
+   IF ext$ = "tmp" OR ext$ = "bmd" THEN
+    KILL workingdir + SLASH + filelist(i)
    END IF
-  LOOP
-  CLOSE #fh
+  END IF
+ NEXT
 
-  KILL tmpdir + "filelist.tmp"
-
-  findfiles tmpdir, ALLFILES, fileTypeFile, NO, tmpdir + "filelist.tmp"
-  fh = FREEFILE
-  OPEN tmpdir + "filelist.tmp" FOR INPUT AS #fh
-  DO UNTIL EOF(fh)
-   LINE INPUT #fh, filename$
-   IF filename$ = "filelist.tmp" THEN CONTINUE DO ' skip this, deal with it later
-   IF NOT isdir(tmpdir & filename$) THEN
-    KILL tmpdir & filename$
-   END IF
-  LOOP
-  CLOSE #fh
-
-  KILL tmpdir + "filelist.tmp"
+ 'Delete contents of/clean up tmpdir
+ findfiles tmpdir, ALLFILES, fileTypeFile, NO, filelist()
+ FOR i as integer = 0 TO UBOUND(filelist)
+  IF NOT isdir(tmpdir & filelist(i)) THEN
+   KILL tmpdir & filelist(i)
+  END IF
+ NEXT
 END SUB
 
 FUNCTION checkfordeath () as integer
@@ -723,21 +713,16 @@ IF killmask AND 16 THEN safekill filebase$ + "_p.tmp"
 END SUB
 
 SUB deletetemps
-'deletes game-state temporary files when exiting back to the titlescreen
+'deletes game-state temporary files from tmpdir when exiting back to the titlescreen
 
- findfiles tmpdir, ALLFILES, fileTypeFile, YES, tmpdir + "filelist.tmp"
- fh = FREEFILE
- OPEN tmpdir + "filelist.tmp" FOR INPUT AS #fh
- DO UNTIL EOF(fh)
-  LINE INPUT #fh, filename$
-  filename$ = LCASE$(filename$)
+ DIM filelist() as string
+ findfiles tmpdir, ALLFILES, fileTypeFile, YES, filelist()
+ FOR i as integer = 0 TO UBOUND(filelist)
+  filename$ = LCASE$(filelist(i))
   IF RIGHT$(filename$,4) = ".tmp" AND (LEFT$(filename$,3) = "map" OR LEFT$(filename$,5) = "state") THEN
-   KILL tmpdir + filename$
+   KILL tmpdir + filelist(i)
   END IF
- LOOP
- CLOSE #fh
-
- KILL tmpdir + "filelist.tmp"
+ NEXT
 END SUB
 
 '--A similar function exists in customsubs.bas for custom. it differs only in error-reporting

commit 9c01db59370879cdd27e537e80896acbeb66ce47

Fix bug preventing lumps from being lumped if there are any lumps ending in .tmp (main part of Bug 977 fix)
The ones excluded are the final ones in the listing returned by findfiles. On
Windows, this seems to be sorted in alphabetical order, so UICOLORS.BIN would
usually be the first file to get the chop.

Bug was introduced in r3897 "findfiles now returns its results in a array instead of a file..."
(2010-10-17, included since Zenzizenzic)

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@5529 7d344553-34f0-0310-a9b1-970ce8f1c3a2

lumpfile.bas
@@ -1067,7 +1067,7 @@ sub fixlumporder (filelist() as string)
 	writepos = 0
 	for readpos = 0 to ubound(filelist)
 		if lcase(right(filelist(readpos), 4)) <> ".tmp" then
-			swap readpos, writepos
+			swap filelist(readpos), filelist(writepos)
 			writepos += 1
 		end if
 	next