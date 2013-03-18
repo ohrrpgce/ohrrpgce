cb6d7e61cf049494af6a42233b5faed6501b1076
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2013-03-18 20:44:31 +0000

    Make the gdbcustom.sh and gdbgame.sh scripts work even if you start then from another directory
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@5725 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- gdbcustom.sh
+++ gdbcustom.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+DIR=`echo "${0}" | sed -e s/"gdbcustom.sh$"/""/`
+cd "${DIR}"
 gdb -x=misc/gdbcmds1.txt -x=misc/gdbcmds2.txt --args ./ohrrpgce-custom -log . "${@}"
--- gdbgame.sh
+++ gdbgame.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+DIR=`echo "${0}" | sed -e s/"gdbgame.sh$"/""/`
+cd $DIR
 gdb -x=misc/gdbcmds1.txt -x=misc/gdbcmds2.txt --args ./ohrrpgce-game -log . "${@}"
