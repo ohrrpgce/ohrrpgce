b93e073699e7245e4cd13b73f95769e880f5892e
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2015-12-22 17:15:50 +0000

    Whoops, I checked in val{game|custom}.sh as symlinks instead of the real files
    Funnily enough, I had deleted the files they pointed to, thinking that I had
    checked them in.
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@7612 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- valcustom.sh
+++ valcustom.sh
@@ -1,1 +1,8 @@
-../src/valcustom.sh
+#!/bin/sh
+# Run Custom under valgrind's memcheck.
+#
+# Add --vgdb-error=0 to pause before starting the program to attach gdb.
+# Add --show-reachable=yes for more complete memory leak checking.
+# With older valgrind, --db-attach=yes instead of --vgdb-error=1 can be convenient.
+
+valgrind --suppressions=misc/valgrind_suppressions.txt --vgdb-error=1 --track-fds=yes --read-var-info=yes --gen-suppressions=yes --leak-check=full ./ohrrpgce-custom $*
