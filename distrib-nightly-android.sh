d615cb66fe8c9cb188e5ff3fbcc5aae87ca8f041
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2016-04-04 18:24:51 +0000

    Check in a copy of James's android build script
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@7902 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ distrib-nightly-android.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Paths in this are specific to James's computer, but it works, and is something to start with
+
+cd ~/src/ohr/wip
+scons fbc=$FBCARM debug=0 android-source=1 game
+scons
+cd ~/src/misc/sdl-android/project/jni/application
+git checkout ohrrpgce
+rm src
+ln -s ohrrpgce src
+cd ~/src/misc/sdl-android
+rm project/bin/MainActivity-debug.apk
+./build.sh
+if [ -f project/bin/MainActivity-debug.apk ] ; then
+  scp -pr project/bin/MainActivity-debug.apk james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/nightly/ohrrpgce-game-android-debug.apk
+fi
