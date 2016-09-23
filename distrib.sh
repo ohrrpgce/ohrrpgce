e8d80a9854b1296d0bd5cc6db563154aa181d487
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2016-09-23 02:49:24 +0000

    Oops, put x86 binaries in the .deb package, not x86_64
    Also skip creating .deb packages if not on a Debian (dpkg missing)
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8155 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.sh
+++ distrib.sh
@@ -43,79 +43,82 @@ rm -f distrib/ohrrpgce-*.tar.bz2
 rm -f distrib/*.deb
 
 package_for_arch() {
-	ARCH=$1
+  ARCH=$1
 
-	echo "Building $ARCH binaries"
-	scons $NJOBS debug=0 arch=$ARCH game custom hspeak unlump relump || exit 1
+  echo "Building $ARCH binaries"
+  scons $NJOBS debug=0 arch=$ARCH game custom hspeak unlump relump || exit 1
 
-	echo "Packaging $ARCH binary distribution of CUSTOM"
+  echo "Packaging $ARCH binary distribution of CUSTOM"
 
-	echo "  Including binaries"
-	cp -p ohrrpgce-game tmp &&
-	cp -p ohrrpgce-custom tmp &&
-	cp -p unlump tmp &&
-	cp -p relump tmp || exit 1
+  echo "  Including binaries"
+  cp -p ohrrpgce-game tmp &&
+  cp -p ohrrpgce-custom tmp &&
+  cp -p unlump tmp &&
+  cp -p relump tmp || exit 1
 
-	echo "  Including hspeak"
-	cp -p hspeak tmp || exit 1
+  echo "  Including hspeak"
+  cp -p hspeak tmp || exit 1
 
-	echo "  Including support files"
-	cp -p ohrrpgce.new tmp &&
-	cp -p plotscr.hsd tmp &&
-	cp -p scancode.hsi tmp || exit 1
+  echo "  Including support files"
+  cp -p ohrrpgce.new tmp &&
+  cp -p plotscr.hsd tmp &&
+  cp -p scancode.hsi tmp || exit 1
 
-	echo "  Including readmes"
-	cp -p README-game.txt tmp &&
-	cp -p README-custom.txt tmp &&
-	cp -p IMPORTANT-nightly.txt tmp &&
-	cp -p LICENSE.txt tmp &&
-	cp -p LICENSE-binary.txt tmp &&
-	cp -p whatsnew.txt tmp || exit 1
+  echo "  Including readmes"
+  cp -p README-game.txt tmp &&
+  cp -p README-custom.txt tmp &&
+  cp -p IMPORTANT-nightly.txt tmp &&
+  cp -p LICENSE.txt tmp &&
+  cp -p LICENSE-binary.txt tmp &&
+  cp -p whatsnew.txt tmp || exit 1
 
-	echo "  Including Vikings of Midgard"
-	cp -p vikings.rpg tmp &&
-	cp -pr "vikings/Vikings script files" tmp &&
-	cp -p "vikings/README-vikings.txt" tmp || exit 1
+  echo "  Including Vikings of Midgard"
+  cp -p vikings.rpg tmp &&
+  cp -pr "vikings/Vikings script files" tmp &&
+  cp -p "vikings/README-vikings.txt" tmp || exit 1
 
-	echo "  Including import"
-	mkdir -p tmp/import
-	cp -pr import/* tmp/import || exit 1
+  echo "  Including import"
+  mkdir -p tmp/import
+  cp -pr import/* tmp/import || exit 1
 
-	echo "  Including docs"
-	mkdir -p tmp/docs
-	cp -p docs/*.html tmp/docs &&
-	cp -p docs/plotdict.xml tmp/docs &&
-	cp -p docs/htmlplot.xsl tmp/docs &&
-	cp -p docs/more-docs.txt tmp/docs || exit 1
+  echo "  Including docs"
+  mkdir -p tmp/docs
+  cp -p docs/*.html tmp/docs &&
+  cp -p docs/plotdict.xml tmp/docs &&
+  cp -p docs/htmlplot.xsl tmp/docs &&
+  cp -p docs/more-docs.txt tmp/docs || exit 1
 
-	echo "  Including help files"
-	cp -pr ohrhelp tmp || exit 1
+  echo "  Including help files"
+  cp -pr ohrhelp tmp || exit 1
 
-	echo "tarring and bzip2ing $ARCH distribution"
-	TODAY=`date "+%Y-%m-%d"`
-	CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
-	mv tmp ohrrpgce
-	tar -jcf distrib/ohrrpgce-linux-$TODAY-$CODE-$ARCH.tar.bz2 ./ohrrpgce --exclude .svn
-	mv ohrrpgce tmp
+  echo "tarring and bzip2ing $ARCH distribution"
+  TODAY=`date "+%Y-%m-%d"`
+  CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
+  mv tmp ohrrpgce
+  tar -jcf distrib/ohrrpgce-linux-$TODAY-$CODE-$ARCH.tar.bz2 ./ohrrpgce --exclude .svn
+  mv ohrrpgce tmp
 
-	echo "Erasing contents of temporary directory"
-	rm -Rf tmp/*
+  echo "Erasing contents of temporary directory"
+  rm -Rf tmp/*
 
-	echo "Prepare minimal $ARCH player zip"
-	cp ohrrpgce-game tmp/
-	strip tmp/ohrrpgce-game
-	zip -j distrib/ohrrpgce-player-linux-bin-minimal-$TODAY-$CODE-$ARCH.zip tmp/ohrrpgce-game LICENSE-binary.txt README-linux-bin-minimal.txt
-	rm tmp/ohrrpgce-game
+  echo "Prepare minimal $ARCH player zip"
+  cp ohrrpgce-game tmp/
+  strip tmp/ohrrpgce-game
+  zip -j distrib/ohrrpgce-player-linux-bin-minimal-$TODAY-$CODE-$ARCH.zip tmp/ohrrpgce-game LICENSE-binary.txt README-linux-bin-minimal.txt
+  rm tmp/ohrrpgce-game
 }
 
 package_for_arch x86
-package_for_arch x86_64
 
-echo "Building x86 Debian/Ubuntu packages"
-cd linux
-if [ -f *.deb ] ; then
-  rm *.deb
+if which dpkg > /dev/null; then
+  echo "Building x86 Debian/Ubuntu packages"
+  cd linux
+  if [ -f *.deb ] ; then
+    rm *.deb
+  fi
+  ./all.sh || exit 1
+  cd ..
+  mv linux/*.deb distrib
 fi
-./all.sh || exit 1
-cd ..
-mv linux/*.deb distrib
+
+package_for_arch x86_64
