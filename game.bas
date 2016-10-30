ohrrpgce/ohrrpgce

commit 16bded794cc70ce687801a9963f8cd36286fc9d6

Fix harm tile bug via a gen pref bitset: when the caterpillar party is disabled, only the leader is harmed. The "harm tiles harm non-caterpillar heroes" bitset defaults to on in new and ancient games.
This bug was introduced in espereble, July 2003. Unfortunately, there's no accurate way to determine whether an RPG was made before espereble. The best I could manage is testing the RPG format version to enable the bugfix for games made before March 2001!

Hey guess what, we still don't store the exact version that an RPG file was made with >:(

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@4797 7d344553-34f0-0310-a9b1-970ce8f1c3a2

game.bas
@@ -1002,12 +1002,19 @@ SUB update_heroes(byval force_npc_check as integer=NO)
    DIM p as integer = readblock(pass, catx(whoi * 5) \ 20, caty(whoi * 5) \ 20)
    IF p AND passHarm THEN
 
-    IF whoi = 0 AND readbit(gen(), genSuspendBits, suspendcaterpillar) = 0 THEN
-     '--if caterpillar is not suspended, only the leader's motion matters (harm whole party)
-     FOR party_slot as integer = 0 TO 3
+    IF whoi = 0 AND readbit(gen(), genBits, 1) = 0 THEN
+     'The caterpillar is disabled, so harm the whole party when the leader steps on a harm tile,
+     'for some customisable defintion of 'whole'
+     DIM howmany as integer
+     IF readbit(gen(), genBits2, 12) THEN  'Harm tiles harm non-caterpillar heroes
+      howmany = herocount  'whole active party
+     ELSE
+      'Old buggy behaviour: just the leader
+      howmany = 1
+     END IF
+     FOR party_slot as integer = 0 TO howmany - 1
       IF hero(party_slot) > 0 THEN
        gam.hero(party_slot).stat.cur.hp = large(gam.hero(party_slot).stat.cur.hp - gmap(9), 0)
-       IF party_slot_to_rank(party_slot) >= caterpillar_size THEN EXIT FOR  'emulate bug in old versions
       END IF
      NEXT
     ELSE

common.bas
@@ -2823,6 +2823,10 @@ IF gen(genVersion) = 4 THEN
   NEXT i
   storeset game + ".pal", 1 + j, 0
  NEXT j
+ 'esperable (2003) introduced the bug where harm tiles no longer damage the
+ 'whole active party if the caterpillar is disabled. Fixed for alectormancy.
+ 'The best we can do is not emulating the bug for versions before March 2001 :(
+ setbit gen(), genBits2, 12, 1  'Harm tiles harm non-caterpillar heroes
 END IF
 '--VERSION 6--
 IF gen(genVersion) = 5 THEN

customsubs.bas
@@ -2106,7 +2106,9 @@ SUB editbitset (array() AS INTEGER, BYVAL wof AS INTEGER, BYVAL last AS INTEGER,
     IF state.pt = i THEN col = uilook(uiSelectedItem + state.tog)
    END IF
    textcolor col, 0
-   printstr menu(i), 8, (i - state.top) * 8, dpage
+   DIM drawstr as string = " " & menu(i)
+   IF state.pt = i THEN drawstr = RIGHT(drawstr, 40)
+   printstr drawstr, 0, (i - state.top) * 8, dpage
   NEXT i
   SWAP vpage, dpage
   setvispage vpage

moresubs.bas
@@ -2138,7 +2138,7 @@ FUNCTION herocount (last as integer = 3) as integer
 END FUNCTION
 
 FUNCTION caterpillar_size () as integer
- 'Returns the number of heroes on the map, regardless of whether caterpillar trailing is enabled
+ 'Returns the number of heroes on the map, regardless of whether caterpillar trailing is suspended
  IF readbit(gen(), genBits, 1) = 1 THEN RETURN herocount
  RETURN 1
 END FUNCTION

subs4.bas
@@ -1562,7 +1562,7 @@ SUB gendata ()
    IF state.pt = 0 THEN EXIT DO
    IF state.pt = 3 THEN
     DIM bittemp(2) AS INTEGER
-    DIM bitname(27) AS STRING
+    DIM bitname(28) AS STRING
     bitname(0) = "Pause on Battle Sub-menus"
     bitname(1) = "Enable Caterpillar Party"
     bitname(2) = "Don't Restore HP on Levelup"
@@ -1591,6 +1591,7 @@ SUB gendata ()
     bitname(25) = "Simulate old fail vs element resist bit"
     bitname(26) = "0 damage when immune to attack elements"
     bitname(27) = "Recreate map slices when changing maps"
+    bitname(28) = "Harm tiles harm non-caterpillar heroes"
     bittemp(0) = gen(genBits)
     bittemp(1) = gen(genBits2)
     bittemp(2) = gen(genBits2+1)

commit 412d64523e905e7e747824f8f61cbf05d0ea28bf

Fix harm tile bug when caterpillar is disabled and there are gaps in the party
Was introduced in r4797 (Sep 2011, Alectormancy)

Reported at http://www.slimesalad.com/forum/viewtopic.php?p=125269

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8185 7d344553-34f0-0310-a9b1-970ce8f1c3a2

game.bas
@@ -1201,7 +1201,9 @@ SUB update_heroes(byval force_step_check as integer=NO)
 
   DIM steppingslot as integer = whoi
   '--If caterpillar is not suspended, only the leader's motion determines a step
-  '--(a limitation of the caterpillar party)
+  '--(a limitation of the caterpillar party.)
+  '--BUG: if the caterpillar isn't properly interpolated, will get weird results like
+  '--jumping over tiles without damage.
   IF readbit(gen(), genSuspendBits, suspendcaterpillar) = 0 THEN steppingslot = 0
 
   IF didgo(steppingslot) = YES AND (herow(steppingslot).xgo MOD 20) = 0 AND (herow(steppingslot).ygo MOD 20) = 0 THEN
@@ -1214,17 +1216,18 @@ SUB update_heroes(byval force_step_check as integer=NO)
    DIM p as integer = readblock(pass, catx(whoi * 5) \ 20, caty(whoi * 5) \ 20)
    IF p AND passHarm THEN
 
+    DIM harm_whole_party as bool = NO
+
     IF whoi = 0 AND readbit(gen(), genBits, 1) = 0 THEN
-     'The caterpillar is disabled, so harm the whole party when the leader steps on a harm tile,
-     'for some customisable defintion of 'whole'
-     DIM howmany as integer
+     'The caterpillar is disabled, so maybe harm the whole party when the leader steps on a harm tile
+     'if backcompat sit, otherwise old buggy behaviour: just the leader
      IF readbit(gen(), genBits2, 12) THEN  'Harm tiles harm non-caterpillar heroes
-      howmany = herocount  'whole active party
-     ELSE
-      'Old buggy behaviour: just the leader
-      howmany = 1
+      harm_whole_party = YES
      END IF
-     FOR party_slot as integer = 0 TO howmany - 1
+    END IF
+
+    IF harm_whole_party THEN
+     FOR party_slot as integer = 0 TO sizeActiveParty - 1
       IF gam.hero(party_slot).id >= 0 THEN
        gam.hero(party_slot).stat.cur.hp = large(gam.hero(party_slot).stat.cur.hp - gmap(9), 0)
       END IF