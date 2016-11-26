cbb8b2e03bbf1e9204502a3582546e2a0d9a6c4a
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2016-11-26 08:46:19 +0000

    web: add git-svn.sh script to mirror svn repo to git
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@8219 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ cron/git-svn.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# TMC runs this script from cron to keep the git-svn mirror up to date.
+
+LOCALREPO=~/src/ohrrpgce-svn
+REMOTEREPO=git@bitbucket.org:rbv/ohrrpgce-svn.git
+SVNREPO=https://rpg.hamsterrepublic.com/source
+
+if [ ! -d "$LOCALREPO" ]; then
+    echo "Cloning from $REMOTEREPO"
+
+    git clone "$REMOTEREPO" "$LOCALREPO" --origin svn
+    cd "$LOCALREPO"
+    git config svn-remote.svn.url "$SVNREPO"
+    git config svn-remote.svn.fetch "wip:refs/remotes/svn/wip"
+    git config svn-remote.svn.fetch "tools:refs/remotes/svn/tools" --add
+    git config svn-remote.svn.fetch "web:refs/remotes/svn/web" --add
+    git config svn-remote.svn.branches "rel/*:refs/remotes/svn/*"
+fi
+
+cd "$LOCALREPO"
+
+git svn fetch &&
+git push svn refs/remotes/svn/*:refs/heads/*
