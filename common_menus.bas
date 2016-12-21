ohrrpgce/ohrrpgce

commit 0e8b85cca48c278b1f224545536b86c1988c5f5d

Remove many manual assignments to state.spacing, which gets overwritten by standardmenu
Also set .autosize=YES in a few menus, removed some manual .size recalculations,
and switched a few menus to .[fullscreen_]scrollbar=YES instead of drawing it
manually.

There are just two menus which don't use standardmenu and so still set
state.spacing themselves. (There are probably many more which reimplement
standardmenu and don't touch .spacing at all)

standardmenu now also calls recalc_menu_size, since usemenu can't do it on the
first tick (leads to graphical glitches), and usemenu may not always be called.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8241 7d344553-34f0-0310-a9b1-970ce8f1c3a2

common_menus.bas
@@ -41,7 +41,7 @@ FUNCTION editbitset (array() as integer, byval wof as integer, byval last as int
  NEXT
  state.last = nextbit - 1
  state.autosize = YES
- state.spacing = 8
+ state.spacing = 8  'Necessary because not using standardmenu
 
  DIM ret as integer = NO
  DIM col as integer
@@ -84,11 +84,10 @@ FUNCTION editbitset (array() as integer, byval wof as integer, byval last as int
    textcolor col, 0
    DIM drawstr as string = " " & menu(i)
    IF state.pt = i THEN drawstr = RIGHT(drawstr, 40)
-   printstr drawstr, 0, (i - state.top) * 8, dpage
+   printstr drawstr, 0, (i - state.top) * state.spacing, dpage
   NEXT i
   WITH state
    .has_been_drawn = YES
-   .spacing = 8
    .rect.x = 0
    .rect.y = 0
    .rect.wide = get_resolution_w()

customsubs.rbas
@@ -108,7 +108,6 @@ FUNCTION tags_menu (byval starttag as integer=0, byval picktag as integer=NO, by
 
  state.autosize = YES
  state.autosize_ignore_lines = 1
- state.spacing = 8
  state.last = gen(genMaxTagname)
 
  state.pt = 0
@@ -2418,7 +2417,6 @@ FUNCTION scriptbrowse_string (byref trigger as integer, byval triggertype as int
   .last = scriptmax
   .autosize = YES
   .autosize_ignore_lines = 2
-  .spacing = 8
  END WITH
  IF firstscript = 2 THEN
   state.pt = 1
@@ -3216,7 +3214,6 @@ SUB edit_global_text_strings()
   .pt = -1
   .first = -1
   .last = menu.curitem - 1
-  .spacing = 8
   .autosize = YES
   .autosize_ignore_lines = 3
  END WITH
@@ -3253,6 +3250,7 @@ SUB edit_global_text_strings()
  
   clearpage dpage
   menuopts.highlight = YES
+  menuopts.scrollbar = YES
   standardmenu menu.text(), state, 232, 0, dpage, menuopts
   'Since both halves of the menu share the same state and both are active,
   'they both toggle state.tog. So work around that
@@ -3260,7 +3258,6 @@ SUB edit_global_text_strings()
   standardmenu menu.description(), state, menu.shaded(), 0, 0, dpage
   rect.wide = vpages(dpage)->w
   rect.high = vpages(dpage)->h - 8
-  draw_scrollbar state, rect, , dpage
   edgeprint "CTRL+S Search", 0, vpages(dpage)->h - 10, uilook(uiDisabledItem), dpage
   IF state.pt >= 0 ANDALSO LEN(menu.help(state.pt)) THEN
    edgeprint "Press F1 for help about this string", 0, vpages(dpage)->h - 20, uilook(uiDisabledItem), dpage
@@ -4417,7 +4414,6 @@ SUB edit_platform_options ()
  DIM st as MenuState
  st.autosize = YES
  st.autosize_ignore_lines = 1
- st.spacing = 10
  st.active = YES
  st.need_update = YES
  
@@ -4492,12 +4488,6 @@ SUB edit_platform_options ()
   IF keyval(scF1) > 1 THEN show_help "general_platform_options"
   IF keyval(scEsc) > 1 THEN EXIT DO
 
-  WITH menu
-   .offset.x = vpages(dpage)->w / 2 * -1
-   .offset.y = vpages(dpage)->h / 2 * -1
-   .min_chars = vpages(dpage)->w / 8
-  END WITH
-  
   IF st.need_update THEN
    DO
     '--normally a single-pass loop. Only repeats if something
@@ -4513,7 +4503,9 @@ SUB edit_platform_options ()
      .anchor.x = -1
      .anchor.y = -1
      .bordersize = -8
-     .min_chars = 40
+     .min_chars = 999  ' Fill the screen
+     .offset.x = vpages(dpage)->w / 2 * -1
+     .offset.y = vpages(dpage)->h / 2 * -1
     END WITH
     append_menu_item menu, "Previous menu..."
     menu.last->t = platformoptEXIT

menus.bas
@@ -403,9 +403,11 @@ SUB standardmenu (menu() as string, byref state as MenuState, shaded() as bool, 
  v_free basicmenu
 END SUB
 
+'Draws an undecorated menu (just lines of coloured text, and optionally a scrollbar), as used throughout Custom.
+'(The in-game user-customisable menus are drawn with draw_menu.)
 'menu may in fact be a vector of any type inheriting from BasicMenuItem:
 ' standardmenu cast(BasicMenuItem vector, menu), ...
-'menu's typetable tells the size in bytes of each menu item
+'The vector's internal typetable tells the size in bytes of each menu item
 SUB standardmenu (byval menu as BasicMenuItem vector, state as MenuState, byval x as integer, byval y as integer, byval page as integer, menuopts as MenuOptions)
 
  IF state.first <> 0 THEN
@@ -423,6 +425,11 @@ SUB standardmenu (byval menu as BasicMenuItem vector, state as MenuState, byval 
   .rect.wide = get_resolution_w()
   .rect.high = small(get_resolution_h(), (.size + 1) * .spacing)
  END WITH
+
+ ' usemenu also calls recalc_menu_size, but usemenu might not be called if the
+ ' menu is inactive (more than one on-screen), and on the first tick before
+ ' .spacing is set the correct .size can't be set either (iff .autosize=YES)
+ recalc_menu_size state
 
  IF state.active THEN
   state.tog XOR= 1

sliceedit.bas
@@ -336,14 +336,16 @@ SUB slice_editor_main (byref ses as SliceEditState, byref edslice as Slice Ptr, 
 
  DIM state as MenuState
  WITH state
-  .spacing = 8
   .need_update = YES
+  .autosize = YES
+  .autosize_ignore_pixels = 14
  END WITH
  DIM menuopts as MenuOptions
  WITH menuopts
   .edged = YES
   .itemspacing = -1
   .highlight = YES
+  .fullscreen_scrollbar = YES
  END WITH
 
  DIM cursor_seek as Slice Ptr = 0
@@ -570,7 +572,6 @@ SUB slice_editor_main (byref ses as SliceEditState, byref edslice as Slice Ptr, 
   IF UpdateScreenSlice() THEN state.need_update = YES
 
   IF state.need_update THEN
-   state.size = vpages(dpage)->h / state.spacing - 4
    slice_editor_refresh(ses, state, menu(), edslice, cursor_seek, slicelookup())
    state.need_update = NO
   ELSE
@@ -589,10 +590,6 @@ SUB slice_editor_main (byref ses as SliceEditState, byref edslice as Slice Ptr, 
    DrawSliceAnts topmost, dpage
   END IF
   IF ses.hide_mode <> hideMenu THEN
-   IF state.last > state.size THEN
-    draw_fullscreen_scrollbar state, , dpage
-   END IF
-
    'Determine the colour for each menu item
    REDIM plainmenu(state.last) as string
    FOR i as integer = 0 TO UBOUND(plainmenu)
@@ -790,9 +787,8 @@ SUB slice_edit_detail (byref ses as SliceEditState, sl as Slice Ptr, slicelookup
  DIM state as MenuState
  WITH state
   .pt = remember_pt
-  .spacing = 8
-  .size = 22
   .need_update = YES
+  .autosize = YES
  END WITH
  DIM menuopts as MenuOptions
  WITH menuopts
@@ -816,7 +812,6 @@ SUB slice_edit_detail (byref ses as SliceEditState, sl as Slice Ptr, slicelookup
 
    slice_edit_detail_refresh state, menu(), sl, rules(), slicelookup()
    state.need_update = NO
-   state.size = vpages(dpage)->h \ state.spacing - 1
   END IF
 
   usemenu state

browse.bas
@@ -265,7 +265,6 @@ DO
   .rect.y = 20
   .rect.wide = get_resolution_w()
   .rect.high = get_resolution_h()
-  .spacing = 9
  END WITH
  FOR i as integer = br.mstate.top TO small(br.mstate.top + br.mstate.size, br.mstate.last)
   textcolor catfg(tree(i).kind), catbg(tree(i).kind)

flexmenu.bas
@@ -1032,7 +1032,6 @@ DIM workmenu(65) as integer
 DIM dispmenu(65) as string
 DIM state as MenuState
 state.autosize = YES
-state.spacing = 8
 
 DIM mainMenu(13) as integer
 mainMenu(0) = AtkBackAct
@@ -1518,7 +1517,6 @@ FUNCTION atk_edit_add_new (recbuf() as integer, byref recindex as integer, previ
   DIM state as MenuState
   state.last = UBOUND(menu)
   state.autosize = YES
-  state.spacing = 8
   state.pt = 1
 
   state.need_update = YES

mapsubs.bas
@@ -224,7 +224,6 @@ SUB map_picker ()
  DIM topmenu() as string
  DIM topmenu_display() as string
  DIM state as MenuState
- state.spacing = 8
  state.autosize = YES
  DIM selectst as SelectTypeState
  

subs.rbas
@@ -524,7 +524,6 @@ DIM workmenu(93) as integer
 DIM dispmenu(93) as string
 DIM state as MenuState
 state.autosize = YES
-state.spacing = 8
 state.need_update = YES
 
 DIM mainMenu(9) as integer
@@ -1534,7 +1533,6 @@ SUB item_editor
  DIM selectst as SelectTypeState
  DIM state as MenuState
  state.autosize = YES
- state.spacing = 8
  state.first = -1
  state.top = -1
  state.last = gen(genMaxItem) + 1
@@ -3306,7 +3304,6 @@ SUB common_elementals_editor(elementals() as single, helpfile as string, byval s
  DIM st as MenuState
  st.last = UBOUND(menu)
  st.autosize = YES
- st.spacing = 8
  st.need_update = YES
 
  FOR i as integer = 0 TO gen(genNumElements) - 1

commit 17b9d64ee282f5c7ffa6fc985f5fc4c99d114ff9

Calc autosize menu size in init_menu_state, to fix a couple menu glitches caused by r8241
The problem was that r8241 removed manual .spacing assignments, without which
usemenu can't calculate .size when .autosize=YES, resulting in .size=0 for one
tick, which causes .top to get set to .pt, which is bad if .pt doesn't start
as .first (e.g. slice detail editor remembered .pt).

This mess is caused by usemenu not having access to the data (MenuDef/
MenuOptions + standardmenu arguments) necessary to know .size itself.
And now we have a bunch of standardmenu-specific stuff in MenuState that ought
to be in MenuOptions instead, which usemenu really should be given.
As a simpler fix, I made MenuOptions an argument to init_menu_state instead,
to do the first-tick calculation. Only works when init_menu_state is actuallly
used.

And our menu code just keeps getting more difficult to understand; this
commit not excepted. I avoid looking at menus.bas if possible!

This also cleaned up editbitset slightly.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8248 7d344553-34f0-0310-a9b1-970ce8f1c3a2

common_menus.bas
@@ -41,7 +41,6 @@ FUNCTION editbitset (array() as integer, byval wof as integer, byval last as int
  NEXT
  state.last = nextbit - 1
  state.autosize = YES
- state.spacing = 8  'Necessary because not using standardmenu
 
  DIM ret as integer = NO
  DIM col as integer
@@ -71,7 +70,10 @@ FUNCTION editbitset (array() as integer, byval wof as integer, byval last as int
   ELSE
    IF enter_space_click(state) THEN EXIT DO
   END IF
+
+  ' Draw
   clearpage dpage
+  set_menustate_size state, MenuOptions(), 0, 0  ' Recalcs .size, .rect, .spacing
   draw_fullscreen_scrollbar state, , dpage
   FOR i as integer = state.top TO small(state.top + state.size, state.last)
    IF i >= 0 THEN
@@ -86,13 +88,6 @@ FUNCTION editbitset (array() as integer, byval wof as integer, byval last as int
    IF state.pt = i THEN drawstr = RIGHT(drawstr, 40)
    printstr drawstr, 0, (i - state.top) * state.spacing, dpage
   NEXT i
-  WITH state
-   .has_been_drawn = YES
-   .rect.x = 0
-   .rect.y = 0
-   .rect.wide = get_resolution_w()
-   .rect.high = small(get_resolution_h(), (.size + 1) * .spacing)
-  END WITH
   SWAP vpage, dpage
   setvispage vpage
   dowait

customsubs.rbas
@@ -2414,10 +2414,10 @@ FUNCTION scriptbrowse_string (byref trigger as integer, byval triggertype as int
  DIM state as MenuState
  WITH state
   .pt = 0
-  .last = scriptmax
   .autosize = YES
   .autosize_ignore_lines = 2
  END WITH
+ init_menu_state state, scriptnames()
  IF firstscript = 2 THEN
   state.pt = 1
  ELSE

menus.bas
@@ -35,7 +35,9 @@ DEFINE_VECTOR_OF_CLASS(MenuDefItem, MenuDefItem)
 
 '(Re-)initialise menu state, preserving .pt if valid
 '.pt is moved to a selectable menu item.
-SUB init_menu_state (byref state as MenuState, menu() as SimpleMenuItem)
+SUB init_menu_state (byref state as MenuState, menu() as SimpleMenuItem, menuopts as MenuOptions = MenuOptions())
+ set_menustate_size state, menuopts, 0, 0  'Position not known, fill with dummy data for now
+
  WITH state
   .first = 0
   .last = UBOUND(menu)
@@ -59,7 +61,9 @@ END SUB
 '
 'menu may in fact be a vector of any type inheriting from BasicMenuItem.
 'menu's typetable tells the size in bytes of each menu item
-SUB init_menu_state (byref state as MenuState, byval menu as BasicMenuItem vector)
+SUB init_menu_state (byref state as MenuState, byval menu as BasicMenuItem vector, menuopts as MenuOptions = MenuOptions())
+ set_menustate_size state, menuopts, 0, 0  'Position not known, fill with dummy data for now
+
  WITH state
   .first = 0
   .last = v_len(menu) - 1
@@ -376,6 +380,29 @@ SUB standard_to_basic_menu (menu() as string, byref state as MenuState, byref ba
  NEXT
 END SUB
 
+' Initialises/updates the size and position data in MenuState, including .size
+' if .autosize is true. This shouldn't be called unless you're reimplementing
+' standardmenu, otherwise call init_menu_state instead.
+' Sets .has_been_drawn=YES to indicate that these members have been initialised,
+' even if not called from standardmenu.
+SUB set_menustate_size(state as MenuState, menuopts as MenuOptions, x as integer, y as integer)
+ WITH state
+  .has_been_drawn = YES
+  IF menuopts.edged THEN .spacing = 9 ELSE .spacing = 8
+  .spacing += menuopts.itemspacing
+  .rect.x = x
+  .rect.y = y
+  .rect.wide = get_resolution_w()
+  .rect.high = small(get_resolution_h(), (.size + 1) * .spacing)
+ END WITH
+
+ ' usemenu also calls recalc_menu_size, but usemenu might not be called if the
+ ' menu is inactive (more than one on-screen), and on the first tick with
+ ' .autosize=YES before .spacing is set the correct .size won't be known either.
+ ' (In that case, you should call init_menu_state, passing menuopts if used.)
+ recalc_menu_size state
+END SUB
+
 SUB standardmenu (menu() as string, byref state as MenuState, byval x as integer, byval y as integer, byval page as integer, menuopts as MenuOptions)
  DIM basicmenu as BasicMenuItem vector
  standard_to_basic_menu menu(), state, basicmenu
@@ -425,20 +452,7 @@ SUB standardmenu (byval menu as BasicMenuItem vector, state as MenuState, byval 
   EXIT SUB
  END IF
 
- WITH state
-  .has_been_drawn = YES
-  IF menuopts.edged THEN .spacing = 9 ELSE .spacing = 8
-  .spacing += menuopts.itemspacing
-  .rect.x = x
-  .rect.y = y
-  .rect.wide = get_resolution_w()
-  .rect.high = small(get_resolution_h(), (.size + 1) * .spacing)
- END WITH
-
- ' usemenu also calls recalc_menu_size, but usemenu might not be called if the
- ' menu is inactive (more than one on-screen), and on the first tick before
- ' .spacing is set the correct .size can't be set either (iff .autosize=YES)
- recalc_menu_size state
+ set_menustate_size state, menuopts, x, y
 
  IF state.active THEN
   state.tog XOR= 1
@@ -827,7 +841,9 @@ FUNCTION getmenuname(byval record as integer) as string
 END FUNCTION
 
 '(Re-)initialise menu state, preserving .pt if valid
-SUB init_menu_state (byref state as MenuState, menu() as string)
+SUB init_menu_state (byref state as MenuState, menu() as string, menuopts as MenuOptions)
+ set_menustate_size state, menuopts, 0, 0  'Position not known, fill with dummy data for now
+
  WITH state
   .first = LBOUND(menu)
   .last = UBOUND(menu)

menus.bi
@@ -11,9 +11,9 @@
 '*** Requires construction (with ClearMenuData or LoadMenuData) ***
 
 '' Generic MenuState Stuff
-DECLARE SUB init_menu_state OVERLOAD (byref state as MenuState, menu() as SimpleMenuItem)
-DECLARE SUB init_menu_state OVERLOAD (byref state as MenuState, byval menu as BasicMenuItem vector)
-DECLARE SUB init_menu_state OVERLOAD (byref state as MenuState, menu() as string)
+DECLARE SUB init_menu_state OVERLOAD (byref state as MenuState, menu() as SimpleMenuItem, menuopts as MenuOptions = MenuOptions())
+DECLARE SUB init_menu_state OVERLOAD (byref state as MenuState, byval menu as BasicMenuItem vector, menuopts as MenuOptions = MenuOptions())
+DECLARE SUB init_menu_state OVERLOAD (byref state as MenuState, menu() as string, menuopts as MenuOptions = MenuOptions())
 DECLARE SUB clamp_menu_state (byref state as MenuState)
 DECLARE SUB append_simplemenu_item (byref menu as SimpleMenuItem vector, caption as string, byval unselectable as bool = NO, byval col as integer = 0, byval dat as integer = 0, byval where as integer = -1)
 DECLARE FUNCTION usemenu OVERLOAD (state as MenuState, byval deckey as integer = scUp, byval inckey as integer = scDown) as bool
@@ -26,6 +26,7 @@ DECLARE SUB standardmenu OVERLOAD (menu() as string, state as MenuState, byval x
 DECLARE SUB standardmenu OVERLOAD (menu() as string, state as MenuState, shaded() as bool, byval x as integer, byval y as integer, byval page as integer, menuopts as MenuOptions = MenuOptions())
 DECLARE SUB standardmenu OVERLOAD (byval menu as BasicMenuItem vector, state as MenuState, byval x as integer, byval y as integer, byval page as integer, menuopts as MenuOptions = MenuOptions())
 DECLARE SUB recalc_menu_size (byref state as MenuState)
+DECLARE SUB set_menustate_size (state as MenuState, menuopts as MenuOptions, x as integer, y as integer)
 
 '' Mouse support
 DECLARE FUNCTION find_menu_item_at_point (state as MenuState, x as integer, y as integer) as integer

sliceedit.bas
@@ -118,7 +118,7 @@ DECLARE SUB slice_editor_refresh_delete (byref index as integer, menu() as Slice
 DECLARE SUB slice_editor_refresh_append (byref index as integer, menu() as SliceEditMenuItem, caption as string, sl as Slice Ptr=0)
 DECLARE SUB slice_editor_refresh_recurse (ses as SliceEditState, byref index as integer, menu() as SliceEditMenuItem, byref indent as integer, edslice as Slice Ptr, sl as Slice Ptr, hidden_slice as Slice Ptr, slicelookup() as string)
 DECLARE SUB slice_edit_detail (byref ses as SliceEditState, sl as Slice Ptr, slicelookup() as string, specialcodes() as SpecialLookupCode)
-DECLARE SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, sl as Slice Ptr, rules() as EditRule, slicelookup() as string)
+DECLARE SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, menuopts as MenuOptions, sl as Slice Ptr, rules() as EditRule, slicelookup() as string)
 DECLARE SUB slice_edit_detail_keys (byref ses as SliceEditState, byref state as MenuState, sl as Slice Ptr, rules() as EditRule, slicelookup() as string, specialcodes() as SpecialLookupCode)
 DECLARE SUB slice_editor_xy (byref x as integer, byref y as integer, byval focussl as Slice Ptr, byval rootsl as Slice Ptr)
 DECLARE FUNCTION slice_editor_filename(byref ses as SliceEditState) as string
@@ -810,7 +810,7 @@ SUB slice_edit_detail (byref ses as SliceEditState, sl as Slice Ptr, slicelookup
    'Invisible slices won't be updated by DrawSlice
    RefreshSliceTreeScreenPos sl
 
-   slice_edit_detail_refresh state, menu(), sl, rules(), slicelookup()
+   slice_edit_detail_refresh state, menu(), menuopts, sl, rules(), slicelookup()
    state.need_update = NO
   END IF
 
@@ -1006,7 +1006,7 @@ SUB sliceed_rule_tog(rules() as EditRule, helpkey as String, byval dataptr as in
  sliceed_rule rules(), helpkey, erToggle, dataptr, -1, 0, group
 END SUB
 
-SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, sl as Slice Ptr, rules() as EditRule, slicelookup() as string)
+SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, menuopts as MenuOptions, sl as Slice Ptr, rules() as EditRule, slicelookup() as string)
  REDIM menu(6) as string
  REDIM rules(0) as EditRule
  rules(0).helpkey = "detail"
@@ -1192,10 +1192,8 @@ SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, sl as
   sliceed_rule rules(), "autosort", erIntgrabber, @.AutoSort, 0, 5
   str_array_append menu(), "Auto-sort children: " & AutoSortCaptions(.AutoSort)
  END WITH
-  
- state.last = UBOUND(menu)
- state.pt = small(state.pt, state.last)
- state.top = small(state.top, state.pt)
+
+ init_menu_state state, menu(), menuopts
 END SUB
 
 FUNCTION slice_edit_detail_browse_slicetype(byref slice_type as SliceTypes) as SliceTypes