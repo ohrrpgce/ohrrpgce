7a8db2cdb0818369b96c979411fa74d9f19ec2fb
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2016-12-23 03:31:07 +0000

    Update valgrind suppressions list
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8260 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- valcustom.sh
+++ valcustom.sh
@@ -2,7 +2,8 @@
 # Run Custom under valgrind's memcheck.
 #
 # Add --vgdb-error=0 to pause before starting the program to attach gdb.
+# Add --vgdb-error=1 to pause and wait for gdb to attach on error (otherwise can't continue)
 # Add --show-reachable=yes for more complete memory leak checking.
 # With older valgrind, --db-attach=yes instead of --vgdb-error=1 can be convenient.
 
-valgrind --suppressions=misc/valgrind_suppressions.txt --vgdb-error=1 --track-fds=yes --read-var-info=yes --gen-suppressions=yes --leak-check=full ./ohrrpgce-custom $*
+valgrind --suppressions=misc/valgrind_suppressions.txt --track-fds=yes --read-var-info=yes --gen-suppressions=yes --leak-check=full ./ohrrpgce-custom $*
