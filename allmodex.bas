ohrrpgce/ohrrpgce

commit 0e32872d695cc20ae8e8dfe25615ab7b4a2cec86

Add palette16_save function, and turn storepal16 into a wrapper.
I put it in loading.rbas rather than allmodex.bas, because that's where
storepal16 is, and because it depends on details like the Game-Custom
communication channel, so it doesn't seem very core. And .pal is going to
be obsolete soon anyway.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8277 7d344553-34f0-0310-a9b1-970ce8f1c3a2

loading.bi
@@ -129,6 +129,7 @@ DECLARE FUNCTION tile_anim_is_empty(pattern_num as integer, tastuf() as integer)
 
 DECLARE SUB getpal16 (array() as integer, byval array_record as integer, byval file_record as integer, byval autotype as integer=-1, byval sprite as integer=0)
 DECLARE SUB storepal16 (array() as integer, byval array_record as integer, byval file_record as integer)
+DECLARE SUB palette16_save (pal as Palette16 ptr, pal_num as integer)
 
 DECLARE SUB convert_mxs_to_rgfx(filename as string, outfile as string)
 DECLARE FUNCTION rgfx_get_frame(filename as string, setnum as integer, framenum as integer) as Frame ptr

loading.rbas
@@ -2985,46 +2985,59 @@ END IF
 
 END SUB
 
+' Save palette to old .pal lump
+SUB Palette16_save_to_PAL(pal as Palette16 ptr, fname as string, pal_num as integer)
+ DIM buf(8) as integer
+ loadrecord buf(), fname, 8, 0
+
+ IF buf(0) <> 4444 THEN
+  showerror "Did not save 16-color palette: pal file appears corrupt"
+  EXIT SUB
+ END IF
+
+ DIM last as integer = buf(1)
+
+ IF pal_num > last THEN
+  '--blank out palettes before extending file
+  FOR i as integer = last + 1 TO pal_num
+   flusharray buf(), 8, 0
+   storerecord buf(), fname, 8, 1 + i
+  NEXT i
+  '--update header
+  buf(0) = 4444
+  buf(1) = pal_num
+  storerecord buf(), fname, 8, 0
+ END IF
+
+ IF pal_num >= 0 THEN '--never write a negative file offset
+  'Write palette to buffer
+  DIM colidx as integer = 0
+  FOR i as integer = 0 TO 15
+   poke8bit buf(), i, pal->col(i)
+  NEXT
+  storerecord buf(), fname, 8, 1 + pal_num
+ END IF
+END SUB
+
+SUB Palette16_save(pal as Palette16 ptr, pal_num as integer)
+ DIM fname as string = game & ".pal"
+ Palette16_save_to_PAL pal, fname, pal_num
+ Palette16_update_cache fname, pal_num
+ #IFDEF IS_CUSTOM
+  IF slave_channel <> NULL_CHANNEL THEN channel_write_line slave_channel, "PAL " & pal_num
+ #ENDIF
+END SUB
+
 'array() holds multiple palettes (records). Store one of them to file.
 SUB storepal16 (array() as integer, byval array_record as integer, byval file_record as integer)
-DIM buf(8) as integer
-
-DIM f as string = game & ".pal"
-loadrecord buf(), f, 8, 0
-
-IF buf(0) <> 4444 THEN
- showerror "Did not save 16-color palette: file appears corrupt"
- EXIT SUB
-END IF
-
-DIM last as integer = buf(1)
-DIM i as integer
-
-IF file_record > last THEN
- '--blank out palettes before extending file
- FOR i = last + 1 TO file_record
-  flusharray buf(), 8, 0
-  storerecord buf(), f, 8, 1 + i
+ ' Convert to Palette16
+ DIM pal as Palette16
+ ' DIM colidx as integer = 0
+ FOR i as integer = 0 TO 15
+  pal.col(i) = peek8bit(array(), i + file_record * 16)
  NEXT i
- '--update header
- buf(0) = 4444
- buf(1) = file_record
- storerecord buf(), f, 8, 0
-END IF
 
-IF file_record >= 0 THEN '--never write a negative file offset
- 'copy palette to buffer
- FOR i = 0 TO 7
-  buf(i) = array(array_record * 8 + i)
- NEXT i
- 'write palette
- storerecord buf(), f, 8, 1 + file_record
-END IF
-
-Palette16_update_cache f, file_record
-#IFDEF IS_CUSTOM
- IF slave_channel <> NULL_CHANNEL THEN channel_write_line slave_channel, "PAL " & file_record
-#ENDIF
+ Palette16_save @pal, file_record
 END SUB
 
 

commit 7c501afc2172540355c7cb580b1e1b09f3e408f1

storepal16 was broken, delete it, and some slight cleanup to pal16 loading code
I broke storepal16 in r8277 (Dec 27 2016). 'file_record * 16' should have been
'array_record * 16'. This caused the palette corruption bug (which I fixed
in r8357 by no longer using storepal16)

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8358 7d344553-34f0-0310-a9b1-970ce8f1c3a2

allmodex.bas
@@ -7507,17 +7507,13 @@ function Palette16_load(fil as string, num as integer, autotype as SpriteType = 
 		return cache->p
 	end if
 
-	if not isfile(fil) then return 0
-
-	dim fh as integer = freefile
-
+	dim fh as integer
 	if openfile(fil, for_binary + access_read, fh) then return 0
 
 	dim mag as short
-
 	get #fh, 1, mag
-
 	if mag = 4444 then
+		' File is in new file format
 		get #fh, , mag
 		if num > mag then
 			close #fh
@@ -7526,6 +7522,9 @@ function Palette16_load(fil as string, num as integer, autotype as SpriteType = 
 
 		seek #fh, 17 + 16 * num
 	else
+		' .pal file is still in ancient BSAVE format, with exactly 100
+		' palettes. This shouldn't happen because upgrade() upgrades it.
+		' Skip 7-byte BSAVE header.
 		seek #fh, 8 + 16 * num
 	end if
 
@@ -7537,9 +7536,10 @@ function Palette16_load(fil as string, num as integer, autotype as SpriteType = 
 		return 0
 	end if
 
-	'see, it's "mag"ic, since it's used for so many things
-	for mag = 0 to 15
-		get #fh, , ret->col(mag)
+	for idx as integer = 0 to 15
+		dim byt as ubyte
+		get #fh, , byt
+		ret->col(idx) = byt
 	next
 	ret->refcount = 1
 

common.rbas
@@ -3366,16 +3366,13 @@ IF gen(genVersion) = 4 THEN
  killfile game + ".pal"
  '--find last used palette
  DIM last as integer = 99
- DIM foundpal as integer = 0
  FOR j as integer = 99 TO 0 STEP -1
   FOR i as integer = 0 TO 7
    IF buffer(j * 8 + i) <> 0 THEN
     last = j
-    foundpal = 1
-    EXIT FOR
+    EXIT FOR, FOR
    END IF
   NEXT i
-  IF foundpal THEN EXIT FOR
  NEXT j
  upgrade_message "Last used palette is " & last
  '--write header

loading.bi
@@ -127,7 +127,6 @@ DECLARE FUNCTION tile_anim_animate_tile (tileid as integer, pattern_num as integ
 DECLARE FUNCTION tile_anim_is_empty(pattern_num as integer, tastuf() as integer) as bool
 
 DECLARE SUB getpal16 (array() as integer, byval array_record as integer, byval file_record as integer, byval autotype as integer=-1, byval sprite as integer=0)
-DECLARE SUB storepal16 (array() as integer, byval array_record as integer, byval file_record as integer)
 DECLARE SUB palette16_save (pal as Palette16 ptr, pal_num as integer)
 
 DECLARE SUB init_4bit_spriteset_defaults(ss as SpriteSet ptr, ptno as SpriteType)

loading.rbas
@@ -3029,18 +3029,6 @@ SUB Palette16_save(pal as Palette16 ptr, pal_num as integer)
  #ENDIF
 END SUB
 
-'array() holds multiple palettes (records). Store one of them to file.
-SUB storepal16 (array() as integer, byval array_record as integer, byval file_record as integer)
- ' Convert to Palette16
- DIM pal as Palette16
- ' DIM colidx as integer = 0
- FOR i as integer = 0 TO 15
-  pal.col(i) = peek8bit(array(), i + file_record * 16)
- NEXT i
-
- Palette16_save @pal, file_record
-END SUB
-
 
 '==========================================================================================
 '                                     Animations