2523e2bc6fe9dd4113a375e72936fdb33adc56a0
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-02-25 18:17:52 +0000

    A script for starting nightly build VMs (and maybe I'll move some other nightly-related stuff into this folder later)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8444 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ nightly/ohr_nightly_vm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+for VMNAME in "Debian 32bit" "Debian 64bit" "Windows 7" ; do
+  echo "===Starting ${VMNAME}==="
+  vboxmanage startvm "${VMNAME}"
+  RUNNING="True"
+  while [ -n "${RUNNING}" ] ; do
+    sleep 5
+    RUNNING=$(vboxmanage showvminfo "${VMNAME}" \
+      | grep "^State:" \
+      | grep "running (since"\
+    )
+  done
+done
+
+
