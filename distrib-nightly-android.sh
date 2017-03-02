5fb28c4a54e0bffc81859e7f34887600ea86ab59
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-03-02 19:07:34 +0000

    Change the paths in my android script (still hard coded to my machine, doesn't use my dropbox anymore)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8484 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib-nightly-android.sh
+++ distrib-nightly-android.sh
@@ -2,14 +2,14 @@
 
 # Paths in this are specific to James's computer, but it works, and is something to start with
 
-cd ~/Dropbox/src/ohr/wip
+cd ~/src/ohr/wip
 scons fbc=$FBCARM debug=0 android-source=1 game
 scons
-cd ~/Dropbox/src/sdl-android/project/jni/application
+cd ~/src/sdl-android/project/jni/application
 git checkout ohrrpgce
 rm src
 ln -s ohrrpgce src
-cd ~/Dropbox/src/sdl-android
+cd ~/src/sdl-android
 rm project/bin/MainActivity-debug.apk
 ./build.sh
 
