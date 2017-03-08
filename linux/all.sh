d89f151f0d34f48f2e61525d8daec2f93748f561
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-03-08 17:48:54 +0000

    Remove the .deb scripts for wander and vikings (these should be done with the "Distribute Game" feature, at some future time)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8511 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/all.sh
+++ linux/all.sh
@@ -5,13 +5,3 @@ if [ -d ohrrpgce ] ; then
   rm -R ohrrpgce
 fi
 python2.7 ./ohrrpgce.py
-
-if [ -d wandering-hamster ] ; then
-  rm -R wandering-hamster
-fi
-python2.7 ./wander.py
-
-if [ -d vikings-of-midgard ] ; then
-  rm -R vikings-of-midgard
-fi
-python2.7 ./viking.py
