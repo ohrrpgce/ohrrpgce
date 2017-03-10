a612a00dad4a1f08167c0d35c7c9b5038a9b9958
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-03-10 18:18:26 +0000

    Add a tool to be run from a cron job on the web server that mackes an archive of nightly builds. This archive is *much* simpler than the now-obsolete one in web/mirror/ohrnightly
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@8513 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ mirror/ohr_friday_archive/ohr_friday_archive.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import datetime
+import shutil
+import re
+
+nightlies = os.path.expanduser("~/HamsterRepublic.com/ohrrpgce-static/nightly/")
+archivedir = os.path.expanduser("~/rpg.hamsterrepublic.com/nightly-archive/")
+now_string = datetime.date.today().strftime("%Y-%m-%d")
+destdir = os.path.join(archivedir, now_string)
+max_archives = 10
+
+if os.path.exists(destdir):
+    if os.path.isdir(destdir):
+        print "Opps! {0} directory already exists.".format(destdir)
+        sys.exit(1)
+    else:
+        print "Opps! {0} already exists, but it ain't a directory!".format(destdir)
+        sys.exit(1)
+
+os.mkdir(destdir)
+
+# copy over the current nightlies
+print "Copying over nightly builds into {0}".format(destdir)
+for filename in os.listdir(nightlies):
+    if filename in [".", "..", ".htaccess", "CONFUSED?.html", "ohrrpgce-wip-default.zip"]:
+        # skip stuff that we don't want to archive
+        continue
+    srcfile = os.path.join(nightlies, filename)
+    shutil.copy2(srcfile, destdir)
+htaccess = os.path.join(destdir, ".htaccess")
+f = open(htaccess, "w")
+f.write("IndexOptions HTMLTable IgnoreClient NameWidth=* FoldersFirst\n")
+f.close()
+
+# erase old nightlies
+
+# First make a list of them
+dir_list = []
+dir_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+for dirname in os.listdir(archivedir):
+    if dirname in [".", ".."]:
+        continue
+    if dir_pattern.match(dirname):
+        dir_list.append(dirname)
+dir_list.sort(reverse=True)
+
+# Then delete the oldest ones
+while len(dir_list) > max_archives:
+    delete_me = os.path.join(archive_dir, dir_list[-1])
+    dir_list = dir_list[:-1]
+    print "Deleting old archive {0}".format(delete_me)
+    shutil.rmtree(delete_me)
