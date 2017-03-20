fc8d7bf753dd3d91232163add0faf6543e41d9b0
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-03-20 02:35:05 +0000

    Updated script for starting nightly build VM's
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8560 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- nightly/ohr_nightly_vm.sh
+++ nightly/ohr_nightly_vm.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-for VMNAME in "Debian 64bit" "Windows 7" ; do
+for VMNAME in "Debian 32bit" "Debian 64bit" "Windows 7" "Mac OS X" ; do
   echo "===Starting ${VMNAME}==="
   vboxmanage startvm "${VMNAME}" --type headless
   RUNNING="True"
+  COUNT=1
   while [ -n "${RUNNING}" ] ; do
     sleep 5
+    COUNT=$(expr "${COUNT}" + 5)
     RUNNING=$(vboxmanage showvminfo "${VMNAME}" \
       | grep "^State:" \
       | grep "running (since"\
     )
+    if [ "${COUNT}" -gt 7200 ] ; then
+      echo "ERROR: ${VMNAME} has run for more than 2 hours, killing it!"
+      killall "VBoxHeadless"
+      sleep 5
+      continue
+    fi
   done
 done
 
