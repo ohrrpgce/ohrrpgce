e1735a9b8123d7a99a00147abfe5df69181de2d4
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-06-12 15:31:56 +0000

    hspeaktest.py: Add various commandline arguments, replacing global variables as well as new ones
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8862 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- hspeaktest.py
+++ hspeaktest.py
@@ -16,22 +16,19 @@ import sys
 import os
 import re
 import subprocess
-
-quiet = True            # Don't print tests that pass
-show_all = False        # Always print hspeak's stdout
-show_failures = False   # Print stdout on failure
+import optparse
 
 def command_output_and_exitcode(cmd, args):
     """Runsa  program and returns (stdout, exitcode) pair."""
     assert isinstance(args, (list, tuple))
     proc = subprocess.Popen([cmd] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.wait()  # Needed to fetch the returncode
     outtext = proc.stdout.read()
     errtext = proc.stderr.read()
     if errtext:
         # HSpeak never writes anything to stderr
         print('%s\n%s\n%s\n%s' % ('-'*40, outtext, errtext, '-'*40))
         raise Exception("Didn't expect HSpeak to write to stderr. This needs updating?")
-    proc.wait()  # Needed to fetch the returncode
     return outtext.strip(), proc.returncode
 
 def read_input_file(inputfile):
@@ -71,7 +68,10 @@ def run_with_line(lines, lineno):
     with open(tempfile, "w") as outfile:
         outfile.write("\n".join(selected_lines))
 
-    ret = command_output_and_exitcode("./hspeak", ['-ybk', tempfile])
+    if options.eui:
+        ret = command_output_and_exitcode('eui', ['hspeak.exw', '-ybk', tempfile])
+    else:
+        ret = command_output_and_exitcode('./hspeak', ['-ybk', tempfile])
     #os.unlink(tempfile)
     return ret, expected_lineno
 
@@ -86,14 +86,25 @@ def test_line(lines, lineno):
     def print_line():
         print("Testing line %d: %s" % (lineno, lines[lineno][1]))
 
-    if not quiet:
+    if not options.hide_passes:
         print_line()
 
     def print_stdout():
-        if quiet:
+        if options.hide_passes:
             print_line()
-        if show_failures or show_all:
-            print('%s\n%s\n%s' % ('-'*40, stdout, '-'*40))
+        if options.show_failures or options.show_all:
+            trim_to = 0
+            if options.trim_output:
+                # Trim everything before the warning/error message
+                COLRED = '\x1b[22;31m'
+                for errstart in (COLRED + 'ERROR', COLRED + 'WARNING', 'ERROR', 'WARNING'):
+                    if errstart in stdout:
+                        trim_to = stdout.find(errstart)
+                        break
+                else:
+                    # No message at all; drop everything
+                    return
+            print('%s\n%s\n%s' % ('-'*40, stdout[trim_to:], '-'*40))
 
     if expected != exitcode:
         print_stdout()
@@ -105,7 +116,7 @@ def test_line(lines, lineno):
         print_stdout()
         print("Test on line %d FAILED: didn't print an error/warning for line %d" % (lineno, expected_lineno))
         return False
-    if show_all:
+    if options.show_all:
         print_stdout()
     return True
 
@@ -116,17 +127,46 @@ def run_all_tests(inputfile):
     for lineno, (tag, line) in enumerate(lines):
         #print tag, line
         if tag and tag != 'COMMENT':
-            ran += 1
-            successes += test_line(lines, lineno)
+            if options.testline == None or int(options.testline) == lineno + 1:  # Lines count from 1
+                ran += 1
+                successes += test_line(lines, lineno)
     print
     print("Ran %d tests, %d failed" % (ran, ran - successes))
     return ran == successes
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: ./hspeaktest.py testfile.hss")
+    parser = optparse.OptionParser(usage="%prog [options] testfile.hss",
+                                   description="Runs HSpeak on a file of testcases, checking whether each line "
+                                   "produces an error, warning, or compiles, according to its annotation.")
+    parser.add_option("--eui",
+                      action="store_true", dest="eui", default=False,
+                      help="Run 'eui hspeak.exw' instead of './hspeak' (slower than compiling hspeak!)")
+    # This is probably too many options...
+    parser.add_option("-a", "--all",
+                      action="store_false", dest="hide_passes", default=True,
+                      help="List all tests run, including those that passed")
+    parser.add_option("-s", "--show",
+                      action="store_true", dest="show_failures", default=False,
+                      help="Show any warning/error messages for failed tests")
+    parser.add_option("-f", "--full",
+                      action="store_false", dest="trim_output", default=True,
+                      help="Show full output from hspeak for failed tests, not just warning/error messages. Implies --show")
+    parser.add_option("-S", "--showall",
+                      action="store_true", dest="show_all", default=False,
+                      help="Show hspeak's full stdout for all tests (implies --all, --show and --full)")
+    parser.add_option("-t", "--testline",
+                      dest="testline", default=None,
+                      help="Test only a specific testcase line")
+
+    (options, _args) = parser.parse_args()
+    if len(_args) != 1:
+        parser.print_help()
         sys.exit(1)
-    inputfile = sys.argv[1]
+    inputfile = _args[0]
+    if options.trim_output == False:
+        options.show_failures = True
+    if options.show_all:
+        options.trim_output = False
 
     if run_all_tests(inputfile):
         sys.exit(0)
