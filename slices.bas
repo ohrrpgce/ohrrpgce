ohrrpgce/ohrrpgce

commit c0d7f7103703dab57bbf64f2af2584e87dce536f

RectangleSlice, TextSlice, and EllipseSlice now support uilook colors  as an alternative to colors specified as master-palette indexes
RectangleSlice and TextSlice also support cursor-color-flashing
 between the two uiSelectedItem colors and the two uiSelectedDisabled colors


git-svn-id: https://rpg.hamsterrepublic.com/source/wip@6519 7d344553-34f0-0310-a9b1-970ce8f1c3a2

slices.bas
@@ -36,6 +36,7 @@ DECLARE Function SliceXAlign(byval sl as Slice Ptr, byval alignTo as Slice Ptr) 
 DECLARE Function SliceYAlign(byval sl as Slice Ptr, byval alignTo as Slice Ptr) as integer
 DECLARE Sub ApplySliceVelocity(byval s as slice ptr)
 DECLARE Sub SeekSliceTarg(byval s as slice ptr)
+DECLARE Function SliceColor(byval n as integer) as integer
 
 END EXTERN
 
@@ -876,7 +877,7 @@ Sub DrawRectangleSlice(byval sl as slice ptr, byval p as integer)
   UpdateRectangleSliceStyle dat
  end if
 
- edgebox sl->screenx, sl->screeny, sl->width, sl->height, dat->bgcol, dat->fgcol, p, dat->translucent, dat->border, dat->fuzzfactor
+ edgebox sl->screenx, sl->screeny, sl->width, sl->height, SliceColor(dat->bgcol), SliceColor(dat->fgcol), p, dat->translucent, dat->border, dat->fuzzfactor
 end sub
 
 Sub CloneRectangleSlice(byval sl as slice ptr, byval cl as slice ptr)
@@ -1033,7 +1034,8 @@ Sub DrawTextSlice(byval sl as slice ptr, byval p as integer)
  WrapTextSlice sl, lines()
 
  dim col as integer = dat->col
- if col = 0 then col = uilook(uiText)
+ if col = 0 then col = uilook(uiText) '--This is backcompat for before it was possible to choose uiText directly using SliceColor
+ col = SliceColor(col)
  dim chars as integer = 0
  dat->insert_tog = dat->insert_tog xor 1
  dim insert_size as integer = 8
@@ -1058,7 +1060,7 @@ Sub DrawTextSlice(byval sl as slice ptr, byval p as integer)
   if dat->outline then
    edgeprint lines(i), sl->screenx, sl->screeny + ypos, col, p
   else
-   textcolor col, dat->bgcol
+   textcolor col, SliceColor(dat->bgcol)
    printstr lines(i), sl->screenx, sl->screeny + ypos, p
   end if
  next
@@ -1757,7 +1759,7 @@ Sub DrawEllipseSlice(byval sl as slice ptr, byval p as integer)
    'fuzzyrect .frame, 0, 0, w, h, dat->fillcol, 37
    dim fillcol as integer = dat->fillcol
    if fillcol = 0 then fillcol = -1
-   ellipse .frame, w / 2 - 0.5, h / 2 - 0.5 , w / 2 - 0.5, dat->bordercol, fillcol, h / 2 - 0.5
+   ellipse .frame, w / 2 - 0.5, h / 2 - 0.5 , w / 2 - 0.5, SliceColor(dat->bordercol), SliceColor(fillcol), h / 2 - 0.5
    .last_draw_size.X = w
    .last_draw_size.Y = h
    .last_draw_bordercol = .bordercol
@@ -2345,6 +2347,19 @@ Sub SliceClamp(byval sl1 as Slice Ptr, byval sl2 as Slice Ptr)
   if diff > 0 then sl2->Y -= abs(diff)
  end if
 end sub
+
+Function SliceColor(byval n as integer) as integer
+ if n >= 0 andalso n <= 255 then return n
+ if n <= -1 andalso n >= -18 then
+  dim uiC as integer = (n * -1) - 1
+  if uiC = uiSelectedItem + 1 orelse uiC = uiSelectedDisabled + 1 then
+   'uiSelectedItem and uiDisabledItem can be selected in animating or non-animating modes
+   if get_tickcount() mod 2 = 0 then uiC = uiC - 1
+  end if
+  return uilook(uiC)
+ end if
+ debugc errError, "Invalid slice color " & n
+End function
 
 '==Slice cloning===============================================================
 

sliceedit.bas
@@ -71,6 +71,8 @@ CONST slgrUPDATERECTSTYLE = 64
 CONST slgrPICKLOOKUP = 128
 '--This system won't be able to expand forever ... :(
 
+CONST slLOWCOLORCODE = -18
+
 '==============================================================================
 
 'This overload of slice_editor is only allowed locally.
@@ -102,6 +104,7 @@ DECLARE FUNCTION edit_slice_lookup_codes(slicelookup() as string, byval start_at
 DECLARE FUNCTION slice_caption (sl as Slice Ptr, slicelookup() as string) as string
 DECLARE SUB slice_editor_copy(byref ses as SliceEditState, byval slice as Slice Ptr, byval edslice as Slice Ptr)
 DECLARE SUB slice_editor_paste(byref ses as SliceEditState, byval slice as Slice Ptr, byval edslice as Slice Ptr)
+DECLARE FUNCTION slice_color_caption(byval n as integer, ifzero as string="0") as string
 
 'Functions that need to be aware of magic numbers for SliceType
 DECLARE FUNCTION slice_edit_detail_browse_slicetype(byref slice_type as SliceTypes) as SliceTypes
@@ -747,10 +750,10 @@ SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, sl as
     dat = .SliceData
     str_array_append menu(), "Style: " & defaultint(dat->style, "None")
     sliceed_rule rules(), "rect_style", erIntgrabber, @(dat->style), -1, 14, slgrUPDATERECTSTYLE
-    str_array_append menu(), "Background color: " & defaultint(dat->bgcol)
-    sliceed_rule rules(), "rect_bg", erIntgrabber, @(dat->bgcol), 0, 255, (slgrUPDATERECTCOL OR slgrPICKCOL)
-    str_array_append menu(), "Foreground color: " & defaultint(dat->fgcol)
-    sliceed_rule rules(), "rect_fg", erIntgrabber, @(dat->fgcol), 0, 255, (slgrUPDATERECTCOL OR slgrPICKCOL)
+    str_array_append menu(), "Background color: " & slice_color_caption(dat->bgcol)
+    sliceed_rule rules(), "rect_bg", erIntgrabber, @(dat->bgcol), slLOWCOLORCODE, 255, (slgrUPDATERECTCOL OR slgrPICKCOL)
+    str_array_append menu(), "Foreground color: " & slice_color_caption(dat->fgcol)
+    sliceed_rule rules(), "rect_fg", erIntgrabber, @(dat->fgcol), slLOWCOLORCODE, 255, (slgrUPDATERECTCOL OR slgrPICKCOL)
     str_array_append menu(), "Border: " & caption_or_int(dat->border, BorderCaptions())
     sliceed_rule rules(), "rect_border", erIntgrabber, @(dat->border), -2, 14, slgrUPDATERECTCOL 
     str_array_append menu(), "Translucency: " & TransCaptions(dat->translucent)
@@ -764,14 +767,14 @@ SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, sl as
     dat = .SliceData
     str_array_append menu(), "Text: " & dat->s
     sliceed_rule rules(), "text_text", erStrgrabber, @(dat->s), 0, 0
-    str_array_append menu(), "Color: " & zero_default(dat->col)
-    sliceed_rule rules(), "text_color", erIntgrabber, @(dat->col), 0, 255, slgrPICKCOL
+    str_array_append menu(), "Color: " & slice_color_caption(dat->col, "Default")
+    sliceed_rule rules(), "text_color", erIntgrabber, @(dat->col), slLOWCOLORCODE, 255, slgrPICKCOL
     str_array_append menu(), "Outline: " & yesorno(dat->outline)
     sliceed_rule_tog rules(), "text_outline", @(dat->outline)
     str_array_append menu(), "Wrap: " & yesorno(dat->wrap)
     sliceed_rule_tog rules(), "text_wrap", @(dat->wrap)
-    str_array_append menu(), "Background Color: " & zero_default(dat->bgcol)
-    sliceed_rule rules(), "text_bg", erIntgrabber, @(dat->bgcol), 0, 255, slgrPICKCOL
+    str_array_append menu(), "Background Color: " & slice_color_caption(dat->bgcol, "Default")
+    sliceed_rule rules(), "text_bg", erIntgrabber, @(dat->bgcol), slLOWCOLORCODE, 255, slgrPICKCOL
    CASE slSprite
     DIM dat as SpriteSliceData Ptr
     dat = .SliceData
@@ -805,10 +808,10 @@ SUB slice_edit_detail_refresh (byref state as MenuState, menu() as string, sl as
    CASE slEllipse
     DIM dat as EllipseSliceData Ptr
     dat = .SliceData
-    str_array_append menu(), "Border Color: " & zero_default(dat->bordercol, "transparent")
-    sliceed_rule rules(), "bordercol", erIntgrabber, @(dat->bordercol), 0, 255, slgrPICKCOL
-    str_array_append menu(), "Fill Color: " & zero_default(dat->fillcol, "transparent")
-    sliceed_rule rules(), "fillcol", erIntgrabber, @(dat->fillcol), 0, 255, slgrPICKCOL
+    str_array_append menu(), "Border Color: " & slice_color_caption(dat->bordercol, "transparent")
+    sliceed_rule rules(), "bordercol", erIntgrabber, @(dat->bordercol), slLOWCOLORCODE, 255, slgrPICKCOL
+    str_array_append menu(), "Fill Color: " & slice_color_caption(dat->fillcol, "transparent")
+    sliceed_rule rules(), "fillcol", erIntgrabber, @(dat->fillcol), slLOWCOLORCODE, 255, slgrPICKCOL
   END SELECT
   str_array_append menu(), "Visible: " & yesorno(.Visible)
   sliceed_rule_tog rules(), "vis", @.Visible
@@ -1167,3 +1170,35 @@ FUNCTION edit_slice_lookup_codes(slicelookup() as string, byval start_at_code as
 
  RETURN result
 END FUNCTION
+
+FUNCTION slice_color_caption(byval n as integer, ifzero as string="0") as string
+ IF n = 0 THEN RETURN ifzero
+ 'Normal colors
+ IF n > 0 ANDALSO n <= 255 THEN RETURN STR(n)
+ 'uilook colors
+ IF n <= -1 ANDALSO n >= slLOWCOLORCODE THEN
+  SELECT CASE (n * -1) - 1
+   CASE uiBackground: RETURN "Background"
+   CASE uiMenuItem: RETURN "Menu item"
+   CASE uiDisabledItem: RETURN "Disabled menu item"
+   CASE uiSelectedItem: RETURN "Selected item A"
+   CASE uiSelectedItem2: RETURN "Selected item B"
+   CASE uiSelectedDisabled: RETURN "Selected disabled A"
+   CASE uiSelectedDisabled+1: RETURN "Selected disabled B"
+   CASE uiHighlight: RETURN "Highlight A"
+   CASE uiHighlight2: RETURN "Highlight B"
+   CASE uiTimeBar: RETURN "Time bar"
+   CASE uiTimeBarFull: RETURN "Time bar full"
+   CASE uiHealthBar: RETURN "Health bar"
+   CASE uiHealthBarFlash: RETURN "Health bar full"
+   CASE uiText: RETURN "Text"
+   CASE uiOutline: RETURN "Text outline"
+   CASE uiDescription: RETURN "Spell description"
+   CASE uiGold: RETURN "Money"
+   CASE uiShadow: RETURN "Vehicle shadow"
+  END SELECT
+ END IF
+ 'Invalid values still print, but !?
+ RETURN n & "(!?)"
+END FUNCTION
+

commit 3bcb7f2ef6fb02ffca3605148717c72a11d0c850

Fix ellipse slice border/fill colour 0 appearing as uiBackground instead of transparent, if uilook(uiBackground) was not 0
If uilook(uiBackground) is 0 then it acts as transparent when used as
border/fill. To solve that would either require using a mask, or drawing the
ellipse directly to the videopage. But if we switch to hardware acceleration,
then the way we're drawing ellipses (onto a temporary Frame) is already the
correct way to do it, and drawing to a videopage won't be supported.  On the
other hand drawing directly would speed up large ellipses and slow down small
ones.

This bug was introduced in r6519 (in 2014).

Also fixed the plotdict not mentioning that border colour 0 is transparent.
Which is weird, but that's how it's always worked, and it's not easy to change
(see above).

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8990 7d344553-34f0-0310-a9b1-970ce8f1c3a2

docs/plotdict.xml
@@ -6695,7 +6695,7 @@ The third argument is the total number of ticks that the animation should run. Y
 		<command id="getellipsebordercol">
 			<canon>get ellipse border col (handle)</canon>
 			<shortname>get ellipse border col</shortname>
-			<description>Given an ellipse slice <p>handle</p>, return the color it uses for its border. This will be a number from 0 to 255 representing a color from the master palette.</description>
+			<description>Given an ellipse slice <p>handle</p>, return the color it uses for its border. This will be a number from 1 to 255 representing a color from the master palette, or 0 meaning transparent.</description>
 			<seealso>
 				<ref>setellipsebordercol</ref>
 				<ref>getellipsebordercol</ref>
@@ -6706,7 +6706,7 @@ The third argument is the total number of ticks that the animation should run. Y
 		<command id="setellipsebordercol">
 			<canon>set ellipse border col (handle, color)</canon>
 			<shortname>set ellipse border col</shortname>
-			<description>Given an ellipse slice <p>handle</p>, change the color it uses for its border. The <p>color</p> should be a number from 0 to 255 representing a color from the master palette.</description>
+			<description>Given an ellipse slice <p>handle</p>, change the color it uses for its border. The <p>color</p> should be a number from 1 to 255 representing a color from the master palette. <p>color</p> 0 means 'transparent', which is not very useful.</description>
 			<seealso>
 				<ref>setellipsebordercol</ref>
 				<ref>getellipsebordercol</ref>

slices.bas
@@ -2034,8 +2034,11 @@ Sub DrawEllipseSlice(byval sl as slice ptr, byval p as integer)
    .frame = frame_new(w, h, , YES)
    'fuzzyrect .frame, 0, 0, w, h, dat->fillcol, 37
    dim fillcol as integer = dat->fillcol
-   if fillcol = 0 then fillcol = -1
-   ellipse .frame, w / 2 - 0.5, h / 2 - 0.5 , w / 2 - 0.5, SliceColor(dat->bordercol), SliceColor(fillcol), h / 2 - 0.5
+   'EllipseSliceData.fillcol 0 means transparent, while ellipse() uses -1 to mean transparent
+   'NOTE: Drawing bordercol or fillcol 0 will be transparent anyway, because when .frame
+   'gets drawn to the videopage, colour 0 counts as transparent. So fillcol=-1 is just an optimisation.
+   if fillcol = 0 then fillcol = -1 else fillcol = SliceColor(fillcol)
+   ellipse .frame, w / 2 - 0.5, h / 2 - 0.5 , w / 2 - 0.5, SliceColor(dat->bordercol), fillcol, h / 2 - 0.5
    .last_draw_size.X = w
    .last_draw_size.Y = h
    .last_draw_bordercol = .bordercol