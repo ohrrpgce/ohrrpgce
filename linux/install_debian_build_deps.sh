5b03a993e067cba555d1375cf9ac15b68840c030
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-07-25 20:20:34 +0000

    Update the install_debian_build_deps.sh script. Was missing libncurses5-dev Also added --no-install-recommends
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9079 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/install_debian_build_deps.sh
+++ linux/install_debian_build_deps.sh
@@ -8,5 +8,16 @@ echo "http://freebasic.net/"
 echo "http://openeuphoria.org/"
 echo "------------------------------------------------------------------"
 
-sudo apt-get install scons g++ libx11-dev libxpm-dev libxrandr-dev libxrender-dev libsdl-mixer1.2-dev timidity gdb
+sudo apt-get install --no-install-recommends \
+  scons \
+  g++ \
+  libx11-dev \
+  libxpm-dev \
+  libxrandr-dev \
+  libxrender-dev \
+  libsdl-mixer1.2-dev \
+  libncurses5-dev \
+  timidity \
+  freepats \
+  gdb
 # gdb isn't strictly required, but is useful anyway
