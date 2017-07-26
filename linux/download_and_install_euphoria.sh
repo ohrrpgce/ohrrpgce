9a8bec6e262308eebca032bb1b98d432b240768e
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-07-26 22:10:08 +0000

    The download_and_install_euphoria.sh script can now select between 32bit or 64bit versions Switch to version 4.1.0 Also remove the code that attempts to install the .deb package version
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9096 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/download_and_install_euphoria.sh
+++ linux/download_and_install_euphoria.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+ARCH="${1}"
+
+if [ "${ARCH}" = "64" -o "${ARCH}" = "32" ] ; then
+  echo "Selected ${ARCH}bit version of Euphoria"
+else
+  echo "You must specify which version of euphoria to install on the command-line. 32 bit or 64 bit. For example:"
+  echo "  ./download_and_install_euphoria.sh 32"
+  echo "  ./download_and_install_euphoria.sh 64"
+  exit 1
+fi
+
 if [ -x "`which euc`" ] ; then
   echo "Euphoria seems to already be installed."
   exit 1
@@ -18,50 +29,57 @@ fi
 mkdir "${DLTEMP}"
 cd "${DLTEMP}"
 
-if [ -x "`which dpkg`" ] ; then
-  wget "https://downloads.sourceforge.net/project/rapideuphoria/Euphoria/4.0.5/euphoria_4.0.5_amd64.deb"
-  echo "Using sudo to install dependencies"
-  sudo apt-get install --no-install-recommends libc6-i386 g++-multilib
-  echo "Using sudo to install Euphoria package"
-  sudo dpkg -i euphoria_4.0.5_amd64.deb
+if [ "${ARCH}" = 64 ] ; then
+  URLARCH="x64"
 else
-  wget "https://downloads.sourceforge.net/project/rapideuphoria/Euphoria/4.0.5/euphoria-4.0.5-Linux-ix86.tar.bz2"
-  tar -jxf "euphoria-4.0.5-Linux-ix86.tar.bz2"
-
-  EUDEST="/usr/local/share/euphoria"
-  EUBIN="/usr/local/bin"
-
-  if [ -x "`which yum`" ] ; then
-    echo "###################################################"
-    echo "WARNING: Euphoria causes SELinux execheap warnings"
-    echo "on Fedora systems. It will still work for compiling"
-    echo "hspeak, but SELinux will be grouchy about it."
-  fi
-
-  echo "###################################################"
-  echo "Installing Euphoria in $EUDEST"
-  echo "If this is okay, type Y or type anything else to cancel"
-  read ASKOK
-  if [ "${ASKOK}" != "Y" -a "${ASKOK}" != "y" ] ; then
-    echo "Giving up."
-    exit 1
-  fi
-  sudo mv euphoria-4.0.5-Linux-ix86 "${EUDEST}"
-  sudo chown -R root:root "${EUDEST}"
-  sudo mv "${EUDEST}"/bin/eub "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eubind "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/euc "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eucoverage "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eudis "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eudist "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eui "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eushroud "${EUBIN}"
-  sudo mv "${EUDEST}"/bin/eutest "${EUBIN}"
-  sudo mkdir "/etc/euphoria"
-  echo "${EUDEST}"/include > "eu.cfg"
-  sudo mv "eu.cfg" "/etc/euphoria/"
-  sudo chown root:root "/etc/euphoria/eu.cfg"
+  URLARCH="x86"
 fi
+URL="https://downloads.sourceforge.net/project/rapideuphoria/Euphoria/4.1.0-beta2/euphoria-4.1.0-Linux-${URLARCH}-57179171dbed.tar.gz"
+TARBALL=$(basename "${URL}")
+EXTRACTDIR=$(echo ${TARBALL} | sed -e s/"-[a-f0-9]\+.tar.gz"/""/)
+wget "${URL}"
+tar -xf "${TARBALL}"
+
+if [ -d "${EXTRACTDIR}" ] ; then
+  echo "Downloaded and extracted ${EXTRACTDIR}"
+else
+  echo "Unable to extract ${EXTRACTDIR} from ${TARBALL}"
+  exit 1
+fi
+
+EUDEST="/usr/local/share/euphoria"
+EUBIN="/usr/local/bin"
+
+if [ -x "`which yum`" ] ; then
+  echo "###################################################"
+  echo "WARNING: Euphoria causes SELinux execheap warnings"
+  echo "on Fedora systems. It will still work for compiling"
+  echo "hspeak, but SELinux will be grouchy about it."
+fi
+
+echo "###################################################"
+echo "Installing Euphoria in $EUDEST"
+echo "If this is okay, type Y or type anything else to cancel"
+read ASKOK
+if [ "${ASKOK}" != "Y" -a "${ASKOK}" != "y" ] ; then
+  echo "Giving up."
+  exit 1
+fi
+sudo mv "${EXTRACTDIR}" "${EUDEST}"
+sudo chown -R root:root "${EUDEST}"
+sudo mv "${EUDEST}"/bin/eub "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eubind "${EUBIN}"
+sudo mv "${EUDEST}"/bin/euc "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eucoverage "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eudis "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eudist "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eui "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eushroud "${EUBIN}"
+sudo mv "${EUDEST}"/bin/eutest "${EUBIN}"
+sudo mkdir "/etc/euphoria"
+echo "${EUDEST}"/include > "eu.cfg"
+sudo mv "eu.cfg" "/etc/euphoria/"
+sudo chown root:root "/etc/euphoria/eu.cfg"
 
 cd ..
 rm -Rf "${DLTEMP}"
