d7f754041181293418eb434a1b1c8f0d41e8ee1b
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-08-10 14:38:38 +0000

    remove gengcc=1 from distrib-nightly-win.bat
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9141 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib-nightly-win.bat
+++ distrib-nightly-win.bat
@@ -15,55 +15,55 @@ pscp -q distrib\ohrrpgce-win-installer-%OHRVERDATE%-%OHRVERCODE%.exe james_paige
 CALL scons hspeak relump.exe unlump.exe
 
 del game*.exe custom*.exe
-call scons gfx=directx+sdl+fb music=sdl debug=0 gengcc=1
+call scons gfx=directx+sdl+fb music=sdl debug=0
 call nightly-gfx-music directx sdl ~ gfx_directx.dll SDL.dll SDL_mixer.dll 
 
 del game*.exe custom*.exe
-call scons gfx=directx+fb music=native debug=0 gengcc=1
+call scons gfx=directx+fb music=native debug=0
 call nightly-gfx-music directx native ~ gfx_directx.dll audiere.dll
 
 del game*.exe custom*.exe
-call scons gfx=directx+fb music=native2 debug=0 gengcc=1
+call scons gfx=directx+fb music=native2 debug=0
 call nightly-gfx-music directx native2 ~ gfx_directx.dll audiere.dll
 
 del game*.exe custom*.exe
-call scons gfx=fb+directx+sdl music=sdl debug=0 gengcc=1
+call scons gfx=fb+directx+sdl music=sdl debug=0
 call nightly-gfx-music fb sdl ~ SDL.dll SDL_mixer.dll 
 
 del game*.exe custom*.exe
-call scons gfx=fb+directx music=native debug=0 gengcc=1
+call scons gfx=fb+directx music=native debug=0
 call nightly-gfx-music fb native ~ audiere.dll
 
 del game*.exe custom*.exe
-call scons gfx=fb+directx music=native2 debug=0 gengcc=1
+call scons gfx=fb+directx music=native2 debug=0
 call nightly-gfx-music fb native2 ~ audiere.dll
 
 REM del game*.exe custom*.exe
-REM call scons gfx=alleg+directx+fb+sdl music=sdl debug=0 gengcc=1
+REM call scons gfx=alleg+directx+fb+sdl music=sdl debug=0
 REM call nightly-gfx-music alleg sdl ~ alleg40.dll SDL.dll SDL_mixer.dll 
 
 REM del game*.exe custom*.exe
-REM call scons gfx=alleg+directx+fb music=native debug=0 gengcc=1
+REM call scons gfx=alleg+directx+fb music=native debug=0
 REM call nightly-gfx-music alleg native ~ alleg40.dll audiere.dll
 
 REM del game*.exe custom*.exe
-REM call scons gfx=alleg+directx+fb music=native2 debug=0 gengcc=1
+REM call scons gfx=alleg+directx+fb music=native2 debug=0
 REM call nightly-gfx-music alleg native2 ~ alleg40.dll audiere.dll
 
 del game*.exe custom*.exe
-call scons gfx=sdl+directx+fb music=sdl debug=0 gengcc=1
+call scons gfx=sdl+directx+fb music=sdl debug=0
 call nightly-gfx-music sdl sdl ~ SDL.dll SDL_mixer.dll 
 
 del game*.exe custom*.exe
-call scons gfx=sdl+directx+fb music=native debug=0 gengcc=1
+call scons gfx=sdl+directx+fb music=native debug=0
 call nightly-gfx-music sdl native ~ audiere.dll SDL.dll
 
 del game*.exe custom*.exe
-call scons gfx=sdl+directx+fb music=native2 debug=0 gengcc=1
+call scons gfx=sdl+directx+fb music=native2 debug=0
 call nightly-gfx-music sdl native2 ~ audiere.dll SDL.dll
 
 del game*.exe custom*.exe
-call scons gfx=directx+sdl+fb music=silence debug=0 gengcc=1
+call scons gfx=directx+sdl+fb music=silence debug=0
 call nightly-gfx-music directx silence ~ SDL.dll gfx_directx.dll
 
 del game*.exe custom*.exe
