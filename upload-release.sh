2343a62155801700fbcb2f55002810e841920a6a
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-08-30 18:15:46 +0000

    Update Linux upload-release.sh script to adjust for the way 32bit/64bit binary packages are now named (this script would be run one time from a 32bit vm and one time from a 64bit vm)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9214 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- upload-release.sh
+++ upload-release.sh
@@ -2,8 +2,8 @@
 
 TODAY=`date "+%Y-%m-%d"`
 CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
-scp -p distrib/ohrrpgce-linux-x86-${TODAY}-${CODE}.tar.bz2 james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
-scp -p distrib/ohrrpgce-player-linux-bin-minimal-${TODAY}-${CODE}.zip james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
+scp -p distrib/ohrrpgce-linux-${TODAY}-${CODE}-*.tar.bz2 james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
+scp -p distrib/ohrrpgce-player-linux-bin-minimal-${TODAY}-${CODE}-*.zip james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
 
 DEBTODAY=`date "+%Y.%m.%d"`
 SVNREV=`svn info | grep "^Revision:" | sed -e "s/Revision: //"`
