360fe7b8936762893b8da6c36e363b0af3ee0b92
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-09-01 17:17:12 +0000

    Add release-win.bat batch file for building and uploading releases on Windows Removed upload-release.bat, since release/release-win.bat does it now
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9242 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-win.bat
+++ release/release-win.bat
@@ -1,1 +1,5 @@
+CD ..
+CALL distrib.bat
+
+REM This only works if you have Putty's pscp.exe in your path.
 pscp distrib/ohrrpgce-* james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
