9055776bd0e2b35ede1b2bc55d62017532ae15d0
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-09-01 17:17:16 +0000

    Add release/release-mac.sh script for mac remove the now obsolete upload-release-mac.sh (we hardly knew ye!)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9243 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ release/release-mac.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SCRIPTDIR="${0%/*}"
+cd "${SCRIPTDIR}"
+cd ..
+
+# This is hardcoded for the paths on James's Mac build box
+CC=clang GCC=/opt/local/bin/gcc-mp-4.7 EUDIR=~james/misc/euphoria/ ./distrib-mac.sh
+
+SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
+
+TODAY=`date "+%Y-%m-%d"`
+CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
+scp -p distrib/OHRRPGCE-${TODAY}-${CODE}.dmg "${SCPDEST}"/archive/
+scp -p distrib/ohrrpgce-mac-minimal-${TODAY}-${CODE}.tar.gz "${SCPDEST}"/archive/
--- upload-release-mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#Not sure where I am supposed to upload utilities-mac.zip
-
-TODAY=`date "+%Y-%m-%d"`
-CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
-scp -p distrib/OHRRPGCE-${TODAY}-${CODE}.dmg james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
-scp -p distrib/ohrrpgce-mac-minimal-${TODAY}-${CODE}.tar.gz james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
-
