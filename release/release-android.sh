f8160477ec1a90ce9540922ea0fa7b80c94fadaa
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-09-22 22:11:33 +0000

    Copy over the new and improved release scripts for dwimmercrafty+1
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/dwimmercrafty@9434 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ release/release-android.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
+
+if [ ! -d "${SDLANDROID}" ] ; then
+  echo "The SDLANDROID env variable should point to the checked out copy of sdl-android ohrrpgce branch"
+  exit 1
+fi
+
+SCRIPTDIR="${0%/*}"
+cd "${SCRIPTDIR}"
+cd ..
+RELDIR="$(pwd)"
+
+CODENAME=$(grep -v "^#" codename.txt | head -1)
+if [ "${CODENAME}" = "wip" ] ; then
+  echo "Doing a release on the wip branch probably isn't what you meant to do"
+  exit 1
+fi
+
+# First update the symlink
+echo "Update ohrrpgce sdl-android symlink for ${CODENAME} release..."
+cd "${SDLANDROID}"/project/jni/application/
+rm ohrrpgce
+ln -s ../../../../ohr/rel/"${CODENAME}"/android ohrrpgce
+cd "${RELDIR}"
+
+# Now build the apk
+./distrib-nightly-android.sh -noupload
+
+# Try to upload the result
+cd "${SDLANDROID}"
+if [ -f project/bin/MainActivity-debug.apk ] ; then
+  TODAY=$(date "+%Y-%m-%d")
+  scp -pr project/bin/MainActivity-debug.apk "${SCPDEST}"/archive/ohrrpgce-game-android-debug-"${TODAY}"-"${CODENAME}".apk
+else
+  echo "Nope, building that apk didn't work for some reason..."
+fi
+
+# Restore the wip symlink
+echo "Restore ohrrpgce sdl-android symlink for wip"
+cd project/jni/application
+rm ohrrpgce
+ln -s ../../../../ohr/wip/android ohrrpgce
---
+++ release/release-linux-x86.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPTDIR="${0%/*}"
+cd "${SCRIPTDIR}"
+cd ..
+
+OHR_SKIP_X86_64="Yes" ./distrib-linux.sh
+
+SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
+
+TODAY=`date "+%Y-%m-%d"`
+CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
+scp -p distrib/ohrrpgce-linux-${TODAY}-${CODE}-*.tar.bz2 "${SCPDEST}"/archive/
+scp -p distrib/ohrrpgce-player-linux-bin-minimal-${TODAY}-${CODE}-*.zip "${SCPDEST}"/archive/
