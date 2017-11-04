ohrrpgce/ohrrpgce

commit 6882e2cf650504e60de244a5e4b092d0fd61c91e

Allow running old games without archinym.lmp lumps which have been renamed and/or unlumped
Search for a .gen lump if archinym.lmp is missing.
Also handle games which were unlumped in the distant past, so the lump names
aren't lowercase.
One such game is the 1997 demo of Wandering Hamster.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9662 7d344553-34f0-0310-a9b1-970ce8f1c3a2

game.bas
@@ -553,13 +553,14 @@ DIM archinym as string
 '--pre-extract (if needed) .gen and load it
 IF usepreunlump THEN
  archinym = readarchinym(workingdir, sourcerpg)
- xbload workingdir & SLASH & archinym & ".gen", gen(), "general game data missing from " + sourcerpg
+ copylump sourcerpg, "*.gen", tmpdir, YES
 ELSE
  copylump sourcerpg, "archinym.lmp", tmpdir, YES
+ 'If archinym.lmp is missing, use the gen lump name
+ copylump sourcerpg, "*.gen", tmpdir, YES
  archinym = readarchinym(tmpdir, sourcerpg)
- copylump sourcerpg, archinym + ".gen", tmpdir, YES
- xbload tmpdir + archinym + ".gen", gen(), "general game data missing from " + sourcerpg
 END IF
+xbload tmpdir + archinym + ".gen", gen(), "general game data missing from " + sourcerpg
 
 DIM forcerpgcopy as integer = NO
 IF gen(genVersion) > CURRENT_RPG_VERSION THEN
@@ -585,7 +586,8 @@ ELSEIF NOT running_as_slave THEN  'Won't unlump or upgrade if running as slave
  #ENDIF
  IF forcerpgcopy THEN
   workingdir = tmpdir + "playing.tmp"
-  copyfiles sourcerpg, workingdir + SLASH
+  'Convert to lowercase while copying (only needed for ancient unlumped games)
+  copyfiles sourcerpg, workingdir, , YES
   usepreunlump = NO
  END IF
 END IF

common.rbas
@@ -1061,11 +1061,19 @@ FUNCTION readarchinym (gamedir as string, sourcefile as string) as string
  END IF
 
  ' for backwards compatibility with ancient games that lack archinym.lmp
- iname = LCASE(trimextension(trimpath(sourcefile)))
- 'IF isfile(gamedir + SLASH + iname + ".gen") = 0 THEN
- ' fatalerror "archinym.lmp unusable, and internal name could not be determined"
- 'ENDIF
- RETURN iname
+ 'iname = LCASE(trimextension(trimpath(sourcefile)))
+ 'IF isfile(gamedir + SLASH + iname + ".gen") THEN RETURN iname
+
+ ' Otherwise just scan the directory for a .GEN lump
+ ' (findfiles is case-insensitive)
+ DIM listing() as string
+ findfiles gamedir, "*.gen", , , listing()
+ IF UBOUND(listing) = 0 THEN
+  debuginfo "But found " & listing(0)
+  RETURN LCASE(trimextension(listing(0)))
+ END IF
+
+ fatalerror sourcefile & " doesn't appear to be an OHRRPGCE game: doesn't contain crucial data files"
 END FUNCTION
 
 FUNCTION maplumpname (byval map_id as integer, oldext as string) as string

custom.bas
@@ -309,6 +309,7 @@ setwindowtitle "O.H.R.RPG.C.E - " + trimpath(sourcerpg)
 
 '--set game according to the archinym
 copylump sourcerpg, "archinym.lmp", workingdir, YES
+copylump sourcerpg, "*.gen", workingdir, YES
 DIM archinym as string
 archinym = readarchinym(workingdir, sourcerpg)
 game = workingdir + SLASH + archinym
@@ -331,7 +332,8 @@ setvispage vpage, NO
 touchfile workingdir + SLASH + "__danger.tmp"
 IF isdir(sourcerpg) THEN
  'work on an unlumped RPG file. Don't take hidden files
- copyfiles sourcerpg, workingdir
+ 'Convert to lowercase while copying (only needed for ancient unlumped games)
+ copyfiles sourcerpg, workingdir, , YES
 ELSE
  unlump sourcerpg, workingdir + SLASH
 END IF

lumpfile.bas
@@ -1040,18 +1040,19 @@ function unlumpfile_internal (lumpfile as string, fmask as string, path as strin
 	return errmsg
 end function
 
+'lump may include * wildcases
 sub copylump(package as string, lump as string, dest as string, byval ignoremissing as integer = NO)
 	if len(dest) and right(dest, 1) <> SLASH then dest = dest + SLASH
 	if isdir(package) then
 		#ifdef DEBUG_FILE_IO
 			debuginfo "copylump " & lump & " in " & package & " -> " & dest
 		#endif
-
-		'unlumped folder
+		'unlumped folder. We downcase the lump name as we copy it
+		dim lumpfile as string = find_file_anycase(package + SLASH + lump)
 		if ignoremissing then
-			if not isfile(package + SLASH + lump) then exit sub
+			if not isfile(lumpfile) then exit sub
 		end if
-		writeablecopyfile package + SLASH + lump, dest + lump
+		writeablecopyfile lumpfile, dest + trimpath(lcase(lumpfile))
 	else
 		'lumpfile
 		'Don't show errors if we don't really care (actually this matters only in one place:

util.bas
@@ -2292,15 +2292,16 @@ FUNCTION writeablecopyfile(src as string, dest as string) as bool
 END FUNCTION
 
 'Copy files in one directory to another (ignores directories)
-SUB copyfiles(src as string, dest as string, byval copyhidden as integer = 0)
+SUB copyfiles(src as string, dest as string, copyhidden as bool = NO, lowercase as bool = NO)
  DIM filelist() as string
  findfiles src, ALLFILES, fileTypeFile, copyhidden, filelist()
  FOR i as integer = 0 TO UBOUND(filelist)
-  writeablecopyfile src + SLASH + filelist(i), dest + SLASH + filelist(i)
+  DIM destname as string = IIF(lowercase, LCASE(filelist(i)), filelist(i))
+  writeablecopyfile src + SLASH + filelist(i), dest + SLASH + destname
  NEXT
 END SUB
 
-FUNCTION copydirectory (src as string, dest as string, byval copyhidden as integer = -1) as string
+FUNCTION copydirectory (src as string, dest as string, byval copyhidden as bool = YES) as string
  'Recursively copy directory src to directory dest. Dest should not already exist
  'returns "" on success, or an error string on failure. Failure might leave behind a partial copy.
  IF isdir(dest) THEN RETURN "copydirectory: Destination """ & dest & """ must not already exist"

util.bi
@@ -326,8 +326,8 @@ declare sub findfiles (directory as string, namemask as string = "", filetype as
 declare function find_file_portably (path as string) as string
 declare function find_file_anycase (path as string, file_type as FileTypeEnum = fileTypeFile) as string
 declare function writeablecopyfile (src as string, dest as string) as bool
-declare sub copyfiles (src as string, dest as string, byval copyhidden as integer = 0)
-declare function copydirectory (src as string, dest as string, byval copyhidden as integer = -1) as string
+declare sub copyfiles (src as string, dest as string, copyhidden as bool = NO, lowercase as bool = NO)
+declare function copydirectory (src as string, dest as string, byval copyhidden as bool = YES) as string
 declare sub killdir (directory as string, recurse as bool = NO)
 declare function makedir (directory as string) as integer
 declare function safekill (filename as string) as bool

commit df7a00159b877832e61a97d798f377aa65638e14

I broke live previewing in r9662: wrong copy of .gen was used
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9664 7d344553-34f0-0310-a9b1-970ce8f1c3a2

game.bas
@@ -551,7 +551,11 @@ setvispage vpage, NO
 DIM archinym as string
 
 '--pre-extract (if needed) .gen and load it
-IF usepreunlump THEN
+IF running_as_slave THEN
+ 'Spawned from Custom, so must read everything from workingdir, not sourcerpg
+ archinym = readarchinym(workingdir, sourcerpg)
+ copylump workingdir, "*.gen", tmpdir, YES
+ELSEIF usepreunlump THEN
  archinym = readarchinym(workingdir, sourcerpg)
  copylump sourcerpg, "*.gen", tmpdir, YES
 ELSE
@@ -561,6 +565,7 @@ ELSE
  archinym = readarchinym(tmpdir, sourcerpg)
 END IF
 xbload tmpdir + archinym + ".gen", gen(), "general game data missing from " + sourcerpg
+killfile tmpdir + archinym + ".gen"  'So it doesn't override the copy in workingdir
 
 DIM forcerpgcopy as integer = NO
 IF gen(genVersion) > CURRENT_RPG_VERSION THEN