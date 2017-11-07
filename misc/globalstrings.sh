eaca90e0c89ddeccbc3ef6b23b714dccc37af103
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-11-07 15:39:24 +0000

    Add globalstrings.sh which finds readglobalstring calls inconsistent with the global string editor
    Interestingly there are quite a few, especially the names of stats
    
    And then if you create a new .rpg file and open the editor, you'll have a
    third set of strings to compare with, and will see even more inconsistencies!
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9683 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ misc/globalstrings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script finds all readglobalstring calls, and diffs them against the
+# definitions of the global strings in the Global Text Strings menu to find
+# inconsistencies or strings which aren't used.
+
+# All unique readglobalstring calls
+USES=$(grep -o "readglobalstring([^()]*)" *.*bas | perl -pe 's~.*readglobalstring\(([^)]*)\)~\1~' | sort -n -u)
+
+# Get list from Global Text Strings menu
+# This strips out the description of the global string, and trailing comments and whitespace
+DEFNS=$(grep -o "GTS_add_to_menu menu, [^']*" *.*bas | perl -e 's/^.*?".*?".*?, *//;' -pe 's/ +$//' | sort -n)
+
+#echo "$USES"
+#echo "$DEFNS"
+
+diff <( echo "$USES" ) <( echo "$DEFNS" )
