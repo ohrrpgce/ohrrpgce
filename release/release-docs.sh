8b59c7463f4dd79ffd3e76c165565c816991540a
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-12-03 23:40:46 +0000

    Fix paths in release-docs.sh (fix from etheldreme branch)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9855 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-docs.sh
+++ release/release-docs.sh
@@ -9,6 +9,6 @@ SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
 scp -p whatsnew.txt "${SCPDEST}"/
 
 docs/update-html.sh
-scp -p docs/plotdict.xml "${SCPDEST}"/ohrrpgce/docs/
-scp -p docs/htmlplot.xsl "${SCPDEST}"/ohrrpgce/docs/
-scp -p docs/plotdictionary.html "${SCPDEST}"/ohrrpgce/docs/
+scp -p docs/plotdict.xml "${SCPDEST}"/docs/
+scp -p docs/htmlplot.xsl "${SCPDEST}"/docs/
+scp -p docs/plotdictionary.html "${SCPDEST}"/docs/
