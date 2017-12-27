ohrrpgce/ohrrpgce

commit 65aa506fea41d324fca03f5c15295a90be81b18f

attackedit: I was crazy to litter the code with calls to update_attack_editor_for_fail_conds just to avoid calling it on state.need_update
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@8948 7d344553-34f0-0310-a9b1-970ce8f1c3a2

attackedit.bas
@@ -1177,7 +1177,6 @@ STATIC warned_old_fail_bit as bool = NO
 
 'load data here
 loadattackdata recbuf(), recindex
-update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
 state.need_update = YES
 
 'As a hack (I blame it on flexmenu itself which tries to be more "flexible" than possible),
@@ -1237,7 +1236,6 @@ DO
    ELSE
     loadattackdata recbuf(), recindex
    END IF
-   update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
 
    state.need_update = YES
   END IF
@@ -1250,7 +1248,6 @@ DO
    saveattackdata recbuf(), recindex
    SWAP rememberindex, recindex
    loadattackdata recbuf(), recindex
-   update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
    state.need_update = YES
    show_name_ticks = 23
   END IF
@@ -1396,14 +1393,12 @@ DO
     saveattackdata recbuf(), recindex
     recindex = attack_chain_browser(recindex)
     loadattackdata recbuf(), recindex
-    update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
     state.need_update = YES
    CASE AtkElementalFails TO AtkElementalFails + 63
     DIM cond as AttackElementCondition
     DeSerAttackElementCond cond, recbuf(), menuoff(workmenu(state.pt))
     percent_cond_editor cond, -1000.0, 1000.0, 4, "Fail", " damage" + menu(workmenu(state.pt))  'Fail when ... damage from <elem>
     SerAttackElementCond cond, recbuf(), menuoff(workmenu(state.pt))
-    update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
     state.need_update = YES
    CASE AtkWepHand0
     xy_position_on_slice weppreview, recbuf(AtkDatWepHand0X), recbuf(AtkDatWepHand0Y), "weapon handle position", "xy_weapon_handle"
@@ -1430,8 +1425,8 @@ DO
   IF helpkey = "attack_damage" THEN
    attack_editor_build_damage_menu recbuf(), menu(), menutype(), caption(), menucapoff(), workmenu(), state, dmgbit(), maskeddmgbit(), damagepreview
   END IF
-  'update_attack_editor_for_fail_conds should not be called here, but only when
-  'the record number changes! (Well, it could be called more often than that)
+  '--regenerate captions for fail conditions
+  update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
   '--in case new attacks have been added
   max(AtkLimChainTo) = gen(genMaxAttack) + 1
   '--in case chain mode has changed
@@ -1589,6 +1584,7 @@ SUB atk_edit_split_bitsets(recbuf() as integer, tempbuf() as integer)
   NEXT i
 END SUB
 
+'Regenerate captions for elemental failure conditions
 SUB update_attack_editor_for_fail_conds(recbuf() as integer, caption() as string, byval AtkCapFailConds as integer)
  DIM cond as AttackElementCondition
  FOR i as integer = 0 TO 63

enemyedit.bas
@@ -647,11 +647,10 @@ DO
      recindex -= 1
      enemy_edit_load recindex, recbuf(), state, caption(), EnCapElemResist
     END IF
-    update_enemy_editor_for_elementals recbuf(), caption(), EnCapElemResist
-    state.need_update = YES
    ELSE
     enemy_edit_load recindex, recbuf(), state, caption(), EnCapElemResist
    END IF
+   state.need_update = YES
   END IF
  END IF
 

commit 8efe66353cd973a5b83b399111d9687c9ab42dfc

Fix editing of attack elemental fail conditions, recently broken in r8948
Any edits to the condition thresholds in the percent_cond_editor submenu were
erased when you left the menu. Editing them out of the menu also suffered
brokenness, and the conditions got overwritten if one was the active menu item
when you switched between attacks.
r8948 was completely wrong. The captions are only meant to reset when you load
an attack.

percent_grabber now resets repr (the caption) itself if it detects it's incorrect
rather than overwriting data with garbage

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@9913 7d344553-34f0-0310-a9b1-970ce8f1c3a2

attackedit.bas
@@ -17,6 +17,7 @@
 
 '--Local SUBs
 DECLARE FUNCTION atk_edit_add_new(recbuf() as integer, preview_box as Slice Ptr) as bool
+DECLARE SUB atk_edit_load_attack(recbuf() as integer, attackid as integer, caption() as string, AtkCapFailConds as integer)
 DECLARE SUB atk_edit_merge_bitsets(recbuf() as integer, tempbuf() as integer)
 DECLARE SUB atk_edit_split_bitsets(recbuf() as integer, tempbuf() as integer)
 DECLARE SUB update_attack_editor_for_fail_conds(recbuf() as integer, caption() as string, byval AtkCapFailConds as integer)
@@ -1212,7 +1213,7 @@ ELSE
 END IF
 
 'load data here
-loadattackdata recbuf(), recindex
+atk_edit_load_attack recbuf(), recindex, caption(), AtkCapFailConds
 state.need_update = YES
 
 'As a hack (I blame it on flexmenu itself which tries to be more "flexible" than possible),
@@ -1264,15 +1265,15 @@ DO
     IF atk_edit_add_new(recbuf(), preview_box) THEN
      'Added a new record (blank or copy)
      saveattackdata recbuf(), recindex
+     'Reload, to set fail cond captions correctly
     ELSE
      'cancelled add, reload the old last record
      recindex -= 1
-     loadattackdata recbuf(), recindex
     END IF
    ELSE
-    loadattackdata recbuf(), recindex
+    'Load attack
    END IF
-
+   atk_edit_load_attack recbuf(), recindex, caption(), AtkCapFailConds
    state.need_update = YES
   END IF
  END IF
@@ -1283,7 +1284,7 @@ DO
   ELSEIF rememberindex >= 0 AND rememberindex <= gen(genMaxAttack) THEN
    saveattackdata recbuf(), recindex
    SWAP rememberindex, recindex
-   loadattackdata recbuf(), recindex
+   atk_edit_load_attack recbuf(), recindex, caption(), AtkCapFailConds
    state.need_update = YES
    show_name_ticks = 23
   END IF
@@ -1451,7 +1452,7 @@ DO
    CASE AtkChainBrowserAct
     saveattackdata recbuf(), recindex
     recindex = attack_chain_browser(recindex)
-    loadattackdata recbuf(), recindex
+    atk_edit_load_attack recbuf(), recindex, caption(), AtkCapFailConds
     state.need_update = YES
    CASE AtkElementalFails TO AtkElementalFails + maxElements - 1
     DIM cond as AttackElementCondition
@@ -1471,7 +1472,7 @@ DO
   END SELECT
  END IF
 
- IF keyval(scAlt) = 0 or isStringField(menutype(workmenu(state.pt))) THEN 'not pressing ALT, or not allowed to
+ IF keyval(scAlt) = 0 OR isStringField(menutype(workmenu(state.pt))) THEN 'not pressing ALT, or not allowed to
   IF editflexmenu(state, workmenu(state.pt), menutype(), menuoff(), menulimits(), recbuf(), caption(), min(), max()) THEN
    state.need_update = YES
   END IF
@@ -1480,7 +1481,7 @@ DO
  IF flexmenu_handle_crossrefs(state, workmenu(state.pt), menutype(), menuoff(), recindex, recbuf(), YES) THEN
   'Reload this attack in case it was changed in recursive call to the editor (in fact, this record might be deleted!)
   recindex = small(recindex, gen(genMaxAttack))
-  loadattackdata recbuf(), recindex
+  atk_edit_load_attack recbuf(), recindex, caption(), AtkCapFailConds
   show_name_ticks = 23
   state.need_update = YES
  END IF
@@ -1492,8 +1493,6 @@ DO
   IF helpkey = "attack_damage" THEN
    attack_editor_build_damage_menu recbuf(), menu(), menutype(), caption(), menucapoff(), workmenu(), state, dmgbit(), maskeddmgbit(), damagepreview
   END IF
-  '--regenerate captions for fail conditions
-  update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
   '--in case new attacks/enemies have been added
   max(AtkLimChainTo) = gen(genMaxAttack) + 1
   max(AtkLimTransmogEnemy) = gen(genMaxEnemy) + 1
@@ -1657,6 +1656,11 @@ SUB atk_edit_split_bitsets(recbuf() as integer, tempbuf() as integer)
   FOR i = 0 TO 7
     recbuf(AtkDatBitsets2 + i) = tempbuf(4 + i)
   NEXT i
+END SUB
+
+SUB atk_edit_load_attack(recbuf() as integer, attackid as integer, caption() as string, AtkCapFailConds as integer)
+ loadattackdata recbuf(), attackid
+ update_attack_editor_for_fail_conds recbuf(), caption(), AtkCapFailConds
 END SUB
 
 'Regenerate captions for elemental failure conditions

common.rbas
@@ -5076,6 +5076,14 @@ FUNCTION percent_grabber(byref float as double, byref repr as string, min as dou
  DIM oldfloat as double = float
  DIM oldrepr as string = repr
 
+ 'In case we forget to initialise repr (e.g. maybe we got called before a
+ ''need_update' block), we don't want to overwrite float with garbage.
+ 'So for safety if repr is bad, throw it away.
+ IF ABS(VAL(repr) - float * 100) > 0.5 * (0.1 ^ decimalplaces) THEN
+  repr = format_percent(float, decimalplaces)
+  'debug "percent_grabber: resetting " & oldrepr & " to " & repr
+ END IF
+
  'Remove negative (because we trim leading 0's later) and percentage signs
  repr = LEFT(repr, LEN(repr) - 1)
  DIM sign as integer = 1