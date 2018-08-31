4e8a0027e68e40c42f691307c84c011fe27355a7
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2018-08-31 05:57:38 +0000

    Import fb_thread.h from FB's rtlib (git commit 038c9f1e)
    Again, this is a header that hardly ever changes
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10555 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ fb/fb_thread.h
@@ -0,0 +1,50 @@
+typedef void (FBCALL *FB_THREADPROC)( void *param );
+
+struct _FBTHREAD;
+typedef struct _FBTHREAD FBTHREAD;
+
+struct _FBMUTEX;
+typedef struct _FBMUTEX FBMUTEX;
+
+struct _FBCOND;
+typedef struct _FBCOND FBCOND;
+
+FBCALL FBTHREAD         *fb_ThreadCreate( FB_THREADPROC proc, void *param, ssize_t stack_size );
+FBCALL void              fb_ThreadWait  ( FBTHREAD *thread );
+FBCALL void              fb_ThreadDetach( FBTHREAD *thread );
+
+       FBTHREAD         *fb_ThreadCall  ( void *proc, int abi, ssize_t stack_size, int num_args, ... );
+
+FBCALL FBMUTEX          *fb_MutexCreate ( void );
+FBCALL void              fb_MutexDestroy( FBMUTEX *mutex );
+FBCALL void              fb_MutexLock   ( FBMUTEX *mutex );
+FBCALL void              fb_MutexUnlock ( FBMUTEX *mutex );
+
+FBCALL FBCOND           *fb_CondCreate  ( void );
+FBCALL void              fb_CondDestroy ( FBCOND *cond );
+FBCALL void              fb_CondSignal  ( FBCOND *cond );
+FBCALL void              fb_CondBroadcast( FBCOND *cond );
+FBCALL void              fb_CondWait    ( FBCOND *cond, FBMUTEX *mutex );
+
+/**************************************************************************************************
+ * per-thread local storage context
+ **************************************************************************************************/
+
+enum {
+	FB_TLSKEY_ERROR,
+	FB_TLSKEY_DIR,
+	FB_TLSKEY_INPUT,
+	FB_TLSKEY_PRINTUSG,
+	FB_TLSKEY_GFX,
+	FB_TLSKEYS
+};
+
+FBCALL void             *fb_TlsGetCtx   ( int index, size_t len );
+FBCALL void              fb_TlsDelCtx   ( int index );
+FBCALL void              fb_TlsFreeCtxTb( void );
+#ifdef ENABLE_MT
+       void              fb_TlsInit     ( void );
+       void              fb_TlsExit     ( void );
+#endif
+
+#define FB_TLSGETCTX(id) ((FB_##id##CTX *)fb_TlsGetCtx( FB_TLSKEY_##id, sizeof( FB_##id##CTX ) ))
