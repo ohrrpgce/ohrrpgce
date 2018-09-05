9af7145438c905f908bdc57c5602180340cf6d36
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2018-09-05 22:51:02 +0000

    Rename several distrib scripts so that they all have consistent names
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10602 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib-nightly-win.bat
+++ distrib-nightly-win.bat
@@ -17,7 +17,7 @@ CALL scons hspeak relump unlump %SCONS_ARGS%
 REM This is the default build (default download is symlinked to it on the server)
 support\rm -f game.exe custom.exe
 call scons music=sdl %SCONS_ARGS%
-call nightly-gfx-music music_sdl gfx_directx.dll SDL.dll SDL_mixer.dll
+call distrib-nightly-win-packnupload music_sdl gfx_directx.dll SDL.dll SDL_mixer.dll
 
 ECHO Packaging ohrrpgce-win-installer.exe ...
 REM Create the installer from the executables we just built: the installer and .zips for default build configs
@@ -32,23 +32,23 @@ IF EXIST distrib\ohrrpgce-wip-win-installer.exe (
 
 support\rm -f game.exe custom.exe
 call scons music=native %SCONS_ARGS%
-call nightly-gfx-music music_native gfx_directx.dll SDL.dll audiere.dll
+call distrib-nightly-win-packnupload music_native gfx_directx.dll SDL.dll audiere.dll
 
 support\rm -f game.exe custom.exe
 call scons music=native2 %SCONS_ARGS%
-call nightly-gfx-music music_native2 gfx_directx.dll SDL.dll audiere.dll
+call distrib-nightly-win-packnupload music_native2 gfx_directx.dll SDL.dll audiere.dll
 
 support\rm -f game.exe custom.exe
 call scons music=silence %SCONS_ARGS%
-call nightly-gfx-music music_silence gfx_directx.dll SDL.dll
+call distrib-nightly-win-packnupload music_silence gfx_directx.dll SDL.dll
 
 REM support\rm -f game.exe custom.exe
 REM call scons gfx=alleg+directx+fb+sdl music=sdl %SCONS_ARGS%
-REM call nightly-gfx-music gfx_alleg-music_sdl alleg40.dll SDL.dll SDL_mixer.dll
+REM call distrib-nightly-win-packnupload gfx_alleg-music_sdl alleg40.dll SDL.dll SDL_mixer.dll
 
 support\rm -f game.exe custom.exe
 call scons music=sdl debug=2
-call nightly-gfx-music music_sdl-debug gfx_directx.dll SDL.dll SDL_mixer.dll misc\gdbcmds1.txt misc\gdbcmds2.txt gdbgame.bat gdbcustom.bat
+call distrib-nightly-win-packnupload music_sdl-debug gfx_directx.dll SDL.dll SDL_mixer.dll misc\gdbcmds1.txt misc\gdbcmds2.txt gdbgame.bat gdbcustom.bat
 
 REM Note that this is duplicated in distrib-nightly-linux.sh
 Echo upload plotdict.xml
