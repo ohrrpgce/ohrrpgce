ohrrpgce/ohrrpgce

commit 7ed0f9198b39b2b8878d820be22209aa37194f0d

When increasing the zoom without changing the window size, enforce a minimum resolution of 320x200
This was already apparently implemented, in gfx_sdl, but it didn't actually
work. E.g. if the resolution was 320x200 and you increased the zoom using
Custom's F9 menu, the window size wouldn't increase.

I fixed it and moved this logic into allmodex so it can be shared between
backends.

Note: there is still a bug in SDL/gfx_sdl where SDL (under X11) sometimes sends
a spurious SDL_VIDEORESIZE event causing the window to revert to its previous
size, screwing things up badly. Apparently because set_scale_factor causes
multiple SDL_SetVideoMode calls in a row

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10769 7d344553-34f0-0310-a9b1-970ce8f1c3a2

gfx_sdl.bas
@@ -782,9 +782,9 @@ SUB gfx_sdl_set_zoom(value as integer, change_windowsize as bool)
   IF value >= 1 AND value <= 16 AND value <> zoom THEN
     zoom = value
     zoom_has_been_changed = YES
+    debuginfo "set_zoom change_windowsize=" & change_windowsize & ", zoom=" & zoom & " old size = " & screensurface->w & "," & screensurface->h
     IF change_windowsize THEN
       gfx_sdl_recenter_window_hint()  'Recenter because the window might go off the screen edge.
-
 
       IF SDL_WasInit(SDL_INIT_VIDEO) THEN
         gfx_sdl_set_screen_mode()
@@ -800,15 +800,16 @@ SUB gfx_sdl_set_zoom(value as integer, change_windowsize as bool)
         END IF
       END WITH
     ELSE
-      'Keep window size the same, unless that would result in a tiny resolution
-      resize_request.w = large(320, screensurface->w \ zoom)
-      resize_request.h = large(200, screensurface->h \ zoom)
+      'Keep window size the same
+      resize_request.w = screensurface->w \ zoom
+      resize_request.h = screensurface->h \ zoom
       resize_requested = YES
     END IF
   END IF
 END SUB
 
 FUNCTION gfx_sdl_setoption(byval opt as zstring ptr, byval arg as zstring ptr) as integer
+  'debuginfo "gfx_sdl_setoption " & *opt & " " & *arg
   DIM ret as integer = 0
   DIM value as integer = str2int(*arg, -1)
   IF *opt = "zoomonly" THEN

allmodex.bas
@@ -865,6 +865,8 @@ sub set_resolution (w as integer, h as integer)
 	debuginfo "set_resolution " & w & "*" & h
 	windowsize.w = large(w, minwinsize.w)
 	windowsize.h = large(h, minwinsize.h)
+	'Ignore any pending resize request
+	gfx_get_resize(XY(0,0))
 	'Update page size
 	screen_size_update
 	'Tell the gfx backend about the new page size. If we delayed this then a following
@@ -893,10 +895,28 @@ end sub
 
 'Set the size that a pixel appears on the screen.
 'Supported by all backends except gfx_alleg.
+'If change_windowsize = YES, this changes the window size and keeps the same resolution,
+'otherwise it changes the resolution and keeps the same window size
 sub set_scale_factor (scale as integer, change_windowsize as bool = YES)
 	'gfx_sdl and gfx_fb, which use blit.c scaling, are limited to 1x-16x
 	scale = bound(scale, 1, 16)
 	debuginfo "Setting graphics scaling to x" & scale & " change_windowsize=" & change_windowsize
+	if change_windowsize = NO then
+		dim winstate as WindowState ptr = gfx_getwindowstate()
+		if winstate->structsize >= 8 THEN  'winstate->windowsize valid
+                        dim newresolution as XYPair = winstate->windowsize \ scale
+			debuginfo " ...current window size " & winstate->windowsize
+			if newresolution.w < 320 orelse newresolution.h < 200 then
+				debuginfo " ...too small, increasing res"
+				'Don't allow a tiny resolution: change both resolution and window size
+				newresolution.w = large(newresolution.w, 320)
+				newresolution.h = large(newresolution.h, 200)
+				set_resolution newresolution.w, newresolution.h
+				'TODO: set both resolution and scale at the same time
+				change_windowsize = YES
+			end if
+		end if
+	end if
 	if change_windowsize = NO then
 		' Only supported by gfx_sdl currently
 		if gfx_setoption("zoomonly", str(scale)) then exit sub

commit d061a42868bc0991679957eb7410f7ea79b88222

gfx_sdl: Fix crash when -z commandline arg used
Introduced in r10769, found by MirceaKitsune

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10834 7d344553-34f0-0310-a9b1-970ce8f1c3a2

gfx_sdl.bas
@@ -768,7 +768,13 @@ SUB gfx_sdl_set_zoom(value as integer, change_windowsize as bool)
   IF value >= 1 AND value <= 16 AND value <> zoom THEN
     zoom = value
     zoom_has_been_changed = YES
-    debuginfo "set_zoom change_windowsize=" & change_windowsize & ", zoom=" & zoom & " old size = " & screensurface->w & "," & screensurface->h
+    DIM temp as string
+    IF screensurface THEN temp = " old size = " & screensurface->w & "," & screensurface->h
+    debuginfo "set_zoom change_windowsize=" & change_windowsize & ", zoom=" & zoom & temp
+    IF change_windowsize = NO ANDALSO screensurface = NULL THEN
+      debug "...but window hasn't been created yet, doesn't make sense!"
+      change_windowsize = YES  'Avoid NULL dereference
+    END IF
     IF change_windowsize THEN
       gfx_sdl_recenter_window_hint()  'Recenter because the window might go off the screen edge.
 