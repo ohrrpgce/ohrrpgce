ohrrpgce/ohrrpgce

commit f0133d1b0f98613817e81306a8d1dce8f66da960

Add Ctrl-B all-bits debug editors to item and enemy editors
Added show_index and show_all/show_hidden args to editbitset, and a
edit_all_bits wrapper around editbitset (in lieu of creating a third overload).

I didn't add a Ctrl-B key to the hero editor because no extra bits are saved
in heroes.reld, so such a debug menu would be almost useless.

Also renamed "Healing poison causes regen and reverse" to
"Healing poison causes regen, and vice versa"

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10962 7d344553-34f0-0310-a9b1-970ce8f1c3a2

attackedit.bas
@@ -321,12 +321,12 @@ dmgbit(49) = "Ignore attacker's extra hits"
 dmgbit(51) = "Show damage without inflicting"
 dmgbit(57) = "Reset target stat to max before hit"
 dmgbit(58) = "Allow Cure to exceed maximum"
-dmgbit(60) = "##Damage MP (obsolete,unused)"
+dmgbit(60) = "##Damage MP (obsolete)"
 dmgbit(61) = "Do not randomize"
 dmgbit(62) = "Damage can be Zero"
 dmgbit(69) = "% based attacks damage instead of set"
 dmgbit(83) = "Don't allow damage to exceed target stat"
-dmgbit(88) = "Healing poison causes regen, and reverse"
+dmgbit(88) = "Healing poison causes regen, and vice versa"
 
 
 '--191 attack bits allowed in menu.
@@ -1377,14 +1377,11 @@ DO
   END IF
  END IF
 
- 'Debug key: edit all bitsets
+ 'Debug key CTRL-B: edit all bitsets
  IF keyval(scCtrl) > 0 AND keyval(scB) > 1 THEN
-  DIM allbits(-1 TO 128) as string
-  FOR i = 0 TO UBOUND(allbits)
-   allbits(i) = "  bit " & i
-  NEXT
+  DIM allbits(127) as string
   FOR i = 0 TO UBOUND(atkbit)
-   allbits(atkbit(i).i) = atkbit(i).s
+   IF atkbit(i).i >= 0 THEN allbits(atkbit(i).i) = atkbit(i).s
   NEXT
   FOR i = 0 TO UBOUND(dmgbit)
    IF LEN(dmgbit(i)) THEN allbits(i) = dmgbit(i)
@@ -1392,19 +1389,14 @@ DO
 
   'Obsolete bits
   FOR i = 0 TO 7
-   allbits(i + 5) = "##" & elementnames(i) & " Damage (obsolete,unused)" '05-12
-   allbits(i + 13) = "##Bonus vs " & readglobalstring(9 + i, "EnemyType (obsolete,unused)" & i+1) '13-20
-   allbits(i + 21) = "##Fail vs " & elementnames(i) & " resistance (obsolete,unused)" '21-28
-   allbits(i + 29) = "##Fail vs " & readglobalstring(9 + i, "EnemyType (obsolete,unused)" & i+1) '29-36
+   allbits(i + 5) = "##" & elementnames(i) & " Damage (obsolete)" '05-12
+   allbits(i + 13) = "##Bonus vs " & readglobalstring(9 + i, "EnemyType (obsolete)" & i+1) '13-20
+   allbits(i + 21) = "##Fail vs " & elementnames(i) & " resistance (obsolete)" '21-28
+   allbits(i + 29) = "##Fail vs " & readglobalstring(9 + i, "EnemyType" & i+1) & " (obsolete)" '29-36
   NEXT i
 
-  'Unhide hidden bits
-  FOR i = 0 TO UBOUND(allbits)
-   IF LEFT(allbits(i), 2) = "##" THEN allbits(i) = MID(allbits(i), 3)
-  NEXT
-
   atk_edit_merge_bitsets recbuf(), buffer()
-  editbitset buffer(), 0, allbits(), "attack_bitsets"
+  editbitset buffer(), 0, allbits(), "attack_bitsets", , , , , YES, YES  'show_index = show_all = YES
   atk_edit_split_bitsets recbuf(), buffer()
   state.need_update = YES
  END IF
@@ -1881,7 +1873,7 @@ SUB attack_editor_build_damage_menu(recbuf() as integer, menu() as string, menut
   END IF
 
   IF attack.targ_stat <> statRegen AND attack.targ_stat <> statPoison THEN
-    maskeddmgbit(88) = ""  'Healing poison causes regen and reverse
+    maskeddmgbit(88) = ""  'Healing poison causes regen, and vice versa
   END IF
 
   'If Damage Math is No Damage
@@ -2046,7 +2038,7 @@ SUB attack_editor_build_damage_menu(recbuf() as integer, menu() as string, menut
       maskeddmgbit(2)  = ""  'Absorb Damage
       maskeddmgbit(57) = ""  'Reset target stat to max before hit
       maskeddmgbit(58) = ""  'Allow Cure to exceed maximum
-      maskeddmgbit(88) = ""  'Healing poison causes regen and reverse
+      maskeddmgbit(88) = ""  'Healing poison causes regen, and vice versa
     END IF
 
     IF attack.show_damage_without_inflicting = NO AND setvalue = NO AND gen(genDamageCap) > 0 THEN
@@ -2114,7 +2106,7 @@ SUB attack_editor_build_damage_menu(recbuf() as integer, menu() as string, menut
       END IF
 
       IF attack.poison_is_negative_regen AND (attack.targ_stat = statPoison OR attack.targ_stat = statRegen) THEN
-        'Healing poison causes regen and reverse
+        'Healing poison causes regen, and vice versa
         DIM negatedstat as integer = IIF(attack.targ_stat = statPoison, statRegen, statPoison)
         DIM negatedstatname as string = caption(menucapoff(AtkTargStat) + negatedstat)
         preview += !"\nIf Target/Attacker " + targetstat + " > Max"

bmodsubs.bas
@@ -697,7 +697,7 @@ FUNCTION inflict (byref h as integer = 0, byref targstat as integer = 0, attacke
    END IF
 
    IF attack.poison_is_negative_regen AND (targstat = statPoison OR targstat = statRegen) THEN
-    'Healing poison causes regen and reverse
+    'Healing poison causes regen, and vice versa
     DIM negatedstat as integer = IIF(targstat = statPoison, statRegen, statPoison)
     ' For both of target.stat and attacker.stat:
     FOR itr as integer = 0 TO 1

common_menus.bas
@@ -56,26 +56,45 @@ npc_facetypes(2) = "Do Not Face Player"
 
 '==============================================================================
 
+'This is a debug menu, which shows all bits (including hidden and unused) and their
+'indices, in order of their index.  So it throws away the ordering in bitmenu(),
+'and acts like editbitset with show_index=YES, show_all=YES.
+'See editbitset for arg documentation.
+FUNCTION edit_all_bits (array() as integer, wof as integer, bitmenu() as IntStrPair, numbits as integer, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
+
+ REDIM fullbitmenu(numbits - 1) as IntStrPair
+ FOR i as integer = 0 TO numbits - 1
+  fullbitmenu(i).i = i
+ NEXT
+
+ FOR i as integer = 0 TO UBOUND(bitmenu)
+  IF bitmenu(i).i >= 0 THEN
+   fullbitmenu(bitmenu(i).i).s = bitmenu(i).s
+  END IF
+ NEXT
+
+ RETURN editbitset(array(), wof, fullbitmenu(), helpkey, remem_pt, immediate_quit, title, prevmenu, YES, YES)
+END FUNCTION
+
 'Edit array of bits. The bits don't have to be consecutive, but they do have to be in ascending order.
-'The bits corresponding to any blank entries in names(), or starting with '##' are hidden/skipped over.
+'The bits corresponding to any blank entries in names(), or starting with '##' are hidden/skipped over,
+'unless show_all is true.
 'If a bit name starts with ! then the diplayed value of the bit is reversed.
-'remem_pt is used to store the selected bit (index in names())
+'remem_pt is used to store the selected bit.
 'If immediate_quit is true, then toggling a bit causes the menu to quit immediately (and return edbitPickedBit).
 'Return value:
 ' edbitCancel    if user pressed ESC
 ' edbitBack      if used selected prevmenu (which is useful if prevmenu is something like "Done")
 ' edbitPickedBit if immediate_quit=YES and the user toggled a bit
-FUNCTION editbitset (array() as integer, wof as integer, names() as string, helpkey as string="editbitset", byref remem_bitnum as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
+FUNCTION editbitset (array() as integer, wof as integer, names() as string, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu", show_index as bool = NO, show_all as bool = NO) as EditBitsetResult
 
- DIM remem_pt as integer = -1  'Index in bitmenu()
- DIM bitmenu(UBOUND(names)) as IntStrPair
+ REDIM bitmenu(UBOUND(names)) as IntStrPair
 
  DIM nextbit as integer = 0
  FOR i as integer = 0 TO UBOUND(names)
-  IF names(i) <> "" ANDALSO LEFT(names(i), 2) <> "##" THEN
+  IF show_all ORELSE LEN(names(i)) THEN
    bitmenu(nextbit).s = names(i)
    bitmenu(nextbit).i = i
-   IF remem_bitnum = i THEN remem_pt = nextbit
    nextbit += 1
   END IF
  NEXT
@@ -85,27 +104,29 @@ FUNCTION editbitset (array() as integer, wof as integer, names() as string, help
   ERASE bitmenu
  END IF
 
- DIM ret as EditBitsetResult
- ret = editbitset(array(), wof, bitmenu(), helpkey, remem_pt, immediate_quit, title, prevmenu)
- IF remem_pt = -1 THEN
-  remem_bitnum = -1
- ELSE
-  remem_bitnum = bitmenu(remem_pt).i
- END IF
- RETURN ret
+ RETURN editbitset(array(), wof, bitmenu(), helpkey, remem_pt, immediate_quit, title, prevmenu, show_index, show_all)
 END FUNCTION
 
 'See above for documentation.
 'This overload takes an array of bits to edit which allows bits be out of order,
 'and to include unselectable section headings.
-'The .i member of bitmenu() is the bit number, which is -1 for unselectable menu items.
-'This overload doesn't hide bits with blank names or ## prefix.
-FUNCTION editbitset (array() as integer, wof as integer, bitmenu() as IntStrPair, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
+'The .i member of bits() is the bit number, which is -1 for unselectable menu items.
+'This overload doesn't hide bits with blank names.  Bits with ## prefix are hidden
+'unless show_hidden=YES.  If you want to see unused bits (like show_all=YES for the
+'other editbitset overload), use edit_all_bits instead.
+FUNCTION editbitset (array() as integer, wof as integer, bits() as IntStrPair, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu", show_index as bool = NO, show_hidden as bool = NO) as EditBitsetResult
 
- DIM selectable(-1 TO UBOUND(bitmenu)) as bool
+ DIM selectable(-1 TO UBOUND(bits)) as bool  'Oversized if there are any hidden bits
  selectable(-1) = YES
- FOR idx as integer = 0 TO UBOUND(bitmenu)
-  selectable(idx) = (bitmenu(idx).i >= 0)
+
+ 'Build bitmenu(), a copy of bits(), with hidden items removed
+ '(Note that bitmenu(-1) for prevmenu doesn't exist)
+ DIM bitmenu() as IntStrPair
+ FOR idx as integer = 0 TO UBOUND(bits)
+  IF show_hidden ORELSE LEFT(bits(idx).s, 2) <> "##" THEN
+   a_append bitmenu(), bits(idx).i, LTRIM(bits(idx).s, "#")
+   selectable(UBOUND(bitmenu)) = (bits(idx).i >= 0)
+  END IF
  NEXT
 
  DIM menupos as XYPair
@@ -164,10 +185,11 @@ FUNCTION editbitset (array() as integer, wof as integer, bitmenu() as IntStrPair
    DIM text as string = IIF(i = -1, prevmenu, bitmenu(i).s)
    IF i >= 0 ANDALSO selectable(i) THEN
     biton = readbit(array(), wof, bitmenu(i).i)
-    IF text[0] = ASC("!") THEN
+    IF LEFT(text, 1) = "!" THEN  'Inverted display
      biton XOR= 1
      text = MID(text, 2)
     END IF
+    IF show_index THEN text = strprintf("%2d ", bitmenu(i).i) & text
     ellipse vpages(dpage), menupos.x + 4, drawat.y + 3, 3, uilook(uiDisabledItem), IIF(biton, uilook(uiSelectedItem), -1)
    ELSE
     biton = 1  'Don't show as disabled

common_menus.bi
@@ -16,8 +16,9 @@ ENUM 'EditBitsetResult
 END ENUM
 TYPE EditBitsetResult as integer
 
-DECLARE FUNCTION editbitset overload (array() as integer, wof as integer, names() as string, helpkey as string="editbitset", byref remem_bitnum as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
-DECLARE FUNCTION editbitset overload (array() as integer, wof as integer, bitmenu() as IntStrPair, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
+DECLARE FUNCTION edit_all_bits (array() as integer, wof as integer, bitmenu() as IntStrPair, numbits as integer, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
+DECLARE FUNCTION editbitset overload (array() as integer, wof as integer, names() as string, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu", show_all as bool = NO, show_index as bool = NO) as EditBitsetResult
+DECLARE FUNCTION editbitset overload (array() as integer, wof as integer, bitmenu() as IntStrPair, helpkey as string="editbitset", byref remem_pt as integer = -1, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu", show_index as bool = NO, show_hidden as bool = NO) as EditBitsetResult
 DECLARE FUNCTION editbools (bools() as bool, names() as string, helpkey as string = "editbitset", byref remem_pt as integer = -2, immediate_quit as bool = NO, title as string = "", prevmenu as string="Previous Menu") as EditBitsetResult
 
 DECLARE SUB edit_general_bitsets()

enemyedit.bas
@@ -89,6 +89,15 @@ a_append enemybits(), -1, ""
 a_append enemybits(), -1, " Other"
 a_append enemybits(), 54, "Harmed by cure"
 
+a_append enemybits(), -1, "##"
+a_append enemybits(), -1, "## Obsolete"
+FOR i as integer = 0 TO 7
+ a_append enemybits(), i + 0,  "##Weak against " & elementnames(i) & " (obsolete)"
+ a_append enemybits(), i + 8,  "##Strong against " & elementnames(i) & " (obsolete)"
+ a_append enemybits(), i + 16, "##Absorbs " & elementnames(i) & " (obsolete)"
+ a_append enemybits(), i + 24, "##Enemy type " & readglobalstring(9 + i, "EnemyType" & i+1) & " (obsolete)"
+NEXT
+
 
 '-------------------------------------------------------------------------
 
@@ -840,6 +849,11 @@ DO
   IF editflexmenu(state, workmenu(state.pt), menutype(), menuoff(), menulimits(), recbuf(), caption(), min(), max()) THEN
    state.need_update = YES
   END IF
+ END IF
+
+ 'Debug key CTRL-B: edit all bitsets
+ IF keyval(scCtrl) > 0 AND keyval(scB) > 1 THEN
+  edit_all_bits recbuf(), EnDatBitset, enemybits(), 80, "enemy_bitsets", remember_bit
  END IF
 
  IF setup_preview_dissolve THEN

heroedit.rbas
@@ -117,8 +117,13 @@ FUNCTION hero_editor (hero_id as integer) as integer
   .last = UBOUND(menu)
  END WITH
 
- DIM hbit(-1 TO 26) as string
- 'Lots of obsolete elemental bits
+ DIM elementnames() as string
+ getelementnames elementnames()
+
+ DIM hbit(47) as string
+ 'Bits 0-23 are obsolete elemental strong/weak/absorb bits. They aren't saved in
+ 'heroes.reld, but might be set if we loaded from .dt0 instead of heroes.reld
+ '(happens first time we edit the hero only).
  hbit(24) = "Rename when added to party"
  hbit(25) = "Permit renaming on status screen"
  hbit(26) = "Do not show spell lists if empty"

itemedit.bas
@@ -129,6 +129,17 @@ FUNCTION individual_item_editor(item_id as integer) as integer
  iidx(15) = 52 'weapon pic
  iidx(16) = 53 'weapon pal
 
+ DIM elementnames() as string
+ getelementnames elementnames()
+
+ DIM bitnames(0 TO 47) as string
+ FOR i as integer = 0 TO 7
+  'Lots of obsolete elemental bits
+  bitnames(i) = "##Weak against " & elementnames(i) & " (obsolete)"
+  bitnames(i + 8) = "##Strong against " & elementnames(i) & " (obsolete)"
+  bitnames(i + 16) = "##Absorbs " & elementnames(i) & " (obsolete)"
+ NEXT
+
  DIM selectst as SelectTypeState
  DIM enable_strgrabber as bool
  DIM state as MenuState
@@ -272,6 +283,11 @@ FUNCTION individual_item_editor(item_id as integer) as integer
      END WITH
     END IF
   END SELECT
+  IF keyval(scCtrl) > 0 ANDALSO keyval(scB) > 1 THEN  'Ctrl+B debug key: edit all bits... except equipability
+   editbitset itembuf(), 70, bitnames(), , , , , , YES, YES 'show_index = show_all = YES
+   state.need_update = YES
+  END IF
+
   IF state.need_update THEN
    state.need_update = NO
    write_item_strings itembuf(), item_name, info

commit f930c06cb6419522656d98d45e5cb78c5b326218

Fix another crash when numelements < 8, in Ctrl-B all-bits debug menu in attackedit
Caused by r10962, the is the third and final case of this bug.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11184 7d344553-34f0-0310-a9b1-970ce8f1c3a2

attackedit.bas
@@ -1389,9 +1389,11 @@ DO
 
   'Obsolete bits
   FOR i = 0 TO 7
-   allbits(i + 5) = "##" & elementnames(i) & " Damage (obsolete)" '05-12
+   DIM elname as string
+   elname = IIF(i <= UBOUND(elementnames), elementnames(i), "element" & i)
+   allbits(i + 5) = "##" & elname & " Damage (obsolete)" '05-12
    allbits(i + 13) = "##Bonus vs " & readglobalstring(9 + i, "EnemyType (obsolete)" & i+1) '13-20
-   allbits(i + 21) = "##Fail vs " & elementnames(i) & " resistance (obsolete)" '21-28
+   allbits(i + 21) = "##Fail vs " & elname & " resistance (obsolete)" '21-28
    allbits(i + 29) = "##Fail vs " & readglobalstring(9 + i, "EnemyType" & i+1) & " (obsolete)" '29-36
   NEXT i
 