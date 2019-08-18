ohrrpgce/ohrrpgce

commit b07675ebd1b5302b8c14f669095f3189ec4e928b

Maintain a log of most recently opened files: log_openfile is called from OPENFILE and elsewhere
Includes any case in which data might be read from a file, including playing
audio, but excludes a few file writes, and copying, moving, renaming or deleting
files.

Also, inworkingdir wasn't declared consistently on C and FB sides (missing
'cdecl')

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11236 7d344553-34f0-0310-a9b1-970ce8f1c3a2

lumpfile.bas
@@ -36,7 +36,11 @@ LMPVTAB(LT_FILE,   FileLump_,    QLMP(destruct),       QLMP(open), QLMP(close), 
  #include "common.bi"  'For running_as_slave
 #endif
 
-DIM can_write_to_workingdir as bool = YES
+dim can_write_to_workingdir as bool = YES
+
+'Log of recently opened files, and when they were opened (TIMER value)
+dim shared recent_files(5) as string
+dim shared recent_file_times(5) as double
 
 
 '----------------------------------------------------------------------
@@ -470,6 +474,7 @@ sub FileLump_open(byref this as FileLump)
 	if this.fhandle = 0 then
 		dim fname as string = this.filename
 		if fname = "" then fname = this.index->unlumpeddir + this.lumpname
+		log_openfile fname
 		this.fhandle = freefile
 		open fname for binary as #this.fhandle
 	end if
@@ -593,6 +598,7 @@ function indexlumpfile (lumpfile as string, byval keepopen as bool = YES) as Lum
 	index = callocate(sizeof(LumpIndex))
 	construct_LumpIndex(*index)
 
+	log_openfile lumpfile
 	lf = freefile
 	open lumpfile for binary access read lock write as #lf
 	if keepopen then index->fhandle = lf
@@ -1424,7 +1430,7 @@ end sub
 'Returns whether the file is a normal lump file in workingdir.
 'This is used to determine whether the file should be hooked.
 'writable: whether attempting to *explicitly* open with write access
-function inworkingdir(filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
+function inworkingdir cdecl (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 	if RIGHT(filename, 10) = "_debug.txt" then return FilterActionEnum.dont_hook
 	'if RIGHT(filename, 12) = "_archive.txt" then return FilterActionEnum.dont_hook
 	'Uncomment this for OPEN tracing (or you could just use strace...)
@@ -1476,3 +1482,34 @@ function channel_wait_for_msg(byref channel as IPCChannel, wait_for_prefix as st
 		debug "warning: channel_wait_for_msg discarding message: " & line_in
 	loop
 end function
+
+extern "C"
+
+'Register a file as being opened, updating the recently opened files log,
+'which can be used for debugging.
+'This is called by OPENFILE on a successful open. Call it manually if not usng
+'OPENFILE (e.g. using an external library to read/process a file).
+'There are a few places where we OPEN FOR OUTPUT/APPEND (not OPENFILE) or export
+'something (e.g. png/gif) that aren't hooked, including debug log writes, but
+'I don't care much about file writes.
+'Also, not called when copying, moving, renaming or deleting a file.
+sub log_openfile(filename as zstring ptr)
+	dim oldest as double = 1e99
+	dim oldest_idx as integer
+
+	for idx as integer = 0 to ubound(recent_files)
+		if recent_files(idx) = *filename then
+			recent_file_times(idx) = TIMER
+			exit sub
+		end if
+		if recent_file_times(idx) < oldest then
+			oldest = recent_file_times(idx)
+			oldest_idx = idx
+		end if
+	next
+
+	recent_files(oldest_idx) = *filename
+	recent_file_times(oldest_idx) = TIMER
+end sub
+
+end extern

lumpfile.bi
@@ -255,8 +255,7 @@ Enum FilterActionEnum Explicit
 	hide = 4      ' Don't open the file, return file not found
 End Enum
 
-type FnStringPredicate as function (filename as string) as boolint
-type FnOpenCallback as function (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
+type FnOpenCallback as function cdecl (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 
 extern "C"
 
@@ -285,12 +284,14 @@ declare sub set_OPEN_hook(lumpfile_filter as FnOpenCallback, lump_writes_allowed
 declare sub clear_OPEN_hook()
 declare function get_filename(fnum as integer) as string
 
+declare sub log_openfile(filename as zstring ptr)
+
 end extern
 
 'This is always true except in Game when live previewing (running off Custom's workingdir)
 extern can_write_to_workingdir as bool
 
-declare function inworkingdir(filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
+declare function inworkingdir cdecl (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 declare function hook_all_files(filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 
 declare function channel_wait_for_msg(byref channel as IPCChannel, wait_for_prefix as string, line_in as string = "", timeout_ms as integer = 500) as integer

allmodex.bas
@@ -6781,6 +6781,7 @@ function frame_import_paletted_png(filename as string, pal() as RGBcolor) as Fra
 
 	dim filebuf as byte ptr
 	dim filebufsize as size_t
+	log_openfile filename
 	if PNGCHKERR(lodepng_load_file(@filebuf, @filebufsize, strptr(filename))) then
 		deallocate filebuf
 		return NULL
@@ -6843,6 +6844,7 @@ function surface_import_png(filename as string, always_32bit as bool) as Surface
 	if iminfo.paletted = NO orelse always_32bit then
 		dim buf as byte ptr
 		dim size as XYPair
+		log_openfile filename
 		if PNGCHKERR(lodepng_decode_file(@buf, @size.w, @size.h, strptr(filename), LCT_RGB, 8)) then
 			return NULL
 		end if
@@ -6974,6 +6976,7 @@ sub jpeginfo (filename as string, byref iminfo as ImageFileInfo)
 	dim jpeg as ujImage
 	jpeg = ujCreate()
 	ujDisableDecoding(jpeg)
+	log_openfile filename
 	ujDecodeFile(jpeg, strptr(filename))
 	dim errcode as integer = ujGetError()
 
@@ -7036,6 +7039,7 @@ end function
 function surface_import_jpeg(filename as string) as Surface ptr
 	dim jpeg as ujImage
 	jpeg = ujCreate()
+	log_openfile filename
 	ujDecodeFile(jpeg, strptr(filename))
 
 	if ujGetError() = UJ_PROGRESSIVE then

audiofile.bas
@@ -202,6 +202,7 @@ function read_ogg_metadata(songfile as string) as string
 '#ifdef HAVE_VORBISFILE
 	dim oggfile as OggVorbis_File
 	dim errcode as integer
+	log_openfile songfile
 	errcode = ov_fopen(songfile, @oggfile)
 	if errcode then
 		dim msg as string
@@ -425,25 +426,26 @@ function find_music_lump(songnum as integer) as string
   songbase = workingdir & SLASH & "song" & songnum
   songfile = ""
 
-  IF isfile(songbase & ".mp3") THEN
+  IF real_isfile(songbase & ".mp3") THEN
     songfile = songbase & ".mp3"
-  ELSEIF isfile(songbase & ".ogg") THEN
+  ELSEIF real_isfile(songbase & ".ogg") THEN
     songfile = songbase & ".ogg"
-  ELSEIF isfile(songbase & ".mod") THEN
+  ELSEIF real_isfile(songbase & ".mod") THEN
     songfile = songbase & ".mod"
-  ELSEIF isfile(songbase & ".xm") THEN
+  ELSEIF real_isfile(songbase & ".xm") THEN
     songfile = songbase & ".xm"
-  ELSEIF isfile(songbase & ".s3m") THEN
+  ELSEIF real_isfile(songbase & ".s3m") THEN
     songfile = songbase & ".s3m"
-  ELSEIF isfile(songbase & ".it") THEN
+  ELSEIF real_isfile(songbase & ".it") THEN
     songfile = songbase & ".it"
-  ELSEIF isfile(songbase & ".mid") THEN
+  ELSEIF real_isfile(songbase & ".mid") THEN
     songfile = songbase & ".mid"
-  ELSEIF isfile(songbase & ".bam") THEN
+  ELSEIF real_isfile(songbase & ".bam") THEN
     songfile = songbase & ".bam"
-  ELSEIF isfile(game & "." & songnum) THEN
+  ELSEIF real_isfile(game & "." & songnum) THEN
     songfile = game & "." & songnum ' old-style BAM naming scheme
   END IF
+  'Can import wav (converting to ogg), but don't need to look for it
   RETURN songfile
 END FUNCTION
 
@@ -452,11 +454,11 @@ function find_sfx_lump (sfxnum as integer) as string
 	dim as string sfxbase
 
 	sfxbase = workingdir & SLASH & "sfx" & sfxnum
-	if isfile(sfxbase & ".ogg") THEN
+	if real_isfile(sfxbase & ".ogg") THEN
 		return sfxbase & ".ogg"
-	elseif isfile(sfxbase & ".mp3") then
+	elseif real_isfile(sfxbase & ".mp3") then
 		return sfxbase & ".mp3"
-	elseif isfile(sfxbase & ".wav") then
+	elseif real_isfile(sfxbase & ".wav") then
 		return sfxbase & ".wav"
 	else
 		return ""

common.rbas
@@ -333,6 +333,7 @@ END SUB
 
 ' Append logfile to archivefile and delete it.
 PRIVATE SUB archive_log_file(logfile as string, archivefile as string)
+ '(Don't call log_openfile)
  DIM dlog as integer = FREEFILE
  OPEN logfile FOR BINARY as dlog
  DIM archive as integer = FREEFILE

filelayer.cpp
@@ -12,6 +12,7 @@
 #include "misc.h"
 #include "errno.h"
 #include "mutex.hpp"
+#include "lumpfile.h"
 
 // This array stores information about any open file that was opened using OPENFILE.
 // Indexed by index into FB's __fb_ctx.fileTB, NOT by file number as returned by FREEFILE.
@@ -356,6 +357,9 @@ FB_RTERROR OPENFILE(FBSTRING *filename, enum OPENBits openbits, int &fnum) {
 		infop = NULL;
 		openfiles_mutex.unlock();
 	} else {
+		// Add to log of recently files
+		log_openfile(cfilename);
+
 		// HACK: hook CLOSE for all files, by permanently modifying FB's internal
 		// file hooks tables, so that we can be sure that FileInfo will get deleted.
 		// This will affect files opened with OPEN instead of OPENFILE too!

lumpfile.h
@@ -13,6 +13,8 @@ void FileWrapper_close(FileWrapper*);
 int FileWrapper_seek(FileWrapper*, int offset, int whence);
 int FileWrapper_read(FileWrapper*, void* buffer, int size, int maxnum);
 
+void log_openfile(const char *filename);
+
 #ifdef __cplusplus
 }
 #endif

music_allegro.bas
@@ -121,7 +121,7 @@ sub music_play(filename as string, fmt as MusicFormatEnum)
 			flen = flen and &h0fff
 			midname = tmpdir & trimpath(songname) & "-" & lcase(hex(flen)) & ".bmd"
 			'check if already converted
-			if isfile(midname) = 0 then
+			if isfile(midname) = NO then
 				bam2mid(songname, midname)
 				'add to list of temp files
 				dim ditem as delitem ptr
@@ -152,6 +152,7 @@ sub music_play(filename as string, fmt as MusicFormatEnum)
 			music_paused = 0
 		end if
 
+		log_openfile(songname)
 		music_song = load_midi(songname)
 		if music_song = 0 then
 			debug "Could not load song " + songname
@@ -296,6 +297,7 @@ function sound_load(slot as integer, f as string, num as integer) as integer
     
     .used = YES
     .effectID = num
+    log_openfile(f)
     .buf = load_wav(f)
     
     if .buf = NULL then

commit 4ccbd37de60de7660ff5c73bc203a2b039af4c67

Fix stack corruption crash in OPENFILE in Windows, introduced in r11236
Caused by me foolishly changing the call signature of inworkingdir (called by
OPENFILE) to CDECL. But strangly it didn't crash until you used Test
Game. Caused both Game and Custom to crash! Custom threw
 (BUG) ERROR: OPENFILE: ENCODING not implemented for hooked files
and then either made it to crashrpt or crashed inside loadpalette, while Game
just crashed in OPENFILE

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11269 7d344553-34f0-0310-a9b1-970ce8f1c3a2

filelayer.hpp
@@ -63,7 +63,6 @@ struct FileInfo {
 extern "C" {
 #endif
 
-	typedef FBCALL boolint (*FnStringPredicate)(FBSTRING *filename);
 	typedef FBCALL enum FilterActionEnum (*FnOpenCallback)(FBSTRING *filename, boolint writable, boolint writes_allowed);
 
 	void send_lump_modified_msg(const char *filename);

lumpfile.bas
@@ -1433,7 +1433,7 @@ end sub
 'writable: whether attempting to *explicitly* open with write access
 '    (false for generic OPENFILE FOR_BINARY calls, which allows reading+writing)
 'writes_allowed: arg passed to set_OPEN_hook: true in Custom, false in Game.
-function inworkingdir cdecl (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
+function inworkingdir (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 	if RIGHT(filename, 10) = "_debug.txt" then return FilterActionEnum.dont_hook
 	'if RIGHT(filename, 12) = "_archive.txt" then return FilterActionEnum.dont_hook
 	'Uncomment this for OPEN tracing (or you could just use strace...)

lumpfile.bi
@@ -255,7 +255,7 @@ Enum FilterActionEnum Explicit
 	hide = 4      ' Don't open the file, return file not found
 End Enum
 
-type FnOpenCallback as function cdecl (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
+type FnOpenCallback as function (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 
 extern "C"
 
@@ -292,7 +292,7 @@ end extern
 'This is always true except in Game when live previewing (running off Custom's workingdir)
 extern can_write_to_workingdir as bool
 
-declare function inworkingdir cdecl (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
+declare function inworkingdir (filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 declare function hook_all_files(filename as string, writable as boolint, writes_allowed as boolint) as FilterActionEnum
 
 declare function channel_wait_for_msg(byref channel as IPCChannel, wait_for_prefix as string, line_in as string = "", timeout_ms as integer = 500) as integer