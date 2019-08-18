ohrrpgce/ohrrpgce

commit 683ef3f7d5a341a9caca8d1d73f1743b2d801705

Don't require psapi.dll on Windows, which doesn't exist on Win98
Instead, it's dynamically loaded. If it's missing, have to be more conservative
about determining whether an old copy of Custom.exe is still running.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10950 7d344553-34f0-0310-a9b1-970ce8f1c3a2

os_windows.bas
@@ -65,23 +65,19 @@ dim shared main_thread_id as integer
 
 '''''' Extra winapi defines
 
-' Missing from FB 0.23 headers
-#ifndef GetProcessImageFileName
-	extern "Windows"
-	#ifdef UNICODE
-		declare function GetProcessImageFileName alias "GetProcessImageFileNameW" (byval hProcess as HANDLE, byval lpImageFileName as LPWSTR, byval nSize as DWORD) as DWORD
-	#else
-		declare function GetProcessImageFileName alias "GetProcessImageFileNameA" (byval hProcess as HANDLE, byval lpImageFileName as LPSTR, byval nSize as DWORD) as DWORD
-	#endif
-	end extern
-#endif
-
 'We #undef'd copyfile
 #ifdef UNICODE
 	declare function CopyFile_ alias "CopyFileW" (byval as LPCWSTR, byval as LPCWSTR, byval as BOOL) as BOOL
 #else
 	declare function CopyFile_ alias "CopyFileA" (byval as LPCSTR, byval as LPCSTR, byval as BOOL) as BOOL
 #endif
+
+extern "C"
+#undef GetProcessMemoryInfo
+dim shared GetProcessMemoryInfo as function (byval Process as HANDLE, byval ppsmemCounters as PPROCESS_MEMORY_COUNTERS, byval cb as DWORD) as WINBOOL
+#undef GetProcessImageFileNameA
+dim shared GetProcessImageFileNameA as function (byval hProcess as HANDLE, byval lpImageFileName as LPSTR, byval nSize as DWORD) as DWORD
+end extern
 
 
 '==========================================================================================
@@ -166,6 +162,14 @@ extern "C"
 
 sub os_init ()
 	main_thread_id = GetCurrentThreadId()
+
+	' psapi.dll is not present on older Windows versions (added between 98 and XP),
+	' but we can do without it.
+	dim psapi as any ptr = dylibload("psapi")
+	if psapi then
+		GetProcessMemoryInfo = dylibsymbol(psapi, "GetProcessMemoryInfo")
+		GetProcessImageFileNameA = dylibsymbol(psapi, "GetProcessImageFileNameA")
+	end if
 end sub
 
 'Currently Android only
@@ -174,6 +178,7 @@ end sub
 
 #macro GET_MEMORY_INFO(memctrs, on_error)
 	' This requires psapi.dll
+	if GetProcessMemoryInfo = NULL then return on_error
 	if GetProcessMemoryInfo(GetCurrentProcess(), @memctrs, sizeof(memctrs)) = 0 then
 		debug "GetProcessMemoryInfo failed: " & error_string
 		return on_error
@@ -192,10 +197,6 @@ end function
 
 function memory_usage_string() as string
 	dim memctrs as PROCESS_MEMORY_COUNTERS
-	if GetProcessMemoryInfo(GetCurrentProcess(), @memctrs, sizeof(memctrs)) = 0 then
-		debug "GetProcessMemoryInfo failed: " & error_string
-		return ""
-	end if
 	GET_MEMORY_INFO(memctrs, "")
 	return "workingset=" & memctrs.WorkingSetSize & " peak workingset=" & memctrs.PeakWorkingSetSize _
 	       & " commit=" & memctrs.PagefileUsage & " peak commit=" & memctrs.PeakPagefileUsage _
@@ -1011,7 +1012,7 @@ end function
 
 'Returns full path to a process given its PID in device form, e.g.
 '\Device\HarddiskVolume1\OHRRPGCE\custom.exe
-'or "" if it doesn't exist or we don't have permission.
+'or "" if it doesn't exist, or "<unknown>" if it can't be determined (e.g. we don't have permission, or running Win98).
 'This function is used primarily to determine whether a process is still running
 function get_process_path (pid as integer) as string
 	dim proc as HANDLE
@@ -1021,17 +1022,18 @@ function get_process_path (pid as integer) as string
 		' OpenProcess sets "Invalid parameter" error if the pid doesn't exist
 		if errcode <> ERROR_INVALID_PARAMETER then
 			debug "get_process_path: OpenProcess(pid=" & pid & ") err " & errcode & " " & get_windows_error(errcode)
+			return "<unknown>"
 		end if
 		return ""
 	end if
 	dim ret as zstring * 256
 	'QueryFullProcessImageName, which returns a normal filename instead of device form, is Win Vista+.
-	if GetProcessImageFileName(proc, ret, 256) = 0 then
+	'GetModuleFileNameExA doesn't return a normal filename either.
+	if GetProcessImageFileNameA andalso GetProcessImageFileNameA(proc, ret, 256) = 0 then
 		dim errcode as integer = GetLastError()
 		debug "get_process_path: GetProcessImageFileName err " & errcode & " " & get_windows_error(errcode)
-	end if
-
-	if len(ret) then
+		ret = "<unknown>"
+	elseif len(ret) then
 		'If a process crashes or is killed (but not if it closes normally), Windows apparently keeps some
 		'information about the dead PID and continues to return its image path.
 		'So check the exitcode to prevent misidentifying a PID as still running.

SConscript
@@ -828,7 +828,7 @@ if win32:
     # winmm needed for MIDI, used by music backends but also by miditest
     # psapi.dll needed just for get_process_path() and memory_usage(). Not present on Win98 unfortunately
     # ole32.dll needed just for open_document()
-    base_libraries += ['winmm', 'psapi', 'ole32', 'ws2_32']
+    base_libraries += ['winmm', 'ole32', 'ws2_32']  # 'psapi' is dynamically loaded
     common_libraries += [libfbgfx]
     commonenv['FBFLAGS'] += ['-s','gui']  # Change to -s console to see 'print' statements in the console!
     commonenv['CXXLINKFLAGS'] += ['-lgdi32', '-Wl,--subsystem,windows']

custom.bas
@@ -1027,7 +1027,7 @@ FUNCTION get_previous_session_info (workdir as string) as SessionInfo
    ' but it's incredibly unlikely
    DIM pid_current_exe as string = get_process_path(ret.pid)
    debuginfo "pid_current_exe = " & pid_current_exe
-   ret.running = (LEN(exe) ANDALSO pid_current_exe = exe)
+   ret.running = (LEN(exe) ANDALSO (pid_current_exe = exe ORELSE exe = "<unknown>"))
   END IF
  ELSE
   'We don't know anything, except that we could work out session_start_time by looking at working.tmp mtimes.

os_unix2.bas
@@ -14,6 +14,7 @@ extern "C"
 ' This may return either the name that the process was called with, which may have a full, relative, or no path,
 ' or just return the full path to the executable. Not necessarily equal to COMMAND(0).
 ' Returns "" if invalid or don't have permission.
+' (Should return "<unknown>" if the pid exists but we can't get the path)
 function get_process_path (pid as integer) as string
 	dim cmdname as string
 #if defined(__GNU_LINUX__) or defined(__FB_ANDROID__)  ' I'm guessing about Android...

commit 97c8b3a0b3ac5a1254e28cc23bc89840bea012a6

Fix stack corruption causing a possible crash on Windows in get_process_path, os_get_documents_dir
Introduced in r10950 and r1120 (2019-01-30 and 2019-04-10)

Again, several function pointers were declared wrong.

Whether this mistake actually corrupted memory depends on whether -gen gcc was
used or not and compiler decisions. GCC handles the stack differently and seems
to be more resistant to the esp pointer being modified.
In a debug (non-gen gcc) build, I found today that Custom always immediately
crashed inside get_process_path due to a crashed previous instance of Custom
causing the line
  debuginfo "pid " & pid & " image " & ret & " may have crashed, exitcode " & exitcode
to execute. But it didn't crash with -gen gcc, and has never crashed on me in
the last 7 months.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11270 7d344553-34f0-0310-a9b1-970ce8f1c3a2

os_windows.bas
@@ -75,7 +75,7 @@ dim shared main_thread_id as integer
 	declare function CopyFile_ alias "CopyFileA" (byval as LPCSTR, byval as LPCSTR, byval as BOOL) as BOOL
 #endif
 
-extern "C"
+extern "Windows"
 #undef GetProcessMemoryInfo
 dim shared GetProcessMemoryInfo as function (byval Process as HANDLE, byval ppsmemCounters as PPROCESS_MEMORY_COUNTERS, byval cb as DWORD) as WINBOOL
 #undef GetProcessImageFileNameA