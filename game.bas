ohrrpgce/ohrrpgce

commit 8f5ff0fccd7d10cc8655491f2516bf9502efacd3

findhero() id arg is no longer offset by 1
(A few more functions with that annoyance left)

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@10439 7d344553-34f0-0310-a9b1-970ce8f1c3a2

game.bas
@@ -3533,14 +3533,14 @@ SUB add_rem_swap_lock_hero (byref box as TextBox)
  '---REMOVE---
  IF box.hero_addrem < 0 THEN
   IF party_size() > 1 THEN
-   i = findhero(-box.hero_addrem, , serrWarn)
+   i = findhero(-box.hero_addrem - 1, , serrWarn)
    IF i > -1 THEN gam.hero(i).id = -1
    IF active_party_size() = 0 THEN forceparty
   END IF
  END IF '---end if < 0
  '---SWAP-IN---
  IF box.hero_swap > 0 THEN
-  i = findhero(box.hero_swap, -1, serrWarn)
+  i = findhero(box.hero_swap - 1, -1, serrWarn)
   IF i > -1 THEN
    FOR o as integer = 0 TO 3
     IF gam.hero(o).id = -1 THEN
@@ -3552,7 +3552,7 @@ SUB add_rem_swap_lock_hero (byref box as TextBox)
  END IF '---end if > 0
  '---SWAP-OUT---
  IF box.hero_swap < 0 THEN
-  i = findhero(-box.hero_swap, , serrWarn)
+  i = findhero(-box.hero_swap - 1, , serrWarn)
   IF i > -1 THEN
    FOR o as integer = 40 TO 4 STEP -1
     IF gam.hero(o).id = -1 THEN
@@ -3565,12 +3565,12 @@ SUB add_rem_swap_lock_hero (byref box as TextBox)
  END IF '---end if < 0
  '---UNLOCK HERO---
  IF box.hero_lock > 0 THEN
-  DIM heroat as integer = findhero(box.hero_lock, , serrWarn)
+  DIM heroat as integer = findhero(box.hero_lock - 1, , serrWarn)
   IF heroat > -1 THEN gam.hero(heroat).locked = NO
  END IF '---end if > 0
  '---LOCK HERO---
  IF box.hero_lock < 0 THEN
-  DIM heroat as integer = findhero(-box.hero_lock, , serrWarn)
+  DIM heroat as integer = findhero(-box.hero_lock - 1, , serrWarn)
   IF heroat > -1 THEN gam.hero(heroat).locked = YES
  END IF '---end if > 0
 
@@ -3705,7 +3705,7 @@ SUB init_text_box_slices(txt as TextBoxState)
   CASE 4' Hero by ID
    'If the hero is in the party, use their current state.
    'if there are multiple copies, use the first.
-   hero_slot = findhero(txt.box.portrait_id + 1)
+   hero_slot = findhero(txt.box.portrait_id)
    IF hero_slot = -1 THEN
     'The hero is not in the party right now, use their default
     DIM her as HeroDef
@@ -4198,13 +4198,12 @@ END SUB
 
 ' Find hero by ID, returning party slot or -1.
 ' direction: 1 to find the first matching hero, or -1 to find the last.
-' NOTE: 'id' is the hero ID + 1!
-' Pass id = -1 to find any hero.
+' Pass id = -1 to find any hero. Otherwise id is the real hero ID, >= 0.
 FUNCTION findhero (byval id as integer, byval direction as integer = 1, errlvl as scriptErrEnum = serrIgnore) as integer
  DIM as integer first, last
  IF direction = -1 THEN first = UBOUND(gam.hero) ELSE last = UBOUND(gam.hero)
  FOR i as integer = first TO last STEP direction
-  IF gam.hero(i).id + 1 = id ORELSE (id = -1 ANDALSO gam.hero(i).id >= 0) THEN
+  IF gam.hero(i).id = id ORELSE (id = -1 ANDALSO gam.hero(i).id >= 0) THEN
    RETURN i
   END IF
  NEXT i

savegame.rbas
@@ -2001,8 +2001,8 @@ FUNCTION saveslot_quick_root_node(byval saveslot as integer) as NodePtr
 END FUNCTION
 
 FUNCTION saveslot_findhero(byval saveroot as NodePtr, byval id as integer) as integer
- 'NOTE: id is the real ID number, not ID+1 like findhero()
- FOR i as integer = 0 to 40
+ 'id is the real ID number
+ FOR i as integer = 0 to sizeParty - 1
   DIM node as NodePtr = NodeByPath(saveroot, "/party/slot[" & i & "]/id")
   IF node THEN
    IF GetInteger(node) = id THEN RETURN i

scriptcommands.bas
@@ -130,7 +130,7 @@ FUNCTION standard_embed_codes(act as string, byval arg as integer) as string
    '--defaults blank if not found
    insert = ""
    '--first search for a copy of the hero in the party
-   DIM where as integer = findhero(arg + 1)
+   DIM where as integer = findhero(arg)
    IF where >= 0 THEN
     insert = gam.hero(where).name
    ELSE
@@ -1463,10 +1463,10 @@ SUB script_functions(byval cmdid as integer)
  CASE 70'--room in active party
   scriptret = active_party_slots() - active_party_size()
  CASE 71'--lock hero
-  DIM hero_slot as integer = findhero(retvals(0) + 1, , serrWarn)
+  DIM hero_slot as integer = findhero(retvals(0), , serrWarn)
   IF hero_slot > -1 THEN gam.hero(hero_slot).locked = YES
  CASE 72'--unlock hero
-  DIM hero_slot as integer = findhero(retvals(0) + 1, , serrWarn)
+  DIM hero_slot as integer = findhero(retvals(0), , serrWarn)
   IF hero_slot > -1 THEN gam.hero(hero_slot).locked = NO
  CASE 74'--set death script
   gen(genGameoverScript) = large(retvals(0), 0)
@@ -1508,7 +1508,7 @@ SUB script_functions(byval cmdid as integer)
   resetcaterpillar_for_one_hero retvals(0), retvals(1) * 20, retvals(2) * 20
   END IF
  CASE 90'--find hero
-  scriptret = findhero(retvals(0) + 1)
+  scriptret = findhero(retvals(0))
  CASE 91'--check equipment
   IF valid_hero_party(retvals(0)) THEN
    scriptret = gam.hero(retvals(0)).equip(bound(retvals(1) - 1, 0, 4)).id
@@ -4013,13 +4013,13 @@ SUB script_functions(byval cmdid as integer)
   END IF
  CASE 67'--delete hero (hero ID)
   IF party_size() > 1 AND retvals(0) >= 0 THEN
-   DIM i as integer = findhero(retvals(0) + 1, , serrWarn)
+   DIM i as integer = findhero(retvals(0), , serrWarn)
    IF i > -1 THEN gam.hero(i).id = -1
    IF active_party_size() = 0 THEN forceparty
    party_change_updates
   END IF
  CASE 68'--swap out hero
-  DIM i as integer = findhero(retvals(0) + 1, , serrWarn)
+  DIM i as integer = findhero(retvals(0), , serrWarn)
   IF i > -1 THEN
    FOR o as integer = 40 TO 4 STEP -1
     IF gam.hero(o).id = -1 THEN
@@ -4030,7 +4030,7 @@ SUB script_functions(byval cmdid as integer)
    NEXT o
   END IF
  CASE 69'--swap in hero
-  DIM i as integer = findhero(retvals(0) + 1, -1, serrWarn)
+  DIM i as integer = findhero(retvals(0), -1, serrWarn)
   IF i > -1 THEN
    FOR o as integer = 0 TO 3
     IF gam.hero(o).id = -1 THEN

commit b4929b815bfc0593045c76f0e309fc6a6b23139e

Fix empty active parties and OOB errors/crashes because findhero(-1) was broken
Was meant to return first hero in party, instead returned 0!  This broke
forceparty, so you could end up with an empty party. findhero(-1) wasn't used
anywhere else.

I broke it in r10439 "findhero() id arg is no longer offset by 1", 2018-07-04

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11303 7d344553-34f0-0310-a9b1-970ce8f1c3a2

game.bas
@@ -4218,7 +4218,9 @@ FUNCTION findhero (byval id as integer, byval direction as integer = 1, errlvl a
  DIM as integer first, last
  IF direction = -1 THEN first = UBOUND(gam.hero) ELSE last = UBOUND(gam.hero)
  FOR i as integer = first TO last STEP direction
-  IF gam.hero(i).id = id ORELSE (id = -1 ANDALSO gam.hero(i).id >= 0) THEN
+  IF id = -1 THEN
+   IF gam.hero(i).id >= 0 THEN RETURN i
+  ELSEIF gam.hero(i).id = id THEN
    RETURN i
   END IF
  NEXT i