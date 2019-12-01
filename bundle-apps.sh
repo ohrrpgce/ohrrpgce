1f167a2b9716218f6ce22a165f80a35273ab511c
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2019-12-01 06:01:00 +0000

    Start adapting bundle-apps.sh to create 64-bit apps
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11447 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- bundle-apps.sh
+++ bundle-apps.sh
@@ -8,19 +8,57 @@
 TODAY=`date "+%Y%m%d"`
 CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
 
-# Sanity checks
-if ! file ohrrpgce-game | grep 386 ; then
-  echo "ohrrpgce-game is missing or not compiled as 32 bit"
-  exit 1
-fi
-if ! file ohrrpgce-custom | grep 386 ; then
-  echo "ohrrpgce-custom is missing or not compiled as 32 bit"
+ARCH=${1:-i386}
+if [ $ARCH != "i386" -a $ARCH != "x86_64" ]; then
+  echo "Usage: ./bundle-apps.sh [i386|x86_64]"
   exit 1
 fi
 
 echo Deleting old apps
 rm -rf OHRRPGCE-Game.app
 rm -rf OHRRPGCE-Custom.app
+
+# Sanity checks
+for BINARY in ohrrpgce-game ohrrpgce-custom hpeak; do
+  if ! file $BINARY | grep $ARCH ; then
+    echo "$BINARY is missing or not compiled for $ARCH"
+    exit 1
+  fi
+done
+
+find_framework() {
+  NAME=$1
+  temp="~/Library/Frameworks/$NAME"
+  [ -d "$temp" ] && return "$temp"
+  temp="/System/Library/Frameworks/$NAME"
+  [ -d "$temp" ] && return "$temp"
+  echo "Can't find $NAME"
+  exit 1
+}
+
+add_frameworks() {
+  APP=$1
+  if [ $ARCH = "i386" ]; then
+    # Use our own SDL* 1.2 frameworks, because there are some small differences:
+    # SDL_mixer is compiled for OS 10.4+ instead of 10.5+
+    # SDL_mixer uses libmad instead of smpeg, which is smaller and doesn't crash on certain mp3s
+    # A couple patches have been applied to SDL_mixer, notably to enable module loop points.
+    # However, it is missing other fixes because it's not the latest SDL_mixer version
+    tar xf mac/Frameworks.tar.gz -C $APP/Contents || exit 1
+  else
+    SRC=find_framework SDL.framework
+    mkdir $APP/Contents/Frameworks
+    cp -ra $SRC $APP/Contents/Frameworks/
+
+    SRC=find_framework SDL_mixer.framework
+    mkdir $APP/Contents/Frameworks
+    cp -ra $SRC $APP/Contents/Frameworks/
+    # We don't use FLAC
+    rm -rf $APP/Contents/Frameworks/SDL_mixer.framework/Versions/A/Frameworks/FLAC.framework
+
+    # TODO: remove unwanted archs from fat binaries
+  fi
+}
 
 echo Bundling OHRRPGCE-Game.app
 cp -R mac/OHRRPGCE-Game.app.template OHRRPGCE-Game.app &&
@@ -31,19 +69,14 @@ mkdir -p OHRRPGCE-Game.app/Contents/MacOS &&
 sed -i -e "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Game.app/Contents/Info.plist &&
 mkdir -p OHRRPGCE-Game.app/Contents/Resources/ohrhelp &&
 cp -R ohrhelp/game_*.txt ohrhelp/share_*.txt ohrhelp/slice*.txt OHRRPGCE-Game.app/Contents/Resources/ohrhelp &&
-cp ohrrpgce-game OHRRPGCE-Game.app/Contents/MacOS/ &&
-tar xf mac/Frameworks.tar.gz -C OHRRPGCE-Game.app/Contents || exit 1
-# Mac tar doesn't have an option to expand symlinks
+cp ohrrpgce-game OHRRPGCE-Game.app/Contents/MacOS/ || exit 1
 
-CWD=`pwd`
+add_frameworks OHRRPGCE-Game.app
+
+# Remove symlinks, because they are a problem on Windows
+# (Mac tar doesn't have an option to expand symlinks)
 echo Un-symlinking OHRRPGCE-Game.app
-cd OHRRPGCE-Game.app/Contents/Frameworks/SDL.framework &&
-rm -rf Versions/Current SDL Headers Resources &&
-cd ../SDL_mixer.framework &&
-rm -rf Versions/Current SDL_mixer Headers Frameworks Resources &&
-cd Versions/A/Frameworks/mikmod.framework &&
-rm -rf Versions/Current mikmod Headers Resources &&
-cd $CWD || exit 1
+find OHRRPGCE-Game.app -type l -exec rm "{}" ";" || exit 1
 
 echo Bundling OHRRPGCE-Custom.app
 cp -R mac/OHRRPGCE-Custom.app.template OHRRPGCE-Custom.app &&
@@ -54,10 +87,11 @@ mkdir -p OHRRPGCE-Custom.app/Contents/MacOS/support &&
 mkdir -p OHRRPGCE-Custom.app/Contents/Resources/ohrhelp &&
 sed -i -e "s/#VERSION#/O.H.R.RPG.C.E version ${CODE} ${TODAY}/g" OHRRPGCE-Custom.app/Contents/Info.plist &&
 cp ohrrpgce-custom OHRRPGCE-Custom.app/Contents/MacOS/ &&
-tar xf mac/Frameworks.tar.gz -C OHRRPGCE-Custom.app/Contents &&
 cp -R ohrhelp/* OHRRPGCE-Custom.app/Contents/Resources/ohrhelp &&
 cp -R data/* OHRRPGCE-Custom.app/Contents/Resources/ &&
 cp support/Terminal_wrapper.sh OHRRPGCE-Custom.app/Contents/MacOS/support/ &&
 cp plotscr.hsd scancode.hsi OHRRPGCE-Custom.app/Contents/MacOS/support/ &&
 tar xf mac/utilities.tar.gz -C OHRRPGCE-Custom.app/Contents/MacOS/support/ && 
 cp hspeak OHRRPGCE-Custom.app/Contents/MacOS/support/ || exit 1
+
+add_frameworks OHRRPGCE-Custom.app
