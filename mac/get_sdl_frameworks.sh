da1f64b25f6d0456de4616606dc043e9699b7d6e
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2019-12-01 16:49:05 +0000

    Add a small helper script to download mac sdl frameworks
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11448 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ mac/get_sdl_frameworks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd $(dirname $0)
+mkdir -p "mac_sdl_framework_downloads"
+cd "mac_sdl_framework_downloads"
+
+if [ ! -e "SDL_mixer-1.2.12.dmg" ] ; then
+  curl -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.dmg"
+fi
+
+if [ ! -e "SDL2_mixer-2.0.4.dmg" ] ; then
+  curl -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.dmg"
+fi
+
+if [ ! -e "SDL-1.2.15.dmg" ] ; then
+  curl -O "https://www.libsdl.org/release/SDL-1.2.15.dmg"
+fi
+
+if [ ! -e "SDL2-2.0.10.dmg" ] ; then
+  curl -O "https://www.libsdl.org/release/SDL2-2.0.10.dmg"
+fi
