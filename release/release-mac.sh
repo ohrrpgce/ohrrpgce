562ff26cf0130131ef3f6170501347944176e373
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2020-01-04 01:41:21 +0000

    Update release-mac.sh wrapper for 64 bit support
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11553 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-mac.sh
+++ release/release-mac.sh
@@ -5,11 +5,12 @@ cd "${SCRIPTDIR}"
 cd ..
 
 # This is hardcoded for the paths on James's Mac build box
-CC=clang GCC=/opt/local/bin/gcc-mp-4.7 EUDIR=~james/misc/euphoria/ ./distrib-mac.sh
+CC=clang GCC=/opt/local/bin/gcc-mp-4.7 EUDIR=~/misc/euphoria/ ARCH=x86_64 ./distrib-mac.sh
+CC=clang GCC=/opt/local/bin/gcc-mp-4.7 EUDIR=~/misc/eu32/ ARCH=i386 ./distrib-mac.sh
 
 SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
 
 TODAY=`date "+%Y-%m-%d"`
 CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
-scp -p distrib/OHRRPGCE-${TODAY}-${CODE}.dmg "${SCPDEST}"/archive/
-scp -p distrib/ohrrpgce-mac-minimal-${TODAY}-${CODE}.tar.gz "${SCPDEST}"/archive/
+scp -p distrib/OHRRPGCE-${TODAY}-${CODE}-*.dmg "${SCPDEST}"/archive/
+scp -p distrib/ohrrpgce-mac-minimal-${TODAY}-${CODE}-*.tar.gz "${SCPDEST}"/archive/
