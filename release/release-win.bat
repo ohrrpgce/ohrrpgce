695b4a5be76c2ae214a61cb9e2ec17e3b0cc8dc4
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2020-01-11 21:34:09 +0000

    Fix release-win.bat script to call distrib-win.bat instead of distrib.bat
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/fufluns@11563 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-win.bat
+++ release/release-win.bat
@@ -1,5 +1,5 @@
 CD ..
-CALL distrib.bat
+CALL distrib-win.bat
 
 REM This only works if you have Putty's pscp.exe in your path.
 pscp distrib/ohrrpgce-* james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
