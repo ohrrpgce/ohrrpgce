f0d88be85df1d94a4bdc172c89f8e2bf67187297
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2020-01-12 01:11:40 +0000

    Add workaround for pdb support files (wip)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11571 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-win.bat
+++ release/release-win.bat
@@ -1,5 +1,11 @@
 CD ..
+
+REM grab a copy of the pdb tools from the wip/support folder
+support\cp ../../wip/support/mspdbsrv.exe ../../wip/support/ms*.dll support
+
+REM Build the release
 CALL distrib-win.bat
 
+REM Upload the release
 REM This only works if you have Putty's pscp.exe in your path.
 pscp distrib/ohrrpgce-* james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce/archive/
