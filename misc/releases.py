c0376ec5b40b03d3368bcc681a17501f577ae6b9
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2020-01-19 04:18:34 +0000

    misc/releases.py now takes commandline option to skip bugfix releases
    And fixed it not showing the oldest release in whatsnew.txt, and show
    gaps in the whatsnew.txt record.
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11581 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- misc/releases.py
+++ misc/releases.py
@@ -1,29 +1,52 @@
 #!/usr/bin/env python2
-"""
-Extract all release dates from whatsnew.txt, and print number of days between releases.
-"""
 from dateutil import parser
+import sys
+
+bugfix_releases = True
+if len(sys.argv) > 1:
+    if sys.argv[1] == '--skip-bugfix':
+        bugfix_releases = False
+    else:
+        print "Extracts all release dates from whatsnew.txt, and print number of days between releases."
+        print "Usage: misc/releases.py [--skip-bugfix]"
+        sys.exit()
+
+last_release_line = -999
 releases = []
-for line in open("whatsnew.txt"):
+for lineno, line in enumerate(open("whatsnew.txt")):
     if not line.startswith(' ') and 'WHAT IS NEW' not in line:
         name = ''
-        # if '+' in line:
-        #     continue   # skip bugfix releases
+        if bugfix_releases == False and '+' in line:
+            continue
+        if 'release notes lost' in line:
+            releases.append((0, 'lost'))
+            continue
         if '[' in line:
             name = line[line.index('['):].strip()
             line = line[:line.index('[')]
         if len(line.strip()) and '?' not in line:
+            if bugfix_releases == False and lineno <= last_release_line + 10:
+                # The previous release was pretty minor, call it a bugfix release
+                del releases[-1]
+            last_release_line = lineno
             releases.append((parser.parse(line.strip()), name[1:-1]))
 
-for release, lastrelease in zip(releases[:-1], releases[1:]):
-    release, name = release
-    date = release.strftime("%Y-%m-%d")
-    print date, name.rjust(14), str((release - lastrelease[0]).days).rjust(4), "days"
+for release, lastrelease in zip(releases, releases[1:] + [None]):
+    date, name = release
+    if date:
+        datestr = date.strftime("%Y-%m-%d")
+        if lastrelease and lastrelease[0]:
+            print datestr, name.rjust(18), str((date - lastrelease[0]).days).rjust(4), "days"
+        else:
+            print datestr, name
+    else:
+        print 'Lost...'
 
 if False:
     # This generates some markup for the timeline on the wikipedia article,
     # but it needs hand-editing to fix overlapping labels.
     for release, name in releases:
+        if not release: continue
         date = release.strftime("%d/%m/%Y")
         if '+' in name:
             text = "  mark:(line,yellow)  #" + name
