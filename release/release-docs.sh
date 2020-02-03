5a510cf31d41ef51d0a7806053439d6614652100
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2020-02-03 06:38:18 +0000

    release-docs.sh didn't upload the icon images for stable releases of the plotdict
    (I've uploaded them myself)
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@11584 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-docs.sh
+++ release/release-docs.sh
@@ -12,3 +12,4 @@ docs/update-html.sh
 scp -p docs/plotdict.xml "${SCPDEST}"/docs/
 scp -p docs/htmlplot.xsl "${SCPDEST}"/docs/
 scp -p docs/plotdictionary.html "${SCPDEST}"/docs/
+scp -p docs/*.png "${SCPDEST}"/docs/
