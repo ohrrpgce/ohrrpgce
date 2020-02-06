316cae395a49f1f0c2bd70817bde1b67dbb4aebe
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2020-02-06 04:42:24 +0000

    git-svn.sh mirror script: push to github mirror in addition to bitbucket
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@11597 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- cron/git-svn.sh
+++ cron/git-svn.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-# TMC runs this script from cron to keep the git-svn mirror up to date.
+# TMC runs this script from cron to keep the git-svn mirrors on bitbucket and github up to date.
 
-LOCALREPO=~/src/ohrrpgce-svn
+LOCALREPO=~/ohr/ohrrpgce-git-svn
 REMOTEREPO=git@bitbucket.org:rbv/ohrrpgce-svn.git
+REMOTEREPO2=git@github.com:ohrrpgce/ohrrpgce.git
 SVNREPO=https://rpg.hamsterrepublic.com/source
 
 if [ ! -d "$LOCALREPO" ]; then
     echo "Cloning from $REMOTEREPO"
 
-    git clone "$REMOTEREPO" "$LOCALREPO" --origin svn
-    # svn/HEAD gets created automatically when cloning, delete it
-    git branch -d -r svn/HEAD
+    git clone "$REMOTEREPO" "$LOCALREPO" --origin svn --no-checkout || exit
     cd "$LOCALREPO"
+    # svn/HEAD gets created automatically when cloning, delete it
+    git branch -d -r svn/HEAD || exit
+    # The bitbucket remote is called 'svn', the github one is called 'gh'
+    git remote add gh $REMOTEREPO2
+    # Instead of running "git svn init" we set up git-svn our own way
     git config svn-remote.svn.url "$SVNREPO"
     git config svn-remote.svn.fetch "wip:refs/remotes/svn/wip"
     git config svn-remote.svn.fetch "tools:refs/remotes/svn/tools" --add
     git config svn-remote.svn.fetch "web:refs/remotes/svn/web" --add
     git config svn-remote.svn.branches "rel/*:refs/remotes/svn/*"
+
+    echo "Rebuilding svn indexes will take a few minutes!"
 fi
 
 cd "$LOCALREPO"
 
-git svn fetch &&
+git svn fetch || exit
 git push svn refs/remotes/svn/*:refs/heads/*
+git push gh refs/remotes/svn/*:refs/heads/*
