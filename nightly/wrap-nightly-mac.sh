cfd9b3e75bb563a7c155dc7693043155e0e3ac8c
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2021-02-04 12:57:51 +0000

    Make sure that the latest distrib-nightly-mac.sh script is used
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12218 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- nightly/wrap-nightly-mac.sh
+++ nightly/wrap-nightly-mac.sh
@@ -9,6 +9,9 @@ if [ -n "True" ] ; then
   echo "To: cron@rpg.hamsterrepublic.com"
   echo "Subject: OHRRPGCE Mac nightly build ($(uname -n))"
   echo ""
+  # Make sure whave the lates mac distrib script
+  # everything else is done in a different folder
+  svn update distrib-nightly-mac.sh
   # This hard-codes some paths specific to James's setup
   CC=clang GCC=/opt/local/bin/gcc-mp-4.7 EUDIR=~james/misc/euphoria/ ./distrib-nightly-mac.sh 2>&1
 fi | tee ~/wrap-nightly-mac-output.txt
