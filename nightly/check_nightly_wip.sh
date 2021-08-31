d365960216c6afa4e21778db883ffd7db7da269f
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2021-08-31 22:50:50 +0000

    Add a script that checks nightly builds by downloading them and printing some info from buildinfo.ini
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12325 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ nightly/check_nightly_wip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+cd $SCRIPT_DIR
+
+if [ -d "./check_nightly" ] ; then
+  echo "Cleanup leftover check_nightly directory..."
+  rm -Rf ./check_nightly
+fi
+
+echo "Create new check_nightly directory"
+mkdir ./check_nightly
+cd ./check_nightly
+
+for NAME in "ohrrpgce-player-win-wip-sdl2.zip" \
+            "ohrrpgce-player-linux-bin-minimal-wip-x86.zip" \
+            "ohrrpgce-player-linux-bin-minimal-wip-x86_64.zip" \
+            "ohrrpgce-mac-minimal-x86.tar.gz" \
+            "ohrrpgce-mac-minimal-x86_64.tar.gz" ; do
+  EXT="${NAME#*.}"
+  BASE="${NAME%%.*}"
+  echo "# $BASE ($EXT)"
+  mkdir ./tmp
+  wget -q "https://hamsterrepublic.com/ohrrpgce/nightly/$NAME" -O "./tmp/$NAME"
+  if [ "$EXT" = "zip" ] ; then
+    unzip -q "./tmp/$NAME" "buildinfo.ini" -d ./
+  fi
+  if [ "$EXT" = "tar.gz" ] ; then
+    tar -z -x "buildinfo.ini" -f "./tmp/$NAME"
+  fi
+  grep -e "build_date" -e "svn_rev" buildinfo.ini | sed 's/^/  /'
+  rm -f buildinfo.ini
+  rm -Rf ./tmp
+
+done
+
--- nightly/ohr_nightly_vm.sh
+++ nightly/ohr_nightly_vm.sh
@@ -21,6 +21,16 @@ for VMNAME in "Debian 32bit" "Debian 64bit" "Windows 7" "Mac OS X" "Debian for A
   done
 done
 
-# After the nightly build finishes, list the remote directory
-# for a convenient overview of what builds succeeded
+# After the nightly build finishes, do some verification
+if [ -n "True" ] ; then
+  echo "From: cron@rpg.hamsterrepublic.com"
+  echo "To: cron@rpg.hamsterrepublic.com"
+  echo "Subject: OHRRPGCE Nightly build check ($(uname -n))"
+  echo ""
+  SCRIPT_DIR=$(dirname "$0")
+  $SCRIPT_DIR/check_nightly_wip.sh 2>&1
+fi | tee ~/wrap-nightly-check-output.txt
+~/src/ohr/wip/nightly/curl_smtp_wrapper.sh ~/wrap-nightly-check-output.txt
+
+# list the remote directory
 ssh james_paige@motherhamster.org ls -l HamsterRepublic.com/ohrrpgce/nightly/ | cut -d " " -f 5-
