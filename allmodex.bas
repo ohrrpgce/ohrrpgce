ohrrpgce/ohrrpgce

commit ef833426f623f35331a88e1330cd13d14e94b969

All keypress/joystick script commands now take player number instead of joystick number as additional arg
Player 0 is the input from all joysticks and keyboard merged, or (for now) the
first joystick in the case of joystickaxis and joystick info commands.

Replaced device_carray() function with player_keyval().

Internally there's no mapping from devices to players yet, joysticks are still
numbered in the same way.

Also, unlike other functions joykeyval still takes a joynum rather than a player.

plotdict not updated yet.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12270 7d344553-34f0-0310-a9b1-970ce8f1c3a2

allmodex.bas
@@ -91,6 +91,7 @@ declare sub masterpal_changed()
 
 declare sub pollingthread(as any ptr)
 declare sub keystate_convert_bit3_to_keybits(keystate() as KeyBits)
+declare function keyval_or_numpad_ex(key as KBScancode, repeat_wait as integer = 0, repeat_rate as integer = 0, real_keys as bool = NO) as KeyBits
 declare function read_inputtext () as string
 declare sub update_mouse_state ()
 
@@ -1777,14 +1778,13 @@ end function
 '                                      Keyboard input
 '==========================================================================================
 
-'Read carray controls array (ccUse etc) for a single device keyboard/joystick).
-'If you want to get the global/combined input you should call carray() instead, which is just
-'an alias to keyval().
+'Read keyboard/joystick input for either a single player or player 0 (default/all).
 'Reads replayed state, if any (real_keys = NO)
-'ccode:   a cc* constant
-'joynum:  -2 for keyboard, -1 for any joystick, 0-3 for single joystick
-function device_carray(ccode as KBScancode, joynum as integer) as KeyBits
-	BUG_IF(ccode < ccLOWEST orelse ccode > ccHIGHEST, "Invalid ccode " & ccode, 0)
+'key:    any scancode, including cc* constants and joystick buttons
+'player: 0 for any input device, 1-3 for single joystick
+function player_keyval(key as KBScancode, player as integer, repeat_wait as integer = 0, repeat_rate as integer = 0) as KeyBits
+	BUG_IF(player < 0, "Invalid player " & player, 0)
+	BUG_IF(key < scKEYVAL_FIRST orelse key > scKEYVAL_LAST, "bad scancode " & key, 0)
 
 	dim inputst as InputState ptr
 	if replay.active then
@@ -1793,17 +1793,51 @@ function device_carray(ccode as KBScancode, joynum as integer) as KeyBits
 		inputst = @real_input
 	end if
 
-	if joynum = -2 then
-		return inputst->carray(ccode)
-	elseif joynum = -1 then
-		dim ret as KeyBits
-		for joynum = 0 to ubound(inputst->joys)
-			ret or= inputst->joys(joynum).carray(ccode)
-		next
-		return ret
-	elseif joynum < ubound(inputst->joys) then
-		return inputst->joys(joynum).carray(ccode)
+	dim ret as KeyBits
+	dim joynum as integer = player - 1
+
+	if joynum > ubound(inputst->joys) then return 0
+
+	if key < 0 then  'Control key or scAny
+		if player <= 0 then
+			'carrays for all input devices are merged together and returned by keyval, so just use that
+			ret = keyval_ex(key, repeat_wait, repeat_rate)  'Equivalent to keyval_or_numpad_ex
+			/'
+			ret = inputst->carray(key)  'Keyboard
+			for joynum = 0 to ubound(inputst->joys)
+				ret or= inputst->joys(joynum).carray(key)
+			next
+			'/
+		elseif key = scAny then
+			for button as integer = joyButton1 to joyLAST
+				ret or= joykeyval(button, joynum, repeat_wait, repeat_rate)
+			next
+			if player = 1 then
+				'Add keyboard scAny
+				'FIXME: this adds in scAny from all joysticks
+				ret or=  keyval_ex(key, repeat_wait, repeat_rate)
+			end if
+		else  'key <= ccHIGHEST
+			'TODO: This is missing repeat_wait/repeat_rate support
+			ret = inputst->joys(joynum).carray(key)
+			if player = 1 then
+				ret or= inputst->carray(key)  'Keyboard
+			end if
+		end if
+	elseif key <= scLAST then  'Keyboard key
+		'Player number is ignored for now
+		ret = keyval_or_numpad_ex(key, repeat_wait, repeat_rate)
+	else  'Joystick button
+		dim button as integer = keybd_to_joy_scancode(key)
+		if player = 0 then
+			for joynum = 0 to ubound(inputst->joys)
+				ret or= joykeyval(button, joynum, repeat_wait, repeat_rate)
+			next
+		else
+			ret = joykeyval(button, joynum, repeat_wait, repeat_rate)
+		end if
 	end if
+	return ret
 end function
 
 'Return numpad scancode that's an alias to 'key'
@@ -1830,7 +1864,7 @@ local function numpad_alias_key(key as KBScancode, real_keys as bool) as KBScanc
 	return 0
 end function
 
-function keyval_or_numpad_ex (key as KBScancode, repeat_wait as integer = 0, repeat_rate as integer = 0, real_keys as bool = NO) as KeyBits
+local function keyval_or_numpad_ex (key as KBScancode, repeat_wait as integer = 0, repeat_rate as integer = 0, real_keys as bool = NO) as KeyBits
 	dim ret as KeyBits = keyval_ex(key, repeat_wait, repeat_rate, real_keys)
 	dim key2 as KBScancode = numpad_alias_key(key, real_keys)
 	if key2 then ret or= keyval_ex(key2, repeat_wait, repeat_rate, real_keys)
@@ -1869,7 +1903,7 @@ function keyval_ex (a as KBScancode, repeat_wait as integer = 0, repeat_rate as 
 
 	ERROR_IF(a < scKEYVAL_FIRST orelse a > scKEYVAL_LAST, "bad scancode " & a, 0)
 	if a > scLAST then
-		'For convenience, poll joystick 0
+		'For convenience, poll joystick (AFAIK this code path isn't used, should be removed)
 		return joykeyval(keybd_to_joy_scancode(a), 0, repeat_wait, repeat_rate, real_keys)
 	end if
 	if a < 0 then
@@ -2425,7 +2459,7 @@ end sub
 'Reads replayed state, if any (real_keys = NO)
 'key:     either a cc* constant (all key mappings to that cc will be removed)
 '         or an ordinary KBScancode or JoyButton (any mapping from that key/button will be removed)
-'joynum:  -2 for keyboard, -1 for any joystick, 0-3 for single joystick (similar to device_carray())
+'joynum:  -2 for keyboard, -1 for any joystick, 0-3 for single joystick (similar to player_keyval())
 sub delete_key_mappings(key as integer, joynum as integer = -2)
 	dim inputst as InputState ptr = iif(replay.active, @replay_input, @real_input)
 
@@ -3128,7 +3162,8 @@ function num_joysticks () as integer
 	return ubound(inputst->joys) + 1
 end function
 
-'The new way to read joystick buttons. Like keyval.
+'Read a single joystick button. Like keyval, but doesn't take a scancode (nor cc* control code)
+'NOTE: this takes a joynum rather than a player number!
 function joykeyval (key as JoyButton, joynum as integer = 0, repeat_wait as integer = 0, repeat_rate as integer = 0, real_keys as bool = NO) as KeyBits
 	ERROR_IF(key > joyLAST, "bad scancode " & key, 0)
 
@@ -3152,8 +3187,10 @@ function JoystickState.keyval (key as JoyButton, repeat_wait as integer = 0, rep
 end function
 
 'Returns a value from -1000 to 1000
-function joystick_axis (axis as integer, joynum as integer = 0) as integer
+'TODO: merge all joysticks into player 0
+function joystick_axis (axis as integer, player as integer = 0) as integer
 	dim inputst as InputState ptr = iif(replay.active, @replay_input, @real_input)
+	dim joynum as integer = large(0, player - 1)
 	if joynum > ubound(inputst->joys) then return 0  'Not an error
 	dim byref joy as JoystickState = inputst->joys(joynum)
 
@@ -3164,8 +3201,10 @@ function joystick_axis (axis as integer, joynum as integer = 0) as integer
 end function
 
 'Can return NULL
-function joystick_info (joynum as integer) as JoystickInfo ptr
+'TODO: merge all joysticks into player 0
+function joystick_info (player as integer) as JoystickInfo ptr
 	dim inputst as InputState ptr = iif(replay.active, @replay_input, @real_input)
+	dim joynum as integer = large(0, player - 1)
 	if joynum > ubound(inputst->joys) then return NULL  'Not an error
 	return inputst->joys(joynum).state.info
 end function

allmodex.bi
@@ -507,15 +507,15 @@ DECLARE FUNCTION keyval_ex (key as KBScancode, repeat_wait as integer = 0, repea
 DECLARE FUNCTION real_keyval (key as KBScancode) as KeyBits
 DECLARE FUNCTION keyval (key as KBScancode) as KeyBits
 DECLARE FUNCTION carray alias "KEYVAL" (key as KBScancode) as KeyBits
-DECLARE FUNCTION device_carray (ccode as KBScancode, joynum as integer) as KeyBits
+DECLARE FUNCTION player_keyval (key as KBScancode, player as integer, repeat_wait as integer = 0, repeat_rate as integer = 0) as KeyBits
 DECLARE FUNCTION slowkey (key as KBScancode, ms as integer) as bool
 DECLARE FUNCTION getinputtext () as string
 
 '================== Joystick ===================
 
 DECLARE FUNCTION joykeyval (key as JoyButton, joynum as integer = 0, repeat_wait as integer = 0, repeat_rate as integer = 0, real_keys as bool = NO) as KeyBits
-DECLARE FUNCTION joystick_axis (axis as integer, joynum as integer = 0) as integer
-DECLARE FUNCTION joystick_info (joynum as integer) as JoystickInfo ptr
+DECLARE FUNCTION joystick_axis (axis as integer, player as integer = 0) as integer
+DECLARE FUNCTION joystick_info (player as integer) as JoystickInfo ptr
 DECLARE FUNCTION keybd_to_joy_scancode (key as KBScancode) as JoyButton
 DECLARE FUNCTION num_joysticks () as integer
 DECLARE SUB disable_joystick_input()

scriptcommands.bas
@@ -362,22 +362,15 @@ END FUNCTION
 '==========================================================================================
 
 
-FUNCTION script_keyval (byval key as KBScancode, byval joynum as integer = 0) as KeyBits
- 'Wrapper around keyval for use by scripts: performs scancode mapping for back-compat
+FUNCTION script_keyval (byval key as KBScancode, byval player as integer = 0) as KeyBits
+ 'Wrapper around player_keyval for use by scripts: performs scancode mapping for back-compat
 
  IF key < scKEYVAL_FIRST ORELSE key > scKEYVAL_LAST THEN
   scripterr current_command_name() & ": invalid scancode keyval(" & key & ")", serrBound
   RETURN 0
  END IF
 
- DIM ret as KeyBits = 0
-
- IF key <= scLAST THEN
-  ret = keyval(key)
- ELSE
-  'keyval does this anyway, but always only checks joystick 0
-  ret = joykeyval(keybd_to_joy_scancode(key), joynum)
- END IF
+ DIM ret as KeyBits = player_keyval(key, player)
 
  IF gam.click_keys THEN
   DIM mask as integer
@@ -405,27 +398,26 @@ FUNCTION script_keyval (byval key as KBScancode, byval joynum as integer = 0) as
   'as if they were indistinguishable.
   SELECT CASE key
    CASE scHome TO scDelete
-    ret OR= keyval(key + scNumpad7 - scHome)
+    ret OR= player_keyval(key + scNumpad7 - scHome, player)
    CASE scNumpad7 TO scNumpad9, scNumpad4 TO scNumpad6, scNumpad1 TO scNumpadPeriod
-    ret OR= keyval(key - scNumpad7 + scHome)
-   CASE scSlash:       ret OR= keyval(scNumpadSlash)
-   CASE scEnter:       ret OR= keyval(scNumpadEnter)
-   CASE scNumlock:     ret OR= keyval(scPause)
-   CASE scNumpadSlash: ret OR= keyval(scSlash)
-   CASE scNumpadEnter: ret OR= keyval(scEnter)
-   CASE scPause:       ret OR= keyval(scNumlock)
+    ret OR= player_keyval(key - scNumpad7 + scHome, player)
+   CASE scSlash:       ret OR= player_keyval(scNumpadSlash, player)
+   CASE scEnter:       ret OR= player_keyval(scNumpadEnter, player)
+   CASE scNumlock:     ret OR= player_keyval(scPause, player)
+   CASE scNumpadSlash: ret OR= player_keyval(scSlash, player)
+   CASE scNumpadEnter: ret OR= player_keyval(scEnter, player)
+   CASE scPause:       ret OR= player_keyval(scNumlock, player)
   END SELECT
  END IF
 
  IF prefbit(47) = NO THEN  '!Map joystick controls to keyboard keys for scripts
-  'Device -1 is all joysticks OR'd together
   SELECT CASE key
-   CASE scUp:     ret OR= device_carray(ccUp, -1)
-   CASE scDown:   ret OR= device_carray(ccDown, -1)
-   CASE scLeft:   ret OR= device_carray(ccLeft, -1)
-   CASE scRight:  ret OR= device_carray(ccRight, -1)
-   CASE scEnter:  ret OR= device_carray(ccUse, -1)
-   CASE scEsc:    ret OR= device_carray(ccMenu, -1)
+   CASE scUp:     ret OR= player_keyval(ccUp, player)
+   CASE scDown:   ret OR= player_keyval(ccDown, player)
+   CASE scLeft:   ret OR= player_keyval(ccLeft, player)
+   CASE scRight:  ret OR= player_keyval(ccRight, player)
+   CASE scEnter:  ret OR= player_keyval(ccUse, player)
+   CASE scEsc:    ret OR= player_keyval(ccMenu, player)
   END SELECT
  END IF
 
@@ -1445,13 +1437,13 @@ SUB script_functions(byval cmdid as integer)
   wrappedsong retvals(0)
  CASE 29'--stop song
   stopsong
- CASE 30'--keyval (scancode, joystick num)
+ CASE 30'--keyval (scancode, player)
   'This used to be keyispressed; which undocumentedly reported two bits
   'instead of true/false.
   a_script_wants_keys()
-  DIM joynum as integer = get_optional_arg(1, 0)
+  DIM player as integer = get_optional_arg(1, 0)
   'keyval() reports a 3rd bit, but didn't at the time that this command was (re-)documented
-  scriptret = script_keyval(retvals(0), joynum) AND 3
+  scriptret = script_keyval(retvals(0), player) AND 3
  CASE 31'--rank in caterpillar
   scriptret = rankincaterpillar(retvals(0))
  CASE 38'--camera follows hero
@@ -2065,7 +2057,7 @@ SUB script_functions(byval cmdid as integer)
   IF valid_plotstr(retvals(0)) AND retvals(1) >= 0 THEN
    plotstr(retvals(0)).s = getsongname(retvals(1))
   END IF
- CASE 235'--key is pressed (scancode, joystick num)
+ CASE 235'--key is pressed (scancode, player)
   a_script_wants_keys()
   IF script_keyval(retvals(0), retvals(1)) THEN scriptret = 1 ELSE scriptret = 0
  CASE 236'--sound is playing
@@ -2117,13 +2109,13 @@ SUB script_functions(byval cmdid as integer)
    END IF
    scriptret = retvals(0)
   END IF
- CASE 242'-- joystick button(button, joystick)
-  IF bound_arg(retvals(0), 1, 32, "button number 1-32") ANDALSO valid_joystick(retvals(1)) THEN
-   DIM key as KeyBits = joykeyval(retvals(0), retvals(1))
+ CASE 242'-- joystick button(button, player)
+  IF bound_arg(retvals(0), 1, 32, "button number 1-32") ANDALSO valid_player_num(retvals(1)) THEN
+   DIM key as KeyBits = player_keyval(scJoyOFFSET + retvals(0), retvals(1))
    scriptret = IIF(key > 0, 1, 0)
   END IF
- CASE 243'-- joystick axis(axis, scale, joystick)
-  IF valid_joystick(retvals(2)) THEN
+ CASE 243'-- joystick axis(axis, scale, player)
+  IF valid_player_num(retvals(2)) THEN
    scriptret = (joystick_axis(retvals(0), retvals(2)) / 1000) * retvals(1)
   END IF
  CASE 249'--party money
@@ -4773,36 +4765,36 @@ SUB script_functions(byval cmdid as integer)
   END IF
  CASE 675 '--speaking npc
   scriptret = -1 - txt.sayer  '0 if no textbox or not triggered by an NPC
- CASE 676 '--keypress (scancode, joynum)
+ CASE 676 '--keypress (scancode, player)
   a_script_wants_keys()
   scriptret = IIF(script_keyval(retvals(0), retvals(1)) > 1, 1, 0)
- CASE 677 '--new keypress (scancode, joynum)
+ CASE 677 '--new keypress (scancode, player)
   a_script_wants_keys()
   scriptret = IIF(script_keyval(retvals(0), retvals(1)) AND 4, 1, 0)
- CASE 678 '--get joystick name (stringid, joynum)
+ CASE 678 '--get joystick name (stringid, player)
   plotstr(retvals(0)).s = ""
-  IF valid_plotstr(retvals(0), serrBadOp) ANDALSO valid_joystick(retvals(1)) THEN
+  IF valid_plotstr(retvals(0), serrBadOp) ANDALSO valid_player_num(retvals(1)) THEN
    DIM info as JoystickInfo ptr = joystick_info(retvals(1))
    IF info THEN
     plotstr(retvals(0)).s = info->name
    END IF
   END IF
- CASE 679 '--joystick button count (joynum)
-  IF valid_joystick(retvals(0)) THEN
+ CASE 679 '--joystick button count (player)
+  IF valid_player_num(retvals(0)) THEN
    DIM info as JoystickInfo ptr = joystick_info(retvals(0))
    IF info THEN
     scriptret = info->num_buttons
    END IF
   END IF
- CASE 680 '--joystick axis count (joynum)
-  IF valid_joystick(retvals(0)) THEN
+ CASE 680 '--joystick axis count (player)
+  IF valid_player_num(retvals(0)) THEN
    DIM info as JoystickInfo ptr = joystick_info(retvals(0))
    IF info THEN
     scriptret = info->num_axes
    END IF
   END IF
- CASE 681 '--joystick hat count (joynum)
-  IF valid_joystick(retvals(0)) THEN
+ CASE 681 '--joystick hat count (player)
+  IF valid_player_num(retvals(0)) THEN
    DIM info as JoystickInfo ptr = joystick_info(retvals(0))
    IF info THEN
     scriptret = info->num_hats
@@ -5618,10 +5610,10 @@ END FUNCTION
 '                        Other script command arg checking/decoding
 '==========================================================================================
 
-'This doesn't check how many joysticks are plugged in, because it's not an error
-'to poll a missing joystick
-FUNCTION valid_joystick(byval joynum as integer) as bool
-  RETURN bound_arg(joynum, 0, 15, "joystick", , serrBadOp)
+'This doesn't check how many players there are/how many joysticks are plugged in, because it's not an error
+'to poll a missing player/joystick
+FUNCTION valid_player_num(byval player as integer) as bool
+  RETURN bound_arg(player, 0, 15, "player number", , serrBadOp)
 END FUNCTION
 
 FUNCTION valid_item_slot(byval item_slot as integer) as bool

scriptcommands.bi
@@ -66,7 +66,7 @@ DECLARE FUNCTION assign_menu_handles (byref menu as menudef) as integer
 DECLARE FUNCTION menu_item_handle_by_slot(byval menuslot as integer, byval mislot as integer, byval visible_only as bool=YES) as integer
 DECLARE FUNCTION find_menu_item_slot_by_string(byval menuslot as integer, s as string, byval mislot as integer=0, byval visible_only as bool=YES) as integer
 
-DECLARE FUNCTION valid_joystick(byval joynum as integer) as bool
+DECLARE FUNCTION valid_player_num(byval player as integer) as bool
 DECLARE FUNCTION valid_item_slot(byval item_slot as integer) as bool
 DECLARE FUNCTION valid_item(byval itemid as integer) as bool
 DECLARE FUNCTION valid_hero_caterpillar_rank(who as integer) as bool

testgame/inputtest.hss
@@ -99,7 +99,7 @@ plotscript, joystick main, begin
     )
 
     update joystick strings (joynum)
-    update buttons (cancelkey, anykey)  #-9 to -1
+    update buttons (cancelkey, anykey, joynum)  #-9 to -1
     update buttons (joy:button1, joy:button32, joynum, true)
     # Duplicate buttons
     update buttons (joy:button1, joy:button32, joynum, true, lookup slice(sli:right stick))

commit a925dbc9a133409dbee99b16c7ae5d0030e03a5e

Fix numlock not affecting script commands, another regression caused by r12270
Since Gorgonzola, Numlock (and shift) are meant to affect whether numpad keys
are mapped to arrow keys, etc. Only script commands reading cc* scancodes were
affected. This bug meant that "keypress(left key)" etc. could return false as
the player walks around.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12412 7d344553-34f0-0310-a9b1-970ce8f1c3a2

allmodex.bas
@@ -91,6 +91,8 @@ declare sub masterpal_changed()
 
 declare sub pollingthread(as any ptr)
 declare sub keystate_convert_bit3_to_keybits(keystate() as KeyBits)
+declare function numpad_alias_key(key as KBScancode, real_keys as bool) as KBScancode
+declare function player_keyval_ex(key as KBScancode, player as integer, repeat_wait as integer = 0, repeat_rate as integer = 0, check_keyboard as bool = YES) as KeyBits
 declare function keyval_or_numpad_ex(key as KBScancode, repeat_wait as integer = 0, repeat_rate as integer = 0, real_keys as bool = NO) as KeyBits
 declare function read_inputtext () as string
 declare sub update_mouse_state ()
@@ -1794,6 +1796,14 @@ end function
 'player: 0 for any input device, 1 is first joystick + keyboard, 2-4 are other joysticks
 'check_keyboard: pass false to ignore keyboard input
 function player_keyval(key as KBScancode, player as integer, repeat_wait as integer = 0, repeat_rate as integer = 0, check_keyboard as bool = YES) as KeyBits
+	'This is a modified copy of keyval_or_numpad_ex
+	dim ret as KeyBits = player_keyval_ex(key, player, repeat_wait, repeat_rate, check_keyboard)
+	dim key2 as KBScancode = numpad_alias_key(key, NO)
+	if key2 then ret or= player_keyval_ex(key2, player, repeat_wait, repeat_rate, check_keyboard)
+	return ret
+end function
+
+local function player_keyval_ex(key as KBScancode, player as integer, repeat_wait as integer = 0, repeat_rate as integer = 0, check_keyboard as bool = YES) as KeyBits
 	BUG_IF(player < 0, "Invalid player " & player, 0)
 	BUG_IF(key < scKEYVAL_FIRST orelse key > scKEYVAL_LAST, "bad scancode " & key, 0)
 
@@ -1802,7 +1812,7 @@ function player_keyval(key as KBScancode, player as integer, repeat_wait as inte
 	if player = 0 then
 		'Merge all inputs
 		for player = 1 to num_joysticks()
-			ret or= player_keyval(key, player, repeat_wait, repeat_rate, check_keyboard)
+			ret or= player_keyval_ex(key, player, repeat_wait, repeat_rate, check_keyboard)
 		next
 		return ret
 	end if
@@ -1825,7 +1835,7 @@ function player_keyval(key as KBScancode, player as integer, repeat_wait as inte
 		end if
 	elseif key <= scLAST then  'Keyboard key
 		if player = 1 andalso check_keyboard then
-			ret = keyval_or_numpad_ex(key, repeat_wait, repeat_rate)
+			ret = keyval_ex(key, repeat_wait, repeat_rate)
 		end if
 	else  'Joystick button
 		dim button as integer = keybd_to_joy_scancode(key)