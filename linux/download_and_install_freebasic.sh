9ed06d359e49a0a035230be19b664b6a3c83b62f
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2021-09-19 20:20:05 +0000

    Update James's convenience script for installing FreeBasic on a nightly build VM
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12451 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- linux/download_and_install_freebasic.sh
+++ linux/download_and_install_freebasic.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-FBC_VERSION=1.07.3
+FBC_VERSION=1.08.1
 
-if [ -x "`which fbc`" ] ; then
-  echo "FreeBasic seems to already be installed."
+ARCH="${1}"
+
+if [ "${ARCH}" = "64" -o "${ARCH}" = "32" ] ; then
+  echo "Selected ${ARCH}bit version of FreeBasic"
+else
+  echo "You must specify which version of Freebasic to install on the command-line. 32 bit or 64 bit. For example:"
+  echo "  ./download_and_install_freebasic.sh 32"
+  echo "  ./download_and_install_freebasic.sh 64"
   exit 1
+fi
+
+if [ "${ARCH}" = "64" ] ; then
+  ARCH_SUFFIX="x86_64"
+else
+  ARCH_SUFFIX="x86"
 fi
 
 if [ ! -x "`which wget`" ] ; then
@@ -20,9 +32,9 @@ fi
 mkdir "${DLTEMP}"
 cd "${DLTEMP}"
 
-wget "https://downloads.sourceforge.net/project/fbc/FreeBASIC-${FBC_VERSION}/Binaries-Linux/FreeBASIC-${FBC_VERSION}-linux-x86_64.tar.gz"
-tar -xvf "FreeBASIC-${FBC_VERSION}-linux-x86_64.tar.gz"
-cd "FreeBASIC-${FBC_VERSION}-linux-x86_64"
+wget "https://downloads.sourceforge.net/project/fbc/FreeBASIC-${FBC_VERSION}/Binaries-Linux/FreeBASIC-${FBC_VERSION}-linux-${ARCH_SUFFIX}.tar.gz"
+tar -xvf "FreeBASIC-${FBC_VERSION}-linux-${ARCH_SUFFIX}.tar.gz"
+cd "FreeBASIC-${FBC_VERSION}-linux-${ARCH_SUFFIX}"
 echo "Running FreeBasic installer with sudo..."
 sudo ./install.sh -i
 
