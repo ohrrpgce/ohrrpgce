35901348c2e4e4a0797fcb9cac0bc81b98257a13
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2021-09-24 14:37:30 +0000

    release-itchio.sh now uses the real release codename and date
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12479 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-itchio.sh
+++ release/release-itchio.sh
@@ -41,14 +41,30 @@ else
 fi
 
 echo "Fetching OHRRPGCE stable release files"
-#rm -Rf ./releases
+rm -Rf ./releases
 mkdir -p releases
 curl -L -s -o releases/ohrrpgce.zip https://hamsterrepublic.com/dl/ohrrpgce.zip
 curl -L -s -o releases/OHRRPGCE-x86_64.dmg https://hamsterrepublic.com/dl/OHRRPGCE-x86_64.dmg
 curl -L -s -o releases/ohrrpgce-linux-x86_64.tar.bz2 https://hamsterrepublic.com/dl/ohrrpgce-linux-x86_64.tar.bz2
+# We don't upload the last one, it is just for buildinfo.ini
+curl -L -s -o releases/ohrrpgce-player-linux-bin-minimal-x86_64.zip https://hamsterrepublic.com/dl/ohrrpgce-player-linux-bin-minimal-x86_64.zip
+
+echo "Parsing release name"
+rm -f releases/buildinfo.ini
+unzip -q releases/ohrrpgce-player-linux-bin-minimal-x86_64.zip buildinfo.ini -d ./releases/
+if [ ! -f "releases/buildinfo.ini" ] ; then
+  echo "Unable to find buildinfo.ini"
+  exit 1
+fi
+BUILD_DATE=$( grep "^build_date=" "releases/buildinfo.ini" | cut -d "=" -f 2 )
+SVN_REV=$( grep "^svn_rev=" "releases/buildinfo.ini" | cut -d "=" -f 2 )
+CODE_NAME=$( grep -a "^code_name=" "releases/buildinfo.ini" | cut -d "=" -f 2 )
+VERSION_NAME="${CODE_NAME}_${BUILD_DATE}.${SVN_REV}"
+echo $VERSION_NAME | iconv -f ISO-8859-1 -t UTF-8 - > buildnumber.txt
+echo "Found version $( cat buildnumber.txt )"
 
 echo "Uploading release to bob-the-hamster.itch.io..."
 echo "The ohrrpgce game project should already exist..."
-./butler push releases/ohrrpgce.zip bob-the-hamster/ohrrpgce:windows-stable
-./butler push releases/OHRRPGCE-x86_64.dmg bob-the-hamster/ohrrpgce:mac-stable
-./butler push releases/ohrrpgce-linux-x86_64.tar.bz2 bob-the-hamster/ohrrpgce:linux-stable
+./butler push releases/ohrrpgce.zip bob-the-hamster/ohrrpgce:windows-stable --userversion-file buildnumber.txt
+./butler push releases/OHRRPGCE-x86_64.dmg bob-the-hamster/ohrrpgce:mac-stable --userversion-file buildnumber.txt
+./butler push releases/ohrrpgce-linux-x86_64.tar.bz2 bob-the-hamster/ohrrpgce:linux-stable --userversion-file buildnumber.txt
