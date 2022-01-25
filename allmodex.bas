ohrrpgce/ohrrpgce

commit c4574d7c51bcd2179256083e82dbd09054b181e0

Add a mechanism for checking version of support utilities and upgrading them as needed
This replaces the zip-version-3.0.txt kludge with a similar generic system,
which is to have an optional <appname>-version.txt next to the .exe containing
numeric and textual version codes. But instead of Custom creating or modifying
the -version.txt file, it will be included in the downloaded .zip, so it's
always kept correct when a support zip is extracted over the existing files.

Also, support/*-version.txt files are packaged with engine downloads that
include support utilities, to avoid unnecessary downloads.

Only utilities for which we care about the version code need a -version.txt file
in the .zip, and right now that's only zip.exe. I've updated zip.zip on HR.com
to include zip-version.txt. The old zip-version-3.0.txt if present is
ignored.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12366 7d344553-34f0-0310-a9b1-970ce8f1c3a2

common.rbas
@@ -3501,6 +3501,23 @@ FUNCTION get_support_dir () as string
  RETURN "" ' not found. booo! :(
 END FUNCTION
 
+'Some helper utilities have an <appname>-version.txt file (included in the download) containing:
+'-line 0: a numeric version code (returned)
+'-line 1: a textual version code (returned in version_text)
+'-rest: comments
+'Returns 0 if the version file doesn't exist.
+'apppath: path to the .exe
+FUNCTION get_helper_app_version(apppath as string, byref version_text as string = "") as integer
+ DIM version_file as string = trimextension(apppath) & "-version.txt"
+ DIM lines() as string
+ version_text = "<unknown>"
+ IF NOT lines_from_file(lines(), version_file, NO) THEN RETURN 0  'expect_exists=NO
+ IF UBOUND(lines) >= 1 THEN
+  version_text = lines(1)
+  RETURN str2int(lines(0), 0)
+ END IF
+END FUNCTION
+
 #MACRO try_app_path(path)
  temp = path
  IF isfile(temp) THEN RETURN temp
@@ -3544,30 +3561,41 @@ END FUNCTION
 
 FUNCTION find_windows_helper_app (appname as string, try_install as bool=NO, download_url as string="") as string
  'Gets the windows version of the helper app, even when run on Linux or Mac (for utils to be run via wine)
- 'Returns an empty string if the app is not found, or the full path if it is found
- 'try_install: download it if not installed
+ 'Returns an empty string if the app is not found or version is too old, or the full path if it is found
+ 'try_install: download it if not installed or too old
  'download_url: only used if try_install is true, to override the default
 
  'For windows, first look in the support subdirectory
  '(This is required so we can replace unusable utilities, eg create_tarball)
  DIM support as string = get_support_dir()
  DIM apppath as string = support & SLASH & appname & ".exe"
+
+ 'Special case logic to force upgrade of certain utilities to a specified version
+ DIM need_version as integer
+ DIM need_version_text as string
  IF appname = "zip" THEN
-  'A little special magic to make that if we are looking for zip.exe we find version 3.0 and not 2.3
-  IF NOT isfile(support & SLASH & appname & "-version-3.0.txt") THEN
-   safekill apppath
-   DIM fh as integer = FREEFILE
-   OPEN support & SLASH & appname & "-version-3.0.txt" FOR OUTPUT AS #fh
-   PRINT #fh, "This file is just here to let the OHRRPGCE know that we have already upgraded zip.exe from version 2.3 to 3.0"
-   CLOSE #fh
+  need_version = 300  'Not 2.3
+  need_version_text = "3.0"
+  'Previously the zip.exe version was indicated by presence of zip-version-3.0.txt, which we now ignore
+ END IF
+
+ DIM have_version as integer
+ IF isfile(apppath) THEN
+  have_version = get_helper_app_version(apppath)
+  IF have_version < need_version THEN
+   'Reject it; it must be redownloaded
+   debuginfo strprintf("Ignoring %s which is version %d, need version %d", strptr(apppath), have_version, need_version)
+  ELSE
+   RETURN apppath
   END IF
  END IF
- IF isfile(apppath) THEN RETURN apppath
+
+ DIM ret as string
 
  #IFDEF __FB_WIN32__
-  IF is_windows_9x() = NO THEN
+  IF is_windows_9x() = NO ANDALSO need_version = 0 THEN
    ' Check whether appname.exe is in the $PATH  (command.com doesn't seem to support this syntax)
-   DIM ret as string
+   ' Don't do this if we require a specific version, because we won't know (presumably there won't be a version.txt)
    ' Prepend a _ because if its args are blank echo prints "ECHO ON."
    run_and_get_output "for %X in (" & appname & ".exe) do echo _%~$PATH:X", ret, , NO 'disable debuginfo spam
    ret = RTRIM(ret, ANY !"\n\r")
@@ -3575,8 +3603,19 @@ FUNCTION find_windows_helper_app (appname as string, try_install as bool=NO, dow
   END IF
  #ENDIF
 
- IF try_install THEN RETURN install_windows_helper_app(appname, download_url)
- RETURN ""
+ IF try_install THEN
+  ret = install_windows_helper_app(appname, download_url)
+  IF LEN(ret) THEN
+   DIM version_text as string
+   have_version = get_helper_app_version(ret, version_text)
+   debuginfo strprintf("Downloaded %s version %s (%d)", strptr(ret), strptr(version_text), have_version)
+   IF have_version < need_version THEN
+    'Someone forgot to put the version file in the download? But we'll proceed with whatever got downloaded
+    showbug "Downloaded a fresh copy of " & appname & " but it was only version " & version_text & ", expected >= " & need_version_text
+   END IF
+  END IF
+ END IF
+ RETURN ret
 END FUNCTION
 
 'Returns "" on failure, otherwise path to the executable
@@ -3591,7 +3630,7 @@ FUNCTION install_windows_helper_app (appname as string, download_url as string="
   download_url = "http://HamsterRepublic.com/ohrrpgce/support/" & appname & "." & extension
  END IF
  DIM choice as integer
- choice = twochoice(appname & ".exe was not found. Would you like to automatically download it from " _
+ choice = twochoice(appname & ".exe was not found (or incorrect version). Would you like to automatically download it from " _
                     & url_hostname(download_url) & "?", , , , , "download_win_support_util")
  IF choice = 0 THEN
   download_file download_url, support
@@ -3600,11 +3639,14 @@ FUNCTION install_windows_helper_app (appname as string, download_url as string="
    RETURN ""
   END IF
   IF appname <> "unzip" THEN
+   'Unzip all files from the .zip
    DIM unzip as string = find_helper_app("unzip", YES)
    IF unzip = "" THEN
     visible_debug "Can't find unzip tool"
     RETURN ""
    END IF
+   safekill apppath
+   safekill trimextension(apppath) & "-version.txt"
    ' -q quiet -o overwrite -C case-insenstive -L make lowercase -j junk directories
    DIM arglist as string
    DIM zipfile as string = support & SLASH & appname & ".zip"

common.bi
@@ -218,6 +218,7 @@ DECLARE FUNCTION spawn_and_wait (app as string, args as string, expect_exitcode_
 DECLARE FUNCTION get_support_dir () as string
 DECLARE FUNCTION find_helper_app (appname as zstring ptr, try_install as bool=NO, download_url as zstring ptr=@"") as string
 DECLARE FUNCTION find_windows_helper_app (appname as string, try_install as bool=NO, download_url as string="") as string
+DECLARE FUNCTION get_helper_app_version (apppath as string, byref version_text as string = "") as integer
 DECLARE FUNCTION install_windows_helper_app (appname as string, download_url as string="") as string
 DECLARE FUNCTION download_file (url as string, dest as string, forcefilename as string="") as bool
 DECLARE FUNCTION missing_helper_message (appname as string) as string

distrib-nightly-win-packnupload.bat
@@ -22,6 +22,7 @@ support\zip -q distrib\%ZIPFILE% docs\plotdictionary.html docs\*.png docs\more-d
 support\zip -q distrib\%ZIPFILE% support\madplay.exe support\LICENSE-madplay.txt
 support\zip -q distrib\%ZIPFILE% support\oggenc.exe support\LICENSE-oggenc.txt
 support\zip -q distrib\%ZIPFILE% support\zip.exe
+support\zip -q distrib\%ZIPFILE% support\*-version.txt
 support\zip -q distrib\%ZIPFILE% support\wget.exe
 support\zip -q distrib\%ZIPFILE% support\CrashRpt*.dll support\CrashSender*.exe support\crashrpt_lang.ini
 support\zip -q distrib\%ZIPFILE% support\LICENSE-crashrpt.txt

distrib-nightly-wine.sh
@@ -52,6 +52,7 @@ function zip_and_upload {
   zip -q distrib/"${ZIPFILE}" support/oggenc.exe support/LICENSE-oggenc.txt
   zip -q distrib/"${ZIPFILE}" support/zip.exe
   zip -q distrib/"${ZIPFILE}" support/wget.exe
+  zip -q distrib/"${ZIPFILE}" support/*-version.txt
   zip -q distrib/"${ZIPFILE}" support/CrashRpt*.dll support/CrashSender*.exe support/crashrpt_lang.ini
   zip -q distrib/"${ZIPFILE}" support/LICENSE-crashrpt.txt
   cp relump.exe support/

distrib-win.bat
@@ -127,6 +127,7 @@ support\cp support\CrashRpt*.dll support\CrashSender*.exe support\crashrpt_lang.
 support\cp support\LICENSE-crashrpt.txt tmpdist\support
 support\cp support\rcedit.exe tmpdist\support
 support\cp support\LICENSE-rcedit.txt tmpdist\support
+support\cp support\*-version.txt tmpdist\support
 support\cp relump.exe tmpdist\support
 support\cp unlump.exe tmpdist\support
 support\cp vikings.rpg tmpdist

distrib-wine.sh
@@ -40,6 +40,7 @@ function ohrrpgce_common_files {
  cp support/unzip.exe tmpdist/support
  cp support/rcedit.exe tmpdist/support
  cp support/LICENSE-rcedit.txt tmpdist/support
+ cp support/*-version.txt tmpdist/support
  cp relump.exe tmpdist/support
  cp unlump.exe tmpdist/support
  cp -r data tmpdist/data

ohrrpgce.iss
@@ -61,6 +61,7 @@ Source: "support\CrashRpt*.dll"; DestDir: "{app}\support\"; Flags: ignoreversion
 Source: "support\CrashSender*.exe"; DestDir: "{app}\support\"; Flags: ignoreversion
 Source: "support\crashrpt_lang.ini"; DestDir: "{app}\support\"; Flags: ignoreversion
 Source: "support\rcedit.exe"; DestDir: "{app}\support\"; Flags: ignoreversion
+Source: "support\*-version.txt"; DestDir: "{app}\support\"; Flags: ignoreversion
 Source: "relump.exe"; DestDir: "{app}\support\"; Flags: ignoreversion
 Source: "unlump.exe"; DestDir: "{app}\support\"; Flags: ignoreversion
 Source: "vikings.rpg"; DestDir: "{app}"; Flags: ignoreversion

util.bas
@@ -4153,6 +4153,7 @@ SUB string_to_file (string_to_write as string, filename as string)
 END SUB
 
 'Read each line of a file into a string array. Return true on success
+'See also string_from_first_line_of_file() if you only want the first.
 FUNCTION lines_from_file(strarray() as string, filename as string, expect_exists as bool = YES) as bool
  REDIM strarray(-1 TO -1)
  DIM as integer fh, openerr

commit 6042aea73570895800ee13f40410ad66ac4f0968

find_windows_helper_app returned "_" on Windows if the program wasn't found!
This lead to some weird errors, such as if hspeak.exe was missing. The problem
was worst when called with try_install=NO, since try_install did still work
which gave users a way around it. For example, this broke reading progressive
jpegs unless jpegtran was already installed.

I broke it in r12366.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12715 7d344553-34f0-0310-a9b1-970ce8f1c3a2

allmodex.bas
@@ -7683,7 +7683,7 @@ function get_jpegtran() as string
 		'a helpful message before the download prompt
 		ret = find_helper_app("jpegtran")
 		if len(ret) = 0 then
-			visible_debug "Can only read progressive JPEGs when the jpegtran program is installed (part of libjpeg-progs)"
+			visible_debug "Can only read progressive JPEGs using the jpegtran program (part of libjpeg-progs)"
 			ret = find_helper_app("jpegtran", YES, "http://jpegclub.org/jpegtran.zip")
 		end if
 	end if

common.rbas
@@ -3653,9 +3653,11 @@ FUNCTION find_windows_helper_app (appname as string, try_install as bool=NO, dow
     'Someone forgot to put the version file in the download? But we'll proceed with whatever got downloaded
     showbug "Downloaded a fresh copy of " & appname & " but it was only version " & version_text & ", expected >= " & need_version_text
    END IF
+   RETURN ret
   END IF
  END IF
- RETURN ret
+
+ RETURN ""
 END FUNCTION
 
 'Returns "" on failure, otherwise path to the executable