04544dacef03569bf79a4d130545756fa104f2d3
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2022-08-28 01:06:53 +0000

    Turn off curl's SSL cert chain validation in mac/get_sdl_frameworks.sh
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13086 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- mac/get_sdl_frameworks.sh
+++ mac/get_sdl_frameworks.sh
@@ -5,17 +5,17 @@ mkdir -p "mac_sdl_framework_downloads"
 cd "mac_sdl_framework_downloads"
 
 if [ ! -e "SDL_mixer-1.2.12.dmg" ] ; then
-  curl -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.dmg"
+  curl -k -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.dmg"
 fi
 
 if [ ! -e "SDL2_mixer-2.6.2.dmg" ] ; then
-  curl -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.6.2.dmg"
+  curl -k -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.6.2.dmg"
 fi
 
 if [ ! -e "SDL-1.2.15.dmg" ] ; then
-  curl -O "https://www.libsdl.org/release/SDL-1.2.15.dmg"
+  curl -k -O "https://www.libsdl.org/release/SDL-1.2.15.dmg"
 fi
 
 if [ ! -e "SDL2-2.24.0.dmg" ] ; then
-  curl -O "https://www.libsdl.org/release/SDL2-2.24.0.dmg"
+  curl -k -O "https://www.libsdl.org/release/SDL2-2.24.0.dmg"
 fi
