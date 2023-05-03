ohrrpgce/ohrrpgce

commit 79740a30baae8d9a5264e26d8527d62123cd1916

Update formation editor and "add enemy to formation" allow proper range of enemy coords
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13147 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bcommon.bi
@@ -17,5 +17,6 @@ DECLARE FUNCTION attack_placement_over_targetpos(attack as AttackData, targpos a
 
 DECLARE FUNCTION get_battlefield_size() as XYPair
 DECLARE FUNCTION get_battle_res() as XYPair
+DECLARE FUNCTION get_formation_bounds() as RectPoints
 
 #ENDIF

formationedit.bas
@@ -332,9 +332,9 @@ SUB hero_formation_editor ()
     IF readmouse.dragging AND mouseLeft THEN
      .pos += (readmouse.pos - readmouse.lastpos)
     END IF
-    'Hero positions are the bottom center of the sprite
-    .pos.x = bound(.pos.x, -500, gen(genResolutionX) + 500)
-    .pos.y = bound(.pos.y, -500, gen(genResolutionY) + 500)
+    'Hero positions are the bottom center of the sprite. Generous bounds.
+    DIM bounds as RectPoints = get_formation_bounds()
+    .pos = bound(.pos, bounds.topleft - 100, bounds.bottomright + 100)
    END WITH
   END IF
   IF positioning_mode = NO THEN
@@ -560,11 +560,10 @@ SUB FormationEditor.each_tick_positioning_mode()
   IF readmouse.dragging AND mouseLeft THEN
    .pos += (readmouse.pos - readmouse.lastpos)
   END IF
-  ' FIXME: battles are still stuck at 320x200 for the moment, but switch to this later
-  ' .pos.x = bound(.pos.x, -size.w\2, gen(genResolutionX) - size.w\2)
-  ' .pos.y = bound(.pos.y, -size.h\2, gen(genResolutionY) - size.h\2)
-  .pos.x = bound(.pos.x, -size.w\2, 320 - size.w\2)
-  .pos.y = bound(.pos.y, -size.h\2, 200 - size.h\2)
+
+  ' Allow placing an enemy anywhere onscreen (or just off), not just inside the 'battlefield'
+  DIM bounds as RectPoints = get_formation_bounds()
+  .pos = bound(.pos, bounds.topleft - size, bounds.bottomright)
  END WITH
 END SUB
 

bcommon.bas
@@ -280,3 +280,14 @@ FUNCTION get_battle_res() as XYPair
   RETURN get_battlefield_size()
  END IF
 END FUNCTION
+
+'The valid bounds for enemy/hero formation coords, not including the sprite size
+'Coords are relative to the top-left of battlefield_sl.
+FUNCTION get_formation_bounds() as RectPoints
+ DIM screensz as XYPair = get_battle_res()
+ DIM battlefieldsz as XYPair = get_battlefield_size()
+ DIM ret as RectPoints
+ ret.topleft = -(screensz - battlefieldsz) \ 2
+ ret.bottomright = screensz + ret.topleft
+ RETURN ret
+END FUNCTION

docs/plotdict.xml
@@ -4687,9 +4687,9 @@ end</example>
 			<description>
 				Adds an enemy with given <p>enemy id</p>
 				(use the constants of the form <p>enemy:name</p> in your exported .hsi file) to the specified <p>formation</p>.
-				<p>x</p> (0 to 320) and <p>y</p> (from roughly 17 to 240, depending on enemy size)
-				give the position of the center of the bottom edge of the enemy
-				- approximately where it appears to stand.
+				give the position of the center of the bottom edge of the enemy (approximately where it appears to stand),
+				relative to the center 320x200 of the screen.
+				Use the formation editor to get coordinates to feed into this command.
 				<p>slot</p> (0 to 7) is an optional argument,
 				if you specify it, then the enemy is created in the suggested slot if empty.
 				If you omit <p>slot</p> or <p>slot</p> isn't empty, the enemy is put in the first empty slot.
@@ -4759,7 +4759,10 @@ end
 		<command id="formationslotx">
 			<canon>formation slot x (formation, slot)</canon>
 			<shortname>formation slot x</shortname>
-			<description>Returns the x position of the center of the enemy in the specified formation slot (0 to 7). An enemies position is the center of the bottom edge of the enemy - approximately where it appears to stand.</description>
+			<description>
+				Returns the x position of the center of the enemy in the specified formation slot (0 to 7).
+				An enemy's position is the center of the bottom edge of the enemy - approximately where it appears to stand.
+			</description>
 			<seealso>
 				<ref>findenemyinformation</ref>
 			</seealso>
@@ -4767,7 +4770,10 @@ end
 		<command id="formationsloty">
 			<canon>formation slot y (formation, slot)</canon>
 			<shortname>formation slot y</shortname>
-			<description>Returns the y position of the bottom edge of the enemy sprite in the specified formation slot (0 to 7). An enemies position is the center of the bottom edge of the enemy - approximately where it appears to stand.</description>
+			<description>
+				Returns the y position of the bottom edge of the enemy sprite in the specified formation slot (0 to 7).
+				An enemy's position is the center of the bottom edge of the enemy - approximately where it appears to stand.
+			</description>
 			<seealso>
 				<ref>findenemyinformation</ref>
 			</seealso>

scriptcommands.bas
@@ -24,6 +24,7 @@
 #include "menustuf.bi"
 #include "bmod.bi"
 #include "bmodsubs.bi"
+#include "bcommon.bi"
 
 ''''' Local functions
 DECLARE SUB run_game ()
@@ -2232,12 +2233,11 @@ SUB script_commands(byval cmdid as integer)
      ' Convert to top-left coord
      .pos.x = retvals(2) - size.w \ 2
      .pos.y = retvals(3) - size.h
-     ' These are the same limits as used in the formation editor
-     ' FIXME: battles are still stuck at 320x200 for the moment, but switch to this later
-     ' .pos.x = bound(.pos.x, -size.w\2, get_resolution().w - size.w\2)
-     ' .pos.y = bound(.pos.y, -size.h\2, get_resolution().h - size.h\2)
-     .pos.x = bound(.pos.x, -size.w\2, 320 - size.w\2)
-     .pos.y = bound(.pos.y, -size.h\2, 200 - size.h\2)
+     ' These are the same limits as used in the formation editors;
+     ' they allow placing an enemy anywhere onscreen (or just off).
+     ' Note: 0,0 is not necessarily the top-left of the screen
+     DIM bounds as RectPoints = get_formation_bounds()
+     .pos = bound(.pos, bounds.topleft - size, bounds.bottomright)
     END WITH
    END IF
    SaveFormation form, retvals(0)

util.bas
@@ -499,7 +499,7 @@ END FUNCTION
 
 FUNCTION bound (point as XYPair, lefttop as XYPair, rightbottom as XYPair) as XYPair
  DIM ret as XYPair = any
- ret.x = IIF(point.x > rightbottom.x, rightbottom.x, IIF(point.y < lefttop.x, lefttop.x, point.x))
+ ret.x = IIF(point.x > rightbottom.x, rightbottom.x, IIF(point.x < lefttop.x, lefttop.x, point.x))
  ret.y = IIF(point.y > rightbottom.y, rightbottom.y, IIF(point.y < lefttop.y, lefttop.y, point.y))
  RETURN ret
 END FUNCTION

util.bi
@@ -697,9 +697,17 @@ DECLARE OPERATOR - (lhs as RectType, rhs as XYPair) as RectType
 DECLARE OPERATOR * (lhs as RectType, rhs as integer) as RectType
 
 'Specify opposite points instead of width and height
+'NOTE: p1 and p2 don't have to be the topleft/bottomright, some code
+'might accept other opposite corners.
 TYPE RectPoints
-  p1 as XYPair
-  p2 as XYPair
+  UNION
+    p1 as XYPair
+    topleft as XYPair
+  END UNION
+  UNION
+    p2 as XYPair
+    bottomright as XYPair
+  END UNION
 END TYPE
 
 'Used for menu and slice anchor points and slice align points

commit e7bf15de201fa95e2b734ee17f5de3efdf13d3a8

Fix display and bounding of position in hero form editor not including the offset
But the previous commit already papered over this mistake (introduced recently
in r13147) which prevented putting heroes on the left half of the screen.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13249 7d344553-34f0-0310-a9b1-970ce8f1c3a2

bcommon.bi
@@ -19,4 +19,6 @@ DECLARE FUNCTION get_battlefield_size() as XYPair
 DECLARE FUNCTION get_battle_res() as XYPair
 DECLARE FUNCTION get_formation_bounds() as RectPoints
 
+#DEFINE HERO_FORM_OFFSET XY(240, 82)
+
 #ENDIF

bmod.rbas
@@ -2220,7 +2220,7 @@ SUB battle_loadall(byval form as integer, byref bat as BattleState, bslot() as B
     setup_battlesprite_slice bslot(i), bat, sprTypeHero, hero.battle_pic, hero.battle_pal
 
     'hero_form specifies bottom center relative to the original hero zone
-    .basepos = hero_form.slots(i).pos + XY(240, 82) - XY(.w \ 2, .h)
+    .basepos = hero_form.slots(i).pos + HERO_FORM_OFFSET - XY(.w \ 2, .h)
     (.pos) = .basepos
     .vis = YES
 

formationedit.bas
@@ -334,7 +334,7 @@ SUB hero_formation_editor ()
     END IF
     'Hero positions are the bottom center of the sprite. Very generous bounds.
     DIM bounds as RectPoints = get_formation_bounds()
-    .pos = bound(.pos, bounds.topleft - 1000, bounds.bottomright + 1000)
+    .pos = bound(.pos, bounds.topleft - HERO_FORM_OFFSET - 1000, bounds.bottomright - HERO_FORM_OFFSET + 1000)
    END WITH
   END IF
   IF positioning_mode = NO THEN
@@ -396,7 +396,8 @@ SUB hero_formation_editor ()
   IF positioning_mode THEN
    edgeprint "Arrow keys or mouse-drag", pMenuX, pMenuY, uilook(uiText), dpage
    edgeprint "ESC or right-click when done", pInfoX, pInfoY, uilook(uiText), dpage
-   edgeprint "x=" & hform.slots(slot).pos.x & " y=" & hform.slots(slot).pos.y, pInfoRight, pMenuY, uilook(uiMenuItem), dpage
+   DIM pos as XYPair = hform.slots(slot).pos + HERO_FORM_OFFSET
+   edgeprint "x=" & pos.x & " y=" & pos.y, pInfoRight, pMenuY, uilook(uiMenuItem), dpage
   ELSE
    menu(0) = "Previous Menu"
    menu(1) = CHR(27) + "Hero Formation " & hero_form_id & CHR(26)
@@ -871,7 +872,7 @@ SUB draw_formation_slices(eform as Formation, hform as HeroFormation, rootslice 
  DIM hrect as Slice Ptr
  FOR i as integer = 0 TO 3
   hrect = LookupSlice(SL_FORMEDITOR_HERO + i, rootslice)
-  hrect->Pos = hform.slots(i).pos + XY(240, 82)
+  hrect->Pos = hform.slots(i).pos + HERO_FORM_OFFSET
   ' Set layering, like slAutoSortBottomY but break ties according to the order in bslot()
   ' (Hero slices are anchored by the bottom-center edge)
   hrect->Sorter = hrect->Y * 1000 + i