117c5d6201b0d16f34ca19edd0101d2af3e92ff3
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2023-06-03 13:10:52 +0000

    Also update dreamhost smtp server in windows nightly batch file wrapper
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13283 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- nightly/wrap-nightly-win.bat
+++ nightly/wrap-nightly-win.bat
@@ -9,7 +9,7 @@ cmd /c distrib-nightly-win.bat 1> c:\nightly\distrib-out.txt 2>&1
 CD C:\nightly
 
 REM https://github.com/muquit/mailsend
-mailsend.exe -to cron@rpg.hamsterrepublic.com -from cron@rpg.hamsterrepublic.com -sub "OHRRPGCE Windows nightly build (%USERDOMAIN%)" -smtp sub5.mail.dreamhost.com -port 465 -user cron@rpg.hamsterrepublic.com -pass REPLACEWITHREALPASSWORD -ssl -auth -mime-type "text/plain" -msg-body distrib-out.txt
+mailsend.exe -to cron@rpg.hamsterrepublic.com -from cron@rpg.hamsterrepublic.com -sub "OHRRPGCE Windows nightly build (%USERDOMAIN%)" -smtp smtp.dreamhost.com -port 465 -user cron@rpg.hamsterrepublic.com -pass REPLACEWITHREALPASSWORD -ssl -auth -mime-type "text/plain" -msg-body distrib-out.txt
 
 CHOICE /m "DONE WILL SHUT DOWN IN 5 SECONDS" /n /d "Y" /t 5
 shutdown /p
