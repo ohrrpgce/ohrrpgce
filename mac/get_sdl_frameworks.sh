ae14fd5b7ffb371df3e527809468d195332e1cfc
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2023-09-04 22:02:07 +0000

    Bump the versions of SDL2 and SDL2_mixer downloaded by mac/get_sdl_frameworks.sh
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13315 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- mac/get_sdl_frameworks.sh
+++ mac/get_sdl_frameworks.sh
@@ -8,14 +8,14 @@ if [ ! -e "SDL_mixer-1.2.12.dmg" ] ; then
   curl -k -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.dmg"
 fi
 
-if [ ! -e "SDL2_mixer-2.6.2.dmg" ] ; then
-  curl -k -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.6.2.dmg"
+if [ ! -e "SDL2_mixer-2.6.3.dmg" ] ; then
+  curl -k -O "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.6.3.dmg"
 fi
 
 if [ ! -e "SDL-1.2.15.dmg" ] ; then
   curl -k -O "https://www.libsdl.org/release/SDL-1.2.15.dmg"
 fi
 
-if [ ! -e "SDL2-2.24.0.dmg" ] ; then
-  curl -k -O "https://www.libsdl.org/release/SDL2-2.24.0.dmg"
+if [ ! -e "SDL2-2.28.3.dmg" ] ; then
+  curl -k -O "https://www.libsdl.org/release/SDL2-2.28.3.dmg"
 fi
