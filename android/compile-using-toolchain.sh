ecb960ca9fb2aa90f20c194d79736e34ae8e1f17
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2023-10-30 01:21:46 +0000

    Further update android/compile-using-toolchain.sh for non-standalone toolchains
    Still doesn't fully work due to gcc/clang not knowing the paths to system
    headers. But I thought it was working before, when I added STANDALONE as a
    config option in the first place.
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13358 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- android/compile-using-toolchain.sh
+++ android/compile-using-toolchain.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 # This is a helper and example script for optionally installing a standalone NDK toolchain,
 # and some examples of compiling commandline FB or C programs for android.
+# WARNING: For years now NDKs no longer support standalong toolchains,
+# and this script isn't fully updated to support those NDKs!
 # It is NOT used to create an .apk
+
+set -e
 
 echo "Please edit the config variables at the top of this file and the stuff to be compiled at the bottom of the file"
 exit
+
+DIR="$(dirname "$(readlink -e "$0")")"
 
 ########################## Config
 
@@ -25,6 +31,8 @@ NDK=/opt/android-ndk-r12b
 # of the NDK, requires ~450MB!).
 # Note: compiling OHR programs with scons won't work without a standalone toolchain,
 # unless it's a newer NDK that already acts like a standalone toolchain (not tested!)
+# (TODO: follow the instructions at https://android.googlesource.com/platform/ndk/+/master/docs/UnifiedHeaders.md
+# to get it working.)
 # NDK standalone toolchains are obsolete beginning with NDK r19, and no longer needed.
 STANDALONE=1
 
@@ -38,7 +46,6 @@ TOOLCHAIN=$HOME/local/android-toolchain-r12b-api9-arm
 #TOOLCHAIN=$NDK/toolchains/aarch64-linux-androideabi-4.9/prebuilt/$HOST
 #TOOLCHAIN=$NDK/toolchains/x86-4.6/prebuilt/$HOST
 
-echo $TOOLCHAIN
 
 ########################## Install stand-alone toolchain if it hasn't been already
 
@@ -61,16 +68,32 @@ fi
 if [ $STANDALONE ]; then
     if [ ! -d $TOOLCHAIN ]; then
         # Defaults to armeabi
-        $NDK/build/tools/make_standalone_toolchain.py --arch $ARCH --api $API --install-dir $TOOLCHAIN
+        "$NDK/build/tools/make_standalone_toolchain.py" --arch $ARCH --api $API --install-dir "$TOOLCHAIN"
     fi
+
+    PATH=$TOOLCHAIN/bin:$PATH
+    echo "TOOLCHAIN = $TOOLCHAIN"
+
+else
+    # SYSROOT is only used when not using a standalone toolchain
+
+    SYSROOT="$NDK/platforms/android-$API/arch-$ARCH"
+    # Newer NDK (r14+?), -isystem arg for unified headers
+    ISYSTEM="$NDK/sysroot/usr/include/$TARGET"
+
+    if [ ! -d $SYSROOT ]; then
+        echo "$SYSROOT doesn't exist, please edit config vars in this file."
+        exit 1
+    fi
+
+    NDK_BIN=$(dirname $("$NDK/ndk-which" gcc))
+    PATH="$NDK_BIN:$PATH"
 fi
 
-# SYSROOT is only used when not using a standalone toolchain
-SYSROOT=$NDK/platforms/android-$API/arch-$ARCH
+
 
 ########################## Examples
 
-PATH=$TOOLCHAIN/bin:$PATH
 
 # An example of compiling testcases for android:
 cd .. && scons -j6 fbc=$FBC target=$TARGET unlump relump vectortest reloadtest filetest utiltest rbtest reloadutil reload2xml
@@ -82,18 +105,25 @@ if [ ! $STANDALONE ]; then
 fi
 
 # An example of compiling a commandline FB program for android:
-$FBC -target $TARGET -g test.bas -v
+$FBC -target $TARGET -sysroot "$SYSROOT" -g test.bas -v
 
 # If you want to try the examples below, not using scons, need to set these
-export CC=$TOOLCHAIN/bin/$TARGET-gcc   # or export CC=clang
-#export CXX=$TOOLCHAIN/bin/$TARGET-g++  # or export CXX=clang++
-export AS=$TOOLCHAIN/bin/$TARGET-as  # or export CXX=clang++
+if [ $STANDALONE ]; then
+    export CC=$TOOLCHAIN/bin/$TARGET-gcc   # or export CC=clang
+    #export CXX=$TOOLCHAIN/bin/$TARGET-g++  # or export CXX=clang++
+    export AS=$TOOLCHAIN/bin/$TARGET-as  # or export CXX=clang++
+    #export OBJDUMP=$TOOLCHAIN/bin/$TARGET-objdump
+else
+    export CC=$("$NDK/ndk-which" gcc)
+    export AS=$("$NDK/ndk-which" as)
+fi
 export GCC=$CC
-#export OBJDUMP=$TOOLCHAIN/bin/$TARGET-objdump
 
 # Examples of compiling a commandline C program for android:
 #$CC hello.c -o hello -g $CFLAGS -march=armv7-a
 #$CC hello.c -static -o hello.o -g $CFLAGS -march=armv7-a
+
+#$CC jnitest.c -o hello -g $CFLAGS -march=armv7-a
 
 # An example of compiling a commandline FB program for android and manually linking it:
 #BASFILE=test
