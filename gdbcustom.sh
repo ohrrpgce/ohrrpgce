4fa024150ba1371e90a8f9d66d012650b55d5dfd
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2025-02-07 06:02:57 +0000

    gdbgame/gdbcustom.sh, on exit, now print errors left behind in g/c_debug.txt
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13761 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- gdbcustom.sh
+++ gdbcustom.sh
@@ -2,3 +2,10 @@
 DIR=`echo "${0}" | sed -e s/"gdbcustom.sh$"/""/`
 cd "${DIR}"
 gdb -x=misc/gdbcmds1.txt --args ./ohrrpgce-custom --rawexx --log . "${@}"
+if [ -f "c_debug.txt" ]; then
+    echo
+    ERRORS="$(grep " ! " c_debug.txt)"
+    COUNT=$(echo "$ERRORS" | wc -l)
+    echo "c_debug.txt left behind with $COUNT error lines. First:"
+    echo "$ERRORS" | head -n 10
+fi
--- gdbgame.sh
+++ gdbgame.sh
@@ -2,3 +2,10 @@
 DIR=`echo "${0}" | sed -e s/"gdbgame.sh$"/""/`
 cd $DIR
 gdb -x=misc/gdbcmds1.txt --args ./ohrrpgce-game --rawexx --log . "${@}"
+if [ -f "g_debug.txt" ]; then
+    echo
+    ERRORS="$(grep " ! " g_debug.txt)"
+    COUNT=$(echo "$ERRORS" | wc -l)
+    echo "g_debug.txt left behind with $COUNT error lines. First:"
+    echo "$ERRORS" | head -n 10
+fi
