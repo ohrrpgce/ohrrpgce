c909168e2a1b7b15bc1827693814c7985cd95f07
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2025-02-16 12:53:51 +0000

    valgame.sh needs to pause so you can see leak messages when run from Custom
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13797 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- valgame.sh
+++ valgame.sh
@@ -7,15 +7,20 @@
 # Add --show-reachable=yes for more complete memory leak checking.
 # With older valgrind, --db-attach=yes instead of --vgdb-error=1 can be convenient.
 
-if [ "$1" = "--no-pause" ]; then
+if [ "$1" = "--no-attach" ]; then
     # For spawning from Custom: don't pass any options that would cause valgrind to pause
     MOREOPTS=
+    # Wait for key so can see leaks before the xterm closes
+    WAIT=1
     shift
 else
-    echo "Pass --no-pause to avoid prompts or pausing to attach gdb"
+    echo "Pass --no-attach to avoid suppression prompts or pausing to attach gdb"
     MOREOPTS="--vgdb-error=1 --gen-suppressions=yes"
+    WAIT=
 fi
 
 DIR="$(dirname "$(readlink -e "$0")")"
 
 valgrind --suppressions=$DIR/misc/valgrind_suppressions.txt --track-fds=yes --read-var-info=yes --leak-check=full $MOREOPTS $DIR/ohrrpgce-game $*
+
+[ -n "$WAIT" ] && (echo "Press any key"; read)
