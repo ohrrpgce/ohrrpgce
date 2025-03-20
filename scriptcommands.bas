ohrrpgce/ohrrpgce

commit 790ee3e722d96d6818a39ea26f21a047a4642b99

Update "set hero/npc frame" for more frames
Note that there aren't proper tests

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13836 7d344553-34f0-0310-a9b1-970ce8f1c3a2

scriptcommands.bas
@@ -1444,8 +1444,12 @@ SUB script_commands(byval cmdid as integer)
    scriptret = 0
   END IF
  CASE 25'--set hero frame
-  IF valid_hero_caterpillar_rank(retvals(0)) THEN
-   herow(retvals(0)).wtog = bound(retvals(1), 0, WALKFRAMES - 1) * wtog_ticks()
+  DIM rank as integer = retvals(0)
+  IF valid_hero_caterpillar_rank(rank) THEN
+   'It's not important to bound to a currently valid frame (but .wtog should not be < 0),
+   'and you can defeat this bound by changing the direction/spriteset. We bound for an
+   'abundance of backcompat (previously clamped to 0/1), and so that "hero frame" is accurate.
+   herow(rank).wtog = bound(retvals(1), 0, max_wtog(herow(rank).sl, herodir(rank)))
   END IF
  CASE 27'--suspend overlay
   setbit gen(), genSuspendBits, suspendoverlay, 1
@@ -1855,6 +1859,7 @@ SUB script_commands(byval cmdid as integer)
   END IF
  CASE 191'--hero frame
   IF valid_hero_caterpillar_rank(retvals(0)) THEN
+   'Note that this can be beyond the last frame, if the direction or spriteset just changed.
    scriptret = wtog_to_frame(herow(retvals(0)).wtog)
   END IF
  CASE 195'--load sound (BACKWARDS COMPATABILITY HACK )
@@ -3671,7 +3676,12 @@ SUB script_commands(byval cmdid as integer)
 
  CASE 26'--set NPC frame
   npcref = getnpcref(retvals(0), 0)
-  IF npcref >= 0 THEN npc(npcref).wtog = bound(retvals(1), 0, WALKFRAMES - 1) * wtog_ticks()
+  IF npcref >= 0 THEN
+   WITH npc(npcref)
+    'See comments on "set hero frame"
+    .wtog = bound(retvals(1), 0, max_wtog(.sl, .dir))
+   END WITH
+  END IF
  CASE 39'--camera follows NPC
   npcref = getnpcref(retvals(0), 0)
   IF npcref >= 0 THEN
@@ -3879,6 +3889,7 @@ SUB script_commands(byval cmdid as integer)
   END IF
  CASE 192'--NPC frame
   npcref = getnpcref(retvals(0), 0)
+  'Note that this can be beyond the last frame, if the direction or spriteset just changed.
   IF npcref >= 0 THEN scriptret = wtog_to_frame(npc(npcref).wtog)
  CASE 193'--NPC extra
   npcref = getnpcref(retvals(0), 0)
@@ -6002,12 +6013,12 @@ FUNCTION valid_item(byval itemID as integer) as bool
  RETURN bound_arg(itemID, 0, gen(genMaxItem), "item ID")
 END FUNCTION
 
-'TODO: Only use this where a command should be able to act on empty caterpillar hero slots
+'Only use this where a command should be able to act on empty caterpillar hero slots!
 FUNCTION valid_hero_caterpillar_rank(who as integer) as bool
  RETURN bound_arg(who, 0, 3, "hero caterpillar party rank")
 END FUNCTION
 
-'TODO: Only use this where a command should be able to act on empty hero slots
+'Only use this where a command should be able to act on empty hero slots!
 '(for compatibility, that's most of them!)
 FUNCTION valid_hero_party(byval who as integer, byval minimum as integer=0) as bool
  RETURN bound_arg(who, minimum, 40, "hero party slot")

const.bi
@@ -411,8 +411,6 @@ CONST frameHURT = 5
 CONST frameWEAK = 6
 CONST frameDEAD = 7
 
-CONST WALKFRAMES = 2  'Number of frames per walkabout direction
-
 '---- Dissolve animations
 CONST dissolveRandomScatter = 0
 CONST dissolveCrossfade = 1

docs/plotdict.xml
@@ -1621,7 +1621,15 @@ walk hero(who, hero direction(hero), 0)</example>
 		<command id="setheroframe">
 			<canon>set hero frame (who, frame)</canon>
 			<shortname>set hero frame</shortname>
-			<description>Sets the walking frame of the specified hero to 0 or 1. The hero is specified by their rank in the walkabout party, 0-3</description>
+			<description>
+				Sets the walking frame of the specified hero.
+				<lb/>
+				This is the number of the walkabout frame, starting from 0,
+				in the group of frames for the hero's current facing direction.
+				If the walkabout set has two frames per direction, this should be 0 or 1.
+				<lb/>
+				The hero is specified by their rank in the walkabout party, 0-3.
+			</description>
 			<seealso>
 				<ref>heroframe</ref>
 			</seealso>
@@ -1629,7 +1637,14 @@ walk hero(who, hero direction(hero), 0)</example>
 		<command id="heroframe">
 			<canon>hero frame (who)</canon>
 			<shortname>hero frame</shortname>
-			<description>Returns the walking frame (0 or 1) of the specified hero. The hero is specified by their rank in the walkabout party, 0-3</description>
+			<description>
+				Returns the walking frame of the specified hero; see <ref>setheroframe</ref>.
+				The hero is specified by their rank in the walkabout party, 0-3.
+				<note>
+					The number returned can be higher than the last valid frame,
+					if you call it immediately after changing hero direction or spriteset.
+				</note>
+			</description>
 		</command>
 		<command id="setheroposition">
 			<canon>set hero position (who, x, y)</canon>
@@ -2108,7 +2123,15 @@ walk NPC(npc, NPC direction(npc), 0)</example>
 		<command id="setnpcframe">
 			<canon>set NPC frame (who, frame)</canon>
 			<shortname>set NPC frame</shortname>
-			<description>Sets the walking frame of the specified NPC to 0 or 1. You can use either an <ref>npcref</ref> or <ref>npcid</ref> to specify which NPC will change.</description>
+			<description>
+				Sets the walking frame of the specified NPC to 0 or 1.
+				<lb/>
+				This is the number of the walkabout frame, starting from 0,
+				in the group of frames for the NPC's current facing direction.
+				If the walkabout set has two frames per direction, this should be 0 or 1.
+				<lb/>
+				You can use either an <ref>npcref</ref> or <ref>npcid</ref> to specify which NPC will change.
+</description>
 			<seealso>
 				<ref>npcframe</ref>
 			</seealso>
@@ -2116,7 +2139,13 @@ walk NPC(npc, NPC direction(npc), 0)</example>
 		<command id="npcframe">
 			<canon>NPC frame (who)</canon>
 			<shortname>NPC frame</shortname>
-			<description>Returns the walking frame (0 or 1) of the specified NPC.</description>
+			<description>
+				Returns the walking frame of the specified NPC. See <ref>setnpcframe</ref>.
+				<note>
+					The number returned can be higher than the last valid frame,
+					if you call it immediately after changing NPC direction or spriteset.
+				</note>
+			</description>
 		</command>
 		<command id="setnpcposition">
 			<canon>set NPC position (who, X, Y)</canon>

game.bas
@@ -1813,6 +1813,7 @@ SUB update_npcs ()
      npc(o).z = heroz(0) 'NPC Z value is matched to the hero in update_vehicle_state for simplicity, but
                          'this is here in case of setheroz or setnpcz or loaded map state or other funkiness happens
      npc(o).dir = herodir(0)
+     'FIXME: this is definitely not going to work properly for more frames
      npc(o).wtog = herow(0).wtog
     END IF
    ELSE

testgame/autotest.hss
@@ -1146,11 +1146,18 @@ script, do hero cater tests, who, begin
   wait for hero(who)
   #FIXME: should probably test misaligned walking too
   # frame
-  variable(fr)
-  for(fr, 1, 8) do(
-    set hero frame(who, (fr ,mod, 2))
-    assert(hero frame(who) == (fr ,mod, 2))
-    w(fr)
+  variable(fr,dir)
+  for(dir, 0, 3) do(
+    for(fr, 0, 1) do(
+      set hero frame(who, fr)
+      set hero direction(who, dir)
+      assert(hero frame(who) == fr)
+      w
+    )
+    set hero frame(who, -1)
+    assert(hero frame(who) == 0)
+    set hero frame(who, 2)
+    assert(hero frame(who) == 1)
   )
   #pixel pos
   variable(px, py)

commit eb0676804c6309caf0bdec74943c9c200834a5ef

Fix "set npc/hero frame" commands, broken in r13836
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13852 7d344553-34f0-0310-a9b1-970ce8f1c3a2

scriptcommands.bas
@@ -1449,7 +1449,7 @@ SUB script_commands(byval cmdid as integer)
    'It's not important to bound to a currently valid frame (but .wtog should not be < 0),
    'and you can defeat this bound by changing the direction/spriteset. We bound for an
    'abundance of backcompat (previously clamped to 0/1), and so that "hero frame" is accurate.
-   herow(rank).wtog = bound(retvals(1), 0, max_wtog(herow(rank).sl, herodir(rank)))
+   herow(rank).wtog = bound(retvals(1) * wtog_ticks(), 0, max_wtog(herow(rank).sl, herodir(rank)))
   END IF
  CASE 27'--suspend overlay
   setbit gen(), genSuspendBits, suspendoverlay, 1
@@ -3679,7 +3679,7 @@ SUB script_commands(byval cmdid as integer)
   IF npcref >= 0 THEN
    WITH npc(npcref)
     'See comments on "set hero frame"
-    .wtog = bound(retvals(1), 0, max_wtog(.sl, .dir))
+    .wtog = bound(retvals(1) * wtog_ticks(), 0, max_wtog(.sl, .dir))
    END WITH
   END IF
  CASE 39'--camera follows NPC