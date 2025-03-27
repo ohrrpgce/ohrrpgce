52dc74274ca2b61420f05069434f78a70743f596
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2025-03-27 00:44:46 +0000

    In release-web.sh use UTC date. The docker image runs in UTC timezone
    This fixes the case where this script breaks if you are in Toronto and you run the script after 8pm EDT
    
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/jocoserious@13867 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-web.sh
+++ release/release-web.sh
@@ -8,7 +8,7 @@ OHRDIR="$(pwd)" "./docker/ohrrpgce-build-env-emscripten/emscr.sh" -c /src/ohr/di
 
 SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
 
-TODAY=`date "+%Y-%m-%d"`
+TODAY=`date --utc "+%Y-%m-%d"`
 CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
 BRANCH=`cat codename.txt | grep -v "^#" | head -2 | tail -1 | tr -d "\r"`
 scp -p distrib/ohrrpgce-player-web-${TODAY}-${BRANCH}.zip "${SCPDEST}"/archive/
