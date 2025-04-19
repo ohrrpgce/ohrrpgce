99e04cdc55638c98b0bbd84d7cd1e894654a6c2f
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2025-04-19 17:06:41 +0000

    For no reason I can comprehend, this .tell() call fixes a bug where reloadbasic fails to write output for just the file achievements.bas under python 3.12.2 on MacOS Sequoia (~Eerie theremin music~)
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@13884 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- reloadbasic/reloadbasic.py
+++ reloadbasic/reloadbasic.py
@@ -375,6 +375,10 @@ class DelayedFileWriter(FileMarker):
         self.lines.sort()
         for index, line in self.lines:
             self.file.write(line)
+        # For no reason I can comprehend, this .tell() call fixes a bug where
+        # reloadbasic fails to write output for just the file achievements.bas
+        # under python 3.12.2 on MacOS Sequoia (~Eerie theremin music~)
+        dummy = self.file.tell()
 
 
 ################################# NodeSpecs ####################################
