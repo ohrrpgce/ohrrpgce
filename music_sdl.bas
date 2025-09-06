ohrrpgce/ohrrpgce

commit 459d5211e81c5a63a6a9048bc5c974d99885bebf

Recognise and play opus files, if possible, but don't allow importing them
They don't show in the browser either.

music_sdl2 only.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14110 7d344553-34f0-0310-a9b1-970ce8f1c3a2

music_sdl.bas
@@ -217,6 +217,8 @@ function music_get_info() as string
 					supported_formats or= FORMAT_OGG
 				elseif form = "FLAC" then
 					supported_formats or= FORMAT_FLAC
+				elseif form = "OPUS" then
+					supported_formats or= FORMAT_OPUS
 				elseif form = "WAVE" then
 					supported_formats or= FORMAT_WAV
 				elseif form = "MOD" or form = "MIKMOD" or form = "XMP" then
@@ -357,7 +359,11 @@ sub music_init()
 		'loaded. So force loading them. (SDL_mixer 2.0.2 only, earlier versions always
 		'loaded everything from Mix_OpenAudio). Increasing startup time just to get
 		'supported_formats is sad, but at least it prevents pauses later.
-		Mix_Init(MIX_INIT_MID or MIX_INIT_OGG or MIX_INIT_MP3 or MIX_INIT_MOD or MIX_INIT_FLAC)
+		var init_formats = MIX_INIT_MID or MIX_INIT_OGG or MIX_INIT_MP3 or MIX_INIT_MOD or MIX_INIT_FLAC
+		#ifdef SDL_MIXER2
+			init_formats or= MIX_INIT_OPUS
+		#endif
+		Mix_Init(init_formats)
 		'SDL_mixer 1.2.12 bug: if compiled against libmad Mix_Init sets the error "Mixer not built with MP3 support"
 		'if Mix_GetError then debug "Mix_Init: " & *Mix_GetError
 		Mix_ClearError

audioedit.bas
@@ -610,6 +610,10 @@ SUB importsong_get_song_info (songname as string, songfile as string, bamfile as
   file_ext = ".flac"
   songfile = temp & file_ext
   songtype = "FLAC"
+ ELSEIF isfile(temp & ".opus") THEN ' Not supported by most builds, and importing isn't permitted
+  file_ext = ".opus"
+  songfile = temp & file_ext
+  songtype = "OPUS"
  ELSEIF isfile(temp & ".mid") THEN
   file_ext = ".mid"
   songfile = temp & file_ext

audiofile.bas
@@ -421,6 +421,8 @@ function getmusictype (file as string) as MusicFormatEnum
 		chk = FORMAT_MP3
 	case "flac"
 		chk = FORMAT_FLAC
+	case "opus"
+		chk = FORMAT_OPUS
 	case "s3m"
 		chk = FORMAT_S3M
 	case "mod"

audiofile.bi
@@ -19,11 +19,12 @@ ENUM MusicFormatEnum
 	FORMAT_MODULES = FORMAT_MOD or FORMAT_XM or FORMAT_IT or FORMAT_S3M
 	FORMAT_WAV = 256
 	FORMAT_FLAC = 512  'Not used or supported yet
+	FORMAT_OPUS = 1024  'Not used or supported yet
 END ENUM
 
-#define VALID_SFX_FORMAT (FORMAT_WAV or FORMAT_OGG or FORMAT_MP3 or FORMAT_FLAC)
+#define VALID_SFX_FORMAT (FORMAT_WAV or FORMAT_OGG or FORMAT_MP3 or FORMAT_FLAC or FORMAT_OPUS)
 '' Music=all
-#define VALID_MUSIC_FORMAT (FORMAT_BAM or FORMAT_MIDI or FORMAT_MODULES or FORMAT_OGG or FORMAT_MP3 or FORMAT_FLAC or FORMAT_WAV)
+#define VALID_MUSIC_FORMAT (FORMAT_BAM or FORMAT_MIDI or FORMAT_MODULES or FORMAT_OGG or FORMAT_MP3 or FORMAT_FLAC or FORMAT_OPUS or FORMAT_WAV)
 
 
 DECLARE FUNCTION isawav(fi as string) as bool

browse.bas
@@ -872,8 +872,9 @@ SUB build_listing(tree() as BrowseMenuEntry, byref br as BrowseMenuState)
      browse_add_files "*.ogg",     fileTypeFile, br, tree()
      browse_add_files "*.mp3",     fileTypeFile, br, tree()
      browse_add_files "*.wav",     fileTypeFile, br, tree()
-     'Can only play FLAC in some builds, so don't allow it.
+     'Can only play FLAC/OPUS in some builds, so don't allow them yet.
      'browse_add_files "*.flac",    fileTypeFile, br, tree()
+     'browse_add_files "*.opus",     fileTypeFile, br, tree()
     CASE browseSfx
      '--disregard fmask. one call per extension
      browse_add_files "*.wav",     fileTypeFile, br, tree()

commit a6608aa559ce1de4e52491d4e1b7fa8604376351

Disable OPUS music support on Mac, it broke the build in r14110
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14112 7d344553-34f0-0310-a9b1-970ce8f1c3a2

music_sdl.bas
@@ -361,7 +361,9 @@ sub music_init()
 		'supported_formats is sad, but at least it prevents pauses later.
 		var init_formats = MIX_INIT_MID or MIX_INIT_OGG or MIX_INIT_MP3 or MIX_INIT_MOD or MIX_INIT_FLAC
 		#ifdef SDL_MIXER2
-			init_formats or= MIX_INIT_OPUS
+			#ifndef __FB_DARWIN__
+				init_formats or= MIX_INIT_OPUS
+			#endif
 		#endif
 		Mix_Init(init_formats)
 		'SDL_mixer 1.2.12 bug: if compiled against libmad Mix_Init sets the error "Mixer not built with MP3 support"