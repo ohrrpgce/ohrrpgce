ohrrpgce/ohrrpgce

commit 60e444461817c69ae0b83e03f7736692ac072f4a

'scons test' no longer prints each testcase by default. Added -q args to test programs.
filetest still prints some awful 'Ignore error' messages and errors, but -q
silences everything else

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14208 7d344553-34f0-0310-a9b1-970ce8f1c3a2

SConscript
@@ -65,6 +65,7 @@ prefix =  ARGUMENTS.get ('prefix', '/usr')
 dry_run = int(ARGUMENTS.get ('dry_run', '0'))  # Only used by uninstall
 buildname = ARGUMENTS.get('buildname', '')
 buildtests = int(ARGUMENTS.get ('buildtests', True))
+showtests = int(ARGUMENTS.get ('showtests', False))
 python = ARGUMENTS.get('python', os.environ.get('PYTHON'))
 if python == None:
     for name in ('python3', 'python', 'py'):
@@ -1911,8 +1912,9 @@ def test_rpg_actions(rpg, more_args = ''):
             else:
                 print(" * {0} FAILED, see g_debug.txt".format(rpg))
                 Exit(1)
-    return [GAME.abspath + run_args + ' --log . --runfast -z 2 ' + T+rpg + ' ' + more_args,
-            Action(check, "ohrrpgce-game {0} exited".format(rpg))]
+    return [Action('', '---- {0} ----'.format(rpg)),
+            GAME.abspath + run_args + ' --log . --runfast -z 2 ' + T+rpg + ' ' + more_args,
+            Action(check, "{0} exited".format(rpg))]
 
 AUTOTEST = Phony ('autotest_rpg',
                   source = [GAME, RPGWithScripts(T+'autotest.rpgdir', T+'autotest.hss')],
@@ -1932,9 +1934,13 @@ HSPEAKTEST = Phony ('hspeaktest', source = HSPEAK, action =
                     [[python, rootdir + 'hspeaktest.py', 'testgame/parser_tests.hss']])
 
 # Note: does not include hspeaktest, because it fails, and Euphoria may not be installed
-tests = [exe.abspath for exe in Flatten([RELOADTEST, RBTEST, VECTORTEST, UTILTEST, FILETEST, COMMONTEST])]
-test_srcs = tests[:] if buildtests else []
+test_exes = [RELOADTEST, RBTEST, VECTORTEST, UTILTEST, FILETEST, COMMONTEST]
+test_srcs = test_exes[:] if buildtests else []
 test_srcs += [AUTOTEST, INTERTEST]  # These are Nodes so can't be used as actions
+
+quiet_flag = '' if showtests else ' -q'
+tests = [Action(exe.abspath + quiet_flag, "---- " + exe.relpath + " ----")
+         for exe in test_exes]
 TESTS = Phony ('test', source = test_srcs, action = tests)
 Alias ('tests', TESTS)
 
@@ -2046,6 +2052,7 @@ Options:
   dry_run=1           For 'uninstall' only. Print files that would be deleted.
   buildtests=0        Affects test targets only: run tests without recompiling
                       anything or reimporting scripts.
+  showtests=1         Affects test programs only: show detailed output
   headless=1          Affects test targets only: run Game/Custom with --nogfx
                       to not need a graphical desktop. Requires gfx_console/fb
   v=0|1|2             Verbosity level for compile commands:

reloadtest.bas
@@ -667,11 +667,13 @@ sub toXMLAndBack(byval debugging as integer)
 	close fh
 
 	safekill "unittest.rld"
-	print
-	print
+	if show_tests then
+		print
+		print
+	end if
 	'Windows cmd doesn't like ./
-	shell "." + SLASH + "xml2reload unittest.xml unittest.rld"
-	print
+	shell "." + SLASH + "xml2reload" + iif(show_tests, "", " -q") + " unittest.xml unittest.rld"
+	if show_tests then print
 end sub
 
 startTest(loadFromXML)

filetest.bas
@@ -422,8 +422,8 @@ startTest(gzipWrite)
 	dim outdatasize as size_t
 	dim starttime as double = timer
 	if compress_gzip(strptr(indata), len(indata), @outdata, @outdatasize) then fail
-	? !"\n  lodepng compressed in " & cint((timer - starttime) * 1e3) & " ms"
-	? "  lodepng compressed size = " & outdatasize
+	testprint !"\n  lodepng compressed in " & cint((timer - starttime) * 1e3) & " ms"
+	testprint "  lodepng compressed size = " & outdatasize
 	'Write
 	dim fil as FILE ptr
 	fil = fopen(strptr(gz_outfile), "wb")
@@ -436,8 +436,8 @@ startTest(gzipWrite)
 	#else
 	'Decompress
 	starttime = timer
-	if safe_shell("gzip -d " & gz_outfile) then fail
-	? "  gzip decompressed in " & cint((timer - starttime) * 1e3) & " ms"
+	if safe_shell("gzip -d " & gz_outfile, , show_tests) then fail
+	testprint "  gzip decompressed in " & cint((timer - starttime) * 1e3) & " ms"
 	'Read result and check
 	dim indata2 as string = read_file(gz_outfile2)
 	if indata <> indata2 then fail
@@ -452,19 +452,20 @@ startTest(gzipRead)
 	#else
 	'Compress
 	starttime = timer
-	if safe_shell("gzip -9 -c " & gz_infile & " > " & gz_outfile) then fail
-	? "  gzip compressed in " & cint((timer - starttime) * 1e3) & " ms"
+	if safe_shell("gzip -9 -c " & gz_infile & " > " & gz_outfile, , show_tests) then fail
+	testprint "  gzip compressed in " & cint((timer - starttime) * 1e3) & " ms"
+	testprint "  gzip compressed in " & cint((timer - starttime) * 1e3) & " ms"
 	#endif
 	'Read
 	dim gzipdata as string = read_file(gz_outfile)
-	? "  gzip compressed size = " & len(gzipdata)
+	testprint "  gzip compressed size = " & len(gzipdata)
 	'Decompress
 	dim outdata as byte ptr
 	dim outdatasize as size_t
 	starttime = timer
 	if decompress_gzip(strptr(gzipdata), len(gzipdata), @outdata, @outdatasize) then fail
-	? "  lodepng decompressed in " & cint((timer - starttime) * 1e3) & " ms"
-	? "  original size = " & outdatasize
+	testprint "  lodepng decompressed in " & cint((timer - starttime) * 1e3) & " ms"
+	testprint "  original size = " & outdatasize
 	'Read original and result and check
 	dim indata as string = read_file(gz_infile)
 	if outdatasize <> len(indata) then fail
@@ -521,7 +522,7 @@ startTest(renameReplaceOpenFile)
         #endif
         dim ttt as double = timer
         if renamefile("_testfile2.tmp", "_testfile3.tmp") = NO then fail
-        print "renamefile took " & cint(1e3 * (timer - ttt)) & "ms"
+        testprint "renamefile took " & cint(1e3 * (timer - ttt)) & "ms"
 
         if filelen("_testfile3.tmp") <> 6 then fail
         if real_isfile("_testfile2.tmp") then fail
@@ -579,7 +580,7 @@ startTest(renameLockedFile)
         #endif
         dim ttt as double = timer
         if renamefile("_testfile5.tmp", "_testfile6.tmp") = NO then fail
-        print "renamefile took " & cint(1e3 * (timer - ttt)) & "ms"
+        testprint "renamefile took " & cint(1e3 * (timer - ttt)) & "ms"
 
         if filelen("_testfile6.tmp") <> 6 then fail
         if real_isfile("_testfile5.tmp") then fail
@@ -592,4 +593,4 @@ endTest
 
 #endif  ' ifndef MINIMAL_OS
 
-? "All tests passed."
+? "All passed."

rbtest.rbas
@@ -390,7 +390,7 @@ startTest(testMoreReadNodeDefaulting)
 	set_warn = NO
 
 	for i as integer = 0 to 1
-		if i = 1 then print "..."; : root = NULL
+		if i = 1 then root = NULL
 
 		dim as integer should_be_zero = -1
 		readnode root

testing.bi
@@ -20,21 +20,36 @@ extern errorpos as integer
 extern errorfile as string
 extern startTest_name as string
 extern startTest_line as integer
+extern show_tests as bool
 dim pauseTime as double
 dim errorpos as integer
 dim errorfile as string
 dim startTest_name as string
 dim startTest_line as integer
+dim show_tests as bool = YES
+
+' Parse command line arguments
+for argidx as integer = 1 to 255
+	dim arg as string = command(argidx)
+	if arg = "" then exit for
+	if arg = "-q" or arg = "--quiet" then
+		show_tests = NO
+	end if
+next
 
 Randomize 42
 
+sub testprint(msg as string = "")
+	if show_tests then print msg
+end sub
+
 sub doTest(byval theTest as testPtr)
 	static num as integer = 0
-	
+
 	num += 1
-	
-	print "Test #" & num & ": " & startTest_name & "... ";
-	
+
+	if show_tests then print "Test #" & num & ": " & startTest_name & "... ";
+
 	dim as double start, finish, diff
 	dim as integer ret
 	
@@ -54,23 +69,25 @@ sub doTest(byval theTest as testPtr)
 		close_lazy_files  'Avoid "double close" messages
 		end num
 	elseif ret = 0 then
-		print "Pass"
+		testprint "Pass"
 	else
-		print "SKIP"
+		testprint "SKIP"
 	end if
 
-	if diff < 1 then
-		diff *= 1000
-		if diff < 10 then
+	if show_tests then
+		if diff < 1 then
 			diff *= 1000
-			print "Took " & int(diff) & !" \u03BCs "
-		elseif diff < 100 then
-			print "Took " & format(diff, "0.0") & " ms "
+			if diff < 10 then
+				diff *= 1000
+				print "Took " & int(diff) & " us "
+			elseif diff < 100 then
+				print "Took " & format(diff, "0.0") & " ms "
+			else
+				print "Took " & int(diff) & " ms "
+			end if
 		else
-			print "Took " & int(diff) & " ms "
+			print "Took " & format(diff, "0.00") & " s "
 		end if
-	else
-		print "Took " & format(diff, "0.00") & " s "
 	end if
 	
 end sub

util.bas
@@ -4377,7 +4377,7 @@ startTest(str_intern_speed)
     strings(i) = str(i) & "token"
     zstrings(i) = strptr(strings(i))
   next
-  ?""
+  testprint
 
   dim strtable as StrHashTable
   strtable.construct(509, type_table(integer), NO)
@@ -4394,7 +4394,7 @@ startTest(str_intern_speed)
     next
   next
   runtime = timer - runtime
-  ?"StrHashTable in " & CINT(1e6 * runtime) & "us"
+  testprint "StrHashTable in " & CINT(1e6 * runtime) & "us"
 
   dim initkeys as integer = interned_strings.numitems
   runtime = timer
@@ -4405,7 +4405,7 @@ startTest(str_intern_speed)
     next
   next
   runtime = timer - runtime
-  ?"intern_string uninterned in " & CINT(1e6 * runtime) & "us"
+  testprint "intern_string uninterned in " & CINT(1e6 * runtime) & "us"
   if interned_fast_lookup.numitems <> initkeys + 1000 then fail
   if interned_strings.numitems <> initkeys + 1000 then fail
 
@@ -4417,7 +4417,7 @@ startTest(str_intern_speed)
     next
   next
   runtime = timer - runtime
-  ?"intern_string intern in " & CINT(1e6 * runtime) & "us"
+  testprint "intern_string intern in " & CINT(1e6 * runtime) & "us"
 
   for i as integer = 0 to 999
     if *zstrings(i) <> strings(i) then fail

xml2reload.bas
@@ -26,21 +26,26 @@ declare sub optimize(node as nodePtr)
 dim shared reloadns as xmlNsPtr
 
 dim as string infile, outfile
+dim as bool verbose = YES
 
 xmlCheckVersion(LIBXML_VERSION)
 
-infile = command(1)
-outfile = command(2)
+' Parse command line
+for argidx as integer = 1 to 255
+	dim arg as string = command(argidx)
+	if arg = "" then exit while
+	if arg = "-q" or arg = "--quiet" then
+		verbose = NO
+	elseif infile = "" then
+		infile = arg
+	elseif outfile = "" then
+		outfile = arg
+	end if
+next
 
-if infile = "" then
+if infile = "" or outfile = "" then
 	print "Usage:"
-	print command(0) & " infile.xml outfile.rld"
-	end
-end if
-
-if outfile = "" then
-	print "Usage:"
-	print command(0) & " infile.xml outfile.rld"
+	print command(0) & " [-q|--quiet] infile.xml outfile.rld"
 	end
 end if
 
@@ -54,13 +59,17 @@ if xmlDoc = null then
 	end
 end if
 
-print "Loaded XML document in " & int((timer - starttime) * 1000) & " ms"
+if verbose then
+	print "Loaded XML document in " & int((timer - starttime) * 1000000) & " us"
+end if
 starttime = timer
 
 dim rldDoc as Docptr
 rldDoc = CreateDocument()
 
-print "Memory usage: " & DocumentMemoryUsage(rldDoc)
+if verbose then
+	print "Memory usage: " & DocumentMemoryUsage(rldDoc)
+end if
 
 dim xmlRoot as xmlNodeptr
 xmlRoot = xmlDocGetRootElement(xmlDoc)
@@ -70,23 +79,29 @@ reloadns = xmlSearchNsByHref(xmlDoc, xmlRoot, @"http://hamsterrepublic.com/ohrrp
 dim rldRoot as NodePtr
 rldRoot = chug(xmlRoot, rldDoc, encNone)
 
-print "Parsed XML document in " & int((timer - starttime) * 1000) & " ms"
+if verbose then
+	print "Parsed XML document in " & int((timer - starttime) * 1000000) & " us"
 
-print "Memory usage: " & DocumentMemoryUsage(rldDoc)
+	print "Memory usage: " & DocumentMemoryUsage(rldDoc)
+end if
 
 starttime = timer
 
 xmlFreeDoc(xmlDoc)
 
-print "Freed XML document in " & int((timer - starttime) * 1000) & " ms"
+if verbose then
+	print "Freed XML document in " & int((timer - starttime) * 1000000) & " us"
+end if
 
 starttime = timer
 
 optimize(rldRoot)
 
-print "Optimised document in " & int((timer - starttime) * 1000) & " ms"
+if verbose then
+	print "Optimised document in " & int((timer - starttime) * 1000000) & " us"
 
-print "Memory usage: " & DocumentMemoryUsage(rldDoc)
+	print "Memory usage: " & DocumentMemoryUsage(rldDoc)
+end if
 
 starttime = timer
 
@@ -94,14 +109,18 @@ SetRootNode(rldDoc, rldRoot)
 
 SerializeBin(outfile, rldDoc)
 
-print "Serialized document in " & int((timer - starttime) * 1000) & " ms"
+if verbose then
+	print "Serialized document in " & int((timer - starttime) * 1000000) & " us"
+end if
 starttime = timer
 
 FreeDocument(rldDoc)
 
-print "Tore down memory in " & int((timer - starttime) * 1000) & " ms"
+if verbose then
+	print "Tore down memory in " & int((timer - starttime) * 1000000) & " us"
 
-print "Finished in " & int((timer - realStart) * 1000) & " ms"
+	print "Finished in " & int((timer - realStart) * 1000000) & " us"
+end if
 
 
 'This sub sets a node's content to binary data

commit e3fcc634cb59ea32615c39e325783cbb5ed5f04a

Fix the scons 4.0 breakage in r14208 differently, to print relative path
git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14213 7d344553-34f0-0310-a9b1-970ce8f1c3a2

SConscript
@@ -1939,7 +1939,7 @@ test_srcs = test_exes[:] if buildtests else []
 test_srcs += [AUTOTEST, INTERTEST]  # These are Nodes so can't be used as actions
 
 quiet_flag = '' if showtests else ' -q'
-tests = [Action(exe.abspath + quiet_flag, "---- " + str(exe) + " ----")
+tests = [Action(exe.abspath + quiet_flag, "---- " + exe.path + " ----")
          for exe in test_exes]
 TESTS = Phony ('test', source = test_srcs, action = tests)
 Alias ('tests', TESTS)

reloadtest.bas
@@ -667,13 +667,11 @@ sub toXMLAndBack(byval debugging as integer)
 	close fh
 
 	safekill "unittest.rld"
-	if show_tests then
-		print
-		print
-	end if
+	testprint
+	testprint
 	'Windows cmd doesn't like ./
 	shell "." + SLASH + "xml2reload" + iif(show_tests, "", " -q") + " unittest.xml unittest.rld"
-	if show_tests then print
+	testprint
 end sub
 
 startTest(loadFromXML)