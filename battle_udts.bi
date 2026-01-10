ohrrpgce/ohrrpgce

commit 1a737cdedea23ae05fd939c2328c391614929b89

Add attacker animations "Run and Hide" and "Unhide" that work like "Jump" and "Land"
Add bitsets that allow other attacks to hide/unhide attackers, regardless of their attacker animations

Add a .hidden member to the BattleSprite type, instead of overloading the meaning of .vis

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@12684 7d344553-34f0-0310-a9b1-970ce8f1c3a2

battle_udts.bi
@@ -78,10 +78,13 @@ TYPE BattleSprite
   vis as bool   'Roughly, but not exactly, visibility:
                 'For combatants: Is alive (or dying interruptted) and visible (didn't jump).
                 '  Gets set to YES while an enemy is performing an on-death attack.
-                '  Gets set to NO while it's dissolving or fleeing after death, or
-                '  or if jumped off-screen
+                '  Gets set to NO while it's dissolving or fleeing after death
                 '  If true, treated as a valid target for attacks that can't hit dead.
                 'For other BattleSprites: is visible
+  hidden as bool ' For combatants, has been hidden by an attacker animation like "Jump" or "Run and Hide"
+                 ' or any attack with "always hides attacker" bitset. Not targetable until unhidden
+                 ' if true, excluded from all targetting except for attacks with attacker animation
+                 ' "Land" or "Unhide" or any attack with the "always unhide attacker" bit
 
   '--stats
   stat as BattleStats

bmod.rbas
@@ -566,6 +566,12 @@ SUB battle_attack_anim_playback (byref attack as AttackData, byref bat as Battle
    bslot(ww).zmov = moveticks
   CASE 25 'anim_checkpoint
     IF autotestmode THEN write_checkpoint
+  CASE 26 'hide(who)
+    ww = popdw
+    bslot(ww).hidden = YES
+  CASE 27 'unhide(who)
+    ww = popdw
+    bslot(ww).hidden = NO
 
   END SELECT
  LOOP UNTIL bat.wait_frames <> 0 OR bat.atk.id = -1
@@ -1476,6 +1482,10 @@ SUB update_battle_slices(bslot() as BattleSprite)
    ELSE
     .sl->Visible = NO
    END IF
+   
+   IF .hidden THEN
+    .sl->Visible = NO
+   END IF
   END WITH
  NEXT i
 END SUB
@@ -2229,6 +2239,14 @@ END SUB
 
 SUB anim_appear(byval who as integer)
  pushdw 5: pushdw who
+END SUB
+
+SUB anim_hide(byval who as integer)
+ pushdw 26: pushdw who
+END SUB
+
+SUB anim_unhide(byval who as integer)
+ pushdw 27: pushdw who
 END SUB
 
 ' For ABS(xmove_ticks) ticks, move xstep pixels in the direction SGN(xmove_ticks), and ditto for ymove_ticks/ystep.

bmodsubs.bas
@@ -1359,6 +1359,11 @@ SUB anim_advance (byval who as integer, attack as AttackData, bslot() as BattleS
 
  DIM target as BattleSprite ptr = @bslot(t(0))
 
+ IF attack.always_unhide_attacker THEN
+  anim_appear who
+  anim_unhide who
+ END IF
+
  SELECT CASE attack.attacker_anim
  CASE atkrAnimStrike, atkrAnimCast, atkrAnimSpinStrike, atkrAnimJump
   IF is_hero(who) THEN
@@ -1445,22 +1450,44 @@ SUB anim_hero (byval who as integer, attack as AttackData, bslot() as BattleSpri
    anim_zmove who, 13, 18
    anim_waitforall
    anim_disappear who
+   anim_hide who
    anim_setframe who, frameSTAND
 
   CASE atkrAnimLand
    anim_setz who, 200
    anim_setframe who, frameLAND
    anim_appear who
+   anim_unhide who
    anim_setcenter who, t(0), 0, 0
    anim_align who, t(0), dirDown, 0
    anim_zmove who, -10, 20
    anim_waitforall
    anim_setframe who, frameHURT
 
+  CASE atkrAnimRunAndHide
+   anim_setframe who, frameJUMP
+   anim_setdir who, 1
+   anim_absmove who, 320 + bslot(t(0)).w / 2, bslot(t(0)).y, 10, 1
+   anim_waitforall
+   anim_disappear who
+   anim_hide who
+   anim_setframe who, frameSTAND
+   anim_setdir who, 0
+
+  CASE atkrAnimUnhide
+   anim_setz who, 0
+   anim_setpos who, 320 + bslot(t(0)).w / 2, bslot(t(0)).y, 0
+   anim_setframe who, frameSTAND
+   anim_appear who
+   anim_unhide who
+   anim_absmove who, bslot(t(0)).x, bslot(t(0)).y, 10, 1
+   anim_waitforall
+
   CASE atkrAnimNull
    'Nothing
 
  END SELECT
+
 END SUB
 
 'Generate attacker animation when an enemy attacks
@@ -1483,11 +1510,27 @@ SUB anim_enemy (byval who as integer, attack as AttackData, bslot() as BattleSpr
   anim_zmove who, 10, 20
   anim_waitforall
   anim_disappear who
+  anim_hide who
  CASE atkrAnimLand
   anim_setz who, 200
   anim_appear who
+  anim_unhide who
   anim_setpos who, bslot(t(0)).x, bslot(t(0)).y, 0
   anim_zmove who, -10, 20
+  anim_waitforall
+ CASE atkrAnimRunAndHide
+  anim_setdir who, 1
+  anim_absmove who, 0 - bslot(t(0)).w / 2, bslot(t(0)).y, 10, 1
+  anim_waitforall
+  anim_disappear who
+  anim_hide who
+  anim_setdir who, 0
+ CASE atkrAnimUnhide
+  anim_setz who, 0
+  anim_setpos who, 0 - bslot(t(0)).w / 2, bslot(t(0)).y, 0
+  anim_appear who
+  anim_unhide who
+  anim_absmove who, bslot(t(0)).x, bslot(t(0)).y, 10, 1
   anim_waitforall
  CASE atkrAnimDashIn, atkrAnimNull, atkrAnimStandingCast, atkrAnimTeleport, atkrAnimStandingStrike
   ' nothing
@@ -1526,6 +1569,12 @@ SUB anim_retreat (byval who as integer, attack as AttackData, bslot() as BattleS
   ' Do nothing
   END SELECT
  END IF
+
+ IF attack.always_hide_attacker THEN
+  anim_disappear who
+  anim_unhide who
+ END IF
+
 END SUB
 
 FUNCTION attack_can_hit_dead(attacker as integer, atk_id as integer, stored_targs_can_be_dead as bool=NO) as bool

attackedit.bas
@@ -296,6 +296,8 @@ a_append atkbit(), -1, ""
 a_append atkbit(), -1, " Effects: on Attacker"
 a_append atkbit(), 52, "Store target"
 a_append atkbit(), 53, "Delete stored targets"
+a_append atkbit(), 92, "Always hide attacker (any attacker animation)"
+a_append atkbit(), 93, "Always unhide attacker (any attacker animation)"
 
 a_append atkbit(), -1, ""
 a_append atkbit(), -1, " Effects: on Target"
@@ -525,18 +527,20 @@ max(AtkLimChainRate) = 100
 min(AtkLimChainRate) = 0
 
 CONST AtkLimAnimAttacker = 14
-max(AtkLimAnimAttacker) = 9
+max(AtkLimAnimAttacker) = 11
 menucapoff(AtkAnimAttacker) = capindex
 addcaption caption(), capindex, "Strike"
 addcaption caption(), capindex, "Cast"
 addcaption caption(), capindex, "Dash In"
 addcaption caption(), capindex, "SpinStrike"
-addcaption caption(), capindex, "Jump (chain to Land)"
-addcaption caption(), capindex, "Land"
+addcaption caption(), capindex, "Jump (hides)"
+addcaption caption(), capindex, "Land (unhides)"
 addcaption caption(), capindex, "Null"
 addcaption caption(), capindex, "Standing Cast"
 addcaption caption(), capindex, "Teleport"
 addcaption caption(), capindex, "Standing Strike"
+addcaption caption(), capindex, "Run and Hide (hides)"
+addcaption caption(), capindex, "Unhide (unhides)"
 
 CONST AtkLimAnimAttack = 15
 max(AtkLimAnimAttack) = 11

bmod.bi
@@ -27,6 +27,8 @@ DECLARE SUB anim_waitforall()
 DECLARE SUB anim_inflict(byval who as integer, byval target_count as integer)
 DECLARE SUB anim_disappear(byval who as integer)
 DECLARE SUB anim_appear(byval who as integer)
+DECLARE SUB anim_hide(byval who as integer)
+DECLARE SUB anim_unhide(byval who as integer)
 DECLARE SUB anim_setframe(byval who as integer, byval frame as integer)
 DECLARE SUB anim_setpos(byval who as integer, byval x as integer, byval y as integer, byval d as integer)
 DECLARE SUB anim_setz(byval who as integer, byval z as integer)

loading.rbas
@@ -3198,6 +3198,8 @@ SUB convertattackdata(buf() as integer, byref atkdat as AttackData)
   .useable_inside_battle          = NOT xreadbit(buf(), 25, 65)
   .dont_display_miss              = xreadbit(buf(), 26, 65)
   .dont_display_fail              = xreadbit(buf(), 27, 65)
+  .always_hide_attacker           = xreadbit(buf(), 28, 65)
+  .always_unhide_attacker         = xreadbit(buf(), 29, 65)
 
   ' If didn't do a full upgrade, then these aren't initialised
   IF getfixbit(fixAttackMultipliers) = 0 THEN

udts.bi
@@ -678,12 +678,14 @@ Enum AttackerAnimation
 	atkrAnimCast = 1
 	atkrAnimDashIn = 2     ' (shows weapon)
 	atkrAnimSpinStrike = 3 ' (shows weapon)
-	atkrAnimJump = 4
-	atkrAnimLand = 5
+	atkrAnimJump = 4       ' (hides attacker)
+	atkrAnimLand = 5       ' (unhides attacker)
 	atkrAnimNull = 6
 	atkrAnimStandingCast = 7
 	atkrAnimTeleport = 8   ' (shows weapon)
 	atkrAnimStandingStrike = 9 ' (shows weapon)
+	atkrAnimRunAndHide = 10    ' (hides attacker)
+	atkrAnimUnHide = 11        ' (unhides attacker)
 End Enum
 
 Enum TransmogStatsRule
@@ -812,6 +814,8 @@ Type AttackData
 	counterattack_provoke as CounterProkeEnum
 	never_trigger_elemental_counterattacks as bool
 	poison_is_negative_regen as bool
+	always_hide_attacker as bool
+	always_unhide_attacker as bool
 End Type
 
 'An item in a hero's spell list definition (actual spell lists in HeroState.spells() array of integers)

commit 57ac30515ecd3156804229dc637f2849b1f04e4a

Fix hidden attackers not being Self-targetable, and reduce some targetability exceptions
The ultimate cause of the problem was the splitting out of BattleSprite.hidden
from .vis (started in r12684, 12685).
r12874 "Fix check_for_unhittable_invisible_foe() to properly handle
BattleSprite.hidden" incorrectly disallowed targeting hidden combatants (cancelling
such attacks in generate_atkscript) with no Self exception, and then r13137 broke
get_valid_targs too, but moderated the problem by adding some exceptions to allow
unhiding attacks to targetting hidden, which added for a different reason:
so if the target of an chained Land attack itself Jumps, the attack isn't cancelled
leaving the attacker hidden.

I've changed those exceptions to now only apply when considering cancelling the
attack, get_valid_targs no longer lets Land attacks target hidden attackers.

Also renamed check_for_unhittable_invisible_foe to is_invalid_target and did other
refactoring.

git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14223 7d344553-34f0-0310-a9b1-970ce8f1c3a2

battle_udts.bi
@@ -76,16 +76,15 @@ TYPE BattleSprite
 
   basepos as XYPair
   d as integer
+  ' A combatant is normally targetable if vis=YES and hidden=NO, aside from exceptions such as attacks that can
+  ' hit dead or hidden.
   vis as bool   'Roughly, but not exactly, liveness:
                 'For combatants: Is alive (or dying interruptted).
                 '  Gets set to YES while an enemy is performing an on-death attack.
                 '  Gets set to NO while it's dissolving or fleeing after death
-                '  If true, treated as a valid target for attacks that can't hit dead.
                 'For other BattleSprites: is visible
-  hidden as bool ' For combatants, has been hidden by an attacker animation like "Jump" or "Run and Hide"
-                 ' or any attack with "always hides attacker" bitset. Not targetable until unhidden
-                 ' if true, excluded from all targetting except for attacks with attBaacker animation
-                 ' "Land" or "Run In" or any attack with the "always unhide attacker" bit
+  hidden as bool ' (Combatants only) Hidden (either invisible or off-screen) and not normally targetable
+                 ' Set by e.g. "Jump" or "Always hides attacker".
   flipped as bool 
 
   '--stats

bmod.rbas
@@ -104,7 +104,7 @@ DECLARE FUNCTION has_blocking_turn_delayed_attacks(byval who as integer) as bool
 DECLARE SUB update_battle_menu (bspr as BattleSprite, bslot() as BattleSprite, bat as BattleState, st() as HeroDef)
 DECLARE SUB init_spell_menu (bat as BattleState, bslot() as BattleSprite, st() as HeroDef)
 DECLARE FUNCTION enemy_is_weak(byref bspr as BattleSprite) as bool
-DECLARE FUNCTION check_for_unhittable_invisible_foe(attacker as integer, target as integer, byref attack as AttackData, byref bat as BattleState, bslot() as BattleSprite) as bool
+DECLARE FUNCTION is_invalid_target(attacker as integer, target as integer, byref attack as AttackData, byref bat as BattleState, bslot() as BattleSprite) as bool
 DECLARE FUNCTION is_among_targets(byval slot as integer, t() as integer) as bool
 DECLARE FUNCTION should_victory_advance(byref bat as BattleState) as bool
 DECLARE SUB append_enemy_attacks_to_battle_menu(ai_attack_list() as integer, byref bspr as BattleSprite)
@@ -964,6 +964,7 @@ SUB battle_spawn_on_first_hit(atk as AttackData, byref bat as BattleState, bslot
 END SUB
 
 SUB battle_targetting(byref bat as BattleState, bslot() as BattleSprite)
+ 'Player controls for selecting target. Called each tick while in targeting mode.
  'Not just for heroes anymore!
 
  DIM do_cancel as bool = NO
@@ -4321,18 +4322,33 @@ FUNCTION projectile_start_position(attack as AttackData, attacker as integer, ba
  RETURN ret
 END FUNCTION
 
-'Return true if target is unhittable and invisible
-FUNCTION check_for_unhittable_invisible_foe(attacker as integer, target as integer, byref attack as AttackData, byref bat as BattleState, bslot() as BattleSprite) as bool
+'Return true if a previously selected target has become invalid.
+'This is a subset of the restrictions applied in get_valid_targs. (Target classes
+'are ignored, because e.g. last-attacker might have changed.)
+'TODO: this may need to track get_valid_targs more closely, for example we
+'don't prevent hitting a bequesting enemy
+FUNCTION is_invalid_target(attacker as integer, target as integer, byref attack as AttackData, byref bat as BattleState, bslot() as BattleSprite) as bool
+ 'Always valid if hidden=NO and vis=YES
  IF bslot(target).hidden THEN
-  IF should_enforce_hidden_untargetability(attack) THEN
-   RETURN YES
+  'Hidden attackers allowed to target themselves with Self (but not with All) attacks
+  IF attack.targ_class <> 2 THEN
+   'Exception: when the target of a chained unhiding attack has hidden (eg, a
+   'group of enemies that jump and target each other) then we shouldn't cancel
+   'the attack as that will leave the attacker hidden.
+   'TODO: so unhiding attacks may need exceptions for other invalid targets
+   'too. The common case of dead targets is already handled by retargeting in
+   'battle_reevaluate_dead_targets.
+   IF attack_unhides_attacker(attack) = NO THEN
+    RETURN YES
+   END IF
   END IF
  END IF
  IF bslot(target).vis THEN RETURN NO
- IF bslot(target).stat.cur.hp <= 0 ANDALSO attack_can_hit_dead(attack, bslot(attacker).stored_targs_can_be_dead) THEN RETURN NO 'Attack can hit dead targets
+
+ IF bslot(target).stat.cur.hp <= 0 ANDALSO attack_can_hit_dead(attack, bslot(attacker).stored_targs_can_be_dead) THEN RETURN NO
  IF bslot(attacker).self_bequesting THEN
   'This is a self-bequest attack
-  IF attacker = target THEN RETURN NO 'Bequesting attacker is always allowed to hit self
+  IF attacker = target THEN RETURN NO 'Bequesting attacker is always allowed to hit self (includes All targ_class)
  END IF
  RETURN YES
 END FUNCTION
@@ -4393,22 +4409,25 @@ SUB generate_atkscript(byref attack as AttackData, byref bat as BattleState, bsl
  bat.atk.has_consumed_costs = NO
  bat.atk.has_spawned = NO
  IF is_enemy(bat.acting) THEN pdir = 1
- 
- 'CANNOT HIT INVISIBLE FOES
+
+ 'Throw out any targets that have become invalid since targeting happened
+ 'Note: if a target dies it will already have been removed if needed by
+ 'battle_reevaluate_dead_targets, and the attack possibly retargeted or
+ 'cancelled earlier. It seems wrong to treat other invalid targets differently.
  FOR i = 0 TO 11
   IF t(i) > -1 THEN
-   IF check_for_unhittable_invisible_foe(bat.acting, t(i), attack, bat, bslot()) THEN
+   IF is_invalid_target(bat.acting, t(i), attack, bat, bslot()) THEN
     t(i) = -1
    END IF
   END IF
  NEXT i
- 'MOVE EMPTY TARGET SLOTS TO THE BACK
+ 'Move empty target slots to the back
  FOR o as integer = 0 TO UBOUND(t) - 1
   FOR i = 0 TO 10
    IF t(i) = -1 THEN SWAP t(i), t(i + 1)
   NEXT i
  NEXT o
- 'COUNT TARGETS
+ 'Count targets
  FOR i = 0 TO 11
   IF t(i) > -1 THEN tcount += 1
  NEXT i
@@ -4996,7 +5015,6 @@ SUB setup_targetting (byref bat as BattleState, bslot() as BattleSprite)
  FOR i = 0 TO 11
   bat.targ.selected(i) = NO ' clear list of selected targets
  NEXT i
-
  bat.targ.hit_dead = NO
 
  'load attack
@@ -5811,6 +5829,8 @@ END FUNCTION
 SUB battle_reevaluate_dead_targets (byval deadguy as integer, byref bat as BattleState, bslot() as BattleSprite)
  '--check for queued attacks that target the dead target
  'NOTE: code here is partially duplicated with battle_check_delays!
+ 'TODO: it seems wrong to be treating dead dissimilarly to other invalid targets, which are only
+ 'removed in generate_atkscript according to is_invalid_target, with no retargeting.
  FOR i as integer = 0 TO UBOUND(atkq)
   WITH atkq(i)
    IF .used THEN

bmodsubs.bas
@@ -1201,10 +1201,6 @@ SUB setheroexperience (byval who as integer, byval amount as integer, byval allo
  tag_updates
 END SUB
 
-FUNCTION visibleandalive (byval who as integer, bslot() as BattleSprite) as bool
- RETURN (bslot(who).vis ANDALSO bslot(who).stat.cur.hp > 0)
-END FUNCTION
-
 SUB export_battle_hero_stats (bslot() as BattleSprite)
  'Export a few specific hero battle stats to the out-of-battle party
  'This may be used frequently in battle
@@ -1411,6 +1407,10 @@ SUB get_valid_targs(tmask() as bool, byval who as integer, byref atk as AttackDa
 
  END SELECT
 
+ 'Apply other restrictions
+
+ 'NOTE: When adding more restrictions you likely need to also add them to is_invalid_target
+
  'enforce attack's disabled enemy target slots
  FOR i = 0 TO 7
   IF atk.cannot_target_enemy_slot(i) THEN tmask(4 + i) = NO
@@ -1424,44 +1424,32 @@ SUB get_valid_targs(tmask() as bool, byval who as integer, byref atk as AttackDa
  FOR i = 0 TO 11
   'If a target is doing an on-death bequest attack,
   ' it cannot be targeted by anyone but itself
+  'FIXME: Should allow it to hit other targets though, this unintentionally prohibits that
   IF bslot(i).bequesting AND i <> who THEN
    tmask(i) = NO
   END IF
  NEXT i
 
- 'Some restrictions are only applied when the target class is not "self"
- FOR i = 0 TO 11
-  'Hidden targets can't be targetted
-  IF should_enforce_hidden_untargetability(atk) THEN
-   IF bslot(i).hidden THEN tmask(i) = NO
-  END IF
+ 'Some untargetability restrictions are only applied when the target class is not "self"
+ IF atk.targ_class <> 2 THEN
+  FOR i = 0 TO 11
 
-  IF should_enforce_untargetability(atk) THEN
-  'enforce untargetability
+   'Hidden targets can't be targetted
+   IF bslot(i).hidden THEN tmask(i) = NO
+
    IF attacker_as_hero THEN
     IF bslot(i).hero_untargetable = YES THEN tmask(i) = NO
    ELSEIF attacker_as_enemy THEN
     IF bslot(i).enemy_untargetable = YES THEN tmask(i) = NO
    END IF
-  END IF
-
- NEXT i
+  NEXT i
+ END IF
 
 END SUB
 
-FUNCTION should_enforce_hidden_untargetability(atk as AttackData) as bool
- IF atk.always_unhide_attacker ORELSE atk.attacker_anim = 5 ORELSE atk.attacker_anim = 11 THEN
-  'Any attack that does "Unhide" is likely to need to ignore untargetability
-  'This fixes jump-land chains when they jump on self, or jump on another jumper
-  RETURN NO
- END IF
- RETURN YES
-END FUNCTION 
-
-FUNCTION should_enforce_untargetability(atk as AttackData) as bool
- IF atk.targ_class = 2 THEN RETURN NO 'Self-targetting attacks should override untargetability
- RETURN YES
-END FUNCTION 
+FUNCTION attack_unhides_attacker(atk as AttackData) as bool
+ RETURN atk.always_unhide_attacker ORELSE atk.attacker_anim = atkrAnimLand ORELSE atk.attacker_anim = atkrAnimRunInUnHide
+END FUNCTION
 
 ' Generate animation for attacker moving forward before attacking
 ' Note: attack_placement_over_target has a special case for the walk-forward-20-pixels behaviour

bmodsubs.bi
@@ -60,7 +60,6 @@ declare function giveheroexperience (byval i as integer, byval exper as integer)
 declare sub setheroexperience (byval who as integer, byval amount as integer, byval allowforget as bool)
 declare function allowed_to_gain_levels(byval heroslot as integer) as bool
 
-declare function visibleandalive (byval who as integer, bslot() as battlesprite) as bool
 declare sub export_battle_hero_stats (bslot() as BattleSprite)
 declare sub import_battle_hero_stats (bslot() as BattleSprite)
 
@@ -68,6 +67,7 @@ declare function has_valid_targs OVERLOAD (byval who as integer, byval atk_id as
 declare function has_valid_targs OVERLOAD (byval who as integer, byref atk as AttackData, bslot() as BattleSprite, bat as BattleState) as bool
 
 declare sub get_valid_targs (tmask() as integer, byval who as integer, byref atk as AttackData, bslot() as BattleSprite, bat as BattleState)
+declare function attack_unhides_attacker (atk as AttackData) as bool
 declare function attack_can_hit_dead OVERLOAD (atk_id as integer, stored_targs_can_be_dead as bool=NO) as bool
 declare function attack_can_hit_dead OVERLOAD (attack as AttackData, stored_targs_can_be_dead as bool=NO) as bool
 declare function autotarget OVERLOAD (who as integer, atk_id as integer, bslot() as BattleSprite, bat as BattleState, t() as integer, queue as bool=YES, override_blocking as integer=-2, dont_retarget as bool=NO, is_counterattack as bool=NO) as bool
@@ -80,6 +80,5 @@ declare function find_preferred_target (tmask() as integer, byval who as integer
 declare sub try_to_reload_files_inbattle ()
 
 declare function should_enforce_hidden_untargetability(atk as AttackData) as bool
-declare function should_enforce_untargetability(atk as AttackData) as bool
 
 #ENDIF