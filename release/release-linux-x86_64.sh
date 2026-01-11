bcd2f68e5862d4843ad40c1fdd2f928b8d70b215
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2026-01-11 01:56:40 +0000

    backport dockerization of linux release scripts to wip
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14239 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-linux-x86_64.sh
+++ release/release-linux-x86_64.sh
@@ -4,16 +4,17 @@ SCRIPTDIR="${0%/*}"
 cd "${SCRIPTDIR}"
 cd ..
 
-OHR_SKIP_X86="Yes" ./distrib-linux.sh
+svn info > svninfo.txt
+OHRDIR="$(pwd)" "./docker/ohrrpgce-build-env-linux-x86_64/lin64.sh" -c /src/ohr/distrib-linux.sh
 
 SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
 
-TODAY=`date "+%Y-%m-%d"`
+TODAY=`date -u "+%Y-%m-%d"`
 CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
 BRANCH=`cat codename.txt | grep -v "^#" | head -2 | tail -1 | tr -d "\r"`
 scp -p distrib/ohrrpgce-linux-${TODAY}-${BRANCH}-*.tar.bz2 "${SCPDEST}"/archive/
 scp -p distrib/ohrrpgce-player-linux-${TODAY}-${BRANCH}-*.zip "${SCPDEST}"/archive/
 
-DEBTODAY=`date "+%Y.%m.%d"`
-SVNREV=`svn info | grep "^Revision:" | sed -e "s/Revision: //"`
+DEBTODAY=`date -u "+%Y.%m.%d"`
+SVNREV=`cat svninfo.txt | grep "^Revision:" | sed -e "s/Revision: //"`
 scp -p distrib/*_${DEBTODAY}.${BRANCH}-${SVNREV}_amd64.deb "${SCPDEST}"/archive/debian/
