761f2f64e31bb5a4c743ec7b93e40476adecdf28
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2026-01-11 01:52:32 +0000

    lexiphanic: fix linux release scripts to run in docker
    git-svn-id: https://rpg.hamsterrepublic.com/source/rel/lexiphanic@14238 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- release/release-linux-x86.sh
+++ release/release-linux-x86.sh
@@ -4,11 +4,12 @@ SCRIPTDIR="${0%/*}"
 cd "${SCRIPTDIR}"
 cd ..
 
-OHR_SKIP_X86_64="Yes" ./distrib-linux.sh
+svn info > svninfo.txt
+OHRDIR="$(pwd)" "./docker/ohrrpgce-build-env-linux-x86/lin32.sh" -c /src/ohr/distrib-linux.sh
 
 SCPDEST="james_paige@motherhamster.org:HamsterRepublic.com/ohrrpgce"
 
-TODAY=`date "+%Y-%m-%d"`
+TODAY=`date -u "+%Y-%m-%d"`
 CODE=`cat codename.txt | grep -v "^#" | head -1 | tr -d "\r"`
 BRANCH=`cat codename.txt | grep -v "^#" | head -2 | tail -1 | tr -d "\r"`
 scp -p distrib/ohrrpgce-linux-${TODAY}-${BRANCH}-*.tar.bz2 "${SCPDEST}"/archive/
