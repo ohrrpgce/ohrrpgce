d46acf5a53e4b425f6ad9d2dfb4ccbe14d5dc86e
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2026-02-24 15:48:00 +0000

    Nightly source and docs now have their own script.
    No longer piggybacks on the nightly 64 bit linux and windows scripts
    
    git-svn-id: https://rpg.hamsterrepublic.com/source/wip@14249 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- nightly/ohr_nightly_docker.sh
+++ nightly/ohr_nightly_docker.sh
@@ -4,6 +4,9 @@ UPLOAD_SERVER="james_paige@motherhamster.org"
 UPLOAD_FOLDER="HamsterRepublic.com/ohrrpgce/nightly/"
 UPLOAD_DEST="$UPLOAD_SERVER:$UPLOAD_FOLDER"
 
+# Run nightly source-and-docs (doesn't need to be in docker right now)
+~/src/nightly/ohr-source-and-docs/wrap-nightly-source-and-docs.sh > /dev/null 2>&1
+
 # Run Android nightly build in docker
 ~/src/nightly/ohr-android/nightly/wrap-nightly-android.sh > /dev/null 2>&1
 
---
+++ nightly/wrap-nightly-source-and-docs.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+echo "ABOUT TO DO A NIGHTLY WIP SOURCE&DOCS DISTRIB"
+echo "---------------------------------------------"
+sleep 10
+
+SCRIPTDIR="${0%/*}"
+SCRIPTDIR="$(realpath $SCRIPTDIR)"
+cd "${SCRIPTDIR}"/..
+
+if [ -n "True" ] ; then
+  echo "From: cron@rpg.hamsterrepublic.com"
+  echo "To: cron@rpg.hamsterrepublic.com"
+  echo "Subject: OHRRPGCE souce-and-docs nightly build ($(uname -n))"
+  echo ""
+
+  svn cleanup
+  # Plotdict gets modified by update-html.sh, remove any modifications or conflicts
+  svn resolve --accept theirs-full --recursive ./docs
+  svn revert --recursive ./docs
+  svn update --trust-server-cert --non-interactive | tee nightly-temp.txt || exit 1
+  UPDATE=`grep "Updated to revision" nightly-temp.txt`
+  rm nightly-temp.txt
+  if [ -z "$UPDATE" ] ; then
+    echo No changes, no need to update nightly.
+    exit 2
+  fi
+
+  echo "Currently in dir: $(pwd)"
+
+  UPLOAD_SERVER="james_paige@motherhamster.org"
+  UPLOAD_FOLDER="HamsterRepublic.com"
+  UPLOAD_DEST="$UPLOAD_SERVER:$UPLOAD_FOLDER"
+
+  echo "Zipping up new nightly snapshot..."
+  rm -f ./distrib/ohrrpgce-source-nightly.zip
+  ./ohrpackage.py unix source ./distrib/ohrrpgce-source-nightly.zip &&
+    echo "Uploading nightly source snapshot..." &&
+    scp -p ./distrib/ohrrpgce-source-nightly.zip $UPLOAD_DEST/ohrrpgce/nightly/
+  
+  # This is duplicated in distrib-nightly-win[-wine].sh except plotdictionary.html
+  # We will remove those soon
+  echo "Uploading plotscripting docs..."
+  cd docs
+  ./update-html.sh
+  cd ..
+  scp -p docs/*.png $UPLOAD_DEST/ohrrpgce/nightly/docs/
+  scp -p docs/plotdict.xml $UPLOAD_DEST/ohrrpgce/nightly/docs/
+  scp -p docs/htmlplot.xsl $UPLOAD_DEST/ohrrpgce/nightly/docs/
+  scp -p docs/plotdictionary.html $UPLOAD_DEST/ohrrpgce/nightly/docs/
+
+  echo "Uploading IMPORTANT-nightly.txt..."
+  scp -p IMPORTANT-nightly.txt $UPLOAD_DEST/ohrrpgce/nightly/
+  
+  echo "distrib-nightly-linux.sh is done."
+
+fi 2>&1 | tee ~/wrap-nightly-source-and-docs-output.txt
+~/src/ohr/wip/nightly/curl_smtp_wrapper.sh ~/wrap-nightly-source-and-docs-output.txt
