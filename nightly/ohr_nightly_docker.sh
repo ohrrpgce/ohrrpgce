2b447f09bf9b6806615b6bd4f2941dbb95f00df1
James Paige <Bob@HamsterRepublic.com>
2026-08-14 10:40:03 -0400

    Update nightly docker script with a workaround for when docker daemon's networking randomly dies sometimes

--- nightly/ohr_nightly_docker.sh
+++ nightly/ohr_nightly_docker.sh
@@ -1,8 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 UPLOAD_SERVER="james_paige@motherhamster.org"
 UPLOAD_FOLDER="HamsterRepublic.com/ohrrpgce/nightly/"
 UPLOAD_DEST="$UPLOAD_SERVER:$UPLOAD_FOLDER"
+
+# Sanity-check the docker network, restart daemon if needed
+dockerfail() {
+  DOCKER_CURL_CHECK=$(docker run --rm curlimages/curl:8.21.0 \
+    -sS https://HamsterRepublic.com/ \
+    | grep "The Fantastically Adequate Hamster Republic Homepage")
+  if [ -z "$DOCKER_CURL_CHECK" ] ; then
+    return 0 # Failed, return true
+  fi
+  return 1 # Ok, return false
+}
+if dockerfail ; then
+  echo "Docker curl test failed, trying to restart docker daemon..."
+  # Current user must be set up to do this passwordlessly in /etc/sudoers.d/docker_restart
+  # Example:
+  # username ALL=(root) NOPASSWD: /usr/bin/systemctl restart docker
+  sudo systemctl restart docker
+  sleep 10
+  if dockerfail ; then
+    echo "ERROR: Restarting docker didn't fix networking, there must be a bigger networking problem."
+    echo "Quitting since builds are likely to fail."
+    exit 1
+  fi
+fi
 
 # Run nightly source-and-docs (doesn't need to be in docker right now)
 ~/src/nightly/ohr-source-and-docs/nightly/wrap-nightly-source-and-docs.sh > /dev/null 2>&1
